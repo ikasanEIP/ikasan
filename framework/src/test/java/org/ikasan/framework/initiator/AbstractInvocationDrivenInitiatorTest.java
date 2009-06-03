fe90ac9ee423ca48f47fb1aa0370b512d0266f32
magicduncan <magicduncan>
2009-06-03 10:50:09 +0000

    #12 separated out Event sourcing from flow invocation for Invocation driven Initators

--- framework/src/test/java/org/ikasan/framework/initiator/AbstractInvocationDrivenInitiatorTest.java
+++ framework/src/test/java/org/ikasan/framework/initiator/AbstractInvocationDrivenInitiatorTest.java
@@ -3,49 +3,143 @@
  */
 package org.ikasan.framework.initiator;
 
-import static org.junit.Assert.*;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.resource.ResourceException;
+
 import junit.framework.Assert;
 
 import org.apache.log4j.Logger;
+import org.ikasan.framework.component.Event;
 import org.ikasan.framework.component.IkasanExceptionHandler;
+import org.ikasan.framework.error.service.ErrorLoggingService;
+import org.ikasan.framework.event.service.EventProvider;
+import org.ikasan.framework.exception.IkasanExceptionAction;
 import org.ikasan.framework.flow.Flow;
+import org.jmock.Expectations;
+import org.jmock.Mockery;
+import org.jmock.Sequence;
 import org.junit.Test;
 
 /**
- * @author duncro
+ * @author Ikasan Development Team
  *
  */
 public class AbstractInvocationDrivenInitiatorTest {
+	
+	Mockery mockery = new Mockery();
+	
+	EventProvider eventProvider = mockery.mock(EventProvider.class);
+	
+	IkasanExceptionHandler exceptionHandler = mockery.mock(IkasanExceptionHandler.class);
 
 	/**
+	 * Tests that an invoke() call on a stopping InvocationDrivenInitiator will be ignored
+	 * 
 	 * Test method for {@link org.ikasan.framework.initiator.AbstractInvocationDrivenInitiator#invoke()}.
 	 */
 	@Test
 	public void testInvoke_willDoNothingIfStopping() {
-		MockAbstractInvocationDrivenInitiator initiator = new MockAbstractInvocationDrivenInitiator("name", "moduleName", null, null);
+		MockAbstractInvocationDrivenInitiator initiator = new MockAbstractInvocationDrivenInitiator("name", "moduleName", null, null, eventProvider);
 		initiator.stop();
 		
 		Assert.assertFalse("invokeFlow should never have been called on a new initiator", initiator.isInvokeFlowCalled());
 		initiator.invoke();
 		Assert.assertFalse("invokeFlow should not have been called on a stopping initiator when invoke is called", initiator.isInvokeFlowCalled());
+	
+		mockery.assertIsSatisfied();
 	}
 	
+	/**
+	 * Tests the happy path of invokeFlow being called with the successfully sourced List of Events as provided by the EventProvider
+	 * 
+	 * @throws ResourceException
+	 */
 	@Test
-	public void testInvoke_willCallInvokeFlowWithResultOfPriorCallToSourceEvents() {
-		MockAbstractInvocationDrivenInitiator initiator = new MockAbstractInvocationDrivenInitiator("name", "moduleName", null, null);
+	public void testInvoke_willCallInvokeFlow_withResultOfPriorCallToEventProvider() throws ResourceException {
+		MockAbstractInvocationDrivenInitiator initiator = new MockAbstractInvocationDrivenInitiator("name", "moduleName", null, null, eventProvider);
+		final List<Event> providedEvents = new ArrayList<Event>();
+        mockery.checking(new Expectations()
+        {
+            {
+                one(eventProvider).getEvents();will(returnValue(providedEvents));
+            }
+        });
+		
+		
 		Assert.assertFalse("invokeFlow should never have been called on a new initiator", initiator.isInvokeFlowCalled());
 		initiator.invoke();
 		Assert.assertTrue("invokeFlow should have been called on a non-stopping initiator when invoke is called", initiator.isInvokeFlowCalled());
-
+		Assert.assertEquals("invokeFlow should be called with the Events provided by the eventProvider", providedEvents, initiator.getInvokeFlowArgument());
+	
+		mockery.assertIsSatisfied();
+	}
+	
+	/**
+	 * Tests the unhappy path of the EventProvider failing (throwing some Throwable). This should be logged, and the throwable dealt with as a result of the
+	 * ExceptionHandler's action
+	 * @throws ResourceException 
+	 */
+	@Test
+	public void testInvoke_willLogErrorCallExceptionHandlerAndHandleAction_whenEventProviderThrows() throws ResourceException{
+		final String initiatorName = "initiatorName";
+		final String moduleName = "moduleName";
+		
+		MockAbstractInvocationDrivenInitiator initiator = new MockAbstractInvocationDrivenInitiator(initiatorName, moduleName, null, exceptionHandler, eventProvider);
+		
+		final ErrorLoggingService errorLoggingService = mockery.mock(ErrorLoggingService.class);
+		initiator.setErrorLoggingService(errorLoggingService);
+		
+		final Throwable throwable = new NullPointerException();
+		final IkasanExceptionAction exceptionAction = mockery.mock(IkasanExceptionAction.class);
+        
+		final Sequence sequence = mockery.sequence("invocationSequence");
+		mockery.checking(new Expectations()
+        {
+            {
+            	//eventProvider fails
+                one(eventProvider).getEvents();
+                inSequence(sequence);
+                will(throwException(throwable));
+                
+                //errorService notified
+                one(errorLoggingService).logError(throwable, moduleName, initiatorName);
+                inSequence(sequence);
+                
+                //exceptionHandlerCalled
+                one(exceptionHandler).invoke(initiatorName, throwable);
+                inSequence(sequence);
+                will(returnValue(exceptionAction));
+            }
+        });
+        
+        initiator.invoke();
+        
+        Assert.assertTrue("handleAction should have been called", initiator.isHandleActionCalled());
+        Assert.assertEquals("exceptionAction passed to handleAction should be that which was returned from the exceptionHandler", exceptionAction, initiator.getHandActionArgument());
+        
+	
+		mockery.assertIsSatisfied();
 	}
 	
 
 
 }
+/**
+ * Mock extension of the abstact class under test
+ * 
+ * @author Ikasan Development Team
+ *
+ */
 class MockAbstractInvocationDrivenInitiator extends AbstractInvocationDrivenInitiator{
 
 	
 	boolean invokeFlowCalled = false;
+	List<Event> invokeFlowArgument = null;
+	IkasanExceptionAction handleActionArgument = null;
+	boolean handleActionCalled = false;
+	
 	/**
 	 * @param name
 	 * @param moduleName
@@ -54,16 +148,23 @@ class MockAbstractInvocationDrivenInitiator extends AbstractInvocationDrivenInit
 	 */
 	public MockAbstractInvocationDrivenInitiator(String name,
 			String moduleName, Flow flow,
-			IkasanExceptionHandler exceptionHandler) {
-		super(name, moduleName, flow, exceptionHandler);
+			IkasanExceptionHandler exceptionHandler, EventProvider eventProvider) {
+		super(name, moduleName, flow, exceptionHandler,eventProvider);
+	}
+
+	@Override
+	protected void handleAction(IkasanExceptionAction action) {
+		handleActionCalled = true;
+		handleActionArgument = action;
 	}
 
 	/* (non-Javadoc)
 	 * @see org.ikasan.framework.initiator.AbstractInvocationDrivenInitiator#invokeFlow()
 	 */
 	@Override
-	protected void invokeFlow() {
+	protected void invokeFlow(List<Event>events) {
 		invokeFlowCalled = true;
+		invokeFlowArgument = events;
 	}
 
 	/* (non-Javadoc)
@@ -89,7 +190,6 @@ class MockAbstractInvocationDrivenInitiator extends AbstractInvocationDrivenInit
 	 */
 	@Override
 	protected Logger getLogger() {
-		// TODO Auto-generated method stub
 		return null;
 	}
 
@@ -98,7 +198,6 @@ class MockAbstractInvocationDrivenInitiator extends AbstractInvocationDrivenInit
 	 */
 	@Override
 	protected void startInitiator() throws InitiatorOperationException {
-		// TODO Auto-generated method stub
 		
 	}
 
@@ -108,7 +207,6 @@ class MockAbstractInvocationDrivenInitiator extends AbstractInvocationDrivenInit
 	@Override
 	protected void startRetryCycle(Integer maxAttempts, long delay)
 			throws InitiatorOperationException {
-		// TODO Auto-generated method stub
 		
 	}
 
@@ -117,7 +215,6 @@ class MockAbstractInvocationDrivenInitiator extends AbstractInvocationDrivenInit
 	 */
 	@Override
 	protected void stopInitiator() throws InitiatorOperationException {
-		// TODO Auto-generated method stub
 		
 	}
 
@@ -125,7 +222,6 @@ class MockAbstractInvocationDrivenInitiator extends AbstractInvocationDrivenInit
 	 * @see org.ikasan.framework.initiator.Initiator#getType()
 	 */
 	public String getType() {
-		// TODO Auto-generated method stub
 		return null;
 	}
 
@@ -133,7 +229,6 @@ class MockAbstractInvocationDrivenInitiator extends AbstractInvocationDrivenInit
 	 * @see org.ikasan.framework.initiator.Initiator#isRecovering()
 	 */
 	public boolean isRecovering() {
-		// TODO Auto-generated method stub
 		return false;
 	}
 
@@ -141,12 +236,22 @@ class MockAbstractInvocationDrivenInitiator extends AbstractInvocationDrivenInit
 	 * @see org.ikasan.framework.initiator.Initiator#isRunning()
 	 */
 	public boolean isRunning() {
-		// TODO Auto-generated method stub
 		return false;
 	}
 	
 	public boolean isInvokeFlowCalled(){
 		return invokeFlowCalled;
+	}
+	public boolean isHandleActionCalled(){
+		return handleActionCalled;
+	}
+	
+	public List<Event> getInvokeFlowArgument(){
+		return invokeFlowArgument;
+	}
+	
+	public IkasanExceptionAction getHandActionArgument(){
+		return handleActionArgument;
 	}
 	
 }
