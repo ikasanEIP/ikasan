23bbf3ee3d34b57b3288a0771034ea5823f7bee5
mitcje <jeff@tribaldog.co.uk>
2009-06-05 09:42:44 +0000

    Switched back to individual scripts as Im not happy with the merge approach at present. to be continued at a later date

---
+++ framework/database/IkasanWireTap.sql
@@ -0,0 +1,59 @@
+--
+-- $Id$
+-- $URL$
+-- 
+-- ====================================================================
+-- Ikasan Enterprise Integration Platform
+-- Copyright (c) 2003-2008 Mizuho International plc. and individual contributors as indicated
+-- by the @authors tag. See the copyright.txt in the distribution for a
+-- full listing of individual contributors.
+--
+-- This is free software; you can redistribute it and/or modify it
+-- under the terms of the GNU Lesser General Public License as
+-- published by the Free Software Foundation; either version 2.1 of
+-- the License, or (at your option) any later version.
+--
+-- This software is distributed in the hope that it will be useful,
+-- but WITHOUT ANY WARRANTY; without even the implied warranty of
+-- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+-- Lesser General Public License for more details.
+--
+-- You should have received a copy of the GNU Lesser General Public
+-- License along with this software; if not, write to the 
+-- Free Software Foundation Europe e.V. Talstrasse 110, 40217 Dusseldorf, Germany 
+-- or see the FSF site: http://www.fsfeurope.org/.
+-- ====================================================================
+
+IF OBJECT_ID('IkasanWiretap') IS NOT NULL
+BEGIN
+    DROP TABLE IkasanWiretap
+    IF OBJECT_ID('IkasanWiretap') IS NOT NULL
+        PRINT '<<< FAILED DROPPING TABLE IkasanWiretap >>>'
+    ELSE
+        PRINT '<<< DROPPED TABLE IkasanWiretap >>>'
+END
+
+CREATE TABLE IkasanWiretap
+(
+    Id                  NUMERIC IDENTITY NOT NULL,
+    ModuleName          VARCHAR(128)  NOT NULL,
+    FlowName            VARCHAR(128)  NOT NULL,
+    ComponentName       VARCHAR(128)  NOT NULL,
+    EventId             VARCHAR(64)   NOT NULL,
+    PayloadId           VARCHAR(64)   NOT NULL,
+    PayloadContent      TEXT          NOT NULL,
+    CreatedDateTime     DATETIME      DEFAULT getDate() NOT NULL,
+    UpdatedDateTime     DATETIME      DEFAULT getDate() NOT NULL,
+    Expiry              DATETIME      NOT NULL
+)
+LOCK DATAROWS
+WITH IDENTITY_GAP=1
+
+CREATE UNIQUE INDEX IkasanWiretap01u ON IkasanWiretap(Id)
+
+IF OBJECT_ID('IkasanWiretap') IS NOT NULL
+    PRINT '<<< CREATED TABLE IkasanWiretap >>>'
+ELSE
+    PRINT '<<< FAILED CREATING TABLE IkasanWiretap >>>'
+    
+
