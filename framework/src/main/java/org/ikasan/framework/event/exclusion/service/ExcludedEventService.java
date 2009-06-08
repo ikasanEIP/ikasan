464d93c1f90cb77d091cbe37e96b5112ce38e755
magicduncan <magicduncan>
2009-06-08 15:51:00 +0000

    #13 added skeleton of the ExcludedEventService

---
+++ framework/src/main/java/org/ikasan/framework/event/exclusion/service/ExcludedEventService.java
@@ -0,0 +1,16 @@
+/**
+ * 
+ */
+package org.ikasan.framework.event.exclusion.service;
+
+import org.ikasan.framework.component.Event;
+
+/**
+ * @author The Ikasan Development Team
+ *
+ */
+public interface ExcludedEventService {
+
+	
+	public void excludeEvent(Event event);
+}
