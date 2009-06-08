a848d222629e37179da81c9d901afe51e295b7a2
karianna <karianna>
2009-06-08 12:06:21 +0000

    #39 Hibernate and ConnectorPersistence to be removed, we no longer should use these

--- connector-base/src/main/java/org/ikasan/connector/ResourceLoader.java
+++ connector-base/src/main/java/org/ikasan/connector/ResourceLoader.java
@@ -31,36 +31,32 @@ import org.ikasan.common.factory.ClassInstantiationUtils;
 
 /**
  * Singleton for loading connector environment resources
- *
+ * 
  * @author Ikasan Development Team
  */
-public class ResourceLoader
-    extends org.ikasan.common.ResourceLoader
+public class ResourceLoader extends org.ikasan.common.ResourceLoader
 {
     /** The logger instance. */
     private static Logger logger = Logger.getLogger(ResourceLoader.class);
-    
-    /** 
-     * The resource name, e.g The properties file that holds the resource
-     */
-//    protected static String RESOURCE_NAME = "connectorResource.properties"; //$NON-NLS-1$
 
     /** instance of the singleton */
     private static ResourceLoader instance = null;
 
     /**
      * Singleton constructor
+     * 
+     * @return The ResourceLoader singleton
      */
     public static ResourceLoader getInstance()
     {
-        if(instance == null)
+        if (instance == null)
         {
-        	org.ikasan.common.ResourceLoader.RESOURCE_NAME = "connectorResource.xml";
+            org.ikasan.common.ResourceLoader.RESOURCE_NAME = "connectorResource.xml";
             instance = new ResourceLoader();
         }
         return instance;
     }
-    
+
     /**
      * Constructor
      */
@@ -70,9 +66,8 @@ public class ResourceLoader
     }
 
     /**
-     * String presentation of the properties settings
-     * TODO Complete this method
-     *
+     * String presentation of the properties settings TODO Complete this method
+     * 
      * @return String presentation of the properties settings
      */
     @Override
@@ -87,86 +82,79 @@ public class ResourceLoader
 
     /**
      * Get a new instance of the ConnectorContext
+     * 
      * @return ConnectorContext
      */
     @Override
     public ConnectorContext newContext()
     {
-        logger.debug("Instantiating context based on class ["  //$NON-NLS-1$
+        logger.debug("Instantiating context based on class [" //$NON-NLS-1$
                 + this.contextImplClass + "]"); //$NON-NLS-1$
-        return (ConnectorContext)ClassInstantiationUtils.instantiate(this.contextImplClass);
-    }
-
-    /**
-     * Get a new instance of the ConnectorPersistence
-     * @return ConnectorPersistence
-     */
-    @Override
-    public ConnectorPersistenceFactory getPersistenceFactory() 
-    {
-        logger.debug("Get persistence singleton based on class ["  //$NON-NLS-1$
-                + this.persistenceImplClass + "]"); //$NON-NLS-1$
-        return (ConnectorPersistenceFactory)ClassInstantiationUtils.getSingleton(this.persistenceImplClass);
+        return (ConnectorContext) ClassInstantiationUtils.instantiate(this.contextImplClass);
     }
 
     /**
      * Get a new instance of the ConnectorXMLParser
+     * 
      * @return ConnectorXMLParser
      */
     @Override
     public ConnectorXMLParser newXMLParser()
     {
-        logger.debug("Instantiating xmlParser based on class ["  //$NON-NLS-1$
+        logger.debug("Instantiating xmlParser based on class [" //$NON-NLS-1$
                 + this.xmlParserImplClass + "]"); //$NON-NLS-1$
-        return (ConnectorXMLParser)ClassInstantiationUtils.instantiate(this.xmlParserImplClass);
+        return (ConnectorXMLParser) ClassInstantiationUtils.instantiate(this.xmlParserImplClass);
     }
 
     /**
      * Get a new instance of the ConnectorXMLTransformer
+     * 
      * @return ConnectorXMLTransformer
      */
     @Override
-    public ConnectorXMLTransformer newXMLTransformer() 
+    public ConnectorXMLTransformer newXMLTransformer()
     {
-        logger.debug("Instantiating xmlTransformer based on class ["  //$NON-NLS-1$
+        logger.debug("Instantiating xmlTransformer based on class [" //$NON-NLS-1$
                 + this.xmlTransformerImplClass + "]"); //$NON-NLS-1$
-        return (ConnectorXMLTransformer)ClassInstantiationUtils.instantiate(this.xmlTransformerImplClass);
+        return (ConnectorXMLTransformer) ClassInstantiationUtils.instantiate(this.xmlTransformerImplClass);
     }
 
     /**
      * Get a new instance of the ConnectorXSLTransformer
+     * 
      * @return ConnectorXSLTransformer
      */
     @Override
-    public ConnectorXSLTransformer newXSLTransformer() 
+    public ConnectorXSLTransformer newXSLTransformer()
     {
-        logger.debug("Instantiating xslTransformer based on class ["  //$NON-NLS-1$
+        logger.debug("Instantiating xslTransformer based on class [" //$NON-NLS-1$
                 + this.xslTransformerImplClass + "]"); //$NON-NLS-1$
-        return (ConnectorXSLTransformer)ClassInstantiationUtils.instantiate(this.xslTransformerImplClass);        
+        return (ConnectorXSLTransformer) ClassInstantiationUtils.instantiate(this.xslTransformerImplClass);
     }
-  
+
     /**
      * Get a new instance of the FrameworkStringTransformer
+     * 
      * @return FrameworkStringTransformer
      */
     @Override
-    public ConnectorStringTransformer newStringTransformer() 
+    public ConnectorStringTransformer newStringTransformer()
     {
-        logger.debug("Instantiating stringTransformer based on class ["  //$NON-NLS-1$
+        logger.debug("Instantiating stringTransformer based on class [" //$NON-NLS-1$
                 + this.stringTransformerImplClass + "]"); //$NON-NLS-1$
-        return (ConnectorStringTransformer)ClassInstantiationUtils.instantiate(this.stringTransformerImplClass);        
+        return (ConnectorStringTransformer) ClassInstantiationUtils.instantiate(this.stringTransformerImplClass);
     }
-    
+
     /**
      * Get a new instance of the ConnectorEnvironment
+     * 
      * @return ConnectorEnvironment
      */
     @Override
-    public ConnectorEnvironment newEnvironment() 
+    public ConnectorEnvironment newEnvironment()
     {
-        logger.debug("Instantiating environment based on class ["  //$NON-NLS-1$
+        logger.debug("Instantiating environment based on class [" //$NON-NLS-1$
                 + this.environmentImplClass + "]"); //$NON-NLS-1$
-        return (ConnectorEnvironment)ClassInstantiationUtils.instantiate(this.environmentImplClass);        
+        return (ConnectorEnvironment) ClassInstantiationUtils.instantiate(this.environmentImplClass);
     }
-    
 }
