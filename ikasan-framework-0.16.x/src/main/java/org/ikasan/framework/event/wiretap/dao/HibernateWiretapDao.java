65350570dc114a8d15d6b583b7dbcf5af935fc8f
karianna <karianna>
2009-08-06 11:25:36 +0000

    IKASAN-100

--- ikasan-framework-0.16.x/src/main/java/org/ikasan/framework/event/wiretap/dao/HibernateWiretapDao.java
+++ ikasan-framework-0.16.x/src/main/java/org/ikasan/framework/event/wiretap/dao/HibernateWiretapDao.java
@@ -41,6 +41,8 @@ import org.hibernate.criterion.Restrictions;
 import org.ikasan.framework.event.wiretap.model.PagedWiretapSearchResult;
 import org.ikasan.framework.event.wiretap.model.WiretapEvent;
 import org.ikasan.framework.event.wiretap.model.WiretapEventHeader;
+import org.ikasan.framework.management.search.ArrayListPagedSearchResult;
+import org.ikasan.framework.management.search.PagedSearchResult;
 import org.springframework.dao.DataAccessException;
 import org.springframework.orm.hibernate3.HibernateCallback;
 import org.springframework.orm.hibernate3.support.HibernateDaoSupport;
@@ -59,20 +61,20 @@ public class HibernateWiretapDao extends HibernateDaoSupport implements WiretapD
     /** Query for finding all wiretap events with the same payloadId */
     private static final String WIRETAP_EVENT_IDS_FOR_PAYLOAD_ID = "select w.id from WiretapEvent w where w.payloadId = ?";
 
-    /*
-     * (non-Javadoc)
-     * 
+    /**
+     * Save the wiretapEvent
+     *  
      * @see
-     * org.ikasan.framework.component.endpoint.wiretap.dao.WiretapDao#save(org
-     * .ikasan.framework.component.endpoint.wiretap.model.WiretapEvent)
+     * org.ikasan.framework.event.wiretap.dao.WiretapDao#save(
+     * org.ikasan.framework.event.wiretap.model.WiretapEvent)
      */
     public void save(WiretapEvent wiretapEvent)
     {
         getHibernateTemplate().save(wiretapEvent);
     }
 
-    /*
-     * (non-Javadoc)
+    /**
+     * Find the Wiretap by its Id
      * 
      * @see
      * org.ikasan.framework.event.wiretap.dao.WiretapDao#findById(java.lang.
@@ -102,6 +104,23 @@ public class HibernateWiretapDao extends HibernateDaoSupport implements WiretapD
         return wiretapEvent;
     }
 
+    /**
+     * Find paging list of wiretaps
+     * 
+     * @param moduleNames - The list of module names
+     * @param componentName - The component name
+     * @param eventId - The event id
+     * @param payloadId - The payload id
+     * @param fromDate - The from date
+     * @param untilDate - The to date
+     * @param payloadContent - The payload content
+     * @param maxResults - Max Results to bring back
+     * @param firstResult - The first result
+     * @return PagedWiretapSearchResult
+     * @throws DataAccessException - Exception if we can't get the data
+     * @deprecated - Use findWiretapEvents instead
+     */
+    @Deprecated
     @SuppressWarnings("unchecked")
     public PagedWiretapSearchResult findPaging(final Set<String> moduleNames, final String componentName, final String eventId, final String payloadId,
             final Date fromDate, final Date untilDate, final String payloadContent, final int maxResults, final int firstResult) throws DataAccessException
@@ -111,14 +130,7 @@ public class HibernateWiretapDao extends HibernateDaoSupport implements WiretapD
             public Object doInHibernate(Session session) throws HibernateException
             {
                 Criteria criteria = session.createCriteria(WiretapEvent.class, "event");
-                // if (restrictionExists(moduleName))
-                // {
                 criteria.add(Restrictions.in("moduleName", moduleNames));
-                // }
-                // if (restrictionExists(flowName))
-                // {
-                // criteria.add(Restrictions.eq("flowName", flowName));
-                // }
                 if (restrictionExists(componentName))
                 {
                     criteria.add(Restrictions.eq("componentName", componentName));
@@ -160,6 +172,88 @@ public class HibernateWiretapDao extends HibernateDaoSupport implements WiretapD
     }
 
     /**
+     * Perform a paged search for <code>WiretapEvent</code>s
+     * 
+     * @param pageNo - The page number to retrieve
+     * @param pageSize - The size of the page
+     * @param orderBy - order by field
+     * @param orderAscending - ascending flag
+     * @param moduleNames - The list of module names
+     * @param componentName - The component name
+     * @param eventId - The event id
+     * @param payloadId - The payload id
+     * @param fromDate - The from date
+     * @param untilDate - The to date
+     * @param payloadContent - The payload content
+     * 
+     * @return PagedSearchResult
+     */
+    @SuppressWarnings("unchecked")
+    public PagedSearchResult<WiretapEvent> findWiretapEvents(final int pageNo, final int pageSize, final String orderBy, final boolean orderAscending,
+            final Set<String> moduleNames, final String componentName, final String eventId, final String payloadId, final Date fromDate, final Date untilDate,
+            final String payloadContent)
+    {
+        return (PagedSearchResult) getHibernateTemplate().execute(new HibernateCallback()
+        {
+            public Object doInHibernate(Session session) throws HibernateException
+            {
+                Criteria criteria = session.createCriteria(WiretapEvent.class);
+                criteria.setMaxResults(pageSize);
+                int firstResult = (pageNo * pageSize);
+                criteria.setFirstResult(firstResult);
+                if (orderBy != null)
+                {
+                    if (orderAscending)
+                    {
+                        criteria.addOrder(Order.asc(orderBy));
+                    }
+                    else
+                    {
+                        criteria.addOrder(Order.desc(orderBy));
+                    }
+                }
+                if (restrictionExists(moduleNames))
+                {
+                    criteria.add(Restrictions.in("moduleName", moduleNames));
+                }
+                if (restrictionExists(componentName))
+                {
+                    criteria.add(Restrictions.eq("componentName", componentName));
+                }
+                if (restrictionExists(eventId))
+                {
+                    criteria.add(Restrictions.eq("eventId", eventId));
+                }
+                if (restrictionExists(payloadId))
+                {
+                    criteria.add(Restrictions.eq("payloadId", payloadId));
+                }
+                if (restrictionExists(payloadContent))
+                {
+                    criteria.add(Restrictions.like("payloadContent", payloadContent, MatchMode.ANYWHERE));
+                }
+                if (restrictionExists(fromDate))
+                {
+                    criteria.add(Restrictions.gt("created", fromDate));
+                }
+                if (restrictionExists(untilDate))
+                {
+                    criteria.add(Restrictions.lt("created", untilDate));
+                }
+                List<WiretapEvent> wiretapResults = criteria.list();
+                criteria.setProjection(Projections.rowCount());
+                Integer rowCount = 0;
+                List<Integer> rowCountList = criteria.list();
+                if (!rowCountList.isEmpty())
+                {
+                    rowCount = rowCountList.get(0);
+                }
+                return new ArrayListPagedSearchResult<WiretapEvent>(wiretapResults, firstResult, rowCount);
+            }
+        });
+    }
+
+    /**
      * Check to see if the restriction exists
      * 
      * @param restrictionValue - The value to check
@@ -167,9 +261,18 @@ public class HibernateWiretapDao extends HibernateDaoSupport implements WiretapD
      */
     static final boolean restrictionExists(Object restrictionValue)
     {
-        return (restrictionValue != null && !"".equals(restrictionValue));
+        // If the value passed in is not null and not an empty string then it
+        // can have a restriction applied
+        if (restrictionValue != null && !"".equals(restrictionValue))
+        {
+            return true;
+        }
+        return false;
     }
 
+    /**
+     * Delete all of the expired wiretaps
+     */
     public void deleteAllExpired()
     {
         getHibernateTemplate().bulkUpdate(HOUSEKEEP_QUERY, new Date());
