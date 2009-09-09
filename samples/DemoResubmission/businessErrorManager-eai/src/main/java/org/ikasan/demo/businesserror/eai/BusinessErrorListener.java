50608d9139490eda0a97e42533aaa02231e35742
magicduncan <magicduncan>
2009-09-09 17:12:30 +0000

    fleshed out DemoResubmission sample app

---
+++ samples/DemoResubmission/businessErrorManager-eai/src/main/java/org/ikasan/demo/businesserror/eai/BusinessErrorListener.java
@@ -0,0 +1,8 @@
+package org.ikasan.demo.businesserror.eai;
+
+import org.ikasan.demo.businesserror.model.BusinessError;
+
+public interface BusinessErrorListener {
+
+	public void onBusinessError(BusinessError businessError);
+}
