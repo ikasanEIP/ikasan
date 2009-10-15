a185e5b32902fddca164c4784723b65d7a0cde52
magicduncan <magicduncan>
2009-10-15 17:28:25 +0000

    added XStream based XML serialiser for ErrorOccurrences

---
+++ feature-resubmission/framework/src/main/java/org/ikasan/framework/error/model/serialisation/ErrorOccurrenceXmlSerialiser.java
@@ -0,0 +1,84 @@
+/*
+ * $Id$
+ * $URL$
+ * 
+ * =============================================================================
+ * Ikasan Enterprise Integration Platform
+ * 
+ * Distributed under the Modified BSD License.
+ * Copyright notice: The copyright for this software and a full listing 
+ * of individual contributors are as shown in the packaged copyright.txt 
+ * file. 
+ * 
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without 
+ * modification, are permitted provided that the following conditions are met:
+ *
+ *  - Redistributions of source code must retain the above copyright notice, 
+ *    this list of conditions and the following disclaimer.
+ *
+ *  - Redistributions in binary form must reproduce the above copyright notice, 
+ *    this list of conditions and the following disclaimer in the documentation 
+ *    and/or other materials provided with the distribution.
+ *
+ *  - Neither the name of the ORGANIZATION nor the names of its contributors may
+ *    be used to endorse or promote products derived from this software without 
+ *    specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" 
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE 
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE 
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE 
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL 
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR 
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER 
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE 
+ * USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ * =============================================================================
+ */
+package org.ikasan.framework.error.model.serialisation;
+
+import org.ikasan.common.xml.serializer.XMLSerializer;
+import org.ikasan.framework.error.model.ErrorOccurrence;
+
+import com.thoughtworks.xstream.XStream;
+import com.thoughtworks.xstream.io.xml.DomDriver;
+
+/**
+ * 
+ * Implementation of the XMLSerializer interface that uses XStream as the
+ * underlying serializer/deserializer
+ * 
+ * @author Ikasan Development Team
+ * 
+ */
+public class ErrorOccurrenceXmlSerialiser implements XMLSerializer<ErrorOccurrence>
+{
+    /** The xstream */
+    private XStream xstream;
+
+    /**
+     * Constructor
+     * 
+     */
+    public ErrorOccurrenceXmlSerialiser()
+    {
+        xstream = new XStream(new DomDriver()); 
+        xstream.alias("errorOccurrence", ErrorOccurrence.class);
+
+
+    }
+
+    public ErrorOccurrence toObject(String xml)
+    {
+        throw new UnsupportedOperationException();
+    }
+
+    public String toXml(ErrorOccurrence subject)
+    {
+        return xstream.toXML(subject);
+    }
+}
+
---
+++ feature-resubmission/framework/src/test/java/org/ikasan/framework/error/model/serialisation/ErrorOccurrenceXmlSerialiserTest.java
@@ -0,0 +1,18 @@
+package org.ikasan.framework.error.model.serialisation;
+
+import java.util.Date;
+
+import org.ikasan.framework.error.model.ErrorOccurrence;
+import org.junit.Test;
+
+public class ErrorOccurrenceXmlSerialiserTest {
+
+	private ErrorOccurrenceXmlSerialiser errorOccurrenceXmlSerialiser = new ErrorOccurrenceXmlSerialiser();
+	@Test
+	public void testToXml() {
+		ErrorOccurrence errorOccurrence = new ErrorOccurrence(new RuntimeException(), "moduleName", "initiatorName", new Date());
+		
+		System.out.println(errorOccurrenceXmlSerialiser.toXml(errorOccurrence));
+	}
+
+}
