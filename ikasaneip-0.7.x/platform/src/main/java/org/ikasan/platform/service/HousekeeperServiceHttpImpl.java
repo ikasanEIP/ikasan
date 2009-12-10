938239369e042a3c5eadd10613f061bafab2810e
karianna <karianna>
2009-12-10 17:45:46 +0000

    IKASAN-308

--- ikasaneip-0.7.x/platform/src/main/java/org/ikasan/platform/service/HousekeeperServiceHttpImpl.java
+++ ikasaneip-0.7.x/platform/src/main/java/org/ikasan/platform/service/HousekeeperServiceHttpImpl.java
@@ -40,62 +40,133 @@
  */
 package org.ikasan.platform.service;
 
+import java.io.IOException;
+
+import org.apache.http.auth.AuthScheme;
+import org.apache.http.auth.AuthScope;
+import org.apache.http.auth.AuthState;
+import org.apache.http.auth.Credentials;
+import org.apache.http.auth.UsernamePasswordCredentials;
+
+import org.apache.http.HttpException;
+import org.apache.http.HttpHost;
+import org.apache.http.HttpRequest;
+import org.apache.http.HttpRequestInterceptor;
 import org.apache.http.HttpResponse;
 import org.apache.http.HttpStatus;
-import org.apache.http.client.HttpClient;
-import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.CredentialsProvider;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.client.protocol.ClientContext;
+import org.apache.http.impl.auth.BasicScheme;
 import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.protocol.BasicHttpContext;
+import org.apache.http.protocol.ExecutionContext;
+import org.apache.http.protocol.HttpContext;
 import org.apache.log4j.Logger;
 import org.ikasan.platform.service.HousekeeperService;
 
 /**
- * A HTTP Implementation of the Housekeeper interface 
+ * A HTTP Implementation of the Housekeeper interface
  * 
  * @author Ikasan Development Team
  */
-public class HousekeeperServiceHttpImpl implements HousekeeperService 
-{
-    /** Logger for this class */
-    private static Logger logger = Logger.getLogger(HousekeeperServiceHttpImpl.class);
-    
-    /** The URL to execute the Wiretap Event Housekeeping */
-    private String wiretapEventHousekeepingUrl;
-    
-    /**
-     * Constructor
-     * @param wiretapEventHousekeepingUrl - wiretapEventHousekeepingUrl to set
-     */
-    public HousekeeperServiceHttpImpl(String wiretapEventHousekeepingUrl)
-    {
-        this.wiretapEventHousekeepingUrl = wiretapEventHousekeepingUrl;
-    }
-    
-    /**
-     * Housekeeps Wiretapped Events
-     * 
-     * TODO Error handling is lazy and captures any exception, 
-     * this may actually be OK but needs review.
-     */
-    public void housekeepWiretapEvents()
-    {
-        HttpClient httpclient = new DefaultHttpClient();
-        HttpGet httpget = new HttpGet(this.wiretapEventHousekeepingUrl);
-        try
-        {
-            logger.info("Calling [" + this.wiretapEventHousekeepingUrl + "]");
-            HttpResponse response = httpclient.execute(httpget);
-            int statusCode = response.getStatusLine().getStatusCode();
-            if (statusCode != HttpStatus.SC_OK)
-            {
-                logger.error("Call failed, Status Code = [" + statusCode + "]");
-                throw new Exception();
-            }
-            logger.info("housekeepWiretapEvents was called successfully.");
-        }
-        catch (Exception e)
-        {
-            logger.error("Call to housekeep failed.");
-        }
-    }
+public class HousekeeperServiceHttpImpl implements HousekeeperService {
+	/** Logger for this class */
+	private static Logger logger = Logger
+			.getLogger(HousekeeperServiceHttpImpl.class);
 
+	/** The URL to execute the Wiretap Event Housekeeping */
+	private String wiretapEventHousekeepingUrl;
+
+	/**
+	 * Constructor
+	 * 
+	 * @param wiretapEventHousekeepingUrl
+	 *            - wiretapEventHousekeepingUrl to set
+	 */
+	public HousekeeperServiceHttpImpl(String wiretapEventHousekeepingUrl) {
+		this.wiretapEventHousekeepingUrl = wiretapEventHousekeepingUrl;
+	}
+
+	/**
+	 * Housekeeps Wiretapped Events
+	 * 
+	 * TODO Error handling is lazy and captures any exception, this may actually
+	 * be OK but needs review.
+	 */
+	public void housekeepWiretapEvents() {
+		// Create the client and set the username and password
+		DefaultHttpClient httpclient = new DefaultHttpClient();
+		Credentials defaultcreds = new UsernamePasswordCredentials(
+				"housekeeper", "housekeeper");
+		// TODO Using AuthScope.ANY is not best practice, we can probably
+		// Get the correct details from the URL passed in
+		httpclient.getCredentialsProvider().setCredentials(AuthScope.ANY,
+				defaultcreds);
+
+		// Generate BASIC scheme object and stick it to the local execution
+		// context
+		BasicHttpContext localcontext = new BasicHttpContext();
+		BasicScheme basicAuth = new BasicScheme();
+		localcontext.setAttribute("preemptive-auth", basicAuth);
+
+		// We're going to Preemptively Auth here
+		httpclient.addRequestInterceptor(new PreemptiveAuth(), 0);
+
+		HttpPost httppost = new HttpPost(this.wiretapEventHousekeepingUrl);
+		try {
+			logger.info("Calling [" + this.wiretapEventHousekeepingUrl + "]");
+			HttpResponse response = httpclient.execute(httppost, localcontext);
+			int statusCode = response.getStatusLine().getStatusCode();
+			// TODO Investigate why we get a 302 back, probably because of the
+			// authentication moving us
+			if (!(statusCode == HttpStatus.SC_MOVED_TEMPORARILY || statusCode == HttpStatus.SC_OK)) {
+				logger.error("Call failed, Status Code = [" + statusCode + "]");
+				throw new Exception();
+			}
+			logger.info("housekeepWiretapEvents was called successfully.");
+		} catch (Exception e) {
+			logger.error("Call to housekeep failed.");
+		} finally {
+			// When HttpClient instance is no longer needed,
+			// shut down the connection manager to ensure
+			// immediate deallocation of all system resources
+			httpclient.getConnectionManager().shutdown();
+		}
+	}
+
+	/** Helper class to sort out preemtive auth */
+	static class PreemptiveAuth implements HttpRequestInterceptor
+	{
+
+		public void process(final HttpRequest request, final HttpContext context)
+				throws HttpException, IOException
+		{
+
+			AuthState authState = (AuthState) context
+					.getAttribute(ClientContext.TARGET_AUTH_STATE);
+
+			// If no auth scheme available yet, try to initialize it preemptively
+			// TODO Trace through this to see if it ever gets called.
+			if (authState.getAuthScheme() == null)
+			{
+				AuthScheme authScheme = (AuthScheme) context.getAttribute("preemptive-auth");
+				CredentialsProvider credsProvider = (CredentialsProvider) context.getAttribute(ClientContext.CREDS_PROVIDER);
+				HttpHost targetHost = (HttpHost) context.getAttribute(ExecutionContext.HTTP_TARGET_HOST);
+				if (authScheme != null)
+				{
+					Credentials creds = credsProvider
+							.getCredentials(new AuthScope(targetHost
+									.getHostName(), targetHost.getPort()));
+					if (creds == null)
+					{
+						throw new HttpException(
+								"No credentials for preemptive authentication");
+					}
+					authState.setAuthScheme(authScheme);
+					authState.setCredentials(creds);
+				}
+			}
+		}
+	}
 }
