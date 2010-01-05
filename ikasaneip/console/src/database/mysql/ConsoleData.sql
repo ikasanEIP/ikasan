a0d47534ccfea520274e92f5c843866ca8a515e4
karianna <karianna>
2010-01-05 11:19:11 +0000

    Vijitha's partial patch for IKASAN-102

--- ikasaneip/console/src/database/mysql/ConsoleData.sql
+++ ikasaneip/console/src/database/mysql/ConsoleData.sql
@@ -3,14 +3,14 @@
 -- ------------
 
 -- Modules - Wiretapped
-INSERT INTO module (Name, Description, DesignDiagramURL) VALUES ('demoFileDelivery-ftpSource', 'Deals with picking up files for demonstration purposes', NULL);
-INSERT INTO module (Name, Description, DesignDiagramURL) VALUES ('demoFileDelivery-ftpTarget', 'Deals with delivering files for demonstration purposes', NULL);
+INSERT INTO Module (Name, Description, DesignDiagramURL) VALUES ('demoFileDelivery-ftpSource', 'Deals with picking up files for demonstration purposes', NULL);
+INSERT INTO Module (Name, Description, DesignDiagramURL) VALUES ('demoFileDelivery-ftpTarget', 'Deals with delivering files for demonstration purposes', NULL);
 
 -- -----------------------------
 -- PointToPointFlowProfile Data
 -- -----------------------------
 
-INSERT INTO pointtopointflowprofile (Name) VALUES ('Demonstration File Delivery');
+INSERT INTO PointToPointFlowProfile (Name) VALUES ('Demonstration File Delivery');
 
 -- ----------------------
 -- ----------------------
@@ -23,23 +23,23 @@ INSERT INTO pointtopointflowprofile (Name) VALUES ('Demonstration File Delivery'
 -- ----------------------------
 
 -- demoFileDelivery-ftpSource
-INSERT INTO pointtopointflow (PointToPointFlowProfileId, FromModuleId, ToModuleId) 
-SELECT pointtopointflowprofile.Id, NULL, module.Id
-FROM pointtopointflowprofile, module
-WHERE pointtopointflowprofile.Name = 'Demonstration File Delivery'
-AND module.Name = 'demoFileDelivery-ftpSource';
+INSERT INTO PointToPointFlow (PointToPointFlowProfileId, FromModuleId, ToModuleId) 
+SELECT PointToPointFlowProfile.Id, NULL, Module.Id
+FROM PointToPointFlowProfile, Module
+WHERE PointToPointFlowProfile.Name = 'Demonstration File Delivery'
+AND Module.Name = 'demoFileDelivery-ftpSource';
 
 -- demoFileDelivery-ftpSource --> demoFileDelivery-ftpTarget
-INSERT INTO pointtopointflow (PointToPointFlowProfileId, FromModuleId, ToModuleId) 
-SELECT pointtopointflowprofile.Id, M1.Id, M2.Id
-FROM pointtopointflowprofile, module M1, module M2
-WHERE pointtopointflowprofile.Name = 'Demonstration File Delivery'
+INSERT INTO PointToPointFlow (PointToPointFlowProfileId, FromModuleId, ToModuleId) 
+SELECT PointToPointFlowProfile.Id, M1.Id, M2.Id
+FROM PointToPointFlowProfile, Module M1, Module M2
+WHERE PointToPointFlowProfile.Name = 'Demonstration File Delivery'
 AND M1.Name = 'demoFileDelivery-ftpSource'
 AND M2.Name = 'demoFileDelivery-ftpTarget';
 
 -- demoFileDelivery-ftpTarget
-INSERT INTO pointtopointflow (PointToPointFlowProfileId, FromModuleId, ToModuleId) 
-SELECT pointtopointflowprofile.Id, module.Id, NULL
-FROM pointtopointflowprofile, module
-WHERE pointtopointflowprofile.Name = 'Demonstration File Delivery'
+INSERT INTO PointToPointFlow (PointToPointFlowProfileId, FromModuleId, ToModuleId) 
+SELECT PointToPointFlowProfile.Id, Module.Id, NULL
+FROM PointToPointFlowProfile, Module
+WHERE PointToPointFlowProfile.Name = 'Demonstration File Delivery'
 AND Module.Name = 'demoFileDelivery-ftpTarget';
