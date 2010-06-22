ee997dce5c6eab17d1208c6262a9394ade3954bd
karianna <karianna>
2010-06-22 12:43:57 +0000

    Tidied version of the rawDrivenMessageInitiator TextMessage encoding handling

--- framework/src/main/java/org/ikasan/framework/initiator/messagedriven/jca/RawMessageDrivenInitiator.java
+++ framework/src/main/java/org/ikasan/framework/initiator/messagedriven/jca/RawMessageDrivenInitiator.java
@@ -56,10 +56,10 @@ import org.ikasan.framework.flow.Flow;
 /**
  * A <code>JmsMessageDrivenInitiator</code> implementation that seeks to create and fire new <code>Event</code>s based
  * on raw JMS messages.
- *
+ * 
  * This implementation places no expectation on the incoming message data. Do not use this for deserialising existing
  * <code>Event</code>s, rather use the <code>EventMessageDrivenInitiator</code>
- *
+ * 
  * @author Ikasan Development Team
  */
 public class RawMessageDrivenInitiator extends JmsMessageDrivenInitiatorImpl
@@ -79,9 +79,53 @@ public class RawMessageDrivenInitiator extends JmsMessageDrivenInitiatorImpl
      */
     private boolean respectPriority;
 
-	/**
+    // The character encoding of the incoming message
+    private String incomingCharacterEncoding = null;
+
+    /*
+     * The intermediate character encoding to transition difficult encodings such as CP1252 to more ISO standard
+     * encodings such as UTF-8
+     * 
+     * e.g. CP1252 --> ISO-88591 --> UTF-8
+     */
+    private String intermediateCharacterEncoding = null;
+
+    // The outgoing encoding of the Event, UTF-8 by default
+    private String outgoingCharacterEncoding = "UTF-8";
+    
+    /**
+     * Set the character encoding of the incoming message
+     * 
+     * @param incomingCharacterEncoding
+     */
+    public void setIncomingCharacterEncoding(String incomingCharacterEncoding)
+    {
+        this.incomingCharacterEncoding = incomingCharacterEncoding;
+    }
+
+    /**
+     * Set the intermediate character encoding used to transition difficult character encodings
+     * 
+     * @param intermediateCharacterEncoding
+     */
+    public void setIntermediateCharacterEncoding(String intermediateCharacterEncoding)
+    {
+        this.intermediateCharacterEncoding = intermediateCharacterEncoding;
+    }
+
+    /**
+     * Set the character encoding on the outgoing Event
+     * 
+     * @param outgoingCharacterEncoding
+     */
+    public void setOutgoingCharacterEncoding(String outgoingCharacterEncoding)
+    {
+        this.outgoingCharacterEncoding = outgoingCharacterEncoding;
+    }
+
+    /**
      * Constructor
-     *
+     * 
      * @param moduleName - name of the module
      * @param name - name of this initiator
      * @param flow - flow to invoke
@@ -95,38 +139,70 @@ public class RawMessageDrivenInitiator extends JmsMessageDrivenInitiatorImpl
 
     /*
      * (non-Javadoc)
-     *
-     * @see
-     * org.ikasan.framework.initiator.messagedriven.jca.JmsMessageDrivenInitiatorImpl#handleTextMessage(javax.jms.TextMessage
-     * )
+     * 
+     * @seeorg.ikasan.framework.initiator.messagedriven.jca.
+     * JmsMessageDrivenInitiatorImpl#handleTextMessage(javax.jms.TextMessage )
      */
     @Override
     protected Event handleTextMessage(TextMessage message) throws JMSException
     {
-        // TODO This will be corrected in the 0.8.0+ code base, do not merge this change in!
+        // TODO This will be corrected in the 0.8.0+ code base, do not merge
+        // this change in!
         byte[] characterEncodedContent;
         try
         {
-            // Forcing UTF-8 encoding as we don't currently detect the encoding
-            characterEncodedContent = message.getText().getBytes("UTF-8");
+            String encodedMessage = null;
+            byte[] incomingMessageBytes = null;
+            // Get the message using the incoming character encoding, else use the default platform character encoding
+            if (this.incomingCharacterEncoding != null && !this.incomingCharacterEncoding.equals(""))
+            {
+                incomingMessageBytes = message.getText().getBytes(this.incomingCharacterEncoding);
+                encodedMessage = new String(incomingMessageBytes, this.incomingCharacterEncoding);
+            }
+            else
+            {
+                incomingMessageBytes = message.getText().getBytes();
+                encodedMessage = new String(incomingMessageBytes);
+            }
+
+            // If supplied, use the intermediate encoding step
+            if (this.intermediateCharacterEncoding != null && !this.intermediateCharacterEncoding.equals(""))
+            {
+                byte[] intermediateMessageBytes = encodedMessage.getBytes(this.intermediateCharacterEncoding);
+                encodedMessage = new String(intermediateMessageBytes, this.intermediateCharacterEncoding);
+            }
+            
+            // Encode the outgoing data for the Event, use UTF-8 as a default even if we are supplied a null            
+            byte[] outgoingMessageBytes = null;
+            if (this.outgoingCharacterEncoding != null && !this.outgoingCharacterEncoding.equals(""))
+            {
+                outgoingMessageBytes = encodedMessage.getBytes(this.outgoingCharacterEncoding);
+                encodedMessage = new String(outgoingMessageBytes, this.outgoingCharacterEncoding);
+                characterEncodedContent = encodedMessage.getBytes(this.outgoingCharacterEncoding);
+            }
+            else
+            {
+                outgoingMessageBytes = encodedMessage.getBytes("UTF-8");
+                encodedMessage = new String(outgoingMessageBytes, "UTF-8");
+                characterEncodedContent = encodedMessage.getBytes("UTF-8");
+            }
         }
         catch (UnsupportedEncodingException e)
         {
-            // Hardly ideal to throw a JMSException at this point, but it matches the method signature
+            // Hardly ideal to throw a JMSException at this point, but it
+            // matches the method signature
             // Fix if we remain with the 0.7.x code base (unlikely)
             throw new JMSException("UnsupportedEncoding");
         }
-
-        // From this point forward, this is roughly what the 'old' code would have done with a TextMessage
+        // From this point forward, this is roughly what the 'old' code would
+        // have done with a TextMessage
         Payload payload = payloadFactory.newPayload(MetaDataInterface.UNDEFINED, Spec.TEXT_XML,
-                MetaDataInterface.UNDEFINED, characterEncodedContent);
-
+            MetaDataInterface.UNDEFINED, characterEncodedContent);
         Event event = new Event(moduleName, name);
-
-        //re-use the message's priority if we are configured to respect it
+        // re-use the message's priority if we are configured to respect it
         if (respectPriority)
         {
-        	event.setPriority( new Integer(message.getJMSPriority()) );
+            event.setPriority(new Integer(message.getJMSPriority()));
         }
         event.setPayload(payload);
         return event;
@@ -140,19 +216,21 @@ public class RawMessageDrivenInitiator extends JmsMessageDrivenInitiatorImpl
 
     /**
      * Respect the priority of received messages by setting this on the Event
-	 * @param respectPriority
-	 */
-	public void setRespectPriority(boolean respectPriority)
-	{
-		this.respectPriority = respectPriority;
-	}
+     * 
+     * @param respectPriority
+     */
+    public void setRespectPriority(boolean respectPriority)
+    {
+        this.respectPriority = respectPriority;
+    }
 
-	/**
-	 * Accessor for respectPriority
-	 * @return respectPriority
-	 */
-	public boolean isRespectPriority()
-	{
-		return this.respectPriority;
-	}
+    /**
+     * Accessor for respectPriority
+     * 
+     * @return respectPriority
+     */
+    public boolean isRespectPriority()
+    {
+        return this.respectPriority;
+    }
 }
