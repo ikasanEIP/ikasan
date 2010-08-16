c2d75e299273da6f1ba6169a89afe8c0d62f2bc2
mitcje <jeff@tribaldog.co.uk>
2010-08-16 16:10:49 +0000

    Merged test project back on to trunk along with a fix for cloning the Event (methiod has changed on 0.8.0 from spawn to clone).

---
+++ ikasaneip/test/src/main/java/org/ikasan/testharness/flow/expectation/service/FlowExpectation.java
@@ -0,0 +1,110 @@
+/*
+ * $Id$
+ * $URL$
+ * 
+ * ====================================================================
+ * Ikasan Enterprise Integration Platform
+ * 
+ * Distributed under the Modified BSD License.
+ * Copyright notice: The copyright for this software and a full listing 
+ * of individual contributors are as shown in the packaged copyright.txt 
+ * file. 
+ * 
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without 
+ * modification, are permitted provided that the following conditions are met:
+ *
+ *  - Redistributions of source code must retain the above copyright notice, 
+ *    this list of conditions and the following disclaimer.
+ *
+ *  - Redistributions in binary form must reproduce the above copyright notice, 
+ *    this list of conditions and the following disclaimer in the documentation 
+ *    and/or other materials provided with the distribution.
+ *
+ *  - Neither the name of the ORGANIZATION nor the names of its contributors may
+ *    be used to endorse or promote products derived from this software without 
+ *    specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" 
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE 
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE 
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE 
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL 
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR 
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER 
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE 
+ * USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ * ====================================================================
+ */
+package org.ikasan.testharness.flow.expectation.service;
+
+import org.ikasan.testharness.flow.Capture;
+import org.ikasan.testharness.flow.comparator.ExpectationComparator;
+
+/**
+ * Public API for setting operations on the FlowExpectation implemenation class.
+ * 
+ * @author Ikasan Development Team
+ * 
+ */
+public interface FlowExpectation
+{
+    /**
+     * Set an expectation with a specific comparator to measure the expecation.
+     * @param <T>
+     * @param expectation
+     * @param expectationComparator
+     */
+    public <T> void expectation(T expectation, ExpectationComparator<?,?> expectationComparator);
+
+    /**
+     * Set an expectation with a specific comparator to measure the expecation
+     * and a description of the expectation test which can help identify this 
+     * expectation at runtime.
+     * @param <T>
+     * @param expectation
+     * @param expectationComparator
+     * @param description
+     */
+    public <T> void expectation(T expectation, ExpectationComparator<?,?> expectationComparator, String description);
+
+    /**
+     * Set an expectation.
+     * @param <T>
+     * @param expectation
+     */
+    public <T> void expectation(T expectation);
+
+    /**
+     * Set an expectation and provide a description for this expecation test
+     * which can help identify this expectation at runtime.
+     * @param <T>
+     * @param expectation
+     * @param description
+     */
+    public <T> void expectation(T expectation, String description);
+
+    /**
+     * ignore an expectation
+     * @param <T>
+     * @param expectation
+     */
+    public <T> void ignore(T expectation);
+
+    /**
+     * ignore an expectation and provide a description which can help
+     * identify this expectation at runtime.
+     * @param <T>
+     * @param expectation
+     * @param description
+     */
+    public <T> void ignore(T expectation, String description);
+
+    /** does an expectation satisfy the captured actual behaiour */
+    public void isSatisfied(Capture<?> actual);
+
+    /** have all expectations been satisfied */
+    public void allSatisfied();
+}
