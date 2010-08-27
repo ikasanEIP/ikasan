16be6b4caa515202fdb84a0a0113b716a5d8586e
karianna <karianna>
2010-08-27 12:54:56 +0000

    Try to make the TUnsubscriber tool a little more useful

--- tools/Ttools/src/main/java/org/ikasan/tools/ttools/TUnsubscriber.java
+++ tools/Ttools/src/main/java/org/ikasan/tools/ttools/TUnsubscriber.java
@@ -47,115 +47,82 @@ import javax.jms.Session;
 import org.apache.log4j.Logger;
 
 /**
- * This is a standalone client for unsubscribing 
- * durable subscriptions from JMS Topics
+ * This is a standalone client for unsubscribing durable subscriptions from JMS Topics
  * 
- * Ensure the classpath is as follows
- * CLASSPATH=$JBOSS_HOME/client/jboss-common-client.jar
+ * Ensure the classpath is as follows CLASSPATH=$JBOSS_HOME/client/jboss-common-client.jar
  * CLASSPATH=$CLASSPATH:$JBOSS_HOME/client/commons-logging.jar
  * CLASSPATH=$CLASSPATH:$JBOSS_HOME/client/jboss-messaging-client.jar
  * 
- * Usage: deregister (unsubscribe) durable subscriptions
- *      args must have:
- *          -clientid <client-id>
- *          -subscriptionname <subscription-name>
- *          -durable
- *          
+ * Usage: deregister (unsubscribe) durable subscriptions args must have: -clientid <client-id> -subscriptionname
+ * <subscription-name> -durable
+ * 
  * @author Ikasan Development Team
  */
-public class TUnsubscriber 
-    extends AbstractJMSHandler
-{ 
-
+public class TUnsubscriber extends AbstractJMSHandler
+{
     /** Logger for this class */
     private static final Logger logger = Logger.getLogger(TUnsubscriber.class);
-    
+
     /**
-     * Main method, kicks off an unsubscription.
-     *
+     * Main method, kicks off an un-subscription.
+     * 
      * @param args
      * @throws NamingException
      */
-    public static void main(String[] args)
-        throws NamingException 
+    public static void main(String[] args) throws NamingException
     {
         TUnsubscriber unsubscriber = new TUnsubscriber();
-        //Initialise the unsubscriber instance
         unsubscriber.init(args);
-        //Unsubscribe
         unsubscriber.unsubscribe();
     }
 
     /**
-     * Unsubscribe from a message queue
+     * Un-subscribe from a message queue/topic
      */
     public void unsubscribe()
     {
         Session session = null;
-        Connection con = null;
+        Connection connection = null;
         String clientID = JMSToolsUtils.getClientID();
         String subscriptionName = JMSToolsUtils.getSubscriptionName();
-        
-        try {
-//            Context ctx = JMSToolsUtils.getContext();
-//            ResourceBundle bundle = JMSToolsUtils.getBundle();
-//            String connectionFactory = bundle.getString(JMSConstants.APP_SERVER_CONNECTIONFACTORY_JNDI_KEY);
-            
+        try
+        {
             if (!JMSToolsUtils.isQueue())
             {
-                //Only durable subscriptions can be unsubscribed
-                //Removes ALL subscriptions with ID clientID.subsciptionName from any topic.
-                //According to JMS specification:
-                //It is erroneous for a client to delete a durable subscription while there is an 
-                //active TopicSubscriber for the subscription, or while a consumed message is part 
-                //of a pending transaction or has not been acknowledged in the session.
-                //A JMSException will be thrown
-//                TopicConnectionFactory fac =(TopicConnectionFactory) ctx.lookup(connectionFactory);
-//
-//                if(JMSToolsUtils.isAuthenticated())
-//                {
-//                    // create standard credentials
-//                    IkasanPasswordCredential ipc = getCredential();
-//                    con = fac.createTopicConnection(ipc.getUsername(), ipc.getPassword());
-//                }
-//                else
-//                    con = fac.createTopicConnection();
-//
-                con = getConnection(JMSToolsUtils.isAuthenticated());
-
-                con.setClientID(clientID);
-                session = con.createSession(false, Session.AUTO_ACKNOWLEDGE);
+                boolean authenticated = JMSToolsUtils.isAuthenticated();
+                connection = getConnection(authenticated);
+                connection.setClientID(clientID);
+                session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
                 session.unsubscribe(subscriptionName);
-                logger.info(" Durable subscription ["+clientID +"."+subscriptionName+"] unsubscribed.");  //$NON-NLS-1$
+                logger.info(" Durable subscription [" + clientID + "." + subscriptionName + "] unsubscribed.");
             }
         }
         catch (JMSException e)
         {
-            logger.fatal("Caught JMSException: [" + e.getMessage() + "]", e); //$NON-NLS-1$ //$NON-NLS-2$
-        } 
+            logger.fatal("Caught JMSException: [" + e.getMessage() + "]", e);
+        }
         catch (NamingException e)
         {
             logger.fatal("Caught NamingException: [" + e.getMessage() + "]", e); //$NON-NLS-1$ //$NON-NLS-2$
         }
         finally
         {
-            //close the connection to topic when no further messages are to be received.
             try
-            { 
-                if (con != null)
+            {
+                if (connection != null)
                 {
-                    con.close();
-                    logger.info(" Closed Connection"); //$NON-NLS-1$
-                } else
+                    connection.close();
+                    logger.info(" Closed Connection");
+                }
+                else
                 {
-                    logger.info(" Connection was null, therefore already closed."); //$NON-NLS-1$
+                    logger.info(" Connection was null, therefore is already closed.");
                 }
             }
             catch (Exception e)
             {
-                logger.fatal("Caught exception: [" + e.getMessage() + "]", e); //$NON-NLS-1$ //$NON-NLS-2$
+                logger.warn("Caught exception: [" + e.getMessage() + "] trying to close the connection", e);
             }
         }
     }
-    
 }
