028d7580492c705b760798286a41eb3b73806a34
karianna <karianna>
2010-09-14 11:25:11 +0000

    IKASAN-457 - Correct parameter reference for change password

--- webconsole/jar/src/main/java/org/ikasan/framework/web/controller/UsersController.java
+++ webconsole/jar/src/main/java/org/ikasan/framework/web/controller/UsersController.java
@@ -1,40 +1,40 @@
 /*
  * $Id$
  * $URL$
- * 
+ *
  * ====================================================================
  * Ikasan Enterprise Integration Platform
- * 
+ *
  * Distributed under the Modified BSD License.
- * Copyright notice: The copyright for this software and a full listing 
- * of individual contributors are as shown in the packaged copyright.txt 
- * file. 
- * 
+ * Copyright notice: The copyright for this software and a full listing
+ * of individual contributors are as shown in the packaged copyright.txt
+ * file.
+ *
  * All rights reserved.
  *
- * Redistribution and use in source and binary forms, with or without 
+ * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are met:
  *
- *  - Redistributions of source code must retain the above copyright notice, 
+ *  - Redistributions of source code must retain the above copyright notice,
  *    this list of conditions and the following disclaimer.
  *
- *  - Redistributions in binary form must reproduce the above copyright notice, 
- *    this list of conditions and the following disclaimer in the documentation 
+ *  - Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
  *    and/or other materials provided with the distribution.
  *
  *  - Neither the name of the ORGANIZATION nor the names of its contributors may
- *    be used to endorse or promote products derived from this software without 
+ *    be used to endorse or promote products derived from this software without
  *    specific prior written permission.
  *
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" 
- * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE 
- * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE 
- * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE 
- * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL 
- * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR 
- * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
  * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
- * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE 
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE
  * USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  * ====================================================================
  */
@@ -62,7 +62,7 @@ import org.springframework.web.bind.annotation.SessionAttributes;
 
 /**
  * The controller for the various user views
- * 
+ *
  * @author Ikasan Development Team
  */
 @Controller
@@ -84,7 +84,7 @@ public class UsersController
 
     /**
      * Constructor
-     * 
+     *
      * @param userService - The user service to use
      */
     @Autowired
@@ -96,7 +96,7 @@ public class UsersController
 
     /**
      * List the users known to the system
-     * 
+     *
      * @param model - The model (map)
      * @return "modules/modules"
      */
@@ -113,7 +113,7 @@ public class UsersController
 
     /**
      * Accepts submission of the createUser form
-     * 
+     *
      * @param model - The model (map)
      * @param user - The user we're trying to create
      * @param result - The result
@@ -141,7 +141,7 @@ public class UsersController
 
     /**
      * List the users known to the system
-     * 
+     *
      * @param username - The name of the user we're trying to view
      * @param model - The model (map)
      * @return path to user view
@@ -157,17 +157,17 @@ public class UsersController
 
     /**
      * Accepts submission of the changePassword form
-     * 
+     *
      * @param user - The user we're changing the password for
      * @param confirmNewPassword - The password again, for confirmation
      * @param model - The model (map)
-     * @param result - The binding result 
+     * @param result - The binding result
      * @return view the user
      */
     @RequestMapping(value = "changePassword.htm", method = RequestMethod.POST)
-    public String changePassword(@ModelAttribute("user") User user, @RequestParam("confim_password") String confirmNewPassword, ModelMap model, BindingResult result)
+    public String changePassword(@ModelAttribute("user") User user, @RequestParam("confirm_password") String confirmNewPassword, ModelMap model, BindingResult result)
     {
-        try 
+        try
         {
             this.userService.changeUsersPassword(user.getUsername(), user.getPassword(), confirmNewPassword);
         }
@@ -180,8 +180,8 @@ public class UsersController
 
     /**
      * Grant a new authority to an existing user
-     * 
-     * @param username - The name of the user we're granting authority to 
+     *
+     * @param username - The name of the user we're granting authority to
      * @param authority - The authority we're granting the user
      * @param model - The model (map)
      * @return to the view users
@@ -196,8 +196,8 @@ public class UsersController
 
     /**
      * Revoke an authority from a existing user
-     * 
-     * @param username - The name of the user we're revoking authority from 
+     *
+     * @param username - The name of the user we're revoking authority from
      * @param authority - The authority we're revoking from the user
      * @param model - The model (map)
      * @return to the view users
@@ -212,7 +212,7 @@ public class UsersController
 
     /**
      * Delete a existing user
-     * 
+     *
      * @param username - The name of the user we're deleting
      * @param model - The model (map)
      * @return to the view users
@@ -226,8 +226,8 @@ public class UsersController
 
     /**
      * Disable an existing user
-     * 
-     * @param username - The name of the user we're disabling 
+     *
+     * @param username - The name of the user we're disabling
      * @param model - The model (map)
      * @return to the view users
      */
@@ -240,7 +240,7 @@ public class UsersController
 
     /**
      * Enable an existing user
-     * 
+     *
      * @param username - The name of the user we're enabling
      * @param model - The model (map)
      * @return to the view users
@@ -254,7 +254,7 @@ public class UsersController
 
     /**
      * Get the list of non granted authorities
-     * 
+     *
      * @param authorities - Full list of authorities
      * @return list of non granted authorities
      */
