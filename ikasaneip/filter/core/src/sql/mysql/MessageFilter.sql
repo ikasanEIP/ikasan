8db3c0c5be6dc637949c4a208d1790be14105ad5
summer_h <summer_h>
2010-11-25 14:03:35 +0000

    IKASAN-549

--- ikasaneip/filter/core/src/sql/mysql/MessageFilter.sql
+++ ikasaneip/filter/core/src/sql/mysql/MessageFilter.sql
@@ -42,15 +42,15 @@
 -- 
 
 -- Drop aleady existing table if any
-DROP TABLE IF ESISTS `ikasan01`.`MessageFilter`;
+DROP TABLE IF ESISTS `Ikasan01`.`MessageFilter`;
 
 -- Create new table
-CREATE TABLE `ikasan01`.`MessageFilter`
+CREATE TABLE `Ikasan01`.`MessageFilter`
 (
-    Criteria        binint(18)    NOT NULL,
-    ClientId        varchar(255)  NOT NULL,
-    CreatedDateTime datetime      NOT NULL,
-    Expiry          datetime      NOT NULL
+    `Criteria`        bigint(18)    NOT NULL,
+    `ClientId`        varchar(256)  NOT NULL,
+    `CreatedDateTime` datetime      NOT NULL,
+    `Expiry`          datetime      NOT NULL,
     PRIMARY KEY     (`Criteria`, `ClientId`)
 )
 ENGINE=InnoDB DEFAULT CHARSET=latin1;
