7a7563a5fec1d2801cca4b83014ee9b467d9e8fd
mitcje <jeff@tribaldog.co.uk>
2011-02-01 09:31:12 +0000

    put common back to get dependencies progressed for now

--- common/src/main/java/org/ikasan/common/util/Env.java
+++ common/src/main/java/org/ikasan/common/util/Env.java
@@ -46,13 +46,14 @@ import java.util.Properties;
 import java.util.regex.Pattern;
 
 import org.apache.log4j.Logger;
+import org.ikasan.common.CommonEnvironment;
 
 /**
  * This class captures all environment variables set in Java system property option.
  * 
  * @author Ikasan Development Team
  */
-public class Env
+public class Env implements CommonEnvironment
 {
     /** Serial ID */
     private static final long serialVersionUID = 1L;
