a51a46c2af36d1dad5696b9eeac524f4ecbc9457
mitcje <jeff@tribaldog.co.uk>
2011-04-04 12:35:41 +0000

    WIP

---
+++ src/main/java/org/ikasan/exceptionHandler/action/StopAction.java
@@ -0,0 +1,81 @@
+/*
+ * $Id$
+ * $URL$
+ * 
+ * ====================================================================
+ * Ikasan Enterprise Integration Platform
+ * 
+ * Distributed under the Modified BSD License.
+ * Copyright notice: The copyright for this software and a full listing 
+ * of individual contributors are as shown in the packaged copyright.txt 
+ * file. 
+ * 
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without 
+ * modification, are permitted provided that the following conditions are met:
+ *
+ *  - Redistributions of source code must retain the above copyright notice, 
+ *    this list of conditions and the following disclaimer.
+ *
+ *  - Redistributions in binary form must reproduce the above copyright notice, 
+ *    this list of conditions and the following disclaimer in the documentation 
+ *    and/or other materials provided with the distribution.
+ *
+ *  - Neither the name of the ORGANIZATION nor the names of its contributors may
+ *    be used to endorse or promote products derived from this software without 
+ *    specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" 
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE 
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE 
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE 
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL 
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR 
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER 
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE 
+ * USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ * ====================================================================
+ */
+package org.ikasan.exceptionHandler.action;
+
+/**
+ * Exception action indicating the Initiator should be stopped
+ * 
+ * @author Ikasan Development Team
+ */
+public class StopAction implements ExceptionAction
+{
+    /** Stop Action */
+    public static final String STOP = "Stop"; 
+    
+    /**
+     * Static instance, probably all that is needed, as one StopAction is the same as another
+     */
+    private static StopAction instance = new StopAction();
+
+    /**
+     * Private constructor prevents construction other than static instance
+     */
+    private StopAction()
+    {
+        // Do Nothing
+    }
+
+    /**
+     * Accessor for static instance
+     * 
+     * @return static instance
+     */
+    public static StopAction instance()
+    {
+        return instance;
+    }
+
+    @Override
+    public String toString()
+    {
+        return STOP;
+    }
+}
