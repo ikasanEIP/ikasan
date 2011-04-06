3c1f6a0860e301ce3abf73a3712ca1667739b221
mitcje <jeff@tribaldog.co.uk>
2011-04-06 16:17:28 +0000

    WIP

---
+++ sample/priceSrc/src/main/java/org/ikasan/sample/priceSrc/component/convertor/PriceConverter.java
@@ -0,0 +1,40 @@
+/*
+ * $Id$
+ * $URL$
+ *
+ * ====================================================================
+ *
+ * Copyright (c) 2000-20010 by Mizuho International plc.
+ * All Rights Reserved.
+ *
+ * ====================================================================
+ *
+ */
+package org.ikasan.sample.priceSrc.component.convertor;
+
+import org.ikasan.sample.priceSrc.tech.PriceTechMessage;
+import org.ikasan.spec.component.transformation.Converter;
+import org.ikasan.spec.component.transformation.TransformationException;
+
+/**
+ * This test class supports the <code>Translator</code> class.
+ * 
+ * @author Ikasan Development Team
+ */
+public class PriceConverter implements Converter<PriceTechMessage,StringBuilder>
+{
+    public StringBuilder convert(PriceTechMessage price) throws TransformationException
+    {
+        StringBuilder sb = new StringBuilder();
+        sb.append("identifier = ");
+        sb.append(price.getIdentifier());
+        sb.append(" bid = ");
+        sb.append(price.getBid());
+        sb.append(" spread = ");
+        sb.append(price.getSpread());
+        sb.append(" at = ");
+        sb.append(price.getDate());
+        
+        return sb;
+    }
+}
