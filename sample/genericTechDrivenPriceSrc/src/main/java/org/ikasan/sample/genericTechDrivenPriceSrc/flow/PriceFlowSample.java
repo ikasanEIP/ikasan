f0865e73a12dd7f07bd92a7db43b9c360bd65860
mitcje <jeff@tribaldog.co.uk>
2011-04-12 15:28:50 +0000

    WIP

---
+++ sample/genericTechDrivenPriceSrc/src/main/java/org/ikasan/sample/genericTechDrivenPriceSrc/flow/PriceFlowSample.java
@@ -0,0 +1,126 @@
+/*
+ * $Id$
+ * $URL$
+ *
+ * ====================================================================
+ *
+ * Copyright (c) 2000-20010 by Mizuho International plc.
+ * All Rights Reserved.
+ *
+ * ====================================================================
+ *
+ */
+package org.ikasan.sample.genericTechDrivenPriceSrc.flow;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.hamcrest.core.IsInstanceOf;
+import org.ikasan.flow.configuration.dao.ConfigurationDao;
+import org.ikasan.flow.configuration.dao.ConfigurationHibernateImpl;
+import org.ikasan.flow.configuration.service.ConfiguredResourceConfigurationService;
+import org.ikasan.flow.event.FlowEventFactory;
+import org.ikasan.flow.visitorPattern.DefaultFlowConfiguration;
+import org.ikasan.flow.visitorPattern.FlowConfiguration;
+import org.ikasan.flow.visitorPattern.FlowElementImpl;
+import org.ikasan.flow.visitorPattern.VisitingFlowElementInvoker;
+import org.ikasan.flow.visitorPattern.VisitingInvokerFlow;
+import org.ikasan.recovery.ScheduledRecoveryManagerFactory;
+import org.ikasan.sample.priceSrc.component.convertor.PriceConverter;
+import org.ikasan.sample.priceSrc.component.endpoint.PriceConsumer;
+import org.ikasan.sample.priceSrc.component.endpoint.PriceProducer;
+import org.ikasan.sample.priceSrc.tech.PriceTechImpl;
+import org.ikasan.spec.component.endpoint.Consumer;
+import org.ikasan.spec.component.endpoint.Producer;
+import org.ikasan.spec.component.transformation.Converter;
+import org.ikasan.spec.configuration.service.ConfigurationService;
+import org.ikasan.spec.flow.Flow;
+import org.ikasan.spec.flow.FlowElement;
+import org.ikasan.spec.flow.FlowElementInvoker;
+import org.ikasan.spec.recovery.RecoveryManager;
+import org.junit.Before;
+import org.junit.Test;
+import org.quartz.SchedulerException;
+import org.quartz.impl.StdSchedulerFactory;
+
+/**
+ * Pure Java based sample of Ikasan EIP for sourcing prices from a tech endpoint.
+ * 
+ * @author Ikasan Development Team
+ */
+public class PriceFlowSample
+{
+    FlowEventFactory flowEventFactory = new FlowEventFactory();
+    ScheduledRecoveryManagerFactory scheduledRecoveryManagerFactory;
+    
+    protected PriceTechImpl getTechImpl()
+    {
+        return new PriceTechImpl();
+    }
+    
+    protected FlowEventFactory getEventFactory()
+    {
+        return new FlowEventFactory();
+    }
+    
+    protected ConfigurationService getConfigurationService()
+    {
+        ConfigurationDao configurationDao = new ConfigurationHibernateImpl();
+        return new ConfiguredResourceConfigurationService(configurationDao, configurationDao);
+    }
+    
+//    protected ExceptionHandler getExceptionHandler()
+//    {
+//        // flow exception handler
+//        int delay = 1000;
+//        int retries = 10;
+//        ExceptionAction stopAction = StopAction.instance();
+//          
+//        IsInstanceOf instanceOfException = new org.hamcrest.core.IsInstanceOf(Exception.class);
+//        MatcherBasedExceptionGroup matcher = new MatcherBasedExceptionGroup(instanceOfException, stopAction);
+//          
+//        List<ExceptionGroup> matchers = new ArrayList<ExceptionGroup>();
+//        matchers.add(matcher);
+//          
+//        ExceptionHandler exceptionHandler = new MatchingExceptionHandler(matchers);
+//
+//        return exceptionHandler;
+//    }
+    
+    @Before
+    public void setup() throws SchedulerException
+    {
+        this.scheduledRecoveryManagerFactory  = 
+            new ScheduledRecoveryManagerFactory(StdSchedulerFactory.getDefaultScheduler());
+    }
+
+    @Test
+    public void test_flow_consumer_translator_producer() throws SchedulerException
+    {
+        Producer producer = new PriceProducer();
+        FlowElement producerFlowElement = new FlowElementImpl("priceProducer", producer);
+
+        Converter priceToStringBuilderConverter = new PriceConverter();
+        FlowElement<Converter> converterFlowElement = new FlowElementImpl("priceToStringBuilder", priceToStringBuilderConverter, producerFlowElement);
+
+        Consumer consumer = new PriceConsumer(getTechImpl(), getEventFactory());
+        FlowElement<Consumer> consumerFlowElement = new FlowElementImpl("priceConsumer", consumer, converterFlowElement);
+
+        // flow configuration wiring
+        FlowConfiguration flowConfiguration = new DefaultFlowConfiguration(consumerFlowElement, getConfigurationService());
+
+        // iterator over each flow element
+        FlowElementInvoker flowElementInvoker = new VisitingFlowElementInvoker();
+
+        RecoveryManager recoveryManager = scheduledRecoveryManagerFactory.getRecoveryManager("flowName", "moduleName", consumer);
+        
+        // container for the complete flow
+        Flow flow = new VisitingInvokerFlow("flowName", "moduleName", 
+            flowConfiguration, flowElementInvoker, recoveryManager);
+        
+        flow.start();
+
+        flow.stop();
+    }
+
+}
---
+++ sample/genericTechDrivenPriceSrc/src/main/java/org/ikasan/sample/genericTechDrivenPriceSrc/tech/PriceTechImpl.java
@@ -0,0 +1,131 @@
+/*
+ * $Id$
+ * $URL$
+ *
+ * ====================================================================
+ *
+ * Copyright (c) 2000-20010 by Mizuho International plc.
+ * All Rights Reserved.
+ *
+ * ====================================================================
+ *
+ */
+package org.ikasan.sample.genericTechDrivenPriceSrc.tech;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+/**
+ * This test demonstrates a stubbed price tech implementation as
+ * such would be provided by any system/application API.
+ * 
+ * @author Ikasan Development Team
+ */
+public class PriceTechImpl implements Runnable
+{
+    /** registered listener on the tech */
+    private PriceTechListener priceTechListener;
+
+    /** price generator */
+    private Random randomGenerator = new Random();
+    
+    /** time between checking to see if delivery is active */
+    private long loop = 1000;
+    
+    /** pause time between price ticks */
+    private long delay = 0;
+    
+    /** thread controller */
+    boolean active = true;
+
+    /** price delivery controller */
+    boolean deliver = false;
+
+    /** identifiers */
+    private List<String> identifiers = new ArrayList<String>();
+    {
+        identifiers.add("abc");
+        identifiers.add("def");
+        identifiers.add("ghi");
+        identifiers.add("jkl");
+        identifiers.add("mno");
+    }
+    
+    /**
+     * Setter for the tech listener.
+     * @param priceTechListener
+     */
+    public void setListener(PriceTechListener priceTechListener)
+    {
+        this.priceTechListener = priceTechListener;
+    }
+
+    public void setDelay(long delay)
+    {
+        this.delay = delay;
+    }
+    
+    /**
+     * Separate thread for invoking the registered listener with a 
+     * tech data message.
+     */
+    public void run()
+    {
+        while(active)
+        {
+            try
+            {
+                while(deliver)
+                {
+                    int selectedIdentifier = randomGenerator.nextInt(5);
+                    int bid = randomGenerator.nextInt();
+                    int spread = randomGenerator.nextInt(10);
+                    PriceTechMessage price = new PriceTechMessage(identifiers.get(selectedIdentifier), bid, spread);
+                    this.priceTechListener.onPrice(price);
+                    
+                    pause(delay);
+                }
+    
+                Thread.sleep(loop);
+            }
+            catch (InterruptedException e)
+            {
+                // dont care
+            }
+        }
+    }
+    
+    public void startDelivery()
+    {
+        this.deliver = true;
+    }
+
+    public void stopDelivery()
+    {
+        this.deliver = false;
+    }
+
+    public boolean isRunning()
+    {
+        return this.deliver;
+    }
+    
+    public void shutdown()
+    {
+        stopDelivery();
+        this.active = false;
+    }
+    
+    private void pause(long period)
+    {
+        try
+        {
+            Thread.sleep(period);
+        }
+        catch(InterruptedException e)
+        {
+            // dont care
+        }
+    }
+}
