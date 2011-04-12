6ad1e44652c46c3d2593bf052c8f35a30a6bda56
mitcje <jeff@tribaldog.co.uk>
2011-04-12 15:28:50 +0000

    WIP

---
+++ experimental-ikasaneip-core/sample/genericTechDrivenPriceSrc/src/main/java/org/ikasan/sample/genericTechDrivenPriceSrc/tech/PriceTechListener.java
@@ -0,0 +1,24 @@
+/*
+ * $Id$
+ * $URL$
+ *
+ * ====================================================================
+ *
+ * Copyright (c) 2000-20010 by Mizuho International plc.
+ * All Rights Reserved.
+ *
+ * ====================================================================
+ *
+ */
+package org.ikasan.sample.genericTechDrivenPriceSrc.tech;
+
+/**
+ * This test demonstrates a stubbed tech listener as
+ * such would be utilised to listen to any system/application API.
+ * 
+ * @author Ikasan Development Team
+ */
+public interface PriceTechListener
+{
+    public void onPrice(PriceTechMessage message);    
+}
---
+++ experimental-ikasaneip-core/sample/genericTechDrivenPriceSrc/src/main/java/org/ikasan/sample/genericTechDrivenPriceSrc/tech/PriceTechMessage.java
@@ -0,0 +1,55 @@
+/*
+ * $Id$
+ * $URL$
+ *
+ * ====================================================================
+ *
+ * Copyright (c) 2000-20010 by Mizuho International plc.
+ * All Rights Reserved.
+ *
+ * ====================================================================
+ *
+ */
+package org.ikasan.sample.genericTechDrivenPriceSrc.tech;
+
+import java.util.Date;
+
+/**
+ * This test demonstrates a stubbed tech data model as
+ * such would be utilised and transmitted by any system/application API.
+ * 
+ * @author Ikasan Development Team
+ */
+public class PriceTechMessage
+{
+    private long time;
+    private String identifier;
+    private int bid;
+    private int spread;
+    
+    public PriceTechMessage(String identifier, int bid, int spread)
+    {
+        this.identifier = identifier;
+        this.bid = bid;
+        this.spread = spread;
+        this.time = System.currentTimeMillis();
+    }
+    
+    public long getTime()
+    {
+        return time;
+    }
+    public String getIdentifier()
+    {
+        return identifier;
+    }
+    public int getBid()
+    {
+        return bid;
+    }
+    public int getSpread()
+    {
+        return spread;
+    }
+
+}
