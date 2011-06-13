11f61fb62f0667aaea13c0d0e0bd29c9cdaea982
mitcje <jeff@tribaldog.co.uk>
2011-06-13 02:05:55 +0000

    http://open.jira.com/browse/IKASAN-598 - Initial deprecation of the framework artefact code. Some of the new artefacts are simply placeholders until we find a more permanent home.

--- service/search/src/main/java/org/ikasan/spec/search/PagedSearchResult.java
+++ service/search/src/main/java/org/ikasan/spec/search/PagedSearchResult.java
@@ -1,7 +1,7 @@
-/* 
- * $Id$
+/*
+ * $Id$ 
  * $URL$
- *
+ * 
  * ====================================================================
  * Ikasan Enterprise Integration Platform
  * 
@@ -38,37 +38,43 @@
  * USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  * ====================================================================
  */
-package org.ikasan.spec.module;
+package org.ikasan.spec.search;
 
-import java.util.Map;
-
-import org.ikasan.spec.flow.Flow;
+import java.util.List;
 
 /**
- * Ikasan module contracts.
+ * This class is intended to be used as a DTO for transporting a subset of search results for
+ * some domain object <T> from a larger set of searched results. This is intended to support
+ * paging.
+ * 
+ * For example we may be performing a search for some domain entities that would return 1000 results
+ * if not paged. For performance and usability reasons, a search result of 1000 entries may not be
+ * desired. This class allows for sub result set to be returned, including enough information to
+ * establish its position within the superset, as well as the size of the super set
+ * 
  * 
  * @author Ikasan Development Team
+ *
  */
-public interface Module
-{
-    /**
-     * Returns the name of the module
-     * 
-     * @return name of the module
-     */
-    public String getName();
+public interface PagedSearchResult<T> extends List<T> {
 
-    /**
-     * Returns a {@link Map} of this module's <code>Flow</code>s
-     * 
-     * @return a map of <code>Flow</code> using the {@link Flow#getName()} as key
-     */
-    public Map<String, Flow> getFlows();
 
-    /**
-     * Returns a human readable description of this module
-     * 
-     * @return String description
-     */
-    public String getDescription();
+	/**
+	 * Accessor for first result index
+	 * 
+	 * @return index of the first result shown here into the larger super set of results
+	 */
+	public int getFirstResultIndex();
+
+	/** 
+	 * @return true if this represents the last page in the super result set
+	 */
+	public boolean isLastPage();
+
+	/**
+	 * Accessor for resultSize
+	 * 
+	 * @return size of the larger super result set
+	 */
+	public long getResultSize();
 }
