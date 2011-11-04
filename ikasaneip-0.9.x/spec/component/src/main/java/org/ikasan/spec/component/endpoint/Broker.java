d7a40c7468a34ad5ca3adc3263bd66d58d2e17ba
mitcje <jeff@tribaldog.co.uk>
2011-11-04 01:05:15 +0000

    simplify the component spec split

---
+++ ikasaneip-0.9.x/spec/component/src/main/java/org/ikasan/spec/component/endpoint/Broker.java
@@ -0,0 +1,59 @@
+/*
+ * $Id: Broker.java 3545 2011-04-04 12:46:31Z mitcje $
+ * $URL: https://open.jira.com/svn/IKASAN/branches/ikasaneip-0.9.x/spec/component/endpoint/src/main/java/org/ikasan/spec/component/endpoint/Broker.java $
+ *
+ * ====================================================================
+ * Ikasan Enterprise Integration Platform
+ * 
+ * Distributed under the Modified BSD License.
+ * Copyright notice: The copyright for this software and a full listing 
+ * of individual contributors are as shown in the packaged copyright.txt 
+ * file. 
+ * 
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without 
+ * modification, are permitted provided that the following conditions are met:
+ *
+ *  - Redistributions of source code must retain the above copyright notice, 
+ *    this list of conditions and the following disclaimer.
+ *
+ *  - Redistributions in binary form must reproduce the above copyright notice, 
+ *    this list of conditions and the following disclaimer in the documentation 
+ *    and/or other materials provided with the distribution.
+ *
+ *  - Neither the name of the ORGANIZATION nor the names of its contributors may
+ *    be used to endorse or promote products derived from this software without 
+ *    specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" 
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE 
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE 
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE 
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL 
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR 
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER 
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE 
+ * USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ * ====================================================================
+ */
+package org.ikasan.spec.component.endpoint;
+
+/**
+ * Component broker for request/response scenario from an endpoint.
+ * 
+ * @return <SOURCE> 
+ *
+ * @author Ikasan Development Teams
+ */
+public interface Broker<TARGET,SOURCE>
+{
+    /**
+     * Consume payload from a source protocol endpoint
+     * @param <TARGET>
+     * @return <SOURCE>
+     * @throws EndpointException
+     */
+    public SOURCE invoke(TARGET target) throws EndpointException;
+}
