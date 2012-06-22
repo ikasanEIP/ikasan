b5d8e7b7074dc09abc77ddeabc0177840e2ad30a
mitcje <jeff@tribaldog.co.uk>
2012-06-22 15:00:33 +0000

    WIP - refactoring for cleanup

--- consumer/src/main/java/org/ikasan/consumer/jms/GenericConsumer.java
+++ consumer/src/main/java/org/ikasan/consumer/jms/GenericConsumer.java
@@ -40,19 +40,12 @@
  */
 package org.ikasan.consumer.jms;
 
-import javax.jms.Connection;
-import javax.jms.ConnectionFactory;
-import javax.jms.Destination;
-import javax.jms.ExceptionListener;
 import javax.jms.JMSException;
 import javax.jms.Message;
-import javax.jms.MessageConsumer;
-import javax.jms.MessageListener;
-import javax.jms.Session;
-import javax.jms.Topic;
 
-import org.apache.log4j.Logger;
 import org.ikasan.spec.component.endpoint.Consumer;
+import org.ikasan.spec.endpoint.EndpointListener;
+import org.ikasan.spec.endpoint.ManagedEndpoint;
 import org.ikasan.spec.event.EventFactory;
 import org.ikasan.spec.event.EventListener;
 import org.ikasan.spec.flow.FlowEvent;
@@ -62,26 +55,11 @@ import org.ikasan.spec.flow.FlowEvent;
  *
  * @author Ikasan Development Team
  */
-public class GenericJmsConsumer 
-    implements Consumer<EventListener<?>>, MessageListener, ExceptionListener
+public class GenericConsumer 
+    implements Consumer<EventListener<?>>, EndpointListener<Message>
 {
-    /** class logger */
-    private static Logger logger = Logger.getLogger(GenericJmsConsumer.class);
-
-    /** JMS Connection Factory */
-    protected ConnectionFactory connectionFactory;
-
-    /** JMS Destination instance */
-    protected Destination destination;
-
-    /** JMS Connection */
-    protected Connection connection;
-
     /** consumer event factory */
     protected EventFactory<FlowEvent<?,?>> flowEventFactory;
-
-    /** session has to be closed prior to connection being closed */
-    protected Session session;
 
     /** consumer event listener */
     protected EventListener eventListener;
@@ -92,32 +70,36 @@ public class GenericJmsConsumer
     /** JMS consumer configuration - default to vanilla instance */
     protected GenericJmsConsumerConfiguration configuration = new GenericJmsConsumerConfiguration();
     
+    /** actual tech endpoint being managed */
+    private ManagedEndpoint managedEndpoint;
+
+    /** tech endpoint listener for callbacks from the endpoint */
+    private EndpointListener<Message> endpointListener;
+    
     /**
      * Constructor
      * @param connectionFactory
      * @param destination
      * @param flowEventFactory
      */
-    public GenericJmsConsumer(ConnectionFactory connectionFactory, Destination destination, 
-            EventFactory<FlowEvent<?,?>> flowEventFactory)
+    public GenericConsumer(EventFactory<FlowEvent<?,?>> flowEventFactory, ManagedEndpoint managedEndpoint)
     {
-        this.connectionFactory = connectionFactory;
-        if(connectionFactory == null)
-        {
-            throw new IllegalArgumentException("connectionFactory cannot be 'null'");
-        }
-        
-        this.destination = destination;
-        if(destination == null)
-        {
-            throw new IllegalArgumentException("destination cannot be 'null'");
-        }
-        
         this.flowEventFactory = flowEventFactory;
         if(flowEventFactory == null)
         {
             throw new IllegalArgumentException("flowEventFactory cannot be 'null'");
         }
+        
+        this.managedEndpoint = managedEndpoint;
+        if(managedEndpoint == null)
+        {
+            throw new IllegalArgumentException("managedEndpoint cannot be 'null'");
+        }
+    }
+
+    public void setEndpointListener(EndpointListener endpointListener)
+    {
+        this.endpointListener = endpointListener;
     }
     
     /**
@@ -125,40 +107,9 @@ public class GenericJmsConsumer
      */
     public void start()
     {
-        MessageConsumer messageConsumer;
-
-        try
-        {
-            if(this.configuration.getUsername() != null && this.configuration.getUsername().trim().length() > 0)
-            {
-                connection = connectionFactory.createConnection(this.configuration.getUsername(), this.configuration.getPassword());
-            }
-            else
-            {
-                connection = connectionFactory.createConnection();
-            }
-
-            connection.setClientID(this.configuration.getClientId());
-            connection.setExceptionListener(this);
-
-            this.session = connection.createSession(this.configuration.isTransacted(), this.configuration.getAcknowledgement());
-
-            if(destination instanceof Topic && this.configuration.isDurable())
-            {
-                messageConsumer = session.createDurableSubscriber((Topic)destination, this.configuration.getSubscriberId());
-            }
-            else
-            {
-                messageConsumer = session.createConsumer(destination, this.configuration.getSubscriberId());
-            }
-            
-            messageConsumer.setMessageListener(this);
-            connection.start();
-        }
-        catch (JMSException e)
-        {
-            throw new RuntimeException(e);
-        }
+        managedEndpoint.setEndpointConfiguration(configuration);
+        managedEndpoint.setListener(endpointListener);
+        managedEndpoint.startManagedEndpoint();
     }
 
     /**
@@ -166,31 +117,8 @@ public class GenericJmsConsumer
      */
     public void stop()
     {
-        if(session != null)
-        {
-            try
-            {
-                session.close();
-                session = null;
-            }
-            catch (JMSException e)
-            {
-                logger.error("Failed to close session", e);
-            }
-        }
-
-        if(connection != null)
-        {
-            try
-            {
-                connection.stop();
-                connection = null;
-            }
-            catch (JMSException e)
-            {
-                throw new RuntimeException(e);
-            }
-        }
+        managedEndpoint.stopManagedEndpoint();
+        managedEndpoint.setListener(null);
     }
 
     /**
@@ -200,7 +128,7 @@ public class GenericJmsConsumer
      */
     public boolean isRunning()
     {
-        return connection != null;
+        return managedEndpoint.isActive();
     }
 
     /**
@@ -236,7 +164,7 @@ public class GenericJmsConsumer
      * Callback method from the JMS connector for exception reporting.
      * @param JMSException
      */
-    public void onException(JMSException jmsException)
+    public void onException(Throwable jmsException)
     {
         this.eventListener.invoke(jmsException);
     }
