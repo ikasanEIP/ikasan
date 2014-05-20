f981fcccaeb589f91ca43718b0ef078ed0c602c2
mitcje <jeff@ikasan.org>
2014-05-20 14:30:12 +0100

    IKASAN-687 - simple java for persistence creation

--- ikasaneip/platform/ikasan-setup/src/main/java/org/ikasan/setup/Persistence.java
+++ ikasaneip/platform/ikasan-setup/src/main/java/org/ikasan/setup/Persistence.java
@@ -42,10 +42,11 @@ package org.ikasan.setup;
 
 import org.apache.log4j.Logger;
 
+import java.io.*;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
-import java.sql.Statement;
+import java.util.Properties;
 
 /**
  * Simple persistence creation utility class for the initial creation
@@ -58,10 +59,24 @@ public class Persistence
     private static Logger logger = Logger.getLogger(Persistence.class);
 
     // required parameters
-    static final String DRIVER_CLASS = "driver";
-    static final String URL = "url";
+    static final String CONF = "conf";
     static final String USER = "user";
     static final String PASS = "pass";
+    static final String HOST = "host";
+    static final String PORT = "port";
+    static final String DATABASE = "database";
+    static final String DRIVER = "driver";
+    static final String URL = "url";
+
+    /** persistence creator instances */
+    PersistenceCreator wiretapPersistenceCreator;
+    PersistenceCreator configurationPersistenceCreator;
+    PersistenceCreator modulePersistenceCreator;
+    PersistenceCreator consolePersistenceCreator;
+    PersistenceCreator securityPersistenceCreator;
+
+    /** connection to persistence */
+    Connection connection;
 
     /**
      * standalone entry
@@ -69,18 +84,122 @@ public class Persistence
      */
     public static void main(String[] args)
     {
-        if( System.getProperty(USER) == null || System.getProperty(PASS) == null ||
-            System.getProperty(URL) == null || System.getProperty(DRIVER_CLASS) == null )
+        try
         {
+            Persistence persistence = new Persistence(
+                    System.getProperty(USER),
+                    System.getProperty(PASS),
+                    System.getProperty(HOST),
+                    System.getProperty(PORT),
+                    System.getProperty(DATABASE),
+                    System.getProperty(CONF));
+
+            persistence.execute();
+        }
+        catch(RuntimeException e)
+        {
+            logger.error("Failed to invoke", e);
             usage();
-            return;
+        }
+    }
+
+    /**
+     * Constructor
+     * @param user
+     * @param password
+     * @param host
+     * @param port
+     * @param database
+     * @param conf
+     */
+    public Persistence(String user, String password, String host, String port, String database, String conf)
+    {
+        Properties dbConfiguration = loadConf(conf);
+
+        this.connection = getConnection(
+                dbConfiguration.getProperty(DRIVER),
+                convertURL(dbConfiguration.getProperty(URL),host, port, database),
+                user, password);
+
+        this.wiretapPersistenceCreator = new WiretapPersistence(connection, dbConfiguration);
+        this.configurationPersistenceCreator = new ConfigurationPersistence(connection, dbConfiguration);
+        this.modulePersistenceCreator = new ModulePersistence(connection, dbConfiguration);
+        this.consolePersistenceCreator = new ConsolePersistence(connection, dbConfiguration);
+        this.securityPersistenceCreator = new SecurityPersistence(connection, dbConfiguration);
+    }
+
+    /**
+     * replace host, port, and db in URL string
+     * @param url
+     * @param hostname
+     * @param port
+     * @param database
+     * @return
+     */
+    protected String convertURL(String url, String hostname, String port, String database)
+    {
+        return url.replaceFirst("HOSTNAME", hostname).
+                replaceFirst("PORT", port).
+                replaceFirst("DATABASE", database);
+    }
+
+    /**
+     * load configuration for persistence
+     * @param conf
+     * @return
+     */
+    protected Properties loadConf(String conf)
+    {
+        InputStream inputStream = null;
+        if(conf != null)
+        {
+            inputStream = getInputStream(new File(conf));
+            if(inputStream != null)
+            {
+                logger.info("loaded conf [" + conf + "] from file system.");
+            }
+            else
+            {
+                inputStream = this.getClass().getClassLoader().getResourceAsStream(conf);
+                logger.info("loaded conf [" + conf + "] from classpath.");
+            }
         }
 
-        new Persistence().execute(
-                System.getProperty(USER),
-                System.getProperty(PASS),
-                System.getProperty(URL),
-                System.getProperty(DRIVER_CLASS));
+        if(inputStream == null)
+        {
+            return null;
+        }
+
+        Properties properties = new Properties();
+        try
+        {
+            properties.loadFromXML(inputStream);
+        }
+        catch (IOException e)
+        {
+            logger.error("Failed to load properties " + conf, e);
+            return null;
+        }
+
+        return properties;
+    }
+
+    /**
+     * load as file
+     * @param file
+     * @return
+     */
+    protected static InputStream getInputStream(File file)
+    {
+        try
+        {
+            return new FileInputStream(file);
+        }
+        catch(FileNotFoundException e)
+        {
+            logger.debug("Failed to load properties from file system" + file.getName(), e);
+            return null;
+        }
     }
 
     /**
@@ -88,149 +207,40 @@ public class Persistence
      */
     protected static void usage()
     {
-        logger.info("java -Duser=username -Dpass=password -Ddriver=com.sybase.jdbc4.jdbc.SybDataSource -Durl=jdbc:sybase:Tds:svc-dbadv_ase_cmi2d:PORT/DATABASE -jar jarFile");
+        logger.info("");
+        logger.info("Example Usage:");
+        logger.info("java -D args where args,");
+        logger.info("\t-Dconf=sybase15.xml - load configuration from classpath sybase15.xml resource");
+        logger.info("\t-Dconf=confName - configuration properties resource");
+        logger.info("\t-Duser=username - database principal");
+        logger.info("\t-Dpass=password - database credential");
+        logger.info("\t-Dhost=hostname - database hostname");
+        logger.info("\t-Dport=port - database port");
+        logger.info("\t-Ddatabase=dbName - database name");
+        logger.info("");
+        logger.info("java -Dconf=sybase15.xml -Duser=username -Dpass=password -Dhost=svc-dbadv_ase_cmi2d -Dport=50100 -Ddatabase=DBNAME -jar jarFile");
     }
 
     /**
      * Execute the persistence creation
-     * @param username
-     * @param password
-     * @param url
-     * @param driverClass
      */
-    public void execute(String username, String password, String url, String driverClass)
+    public void execute()
     {
+        this.wiretapPersistenceCreator.execute();
+        this.configurationPersistenceCreator.execute();
+        this.modulePersistenceCreator.execute();
+        this.consolePersistenceCreator.execute();
+        this.securityPersistenceCreator.execute();
+
         try
         {
-            Connection connection = getConnection(driverClass, url, username, password);
-            this.createWiretapPersistence(connection);
-            this.createConfigurationPersistence(connection);
-            this.createModulePersistence(connection);
-            this.createConsolePersistence(connection);
-            this.createSecurityPersistence(connection);
+            this.connection.close();
         }
         catch(SQLException e)
         {
             throw new RuntimeException(e);
         }
      }
-
-    /**
-     * Wiretap required persistence
-     * @param connection
-     * @throws SQLException
-     */
-    protected void createWiretapPersistence(Connection connection) throws SQLException
-    {
-        Statement statement = null;
-        try
-        {
-            statement = connection.createStatement();
-            statement.executeUpdate(SybaseConstants.CREATE_WIRETAP_TABLE);
-            logger.info("Created IkasanWiretap Table");
-
-            statement.executeUpdate(SybaseConstants.CREATE_FLOW_EVENT_TRIGGER_TABLE);
-            logger.info("Created FlowEventTrigger Table");
-
-            statement.executeUpdate(SybaseConstants.CREATE_FLOW_EVENT_TRIGGER_PARAMS_TABLE);
-            logger.info("Created FlowEventTriggerParameters Table");
-        }
-        finally
-        {
-            if(statement != null) statement.close();
-        }
-    }
-
-    /**
-     * Configuration required persistence
-     * @param connection
-     * @throws SQLException
-     */
-    protected void createConfigurationPersistence(Connection connection) throws SQLException
-    {
-        Statement statement = null;
-        try
-        {
-            statement = connection.createStatement();
-            statement.executeUpdate(SybaseConstants.CREATE_CONFIGURATION_TABLE);
-            logger.info("Created Configuration Table");
-
-            statement.executeUpdate(SybaseConstants.CREATE_CONFIGURATION_PARAMS_TABLE);
-            logger.info("Created ConfigurationParameter Table");
-        }
-        finally
-        {
-            if(statement != null) statement.close();
-        }
-    }
-
-    /**
-     * Module required persistence
-     * @param connection
-     * @throws SQLException
-     */
-    protected void createModulePersistence(Connection connection) throws SQLException
-    {
-        Statement statement = null;
-        try
-        {
-            statement = connection.createStatement();
-            statement.executeUpdate(SybaseConstants.CREATE_STARTUP_MODULE_TABLE);
-            logger.info("Created Module StartupControl Table");
-        }
-        finally
-        {
-            if(statement != null) statement.close();
-        }
-    }
-
-    /**
-     * Console required persistence
-     * @param connection
-     * @throws SQLException
-     */
-    protected void createConsolePersistence(Connection connection) throws SQLException
-    {
-        Statement statement = null;
-        try
-        {
-            statement = connection.createStatement();
-            statement.executeUpdate(SybaseConstants.CREATE_CONSOLE_MODULE_TABLE);
-            logger.info("Created Console Module Table");
-            statement.executeUpdate(SybaseConstants.CREATE_CONSOLE_POINT_TO_POINT_PROFILE_TABLE);
-            logger.info("Created Console PointToPointProfile Table");
-            statement.executeUpdate(SybaseConstants.CREATE_CONSOLE_POINT_TO_POINT_TABLE);
-            logger.info("Created Console PointToPoint Table");
-        }
-        finally
-        {
-            if(statement != null) statement.close();
-        }
-    }
-
-    /**
-     * Security required persistence
-     * @param connection
-     * @throws SQLException
-     */
-    protected void createSecurityPersistence(Connection connection) throws SQLException
-    {
-        Statement statement = null;
-        try
-        {
-            statement = connection.createStatement();
-            statement.executeUpdate(SybaseConstants.CREATE_SECURITY_USERS_TABLE);
-            logger.info("Created Security User Table");
-            statement.executeUpdate(SybaseConstants.CREATE_SECURITY_AUTHORITIES_TABLE);
-            logger.info("Created Security Authority Table");
-            statement.executeUpdate(SybaseConstants.CREATE_SECURITY_USERS_AUTHORITIES_TABLE);
-            logger.info("Created Security UsersAuthorities Table");
-        }
-        finally
-        {
-            if(statement != null) statement.close();
-        }
-    }
 
     /**
      * Get a connection to the database
