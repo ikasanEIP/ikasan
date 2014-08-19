58908ed1e9468a76cb0ec7d140b1efc6b88d731d
edwaki <kieron.edwards@uk.mizuho-sc.com>
2014-08-19 21:29:07 +0100

    IKASAN-709: MongoBroker should be able to be configured with with one or more Bson Encoding Transformers.

---
+++ ikasaneip/component/endpoint/mongo-endpoint/src/main/java/org/ikasan/component/endpoint/mongo/encoding/BsonEncodingJsonNullToNullTransformer.java
@@ -0,0 +1,36 @@
+package org.ikasan.component.endpoint.mongo.encoding;
+
+import net.sf.json.JSONNull;
+
+import org.apache.log4j.Logger;
+import org.bson.Transformer;
+
+/**
+ * Transforms net.sf.json.JSONNull objects to null, so can be encoded by the BSONEncoder
+ * This a work-around for how the BSONEncoder is unable to serialise instances of these objects
+ * 
+ * @author Ikasan Development Team
+ */
+public class BsonEncodingJsonNullToNullTransformer implements Transformer
+{
+    /** logger instance */
+    private static Logger logger = Logger.getLogger(BsonEncodingJsonNullToNullTransformer.class);
+
+    /**
+     * 
+     */
+    @Override
+    public Object transform(Object o)
+    {
+        Object transformed = o;
+        if (o instanceof JSONNull){
+            logger.debug("Got a net.sf.json.JSONNull value will transform to a null value for handling by the BSONEncoder");
+            return null;
+        }
+        else {
+            logger.warn(String.format("The object to be transformed [%1$s] is not a type net.sf.json.JSONNull class ensure this transformer is mapped to this class", o));
+        }
+        return transformed;
+
+    }
+}
