4e773b96de5a4a0f9c142c1432a28ea9bce4a878
mitcje <jeff@ikasan.org>
2014-10-17 18:36:32 +0100

    IKASAN-708 - standaloneJVM sample with tech endpoint re-useable implementation

---
+++ ikasaneip/sample/standaloneJVM/src/main/java/org/ikasan/sample/component/producer/SimpleProducer.java
@@ -0,0 +1,82 @@
+/*
+ * $Id$
+ * $URL$
+ *
+ * ====================================================================
+ * Ikasan Enterprise Integration Platform
+ *
+ * Distributed under the Modified BSD License.
+ * Copyright notice: The copyright for this software and a full listing
+ * of individual contributors are as shown in the packaged copyright.txt
+ * file.
+ *
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ *  - Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer.
+ *
+ *  - Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ *
+ *  - Neither the name of the ORGANIZATION nor the names of its contributors may
+ *    be used to endorse or promote products derived from this software without
+ *    specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE
+ * USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ * ====================================================================
+ */
+package org.ikasan.sample.component.producer;
+
+import org.apache.log4j.Logger;
+import org.ikasan.builder.FlowBuilder;
+import org.ikasan.builder.ModuleBuilder;
+import org.ikasan.configurationService.service.ConfiguredResourceConfigurationService;
+import org.ikasan.recovery.RecoveryManagerFactory;
+import org.ikasan.spec.component.endpoint.Consumer;
+import org.ikasan.spec.component.endpoint.Producer;
+import org.ikasan.spec.component.routing.Router;
+import org.ikasan.spec.component.routing.RouterException;
+import org.ikasan.spec.component.transformation.Converter;
+import org.ikasan.spec.component.transformation.TransformationException;
+import org.ikasan.spec.flow.Flow;
+import org.ikasan.spec.flow.FlowElement;
+import org.ikasan.spec.flow.FlowEvent;
+import org.ikasan.spec.flow.FlowEventListener;
+import org.ikasan.spec.module.Module;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Simple Producer logging the message count on every 200,000 publishes.
+ * @author Ikasan Development Team.
+ */
+public class SimpleProducer<T> implements Producer<T>
+{
+    Logger logger = Logger.getLogger(SimpleProducer.class);
+
+    static int msgCount = 0;
+
+    @Override
+    public void invoke(T payload) throws TransformationException
+    {
+        if(++msgCount % 200000 == 0)
+        {
+            logger.info("Published [" + msgCount + "] messages.");
+        }
+    }
+}
+
---
+++ ikasaneip/sample/standaloneJVM/src/main/java/org/ikasan/sample/standalone/SimpleExample.java
@@ -0,0 +1,79 @@
+/*
+ * $Id$
+ * $URL$
+ *
+ * ====================================================================
+ * Ikasan Enterprise Integration Platform
+ *
+ * Distributed under the Modified BSD License.
+ * Copyright notice: The copyright for this software and a full listing
+ * of individual contributors are as shown in the packaged copyright.txt
+ * file.
+ *
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ *  - Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer.
+ *
+ *  - Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ *
+ *  - Neither the name of the ORGANIZATION nor the names of its contributors may
+ *    be used to endorse or promote products derived from this software without
+ *    specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE
+ * USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ * ====================================================================
+ */
+package org.ikasan.sample.standalone;
+
+import org.ikasan.builder.FlowBuilder;
+import org.ikasan.builder.ModuleBuilder;
+import org.ikasan.sample.component.consumer.SimpleConsumer;
+import org.ikasan.sample.component.converter.SimpleConverter;
+import org.ikasan.sample.component.producer.SimpleProducer;
+import org.ikasan.sample.component.router.SimpleRouter;
+import org.ikasan.spec.module.Module;
+
+/**
+ * Simple Standalone JVM Example of Ikasan i9.
+ * @author Ikasan Development Team.
+ */
+public class SimpleExample
+{
+    /**
+     * Create a module with a single flow which
+     *  - consumes Integers from a techEndpoint
+     *  - converts Integer to String
+     *  - routes even numbered strings to one producer and odd numbered strings to another producer
+     *  - one of two produces get invoked based on odd or even payload content
+     *
+     * @param moduleName
+     * @return
+     */
+    public Module createModule(String moduleName)
+    {
+        return ModuleBuilder.newModule(moduleName)
+                .addFlow(FlowBuilder.newFlow("flowName", moduleName).withDescription("Simple Module Example")
+                        .consumer("consumerName", new SimpleConsumer())     // of Integer
+                        .converter("converterName", new SimpleConverter()) // to String
+                        .router("routerName", new SimpleRouter())
+                            .when("odd").publisher("oddValuePublisher", new SimpleProducer())
+                            .otherise().publisher("evenValuePublisher", new SimpleProducer())
+                        .build())
+            .build();
+    }
+}
