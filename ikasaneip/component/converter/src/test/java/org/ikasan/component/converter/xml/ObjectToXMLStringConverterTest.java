bf8c7bffa99691c2eee2b51f07ca19186636e3fb
mitcje <jeff@ikasan.org>
2014-11-13 15:47:01 +0000

    IKASAN-733 - Added first leg of Object to XML and included XML schema validation.

--- ikasaneip/component/converter/src/test/java/org/ikasan/component/converter/xml/ObjectToXMLStringConverterTest.java
+++ ikasaneip/component/converter/src/test/java/org/ikasan/component/converter/xml/ObjectToXMLStringConverterTest.java
@@ -1,11 +1,42 @@
 /*
- * $Id: ObjectToXMLStringConverterTest.java 25470 2013-08-28 22:19:04Z jeffmitchell $
- * $URL: http://svc-vcs:18080/svn/MSUSA/middleware/branches/ion-marketDataSrc-trunk-moved/jar/src/test/java/com/mizuho/middleware/marketdatasrc/component/converter/ObjectToXMLStringConverterTest.java $
+ * $Id$
+ * $URL$
  *
  * ====================================================================
- * (C) Copyright Mizuho Securities USA
- * ====================================================================
+ * Ikasan Enterprise Integration Platform
  *
+ * Distributed under the Modified BSD License.
+ * Copyright notice: The copyright for this software and a full listing
+ * of individual contributors are as shown in the packaged copyright.txt
+ * file.
+ *
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ *  - Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer.
+ *
+ *  - Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ *
+ *  - Neither the name of the ORGANIZATION nor the names of its contributors may
+ *    be used to endorse or promote products derived from this software without
+ *    specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE
+ * USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ * ====================================================================
  */
 package org.ikasan.component.converter.xml;
 
@@ -25,10 +56,8 @@ import org.custommonkey.xmlunit.DifferenceListener;
 import org.ikasan.spec.component.transformation.Converter;
 import org.ikasan.spec.component.transformation.TransformationException;
 import org.ikasan.spec.configuration.ConfiguredResource;
-import org.jmock.Expectations;
 import org.jmock.Mockery;
 import org.jmock.lib.legacy.ClassImposteriser;
-import org.junit.Ignore;
 import org.junit.Test;
 import org.w3c.dom.Node;
 import org.xml.sax.SAXException;
@@ -36,7 +65,7 @@ import org.xml.sax.SAXException;
 /**
  * Functional unit test cases for <code>ObjectToXMLStringConverter</code>.
  * 
- * @author jeffmitchell
+ * @author Ikasan Development Team
  */
 public class ObjectToXMLStringConverterTest
 {
