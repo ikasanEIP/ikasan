681182954ac042f6743babbda796c851f3a44ee2
mitcje <jeff@ikasan.org>
2015-05-06 15:01:33 +0100

    IKASAN-721 - Provide basic serialiser services required for the error-reporting and exclusion event services.

---
+++ ikasaneip/serialiser/src/test/java/org/ikasan/serialiser/service/SerialiserServiceDefaultImplTest.java
@@ -0,0 +1,94 @@
+/* 
+ * $Id$
+ * $URL$
+ *
+ * ====================================================================
+ * Ikasan Enterprise Integration Platform
+ * 
+ * Distributed under the Modified BSD License.
+ * Copyright notice: The copyright for this software and a full listing 
+ * of individual contributors are as shown in the packaged copyright.txt 
+ * file. 
+ * 
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without 
+ * modification, are permitted provided that the following conditions are met:
+ *
+ *  - Redistributions of source code must retain the above copyright notice, 
+ *    this list of conditions and the following disclaimer.
+ *
+ *  - Redistributions in binary form must reproduce the above copyright notice, 
+ *    this list of conditions and the following disclaimer in the documentation 
+ *    and/or other materials provided with the distribution.
+ *
+ *  - Neither the name of the ORGANIZATION nor the names of its contributors may
+ *    be used to endorse or promote products derived from this software without 
+ *    specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" 
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE 
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE 
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE 
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL 
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR 
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER 
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE 
+ * USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ * ====================================================================
+ */
+package org.ikasan.serialiser.service;
+
+import junit.framework.Assert;
+import org.junit.Test;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+
+/**
+ * Test class for SerialiserServiceDefaultImpl.
+ * 
+ * @author Ikasan Development Team
+ */
+public class SerialiserServiceDefaultImplTest
+{
+    /**
+     * Test exclusion add, contains, remove
+     */
+    @Test
+    public void test_serialise_using_fst() throws IOException, ClassNotFoundException {
+        SerialiserServiceFSTImpl serialiserService = new SerialiserServiceFSTImpl();
+
+        PrimitiveClass primitiveClass = new PrimitiveClass();
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+
+        // serialise
+        serialiserService.serialise(baos, primitiveClass);
+
+        // deserialise
+        Object rehydrated = serialiserService.deserialise(new ByteArrayInputStream(baos.toByteArray()));
+        Assert.assertTrue(rehydrated instanceof PrimitiveClass);
+    }
+
+    /**
+     * Test exclusion add, contains, remove
+     */
+    @Test
+    public void test_serialise_using_kyro() throws IOException, ClassNotFoundException
+    {
+        SerialiserServiceKyroImpl serialiserService = new SerialiserServiceKyroImpl();
+
+        PrimitiveClass primitiveClass = new PrimitiveClass();
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+
+        // serialise
+        serialiserService.serialise(baos, primitiveClass);
+
+        // deserialise
+        Object rehydrated = serialiserService.deserialise(new ByteArrayInputStream(baos.toByteArray()));
+        Assert.assertTrue(rehydrated instanceof PrimitiveClass);
+    }
+
+}
