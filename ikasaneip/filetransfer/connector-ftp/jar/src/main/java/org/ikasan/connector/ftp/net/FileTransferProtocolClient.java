f959991ad56d1c52014a192a034fd1b8a271bf4d
Andrzej Majewski <andr2ot@gmail.com>
2015-05-13 09:03:40 +0100

    Revert "IKASAN-727 - adjust ftp related hibernate mapping siles with new DTD"
    This reverts commit 92d20361f9443914600cdaacc958dc950287d688.

--- ikasaneip/filetransfer/connector-ftp/jar/src/main/java/org/ikasan/connector/ftp/net/FileTransferProtocolClient.java
+++ ikasaneip/filetransfer/connector-ftp/jar/src/main/java/org/ikasan/connector/ftp/net/FileTransferProtocolClient.java
@@ -364,8 +364,7 @@ public class FileTransferProtocolClient implements FileTransferProtocol {
                 {
                     logger.debug("Connecting to remote host [" + this.remoteHostname + ":" + this.remotePort + "] from local host [" + this.localHostname + ":"
                             + localPort + "].");
-                    //ftpClient.connect(InetAddress.getByName(this.remoteHostname), this.remotePort, InetAddress.getByName(this.localHostname), localPort);
-                    ftpClient.connect(InetAddress.getByName(this.remoteHostname), this.remotePort);
+                    ftpClient.connect(InetAddress.getByName(this.remoteHostname), this.remotePort, InetAddress.getByName(this.localHostname), localPort);
 
                     int reply = ftpClient.getReplyCode();
                     if(!FTPReply.isPositiveCompletion(reply))
