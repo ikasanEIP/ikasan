575a2758e848ecaa936a2660436fd03e310067ca
stewmi <michael.stewart@uk.mizuho-sc.com>
2015-06-17 15:24:55 +0100

    IKASAN-780 Making all migration scripts up to date.

---
+++ ikasaneip/dashboard/sql/sybase/create-errorAndExclussionTables.sql
@@ -0,0 +1,41 @@
+DROP TABLE ExclusionEvent
+DROP TABLE ErrorOccurrence
+
+CREATE TABLE ExclusionEvent
+(
+Id NUMERIC IDENTITY NOT NULL,
+ModuleName VARCHAR(255) NOT NULL,
+FlowName VARCHAR(255) NOT NULL,
+Identifier VARCHAR(255) NOT NULL,
+Event IMAGE NULL,
+ErrorUri VARCHAR(255) NOT NULL,
+Timestamp NUMERIC DEFAULT 0 NOT NULL
+)
+LOCK DATAROWS
+WITH IDENTITY_GAP=1
+
+CREATE UNIQUE INDEX
+IkasanExclusionEvent01u ON
+ExclusionEvent(Id,ModuleName,FlowName,Identifier)
+
+
+CREATE TABLE ErrorOccurrence
+(
+	Uri VARCHAR(255) NOT NULL,
+	ModuleName VARCHAR(255) NOT NULL,
+	FlowName VARCHAR(255) NOT NULL,
+	FlowElementName VARCHAR(255) NOT NULL,
+	ErrorDetail TEXT NULL,
+	EventLifeIdentifier VARCHAR(255) NULL,
+	EventRelatedIdentifier VARCHAR(255) NULL,
+	Action VARCHAR(255) NULL,
+	Event IMAGE NULL,
+	Timestamp NUMERIC DEFAULT 0 NOT NULL,
+	Expiry NUMERIC NOT NULL
+	)
+	LOCK DATAROWS
+	WITH IDENTITY_GAP=1
+
+	CREATE UNIQUE INDEX
+	IkasanErrorOccurrence01u ON
+	ErrorOccurrence(ModuleName,FlowName,FlowElementName,Uri,Timestamp)
