e2332fe6ce9e7992ac02745f9eef0bb6d9e890f5
mitcje <jeff@tribaldog.co.uk>
2015-08-12 18:40:06 +0800

    IKASAN-932 WIP

--- ikasaneip/developer/mvn-archetype/ikasan-jboss-6/src/main/resources/archetype-resources/jar/src/test/java/org/ikasan/sample/test/flow/SourceScheduledFlowTest.java
+++ ikasaneip/developer/mvn-archetype/ikasan-jboss-6/src/main/resources/archetype-resources/jar/src/test/java/org/ikasan/sample/test/flow/SourceScheduledFlowTest.java
@@ -1,8 +1,14 @@
 package org.ikasan.sample.test.flow;
 
+import org.apache.commons.io.FileUtils;
+import org.ikasan.component.endpoint.filesystem.messageprovider.FileConsumerConfiguration;
+import org.ikasan.component.endpoint.jms.spring.consumer.SpringMessageConsumerConfiguration;
+import org.ikasan.component.endpoint.jms.spring.producer.SpringMessageProducerConfiguration;
 import org.ikasan.component.endpoint.quartz.consumer.*;
 import org.ikasan.flow.visitorPattern.VisitingInvokerFlow;
 import org.ikasan.platform.IkasanEIPTest;
+import org.ikasan.spec.configuration.ConfiguredResource;
+import org.ikasan.spec.flow.FlowElement;
 import org.ikasan.testharness.flow.FlowObserver;
 import org.ikasan.testharness.flow.FlowSubject;
 import org.ikasan.testharness.flow.FlowTestHarness;
@@ -10,6 +16,7 @@ import org.ikasan.testharness.flow.FlowTestHarnessImpl;
 import org.ikasan.testharness.flow.expectation.model.*;
 import org.ikasan.testharness.flow.expectation.service.OrderedExpectation;
 import org.jmock.Mockery;
+import org.junit.After;
 import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -18,6 +25,12 @@ import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import javax.annotation.Resource;
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 /**
  * Sample Scheduled Flow test.
@@ -31,14 +44,17 @@ import javax.annotation.Resource;
         "/monitor-service-conf.xml",
         "/monitor-conf.xml",
         "/shared-conf.xml",
-        "/sample-scheduled-flow-conf.xml",
+        "/source-scheduled-flow-conf.xml",
+        "/filesystem-conf.xml",
+        "/jms-conf.xml",
+        "/scheduled-conf.xml",
         "/exception-conf.xml",
         "/ikasan-transaction-conf.xml",
         "/mock-conf.xml",
         "/substitute-components.xml",
         "/h2db-datasource-conf.xml"
 })
-public class SampleScheduledFlowTest extends IkasanEIPTest
+public class SourceScheduledFlowTest extends IkasanEIPTest
 {
     /** mockery instance */
     @Resource
@@ -46,13 +62,17 @@ public class SampleScheduledFlowTest extends IkasanEIPTest
 
     /** flow on test */
     @Resource
-    VisitingInvokerFlow sampleScheduledFlow;
+    VisitingInvokerFlow sourceFlow;
 
     /**
      * Captures the actual components invoked and events created within the flow
      */
     @Resource
     FlowSubject testHarnessFlowEventListener;
+
+    // test file details
+    String testFilename = "test.txt";
+    File testFile = new File(testFilename);
 
     /**
      * Setup will clear down any previously defined observers and ignore all exception transformations.
@@ -69,9 +89,12 @@ public class SampleScheduledFlowTest extends IkasanEIPTest
     @SuppressWarnings("unchecked")
     @Test
     @DirtiesContext
-    public void test_successful_sampleFlow_invocation()
+    public void test_successful_sampleFlow_invocation() throws IOException
     {
         flowTest_setup();
+
+        // create test file
+        FileUtils.write(testFile, "test data");
 
         //
         // setup expectations
@@ -87,9 +110,29 @@ public class SampleScheduledFlowTest extends IkasanEIPTest
         testHarnessFlowEventListener.addObserver((FlowObserver) flowTestHarness);
         testHarnessFlowEventListener.setIgnoreEventCapture(true);
 
+        FlowElement consumerFlowElement = this.sourceFlow.getFlowElement("Scheduled Consumer Name");
+        FileConsumerConfiguration configuration = ((ConfiguredResource<FileConsumerConfiguration>)consumerFlowElement.getFlowComponent()).getConfiguration();
+        List<String> filenames = new ArrayList<String>();
+        filenames.add(testFilename);
+        configuration.setFilenames(filenames);
+
+        // configure AMQ to provide in-memory destinations for the test
+        Map<String,String> jndiProperties = new HashMap<String,String>();
+        jndiProperties.put("java.naming.factory.initial", "org.apache.activemq.jndi.ActiveMQInitialContextFactory");
+        jndiProperties.put("java.naming.provider.url", "vm://localhost?broker.persistent=false");
+
+        // configure the JMS producer for the test
+        FlowElement<?> producerFlowElement = this.sourceFlow.getFlowElement("Scheduled Producer Name");
+        ConfiguredResource<SpringMessageProducerConfiguration> configuredProducer = (ConfiguredResource)producerFlowElement.getFlowComponent();
+        SpringMessageProducerConfiguration producerConfiguration = configuredProducer.getConfiguration();
+        producerConfiguration.setConnectionFactoryName("ConnectionFactory");
+        producerConfiguration.setConnectionFactoryJndiProperties(jndiProperties);
+        producerConfiguration.setDestinationJndiName("dynamicTopics/queue");
+        producerConfiguration.setDestinationJndiProperties(jndiProperties);
+
         // start the flow
-        this.sampleScheduledFlow.start();
-        Assert.assertEquals("flow should be running", "running", this.sampleScheduledFlow.getState());
+        this.sourceFlow.start();
+        Assert.assertEquals("flow should be running", "running", this.sourceFlow.getState());
 
         // wait for the flow to fully execute
         try
@@ -102,14 +145,23 @@ public class SampleScheduledFlowTest extends IkasanEIPTest
         }
 
         // stop the flow
-        this.sampleScheduledFlow.stop();
+        this.sourceFlow.stop();
 
-        Assert.assertEquals("flow should be stopped", "stopped", this.sampleScheduledFlow.getState());
+        Assert.assertEquals("flow should be stopped", "stopped", this.sourceFlow.getState());
 
         // run flow assertions
         flowTestHarness.assertIsSatisfied();
 
         // mocked assertions
         mockery.assertIsSatisfied();
+    }
+
+    @After
+    public void teardown()
+    {
+        if(testFile.exists())
+        {
+            FileUtils.deleteQuietly(testFile);
+        }
     }
 }
