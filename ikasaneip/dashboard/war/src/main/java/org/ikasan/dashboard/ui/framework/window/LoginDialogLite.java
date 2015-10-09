628e6551192bb4b1620c67b42596b35a521f3c65
stewmi <michael.stewart@uk.mizuho-sc.com>
2015-10-09 13:23:14 +0100

    IKASAN-971 finalising front end functionality

--- ikasaneip/dashboard/war/src/main/java/org/ikasan/dashboard/ui/framework/window/LoginDialogLite.java
+++ ikasaneip/dashboard/war/src/main/java/org/ikasan/dashboard/ui/framework/window/LoginDialogLite.java
@@ -41,12 +41,16 @@
 package org.ikasan.dashboard.ui.framework.window;
 
 import org.apache.log4j.Logger;
-import org.ikasan.dashboard.ui.framework.group.VisibilityGroup;
-import org.ikasan.dashboard.ui.framework.panel.NavigationPanel;
+import org.ikasan.dashboard.ui.framework.data.LoginFieldGroup;
 import org.ikasan.security.service.AuthenticationService;
-import org.vaadin.teemu.VaadinIcons;
+import org.ikasan.security.service.authentication.IkasanAuthentication;
+import org.springframework.security.core.Authentication;
 
 import com.vaadin.data.Validator.InvalidValueException;
+import com.vaadin.data.fieldgroup.FieldGroup;
+import com.vaadin.data.fieldgroup.FieldGroup.CommitException;
+import com.vaadin.data.util.ObjectProperty;
+import com.vaadin.data.util.PropertysetItem;
 import com.vaadin.data.validator.StringLengthValidator;
 import com.vaadin.event.ShortcutAction.KeyCode;
 import com.vaadin.ui.Alignment;
@@ -57,7 +61,6 @@ import com.vaadin.ui.GridLayout;
 import com.vaadin.ui.HorizontalLayout;
 import com.vaadin.ui.Label;
 import com.vaadin.ui.Notification;
-import com.vaadin.ui.Notification.Type;
 import com.vaadin.ui.PasswordField;
 import com.vaadin.ui.TextField;
 import com.vaadin.ui.Window;
@@ -68,14 +71,14 @@ import com.vaadin.ui.themes.ValoTheme;
  * @author Ikasan Development Team
  *
  */
-public class AdminPasswordDialog extends Window
+public class LoginDialogLite extends Window
 {
     private static final long serialVersionUID = 2394313614920487219L;
     
     /** Logger instance */
-    private static Logger logger = Logger.getLogger(AdminPasswordDialog.class);
+    private static Logger logger = Logger.getLogger(LoginDialogLite.class);
     
-    private String password;
+    private IkasanAuthentication ikasanAuthentication;
 
     /**
      * Constructor
@@ -85,10 +88,10 @@ public class AdminPasswordDialog extends Window
      * @param visibilityGroup
      * @param commitHandler
      */
-    public AdminPasswordDialog()
+    public LoginDialogLite(AuthenticationService authenticationService)
     {
         super();
-        init();
+        init(authenticationService);
     }
 
     /**
@@ -100,117 +103,121 @@ public class AdminPasswordDialog extends Window
      * @param userDetailsHelper
      * @param commitHandler
      */
-    protected void init()
+    protected void init(final AuthenticationService authenticationService)
     {
         super.setModal(true);
         super.setResizable(false);
         super.center();
-        this.setWidth(450, Unit.PIXELS);
-    	this.setHeight(220, Unit.PIXELS);
-    	
-    	super.setClosable(false);
+        this.setWidth(300, Unit.PIXELS);
+    	this.setHeight(200, Unit.PIXELS);
+
+        PropertysetItem item = new PropertysetItem();
+        item.addItemProperty(LoginFieldGroup.USERNAME, new ObjectProperty<String>(""));
+        item.addItemProperty(LoginFieldGroup.PASSWORD, new ObjectProperty<String>(""));
        
-        GridLayout form = new GridLayout(2, 5);
+        GridLayout form = new GridLayout(2, 4);
         form.setColumnExpandRatio(0, .15f);
         form.setColumnExpandRatio(1, .85f);
         form.setWidth(100, Unit.PERCENTAGE);
         form.setMargin(true);
         form.setSpacing(true);
         
-        Label newTypeLabel =new Label("Initial Administration Password");
+        Label newTypeLabel =new Label("Administrator Credentials Required");
         newTypeLabel.setStyleName(ValoTheme.LABEL_HUGE);
 		form.addComponent(newTypeLabel, 0, 0, 1, 0);
-		
-		Label adminPasswordHint = new Label();
-		adminPasswordHint.setCaptionAsHtml(true);
-		adminPasswordHint.setCaption(VaadinIcons.QUESTION_CIRCLE_O.getHtml() + 
-				" The initial admin password will be used to log into the application as the 'admin' user.");
-		adminPasswordHint.addStyleName(ValoTheme.LABEL_TINY);
-		adminPasswordHint.addStyleName(ValoTheme.LABEL_LIGHT);
-		
-		form.addComponent(adminPasswordHint, 0, 1, 1, 1);
         
-		Label passwordLabel = new Label("Password:");
-		passwordLabel.setSizeUndefined();
+		Label usernameLabel = new Label("Username:");
+		usernameLabel.setSizeUndefined();
+		form.addComponent(usernameLabel, 0, 1);
+		form.setComponentAlignment(usernameLabel, Alignment.MIDDLE_RIGHT);
+		
+        final TextField userNameField = new TextField();
+        userNameField.addValidator(new StringLengthValidator(
+            "The username must not be empty",
+            1, null, true));
+        userNameField.setValidationVisible(false);
+        userNameField.setStyleName("ikasan");
+        form.addComponent(userNameField, 1, 1);
+
+        Label passwordLabel = new Label("Password:");
+        passwordLabel.setSizeUndefined();
 		form.addComponent(passwordLabel, 0, 2);
 		form.setComponentAlignment(passwordLabel, Alignment.MIDDLE_RIGHT);
 		
         final PasswordField passwordField = new PasswordField();
-        passwordField.addValidator(new StringLengthValidator(
-            "The username must not be empty",
-            1, null, true));
-        passwordField.setValidationVisible(false);
         passwordField.setStyleName("ikasan");
-        form.addComponent(passwordField, 1, 2);
-
-        Label passwordConfirmLabel = new Label("Confirm Password:");
-        passwordConfirmLabel.setSizeUndefined();
-		form.addComponent(passwordConfirmLabel, 0, 3);
-		form.setComponentAlignment(passwordConfirmLabel, Alignment.MIDDLE_RIGHT);
-		
-        final PasswordField passwordConfirmField = new PasswordField();
-        passwordConfirmField.setStyleName("ikasan");
-        passwordConfirmField.addValidator(new StringLengthValidator(
+        passwordField.addValidator(new StringLengthValidator(
             "The password must not be empty",
             1, null, true));
-        passwordConfirmField.setValidationVisible(false);
-        form.addComponent(passwordConfirmField, 1, 3);
+        passwordField.setValidationVisible(false);
+        form.addComponent(passwordField, 1, 2);
 
+        final FieldGroup binder = new FieldGroup(item);
+        binder.bind(userNameField, LoginFieldGroup.USERNAME);
+        binder.bind(passwordField, LoginFieldGroup.PASSWORD);
 
         HorizontalLayout buttons = new HorizontalLayout();
         buttons.setSpacing(true);
         
-        Button saveButton = new Button("Save");
-        saveButton.addStyleName(ValoTheme.BUTTON_SMALL);
-        saveButton.setClickShortcut(KeyCode.ENTER);
+        Button loginButton = new Button("Login");
+        loginButton.addStyleName(ValoTheme.BUTTON_SMALL);
+        loginButton.setClickShortcut(KeyCode.ENTER);
 
-        saveButton.addClickListener(new ClickListener() 
+        loginButton.addClickListener(new ClickListener() 
         {
             @Override
             public void buttonClick(ClickEvent event) 
             {
                 try 
                 {
+                    userNameField.validate();
                     passwordField.validate();
-                    passwordConfirmField.validate();
-                    
-                    if(!passwordField.getValue()
-                    		.equals(passwordConfirmField.getValue()))
-                    {
-                    	Notification.show("Password and confirmation must be the same!", Type.ERROR_MESSAGE);
-                    	
-                    	return;
-                    }
-                } 
-                catch (InvalidValueException e) 
-                {
+                } catch (InvalidValueException e) {
+                    userNameField.setValidationVisible(true);
                     passwordField.setValidationVisible(true);
-                    passwordConfirmField.setValidationVisible(true);
                     return;
                 }
 
-                password = passwordField.getValue();
-                
-                passwordField.setValue("");
-                passwordConfirmField.setValue("");
-                close();
-
-   
+                try 
+                {
+                    binder.commit();
+                    ikasanAuthentication = (IkasanAuthentication)authenticationService.login
+                    		(userNameField.getValue(), passwordField.getValue());
+                    userNameField.setValue("");
+                    passwordField.setValue("");
+                    close();
+                } catch (Exception e) 
+                {
+                    Notification.show(e.getMessage(), Notification.Type.ERROR_MESSAGE);
+                }
             }
         });
-        buttons.addComponent(saveButton);
-        
-        form.addComponent(buttons, 0, 4, 1, 4);
+        buttons.addComponent(loginButton);
+
+        Button cancelButton = new Button("Cancel");
+        cancelButton.setStyleName(ValoTheme.BUTTON_SMALL);
+        cancelButton.addClickListener(new ClickListener() 
+        {
+            @Override
+            public void buttonClick(ClickEvent event) 
+            {
+                close();
+                binder.discard();
+            }
+        });
+        buttons.addComponent(cancelButton);
+
+        form.addComponent(buttons, 0, 3, 1, 3);
         form.setComponentAlignment(buttons, Alignment.MIDDLE_CENTER);
         
         this.setContent(form);
     }
 
 	/**
-	 * @return the password
+	 * @return the ikasanAuthentication
 	 */
-	public String getPassword()
+	public IkasanAuthentication getIkasanAuthentication()
 	{
-		return password;
+		return ikasanAuthentication;
 	}
 }
