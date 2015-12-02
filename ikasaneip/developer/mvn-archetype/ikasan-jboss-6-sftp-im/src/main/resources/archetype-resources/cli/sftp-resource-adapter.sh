91799fb9ca29733e5589e78af76665ac01fb62cc
mitcje <jeff@ikasan.org>
2015-12-02 14:24:33 +0800

    IKASAN-1024

--- ikasaneip/developer/mvn-archetype/ikasan-jboss-6-sftp-im/src/main/resources/archetype-resources/cli/sftp-resource-adapter.sh
+++ ikasaneip/developer/mvn-archetype/ikasan-jboss-6-sftp-im/src/main/resources/archetype-resources/cli/sftp-resource-adapter.sh
@@ -17,20 +17,20 @@ $JBOSS_HOME/bin/jboss-cli.sh --connect <<EOF
 # Creates the SFTP resource-adapter if it doesnt already exist
 #
 
-if (outcome != success) of /profile=$PROFILE/subsystem=resource-adapters/resource-adapter=ikasan-sftp-ra-1.0.5-SNAPSHOT/connection-definitions=ConsumerSftpConnectionFactory:read-resource
-    /profile=$PROFILE/subsystem=resource-adapters/resource-adapter=ikasan-sftp-ra-1.0.5-SNAPSHOT/connection-definitions=ConsumerSftpConnectionFactory:add(class-name=org.ikasan.connector.sftp.outbound.SFTPManagedConnectionFactory,jndi-name=java:/sftp/moduleName/flowName/ConsumerSftpConnectionFactory,pad-xid=false,min-pool-size=1,max-pool-size=1,pool-prefill=false,pool-use-strict-min=false,flush-strategy=FailingConnectionOnly,wrap-xa-resource=true,interleaving=false)
+if (outcome != success) of /profile=$PROFILE/subsystem=resource-adapters/resource-adapter=ikasan-sftp-ra-${dashed.ikasan.version}/connection-definitions=ConsumerSftpConnectionFactory:read-resource
+    /profile=$PROFILE/subsystem=resource-adapters/resource-adapter=ikasan-sftp-ra-${dashed.ikasan.version}/connection-definitions=ConsumerSftpConnectionFactory:add(class-name=org.ikasan.connector.sftp.outbound.SFTPManagedConnectionFactory,jndi-name=java:/sftp/moduleName/flowName/ConsumerSftpConnectionFactory,pad-xid=false,min-pool-size=1,max-pool-size=1,pool-prefill=false,pool-use-strict-min=false,flush-strategy=FailingConnectionOnly,wrap-xa-resource=true,interleaving=false)
 end-if
 
-if (outcome != success) of /profile=$PROFILE/subsystem=resource-adapters/resource-adapter=ikasan-sftp-ra-1.0.5-SNAPSHOT/connection-definitions=ProducerSftpConnectionFactory:read-resource
-    /profile=$PROFILE/subsystem=resource-adapters/resource-adapter=ikasan-sftp-ra-1.0.5-SNAPSHOT/connection-definitions=ProducerSftpConnectionFactory:add(class-name=org.ikasan.connector.sftp.outbound.SFTPManagedConnectionFactory,jndi-name=java:/sftp/moduleName/flowName/ProducerSftpConnectionFactory,pad-xid=false,min-pool-size=1,max-pool-size=1,pool-prefill=false,pool-use-strict-min=false,flush-strategy=FailingConnectionOnly,wrap-xa-resource=true,interleaving=false)
+if (outcome != success) of /profile=$PROFILE/subsystem=resource-adapters/resource-adapter=ikasan-sftp-ra-${dashed.ikasan.version}/connection-definitions=ProducerSftpConnectionFactory:read-resource
+    /profile=$PROFILE/subsystem=resource-adapters/resource-adapter=ikasan-sftp-ra-${dashed.ikasan.version}/connection-definitions=ProducerSftpConnectionFactory:add(class-name=org.ikasan.connector.sftp.outbound.SFTPManagedConnectionFactory,jndi-name=java:/sftp/moduleName/flowName/ProducerSftpConnectionFactory,pad-xid=false,min-pool-size=1,max-pool-size=1,pool-prefill=false,pool-use-strict-min=false,flush-strategy=FailingConnectionOnly,wrap-xa-resource=true,interleaving=false)
 end-if
 
 #
 # Rollback
 #
 #
-#   /profile=$PROFILE/subsystem=resource-adapters/resource-adapter=ikasan-sftp-ra-1.0.5-SNAPSHOT/connection-definitions=ConsumerSftpConnectionFactory:remove()
-#   /profile=$PROFILE/subsystem=resource-adapters/resource-adapter=ikasan-sftp-ra-1.0.5-SNAPSHOT/connection-definitions=ProducerSftpConnectionFactory:remove()
+#   /profile=$PROFILE/subsystem=resource-adapters/resource-adapter=ikasan-sftp-ra-${dashed.ikasan.version}/connection-definitions=ConsumerSftpConnectionFactory:remove()
+#   /profile=$PROFILE/subsystem=resource-adapters/resource-adapter=ikasan-sftp-ra-${dashed.ikasan.version}/connection-definitions=ProducerSftpConnectionFactory:remove()
 
 exit
 EOF
