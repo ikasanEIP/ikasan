49ea08d7b16a686526b444a814d7babdb0880449
Costas Sterpis <costas.sterpis@uk.mizuho-sc.com>
2016-02-25 10:09:15 +0000

    IKASAN-1110 Add parametrised housekeeping query for wiretaps, systemEvents and messageFilters

--- ikasaneip/filter/src/main/java/org/ikasan/filter/duplicate/dao/HibernateFilteredMessageDaoImpl.java
+++ ikasaneip/filter/src/main/java/org/ikasan/filter/duplicate/dao/HibernateFilteredMessageDaoImpl.java
@@ -43,11 +43,9 @@ package org.ikasan.filter.duplicate.dao;
 
 import java.util.List;
 
-import org.hibernate.Criteria;
-import org.hibernate.HibernateException;
-import org.hibernate.Query;
-import org.hibernate.Session;
+import org.hibernate.*;
 import org.hibernate.criterion.DetachedCriteria;
+import org.hibernate.criterion.Projections;
 import org.hibernate.criterion.Restrictions;
 import org.ikasan.filter.duplicate.model.FilterEntry;
 import org.springframework.orm.hibernate4.HibernateCallback;
@@ -61,35 +59,22 @@ import org.springframework.orm.hibernate4.support.HibernateDaoSupport;
  */
 public class HibernateFilteredMessageDaoImpl extends HibernateDaoSupport implements FilteredMessageDao
 {
-	private static final String EXPIRY = "expiry";
-	
+    private static final String EXPIRY = "expiry";
+
     /** Query used for housekeeping expired filtered messages */
     private static final String HOUSEKEEP_QUERY = "delete DefaultFilterEntry m where m.expiry <= :" + EXPIRY;
 
     /** Flag for batch housekeeping option. Defaults to true */
-    private boolean batchedHousekeep = true;
+    private boolean batchHousekeepDelete = true;
 
-    /** The batch size used when {@link #batchedHousekeep} option is set. Default to 100*/
-    private int batchSize = 100;
+    /** The batch size used when {@link #batchHousekeepDelete} option is set. Default to 100*/
+    private int housekeepingBatchSize = 100;
 
-    /**
-     * Setter for {@link #batchedHousekeep} flag for overriding default
-     * value
-     * @param batchedHousekeep
-     */
-    public void setBatchedHousekeep(boolean batchedHousekeep)
-    {
-        this.batchedHousekeep = batchedHousekeep;
-    }
+    /** Batch size used when in a single transaction */
+    private Integer transactionBatchSize = 1000;
 
-    /**
-     * Setter for {@link #batchSize} for overriding default value
-     * @param batchSize
-     */
-    public void setBatchSize(int batchSize)
-    {
-        this.batchSize = batchSize;
-    }
+    private String housekeepQuery;
+
 
     /*
      * (non-Javadoc)
@@ -127,19 +112,19 @@ public class HibernateFilteredMessageDaoImpl extends HibernateDaoSupport impleme
      */
     public void deleteAllExpired()
     {
-        if (!this.batchedHousekeep)
+        if (!this.batchHousekeepDelete)
         {
-        	getHibernateTemplate().execute(new HibernateCallback<Object>()
-	        {
-	            public Object doInHibernate(Session session) throws HibernateException
-	            {
-	            	
-	                Query query = session.createQuery(HOUSEKEEP_QUERY);
-	                query.setParameter(EXPIRY, System.currentTimeMillis());
-	            	query.executeUpdate();
-	                return null;
-	            }
-	        });
+            getHibernateTemplate().execute(new HibernateCallback<Object>()
+            {
+                public Object doInHibernate(Session session) throws HibernateException
+                {
+
+                    Query query = session.createQuery(HOUSEKEEP_QUERY);
+                    query.setParameter(EXPIRY, System.currentTimeMillis());
+                    query.executeUpdate();
+                    return null;
+                }
+            });
         }
         else
         {
@@ -150,31 +135,48 @@ public class HibernateFilteredMessageDaoImpl extends HibernateDaoSupport impleme
     /**
      * Delete expired messages 100 at a time until non is left
      */
-    private void batchDeleteAllExpired()
+    protected void batchDeleteAllExpired()
     {
-        List<FilterEntry> expired = this.findExpiredMessages();
-        while(expired != null)
+        logger.info("MessageFilter batch delete.");
+
+        int numDeleted = 0;
+
+        while (housekeepablesExist() && numDeleted < this.transactionBatchSize)
         {
-            this.getHibernateTemplate().deleteAll(expired);
-            expired = this.findExpiredMessages();
+            getHibernateTemplate().execute(new HibernateCallback<Object>()
+            {
+                public Object doInHibernate(Session session) throws HibernateException{
+
+
+                    String formattedQuery = housekeepQuery.replace("_bs_", String.valueOf(housekeepingBatchSize))
+                            .replace("_ex_", String.valueOf(System.currentTimeMillis()));
+
+                    SQLQuery query = session.createSQLQuery(formattedQuery);
+                    query.executeUpdate();
+
+                    return null;
+                }
+            });
+
+            numDeleted += housekeepingBatchSize;
         }
     }
 
     /**
      * Find expired entries
-     * @return List of max 100 expired filter entries 
+     * @return List of max 100 expired filter entries
      */
     @SuppressWarnings("unchecked")
-    private List<FilterEntry> findExpiredMessages()
+    public List<FilterEntry> findExpiredMessages()
     {
         List<FilterEntry> foundMessages = (List<FilterEntry>) this.getHibernateTemplate().execute( new HibernateCallback<Object>()
         {
-            
+
             public Object doInHibernate(Session session) throws HibernateException
             {
                 Criteria criteria = session.createCriteria(FilterEntry.class);
                 criteria.add(Restrictions.lt(FilterEntry.EXPRIY_PROP_KEY, System.currentTimeMillis()));
-                criteria.setMaxResults(batchSize);
+                criteria.setMaxResults(housekeepingBatchSize);
                 return criteria.list();
             }
         });
@@ -187,6 +189,66 @@ public class HibernateFilteredMessageDaoImpl extends HibernateDaoSupport impleme
         {
             return foundMessages;
         }
+    }
+
+    /**
+     * Checks if there are housekeepable items in existance, ie expired WiretapFlowEvents
+     *
+     * @return true if there is at least 1 expired WiretapFlowEvent
+     */
+    public boolean housekeepablesExist()
+    {
+        return (Boolean) getHibernateTemplate().execute(new HibernateCallback<Object>()
+        {
+            public Object doInHibernate(Session session) throws HibernateException
+            {
+                Criteria criteria = session.createCriteria(FilterEntry.class);
+                criteria.add(Restrictions.lt("expiry", System.currentTimeMillis()));
+                criteria.setProjection(Projections.rowCount());
+                Long rowCount = 0L;
+                List<Long> rowCountList = criteria.list();
+                if (!rowCountList.isEmpty())
+                {
+                    rowCount = rowCountList.get(0);
+                }
+                logger.info(rowCount+", MessageFilter housekeepables exist");
+                return new Boolean(rowCount>0);
+            }
+        });
+    }
+
+    /* (non-Javadoc)
+     * @see org.ikasan.filter.duplicate.dao.FilteredMessageDao#setTransactionBatchSize(int)
+     */
+    @Override
+    public void setTransactionBatchSize(int transactionBatchSize)
+    {
+        this.transactionBatchSize = transactionBatchSize;
+    }
+
+    @Override
+    public void setHousekeepQuery(String housekeepQuery)
+    {
+        this.housekeepQuery = housekeepQuery;
+    }
+
+    /**
+     * Setter for {@link #batchHousekeepDelete} flag for overriding default
+     * value
+     * @param batchHousekeepDelete
+     */
+    public void setBatchHousekeepDelete(boolean batchHousekeepDelete)
+    {
+        this.batchHousekeepDelete = batchHousekeepDelete;
+    }
+
+    /**
+     * Setter for {@link #housekeepingBatchSize} for overriding default value
+     * @param housekeepingBatchSize
+     */
+    public void setHousekeepingBatchSize(int housekeepingBatchSize)
+    {
+        this.housekeepingBatchSize = housekeepingBatchSize;
     }
 
 }
