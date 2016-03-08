c962ecdd2c5776fc980b00c7fa55495d556fe6c1
Darren Rawson <dr@justintegration.co.uk>
2016-03-08 10:53:38 +0000

    IKASAN-771 - add test cases for new history service and dao

--- ikasaneip/wiretap/src/main/java/org/ikasan/history/dao/HibernateMessageHistoryDao.java
+++ ikasaneip/wiretap/src/main/java/org/ikasan/history/dao/HibernateMessageHistoryDao.java
@@ -42,15 +42,12 @@ package org.ikasan.history.dao;
 
 import org.hibernate.Criteria;
 import org.hibernate.HibernateException;
-import org.hibernate.Query;
 import org.hibernate.Session;
-import org.hibernate.criterion.MatchMode;
 import org.hibernate.criterion.Order;
 import org.hibernate.criterion.Projections;
 import org.hibernate.criterion.Restrictions;
 import org.ikasan.spec.history.MessageHistoryEvent;
 import org.ikasan.spec.search.PagedSearchResult;
-import org.ikasan.spec.wiretap.WiretapEvent;
 import org.ikasan.wiretap.model.ArrayListPagedSearchResult;
 import org.springframework.orm.hibernate4.HibernateCallback;
 import org.springframework.orm.hibernate4.support.HibernateDaoSupport;
@@ -70,8 +67,8 @@ public class HibernateMessageHistoryDao extends HibernateDaoSupport implements M
 
     private static final String EXPIRY = "expiry";
 
-    /** Query used for housekeeping expired wiretap events */
-    private static final String HOUSEKEEP_DELETE_QUERY = "delete MessageHistory w where w.expiry <= :" + EXPIRY;
+    /** Hibernate query used for housekeeping expired wiretap events, note the Object name, not the table name since thats mapped by HBM config */
+    private static final String HOUSEKEEP_DELETE_QUERY = "delete MessageHistoryFlowEvent w where w.expiry <= :" + EXPIRY;
 
     @Override
     public void save(MessageHistoryEvent messageHistoryEvent)
@@ -82,7 +79,7 @@ public class HibernateMessageHistoryDao extends HibernateDaoSupport implements M
     @Override
     public PagedSearchResult<MessageHistoryEvent> findMessageHistoryEvents(final int pageNo, final int pageSize, final String orderBy,
                                                                            final boolean orderAscending, final Set<String> moduleNames,
-                                                                           final String flowName, final String componentName,
+                                                                           final String flowName,
                                                                            final String lifeId, final String relatedLifeId,
                                                                            final Date fromDate, final Date toDate)
     {
@@ -136,17 +133,13 @@ public class HibernateMessageHistoryDao extends HibernateDaoSupport implements M
                 {
                     criteria.add(Restrictions.eq("flowName", flowName));
                 }
-                if (restrictionExists(componentName))
-                {
-                    criteria.add(Restrictions.eq("componentName", componentName));
-                }
                 if (restrictionExists(lifeId))
                 {
-                    criteria.add(Restrictions.eq("eventId", lifeId));
+                    criteria.add(Restrictions.eq("lifeIdentifier", lifeId));
                 }
                 if (restrictionExists(relatedLifeId))
                 {
-                    criteria.add(Restrictions.eq("relatedLifeId", relatedLifeId));
+                    criteria.add(Restrictions.eq("relatedLifeIdentifier", relatedLifeId));
                 }
                 if (restrictionExists(fromDate))
                 {
@@ -156,17 +149,70 @@ public class HibernateMessageHistoryDao extends HibernateDaoSupport implements M
                 {
                     criteria.add(Restrictions.le("endTime", toDate.getTime()));
                 }
-
                 return criteria;
             }
         });
-
     }
 
     @Override
-    public PagedSearchResult<MessageHistoryEvent> getMessageHistoryEvent(int pageNo, int pageSize, String orderBy, boolean orderAscending, String lifeId, String relatedLifeId)
+    public PagedSearchResult<MessageHistoryEvent> getMessageHistoryEvent(final int pageNo, final int pageSize, final String orderBy,
+            final boolean orderAscending, final String lifeId, final String relatedLifeId)
     {
-        return null;
+        return getHibernateTemplate().execute(new HibernateCallback<PagedSearchResult<MessageHistoryEvent>>()
+        {
+            public PagedSearchResult<MessageHistoryEvent> doInHibernate(Session session) throws HibernateException
+            {
+                Criteria dataCriteria = getCriteria(session);
+                dataCriteria.setMaxResults(pageSize);
+                int firstResult = pageNo * pageSize;
+                dataCriteria.setFirstResult(firstResult);
+                if (orderBy != null)
+                {
+                    if (orderAscending)
+                    {
+                        dataCriteria.addOrder(Order.asc(orderBy));
+                    }
+                    else
+                    {
+                        dataCriteria.addOrder(Order.desc(orderBy));
+                    }
+                }
+                List<MessageHistoryEvent> messageHistoryResults = dataCriteria.list();
+
+                Criteria metaDataCriteria = getCriteria(session);
+                metaDataCriteria.setProjection(Projections.rowCount());
+                Long rowCount = 0L;
+                List<Long> rowCountList = metaDataCriteria.list();
+                if (!rowCountList.isEmpty())
+                {
+                    rowCount = rowCountList.get(0);
+                }
+
+                return new ArrayListPagedSearchResult<>(messageHistoryResults, firstResult, rowCount);
+            }
+
+            /**
+             * Create a criteria instance for each invocation of data or metadata queries.
+             * @param session the hibernate Session
+             * @return a Criteria based on the provided search parameters
+             */
+            private Criteria getCriteria(Session session)
+            {
+                Criteria criteria = session.createCriteria(MessageHistoryEvent.class);
+
+                if (restrictionExists(lifeId) && !restrictionExists(relatedLifeId))
+                {
+                    criteria.add(Restrictions.eq("lifeIdentifier", lifeId));
+                }
+                if (restrictionExists(relatedLifeId))
+                {
+                    criteria.add(Restrictions.or(
+                            Restrictions.eq("lifeIdentifier", lifeId),
+                            Restrictions.eq("relatedLifeIdentifier", relatedLifeId)));
+                }
+                return criteria;
+            }
+        });
     }
 
     @Override
@@ -176,10 +222,9 @@ public class HibernateMessageHistoryDao extends HibernateDaoSupport implements M
         {
             public Object doInHibernate(Session session) throws HibernateException
             {
-
-                Query query = session.createQuery(HOUSEKEEP_DELETE_QUERY);
-                query.setParameter(EXPIRY, System.currentTimeMillis());
-                query.executeUpdate();
+                // rely on the DELETE CASCADE option on the FK to remove from the child tables as well
+                String delete = "DELETE FROM MessageHistory WHERE Expiry <= " + System.currentTimeMillis();
+                session.createSQLQuery(delete).executeUpdate();
                 return null;
             }
         });
@@ -194,7 +239,7 @@ public class HibernateMessageHistoryDao extends HibernateDaoSupport implements M
             public Object doInHibernate(Session session) throws HibernateException
             {
                 Criteria criteria = session.createCriteria(MessageHistoryEvent.class);
-                criteria.add(Restrictions.lt("expiry", System.currentTimeMillis()));
+                criteria.add(Restrictions.le("expiry", System.currentTimeMillis()));
                 criteria.setProjection(Projections.rowCount());
                 Long rowCount = 0L;
                 List<Long> rowCountList = criteria.list();
---
+++ ikasaneip/wiretap/src/test/java/org/ikasan/history/service/MessageHistoryServiceImplTest.java
@@ -0,0 +1,156 @@
+/*
+ * $Id$
+ * $URL$
+ *
+ * ====================================================================
+ * Ikasan Enterprise Integration Platform
+ *
+ * Distributed under the Modified BSD License.
+ * Copyright notice: The copyright for this software and a full listing
+ * of individual contributors are as shown in the packaged copyright.txt
+ * file.
+ *
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ *  - Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer.
+ *
+ *  - Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ *
+ *  - Neither the name of the ORGANIZATION nor the names of its contributors may
+ *    be used to endorse or promote products derived from this software without
+ *    specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE
+ * USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ * ====================================================================
+ */
+package org.ikasan.history.service;
+
+import org.ikasan.history.dao.MessageHistoryDao;
+import org.ikasan.history.model.HistoryEventFactory;
+import org.ikasan.spec.flow.FlowInvocationContext;
+import org.ikasan.spec.history.MessageHistoryEvent;
+import org.jmock.Expectations;
+import org.jmock.Mockery;
+import org.jmock.lib.legacy.ClassImposteriser;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.Collections;
+import java.util.Date;
+
+/**
+ * Test cases for MessageHistoryService
+ *
+ * @author Ikasan Development Team
+ */
+public class MessageHistoryServiceImplTest
+{
+    private final Mockery mockery = new Mockery()
+    {
+        {
+            setImposteriser(ClassImposteriser.INSTANCE);
+        }
+    };
+
+    MessageHistoryDao messageHistoryDao = mockery.mock(MessageHistoryDao.class);
+    FlowInvocationContext flowInvocationContext = mockery.mock(FlowInvocationContext.class);
+    HistoryEventFactory historyEventFactory = mockery.mock(HistoryEventFactory.class);
+    MessageHistoryEvent messageHistoryEvent = mockery.mock(MessageHistoryEvent.class);
+
+    MessageHistoryServiceImpl messageHistoryService = new MessageHistoryServiceImpl(messageHistoryDao);
+
+    @Before
+    public void setup()
+    {
+        messageHistoryService.setHistoryEventFactory(historyEventFactory);
+    }
+
+    @Test
+    public void test_save()
+    {
+        mockery.checking(new Expectations(){{
+            oneOf(historyEventFactory).newEvent("moduleName", "flowName", flowInvocationContext);
+            will(returnValue(messageHistoryEvent));
+            oneOf(messageHistoryDao).save(messageHistoryEvent);
+        }});
+        messageHistoryService.save(flowInvocationContext, "moduleName", "flowName");
+        mockery.assertIsSatisfied();
+    }
+
+    @Test
+    public void test_housekeep()
+    {
+        mockery.checking(new Expectations(){{
+            oneOf(messageHistoryDao).deleteAllExpired();
+        }});
+        messageHistoryService.housekeep();
+        mockery.assertIsSatisfied();
+    }
+
+    @Test
+    public void test_housekeepablesExist()
+    {
+        mockery.checking(new Expectations()
+        {{
+                oneOf(messageHistoryDao).housekeepablesExist();
+                will(returnValue(true));
+            }});
+        Assert.assertTrue(messageHistoryService.housekeepablesExist());
+        mockery.assertIsSatisfied();
+    }
+
+    @Test
+    public void test_findMessageHistoryEvents()
+    {
+        mockery.checking(new Expectations()
+        {{
+                oneOf(messageHistoryDao).findMessageHistoryEvents(0, 0, "orderBy", true, Collections.<String>emptySet(),
+                        "flowName", "lifeId", "relatedLifeId", new Date(0L), new Date(0L));
+                will(returnValue(null));
+            }});
+        messageHistoryService.findMessageHistoryEvents(0, 0, "orderBy", true, Collections.<String>emptySet(),
+                "flowName", "lifeId", "relatedLifeId", new Date(0L), new Date(0L));
+        mockery.assertIsSatisfied();
+    }
+
+    @Test
+    public void test_getMessageHistoryEvents_with_relatedId()
+    {
+        mockery.checking(new Expectations()
+        {{
+                oneOf(messageHistoryDao).getMessageHistoryEvent(0, 0, "orderBy", true, "lifeId", "lifeId");
+                will(returnValue(null));
+            }});
+        messageHistoryService.getMessageHistoryEvent(0, 0, "orderBy", true, "lifeId", true);
+        mockery.assertIsSatisfied();
+    }
+
+    @Test
+    public void test_getMessageHistoryEvents_without_relatedId()
+    {
+        mockery.checking(new Expectations()
+        {{
+                oneOf(messageHistoryDao).getMessageHistoryEvent(0, 0, "orderBy", true, "lifeId", null);
+                will(returnValue(null));
+            }});
+        messageHistoryService.getMessageHistoryEvent(0, 0, "orderBy", true, "lifeId", false);
+        mockery.assertIsSatisfied();
+    }
+
+}
