a3fc245039b25dd485777b4ee6c29d8bc1fbb597
Andrzej Majewski <andr2ot@gmail.com>
2017-04-06 22:14:57 +0100

    Revert Policy and role haseCode and equals implementations (cherry picked from commit 7d67a3e)

--- ikasaneip/security/src/main/java/org/ikasan/security/model/Role.java
+++ ikasaneip/security/src/main/java/org/ikasan/security/model/Role.java
@@ -192,30 +192,70 @@ public class Role implements Comparable<Role>
         this.policies = policies;
     }
 
-	@Override
-	public boolean equals(Object o) {
-		if (this == o) return true;
-		if (o == null || getClass() != o.getClass()) return false;
+    @Override
+    public int hashCode()
+    {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result
+                + ((createdDateTime == null) ? 0 : createdDateTime.hashCode());
+        result = prime * result
+                + ((description == null) ? 0 : description.hashCode());
+        result = prime * result + ((id == null) ? 0 : id.hashCode());
+        result = prime * result + ((name == null) ? 0 : name.hashCode());
+        result = prime * result
+                + ((updatedDateTime == null) ? 0 : updatedDateTime.hashCode());
+        return result;
+    }
 
-		Role role = (Role) o;
+    /* (non-Javadoc)
+     * @see java.lang.Object#equals(java.lang.Object)
+     */
+    @Override
+    public boolean equals(Object obj)
+    {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Role other = (Role) obj;
+        if (createdDateTime == null)
+        {
+            if (other.createdDateTime != null)
+                return false;
+        } else if (!createdDateTime.equals(other.createdDateTime))
+            return false;
+        if (description == null)
+        {
+            if (other.description != null)
+                return false;
+        } else if (!description.equals(other.description))
+            return false;
+        if (id == null)
+        {
+            if (other.id != null)
+                return false;
+        } else if (!id.equals(other.id))
+            return false;
+        if (name == null)
+        {
+            if (other.name != null)
+                return false;
+        } else if (!name.equals(other.name))
+            return false;
+        if (updatedDateTime == null)
+        {
+            if (other.updatedDateTime != null)
+                return false;
+        } else if (!updatedDateTime.equals(other.updatedDateTime))
+            return false;
+        return true;
+    }
 
-		if (id != null ? !id.equals(role.id) : role.id != null) return false;
-		if (name != null ? !name.equals(role.name) : role.name != null) return false;
-		if (description != null ? !description.equals(role.description) : role.description != null) return false;
-		return policies != null ? policies.equals(role.policies) : role.policies == null;
 
-	}
-
-	@Override
-	public int hashCode() {
-		int result = id != null ? id.hashCode() : 0;
-		result = 31 * result + (name != null ? name.hashCode() : 0);
-		result = 31 * result + (description != null ? description.hashCode() : 0);
-		result = 31 * result + (policies != null ? policies.hashCode() : 0);
-		return result;
-	}
-
-	/* (non-Javadoc)
+    /* (non-Javadoc)
          * @see java.lang.Object#toString()
          */
 	@Override
