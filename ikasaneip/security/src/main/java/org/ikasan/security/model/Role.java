c1e079704c5dcff914a65dd113431864e4c05f27
Andrzej Majewski <andr2ot@gmail.com>
2017-06-02 12:52:22 +0100

    IKASAN-1263 -  fix equals and hashCode method for Role And Policy to only use Name rather then name and description

--- ikasaneip/security/src/main/java/org/ikasan/security/model/Role.java
+++ ikasaneip/security/src/main/java/org/ikasan/security/model/Role.java
@@ -197,9 +197,6 @@ public class Role implements Comparable<Role>
     {
         final int prime = 31;
         int result = 1;
-        result = prime * result
-                + ((description == null) ? 0 : description.hashCode());
-        result = prime * result + ((id == null) ? 0 : id.hashCode());
         result = prime * result + ((name == null) ? 0 : name.hashCode());
         return result;
     }
