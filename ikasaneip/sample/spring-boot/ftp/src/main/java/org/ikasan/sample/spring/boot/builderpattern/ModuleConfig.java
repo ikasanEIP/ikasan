c04e8f836ac3c9f3889b1d7d61f1f2fa6d11be57
Andrzej Majewski <andr2ot@gmail.com>
2017-08-21 11:26:59 +0100

    IKASAN-1283 - updating ftp/sftp samples to after sample-standalone branch update

--- ikasaneip/sample/spring-boot/ftp/src/main/java/org/ikasan/sample/spring/boot/builderpattern/ModuleConfig.java
+++ ikasaneip/sample/spring-boot/ftp/src/main/java/org/ikasan/sample/spring/boot/builderpattern/ModuleConfig.java
@@ -2,6 +2,7 @@ package org.ikasan.sample.spring.boot.builderpattern;
 
 import org.ikasan.builder.BuilderFactory;
 import org.ikasan.builder.FlowBuilder;
+import org.ikasan.builder.ModuleBuilder;
 import org.ikasan.component.endpoint.quartz.consumer.ScheduledConsumer;
 import org.ikasan.component.endpoint.util.producer.DevNull;
 import org.ikasan.spec.component.endpoint.Producer;
@@ -9,6 +10,7 @@ import org.ikasan.spec.component.transformation.Converter;
 import org.ikasan.spec.flow.Flow;
 import org.ikasan.spec.module.Module;
 import org.springframework.beans.factory.config.AutowireCapableBeanFactory;
+import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.ImportResource;
@@ -45,28 +47,27 @@ public class ModuleConfig {
     private Producer ftpProducer;
 
     @Resource
-    private AutowireCapableBeanFactory beanFactory;
+    private ApplicationContext context;
 
     @Bean
     public Module getModule(){
 
-        FlowBuilder ftpToLogFlowBuilder = BuilderFactory.flowBuilder("ftpToLogFlow", "sample-boot-ftp");
-        beanFactory.autowireBean(ftpToLogFlowBuilder);
+        ModuleBuilder mb = new ModuleBuilder(context,"sample-boot-ftp-module");
+        FlowBuilder ftpToLogFlowBuilder = mb.getFlowBuilder("ftpToLogFlow");
         Flow ftpToLogFlow = ftpToLogFlowBuilder
                 .withDescription("Ftp to Log")
                 .consumer("Ftp Consumer", ftpConsumer)
                 .converter("FTP payload to String Converter",payloadToStringConverter)
                 .producer("Log", new DevNull()).build();
 
-        FlowBuilder timeGeneratorToFtpFlowBuilder = BuilderFactory.flowBuilder("timeGeneratorToFtpFlow", "sample-boot-ftp");
-        beanFactory.autowireBean(timeGeneratorToFtpFlowBuilder);
+        FlowBuilder timeGeneratorToFtpFlowBuilder = mb.getFlowBuilder("timeGeneratorToFtpFlow");
         Flow timeGeneratorToFtpFlow = timeGeneratorToFtpFlowBuilder
                 .withDescription("Generates random string and send it to ftp as file")
                 .consumer("Scheduled Consumer", fileGeneratorScheduledConsumer)
                 .converter("Random String Generator",filePayloadGeneratorConverter)
                 .producer("Ftp Producer", ftpProducer).build();
 
-        Module module = BuilderFactory.moduleBuilder("sample-boot-ftp").withDescription("Sample Spring Boot FTP Module")
+        Module module = mb.withDescription("Sample Spring Boot FTP Module")
                 .addFlow(ftpToLogFlow).addFlow(timeGeneratorToFtpFlow).build();
         return module;
     }
--- ikasaneip/sample/spring-boot/sftp/src/main/java/org/ikasan/sample/spring/boot/builderpattern/ModuleConfig.java
+++ ikasaneip/sample/spring-boot/sftp/src/main/java/org/ikasan/sample/spring/boot/builderpattern/ModuleConfig.java
@@ -2,6 +2,7 @@ package org.ikasan.sample.spring.boot.builderpattern;
 
 import org.ikasan.builder.BuilderFactory;
 import org.ikasan.builder.FlowBuilder;
+import org.ikasan.builder.ModuleBuilder;
 import org.ikasan.component.endpoint.quartz.consumer.ScheduledConsumer;
 import org.ikasan.component.endpoint.util.producer.DevNull;
 import org.ikasan.spec.component.endpoint.Producer;
@@ -9,6 +10,7 @@ import org.ikasan.spec.component.transformation.Converter;
 import org.ikasan.spec.flow.Flow;
 import org.ikasan.spec.module.Module;
 import org.springframework.beans.factory.config.AutowireCapableBeanFactory;
+import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.ImportResource;
@@ -45,28 +47,28 @@ public class ModuleConfig {
     private Producer sftpProducer;
 
     @Resource
-    private AutowireCapableBeanFactory beanFactory;
+    private ApplicationContext context;
+
 
     @Bean
     public Module getModule(){
 
-        FlowBuilder sftpToLogFlowBuilder = BuilderFactory.flowBuilder("sftpToLogFlow", "sample-boot-sftp");
-        beanFactory.autowireBean(sftpToLogFlowBuilder);
+        ModuleBuilder mb = new ModuleBuilder(context,"sample-boot-sftp-module");
+        FlowBuilder sftpToLogFlowBuilder = mb.getFlowBuilder("sftpToLogFlow");
         Flow sftpToLogFlow = sftpToLogFlowBuilder
                 .withDescription("Sftp to Log")
                 .consumer("Sftp Consumer", sftpConsumer)
                 .converter("SFTP payload to String Converter",payloadToStringConverter)
                 .producer("Log", new DevNull()).build();
 
-        FlowBuilder timeGeneratorToSftpFlowBuilder = BuilderFactory.flowBuilder("timeGeneratorToSftpFlow", "sample-boot-sftp");
-        beanFactory.autowireBean(timeGeneratorToSftpFlowBuilder);
+        FlowBuilder timeGeneratorToSftpFlowBuilder = mb.getFlowBuilder("timeGeneratorToSftpFlow");
         Flow timeGeneratorToSftpFlow = timeGeneratorToSftpFlowBuilder
                 .withDescription("Generates random string and send it to sftp as file")
                 .consumer("Scheduled Consumer", fileGeneratorScheduledConsumer)
                 .converter("Random String Generator",filePayloadGeneratorConverter)
                 .producer("Sftp Producer", sftpProducer).build();
 
-        Module module = BuilderFactory.moduleBuilder("sample-boot-sftp").withDescription("SFTP Sample Module")
+        Module module = mb.withDescription("SFTP Sample Module")
                 .addFlow(sftpToLogFlow).addFlow(timeGeneratorToSftpFlow).build();
         return module;
     }
