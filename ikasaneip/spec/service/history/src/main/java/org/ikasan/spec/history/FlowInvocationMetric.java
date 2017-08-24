261115cc1b4988ad36ede74e29cfc001af7efd8c
Michael Stewart <mick_stewart73@hotmail.com>
2017-08-24 19:23:10 +0100

    IKASAN-1287 adding errorUri to the metrics

--- ikasaneip/spec/service/history/src/main/java/org/ikasan/spec/history/FlowInvocationMetric.java
+++ ikasaneip/spec/service/history/src/main/java/org/ikasan/spec/history/FlowInvocationMetric.java
@@ -7,35 +7,128 @@ import java.util.Set;
  */
 public interface FlowInvocationMetric<EVENT>
 {
+    /**
+     * Get the module name
+     * @return
+     */
     public String getModuleName();
 
+    /**
+     * Set the module name
+     *
+     * @param moduleName
+     */
     public void setModuleName(String moduleName);
 
+    /**
+     * Get the flow name
+     *
+     * @return
+     */
     public String getFlowName();
 
+    /**
+     * Set the flow name
+     *
+     * @param flowName
+     */
     public void setFlowName(String flowName);
 
+    /**
+     * Get the flow invocation start time
+     *
+     * @return
+     */
     public long getInvocationStartTime();
 
+    /**
+     * Set the flow invocation start time.
+     *
+     * @param invocationStartTime
+     */
     public void setInvocationStartTime(long invocationStartTime);
 
+    /**
+     * Get the flow invocation end time
+     *
+     * @return
+     */
     public long getInvocationEndTime();
 
+    /**
+     * Set the flow invocation end time
+     *
+     * @param invocationEndTime
+     */
     public void setInvocationEndTime(long invocationEndTime);
 
+    /**
+     * Get the final action.
+     *
+     * @return
+     */
     public String getFinalAction();
 
+    /**
+     * Set the final action.
+     *
+     * @param finalAction
+     */
     public void setFinalAction(String finalAction);
 
+    /**
+     * Get the flow invocation events
+     *
+     * @return
+     */
     public Set<EVENT> getFlowInvocationEvents();
 
+    /**
+     * Set the flow invocation events.
+     *
+     * @param events
+     */
     public void setFlowInvocationEvents(Set<EVENT> events);
 
+    /**
+     * Get the harvested flag.
+     *
+     * @return
+     */
     public Boolean getHarvested();
 
+    /**
+     * Set the harvested flag.
+     *
+     * @param harvested
+     */
     public void setHarvested(Boolean harvested);
 
+    /**
+     * Get the metric expiry.
+     *
+     * @return
+     */
     public long getExpiry();
 
+    /**
+     * Set the metric expiry.
+     *
+     * @param expiry
+     */
     public void setExpiry(long expiry);
+
+    /**
+     * Get the error URI.
+     *
+     * @return
+     */
+    public String getErrorUri();
+
+    /**
+     * Set the error URI.
+     *
+     * @param errorUri
+     */
+    public void setErrorUri(String errorUri);
 }
