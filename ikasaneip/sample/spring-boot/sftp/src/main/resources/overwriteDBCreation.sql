097905be68417f45de4adc7b1995be68dbce2ca5
Andrzej Majewski <andr2ot@gmail.com>
2017-09-13 17:04:22 +0100

    IKASAN-1294 - fix authentication issue with blue console

--- ikasaneip/sample/spring-boot/sftp/src/main/resources/overwriteDBCreation.sql
+++ ikasaneip/sample/spring-boot/sftp/src/main/resources/overwriteDBCreation.sql
@@ -4,24 +4,24 @@
       INSERT INTO Users (Id, Username, Password, Enabled, FirstName, Surname,PreviousAccess )
 					VALUES (2,'api', 'd033e22ae348aeb5660fc2140aec35850c4da997', 1 , 'API', 'API',10);
 
---      INSERT INTO SecurityPrincipal (Name , PrincipalType, Description, CreatedDateTime ,UpdatedDateTime)
---        VALUES ( 'admin','Admin user', 'The administrator user' ,'1970-01-01 00:00:00','1970-01-01 00:00:00');
+      INSERT INTO SecurityPrincipal (Name , PrincipalType, Description, CreatedDateTime ,UpdatedDateTime)
+        VALUES ( 'admin','Admin user', 'The administrator user' ,'1970-01-01 00:00:00','1970-01-01 00:00:00');
 
 --      INSERT INTO SecurityPrincipal(Name , PrincipalType, Description, CreatedDateTime ,UpdatedDateTime)
 --        VALUES ( 'user','user', 'The user' ,'1970-01-01 00:00:00','1970-01-01 00:00:00');
 
---      INSERT INTO SecurityRole (Name ,  Description, CreatedDateTime ,UpdatedDateTime)
---        VALUES ( 'ADMIN', 'Users who may perform administration functions on the system','1970-01-01 00:00:00','1970-01-01 00:00:00' );
+--       INSERT INTO SecurityRole (Name ,  Description, CreatedDateTime ,UpdatedDateTime)
+--         VALUES ( 'ADMIN', 'Users who may perform administration functions on the system','1970-01-01 00:00:00','1970-01-01 00:00:00' );
 --      INSERT INTO SecurityRole (Name ,  Description,CreatedDateTime ,UpdatedDateTime )
 --        VALUES ( 'User', 'Users who have a read only view on the system','1970-01-01 00:00:00','1970-01-01 00:00:00' );
-      INSERT INTO SecurityPolicy (Name ,  Description, CreatedDateTime ,UpdatedDateTime )
-        VALUES ( 'ALL', 'Policy to do everything','1970-01-01 00:00:00','1970-01-01 00:00:00' );
-
---       INSERT INTO SecurityPolicy (Name ,  Description, CreatedDateTime ,UpdatedDateTime )
---         VALUES ( 'ReadBlueConsole', 'Policy to do view BlueConsole','1970-01-01 00:00:00','1970-01-01 00:00:00' );
+--        INSERT INTO SecurityPolicy (Name ,  Description, CreatedDateTime ,UpdatedDateTime )
+--         VALUES ( 'ALL', 'Policy to do everything','1970-01-01 00:00:00','1970-01-01 00:00:00' );
 --
---       INSERT INTO SecurityPolicy (Name ,  Description, CreatedDateTime ,UpdatedDateTime )
---         VALUES ( 'WriteBlueConsole', 'Policy to do write BlueConsole','1970-01-01 00:00:00','1970-01-01 00:00:00' );
+--        INSERT INTO SecurityPolicy (Name ,  Description, CreatedDateTime ,UpdatedDateTime )
+--          VALUES ( 'ReadBlueConsole', 'Policy to do view BlueConsole','1970-01-01 00:00:00','1970-01-01 00:00:00' );
+
+       INSERT INTO SecurityPolicy (Name ,  Description, CreatedDateTime ,UpdatedDateTime )
+         VALUES ( 'WriteBlueConsole', 'Policy to do write BlueConsole','1970-01-01 00:00:00','1970-01-01 00:00:00' );
 
 
 			 INSERT
