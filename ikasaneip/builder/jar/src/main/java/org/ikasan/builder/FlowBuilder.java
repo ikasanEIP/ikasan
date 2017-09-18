45d1f0697ef025c5b0b53fd7e0ddd8bceb2485e3
Kieron Edwards <kieron.edwards@theagilecontractor.com>
2017-09-18 21:04:16 +0100

    IKASAN-1297 - FlowBuilder.PrimaryRouteBuilder and RouteBuilder missing splitter builder method (#336)

--- ikasaneip/builder/jar/src/main/java/org/ikasan/builder/FlowBuilder.java
+++ ikasaneip/builder/jar/src/main/java/org/ikasan/builder/FlowBuilder.java
@@ -60,6 +60,7 @@ import org.ikasan.spec.component.filter.Filter;
 import org.ikasan.spec.component.routing.MultiRecipientRouter;
 import org.ikasan.spec.component.routing.SingleRecipientRouter;
 import org.ikasan.spec.component.sequencing.Sequencer;
+import org.ikasan.spec.component.splitting.Splitter;
 import org.ikasan.spec.component.transformation.Converter;
 import org.ikasan.spec.component.transformation.Translator;
 import org.ikasan.spec.configuration.ConfigurationService;
@@ -679,6 +680,12 @@ public class FlowBuilder implements ApplicationContextAware
 
         public PrimaryRouteBuilder translator(String name, Translator translator) {
             this.route.addFlowElement(new FlowElementImpl(name, translator, new TranslatorFlowElementInvoker()));
+            return this;
+        }
+
+        public PrimaryRouteBuilder splitter(String name, Splitter splitter)
+        {
+            this.route.addFlowElement(new FlowElementImpl(name, splitter, new SplitterFlowElementInvoker()));
             return this;
         }
 
