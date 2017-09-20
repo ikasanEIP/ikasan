d60ec280154fb9629f8fd28ea80e5ed68f4bcb9b
Kieron Edwards <kieron.edwards@theagilecontractor.com>
2017-09-20 15:22:57 +0100

    2.0.x ikasan 1298 (#341)
    * IKASAN-1298 - IkasanApplication.getIkasanApplication does not allow for component scanning of non ikasan packages
    
    * IKASAN-1298: Make the context() method private
    
    * IKASAN-1298 - Make DefaultSpringBootIkasanApplication final, add method to allow for 3rd code to create spring boot ikasan application using their application class

--- ikasaneip/builder/jar/src/main/java/org/ikasan/builder/DefaultSpringBootIkasanApplication.java
+++ ikasaneip/builder/jar/src/main/java/org/ikasan/builder/DefaultSpringBootIkasanApplication.java
@@ -5,9 +5,13 @@ import org.ikasan.spec.module.ModuleContainer;
 import org.ikasan.spec.module.ModuleInitialisationService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.BeansException;
 import org.springframework.boot.ExitCodeGenerator;
 import org.springframework.boot.SpringApplication;
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.stereotype.Component;
 
 import java.util.List;
 
@@ -15,7 +19,8 @@ import java.util.List;
  * IM SpringBoot Developers can use this to easily create an Ikasan Spring boot application,
  * whilst having full control of the spring boot entry class and associated annotations
  */
-public class DefaultSpringBootIkasanApplication implements IkasanApplication{
+@Component
+public final class DefaultSpringBootIkasanApplication implements IkasanApplication, ApplicationContextAware{
 
     /**
      * DefaultSpringBootIkasanApplication Logger
@@ -24,30 +29,26 @@ public class DefaultSpringBootIkasanApplication implements IkasanApplication{
 
     private ApplicationContext context;
 
-    protected DefaultSpringBootIkasanApplication(Class<?> applicationClass, String[] args) {
-        logger.debug("Spring boot constructor called");
-        context = SpringApplication.run(applicationClass,args);}
+    public static IkasanApplication createDefaultSpringBootIkasanApplication(Class<?> applicationClass, String[] args) {
+        logger.debug("Spring boot createDefaultSpringBootIkasanApplication");
+        final ConfigurableApplicationContext context = SpringApplication.run(applicationClass, args);
+        final DefaultSpringBootIkasanApplication bean = context.getBean(DefaultSpringBootIkasanApplication.class);
+        return bean;
+    }
 
-    /**
-     * Default constructor
-     *
-     * This should never be called by client code, its required by Spring.
-     *
-     * TODO - Investigate why spring boot is calling this - this instance should never get used,
-     * TODO - there is no need to create two instances of this class, this needs to be prevented.
-     */
+
     public DefaultSpringBootIkasanApplication(){
         logger.debug("Default constructor called - provided for spring only");
     }
 
     public BuilderFactory getBuilderFactory()
     {
-        return context().getBean(BuilderFactory.class);
+        return context.getBean(BuilderFactory.class);
     }
 
     public void run(Module module)
     {
-        ModuleInitialisationService service = context().getBean(ModuleInitialisationService.class);
+        ModuleInitialisationService service = context.getBean(ModuleInitialisationService.class);
         service.register(module);
         logger.info("Module [" + module.getName() + "] successfully bootstrapped.");
     }
@@ -55,7 +56,7 @@ public class DefaultSpringBootIkasanApplication implements IkasanApplication{
     // TODO - add close or shutdown per module ?
     public void close()
     {
-        SpringApplication.exit(context(), new ExitCodeGenerator(){
+        SpringApplication.exit(context, new ExitCodeGenerator(){
             @Override public int getExitCode()
             {
                 return 0;
@@ -66,36 +67,32 @@ public class DefaultSpringBootIkasanApplication implements IkasanApplication{
     @Override
     public Module getModule(String moduleName)
     {
-        ModuleContainer moduleContainer = context().getBean(ModuleContainer.class);
+        ModuleContainer moduleContainer = context.getBean(ModuleContainer.class);
         return moduleContainer.getModule(moduleName);
     }
 
     @Override
     public List<Module> getModules()
     {
-        ModuleContainer moduleContainer = context().getBean(ModuleContainer.class);
+        ModuleContainer moduleContainer = context.getBean(ModuleContainer.class);
         return moduleContainer.getModules();
     }
 
     @Override
     public <COMPONENT> COMPONENT getBean(Class className)
     {
-        return (COMPONENT) context().getBean(className);
+        return (COMPONENT) context.getBean(className);
     }
 
     @Override
     public <COMPONENT> COMPONENT getBean(String name, Class className)
     {
-        return (COMPONENT) context().getBean(name, className);
+        return (COMPONENT) context.getBean(name, className);
     }
 
-    public ApplicationContext context() {
-        if (context == null){
-            throw new RuntimeException("Context is null - this is because you have used the default constructor, " +
-                    "this is required by spring. Please ensure you only use the spring boot constructor specifying " +
-                    "your spring boot class and arguments.");
-        } else {
-            return context;
-        }
+
+    @Override
+    public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+        this.context=applicationContext;
     }
 }
