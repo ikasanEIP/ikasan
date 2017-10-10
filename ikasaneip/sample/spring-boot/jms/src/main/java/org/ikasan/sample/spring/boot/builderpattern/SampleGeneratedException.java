6268028439b80a5d592cc8f5de59c5567a0554b4
andrzej-majewski <andr2ot@gmail.com>
2017-10-10 11:17:28 +0100

    IKASAN-1281 - update samples\spring-boot\jms to provided integration test for exclusion/recovery/stoppedInError

---
+++ ikasaneip/sample/spring-boot/jms/src/main/java/org/ikasan/sample/spring/boot/builderpattern/SampleGeneratedException.java
@@ -0,0 +1,17 @@
+package org.ikasan.sample.spring.boot.builderpattern;
+
+/**
+ * Created by majean on 09/10/2017.
+ */
+public class SampleGeneratedException extends RuntimeException
+{
+    public SampleGeneratedException(Throwable e){
+        super(e);
+    }
+
+    public SampleGeneratedException(String m){
+        super(m);
+    }
+
+
+}
