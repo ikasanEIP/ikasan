a2aef7ebe1205624c5c58691877d656a58c89b98
mitcje <jeff@ikasan.org>
2017-12-11 22:19:43 +0000

    IKASAN-1357 - Provide a better recovery manager factory and scheduled RM implementation.

--- ikasaneip/sample/spring-boot/file/src/main/java/com/ikasan/sample/spring/boot/builderpattern/Application.java
+++ ikasaneip/sample/spring-boot/file/src/main/java/com/ikasan/sample/spring/boot/builderpattern/Application.java
@@ -42,6 +42,7 @@ package com.ikasan.sample.spring.boot.builderpattern;
 
 import org.ikasan.builder.*;
 import org.ikasan.flow.visitorPattern.invoker.FilterInvokerConfiguration;
+import org.ikasan.flow.visitorPattern.invoker.TranslatorInvokerConfiguration;
 import org.ikasan.spec.flow.Flow;
 import org.ikasan.spec.module.Module;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -78,7 +79,7 @@ public class Application
                 .withDescription("Sample file to JMS flow")
                 .withExceptionResolver( componentFactory.getSourceFlowExceptionResolver() )
                 .consumer("File Consumer", componentFactory.getFileConsumer())
-                .filter("myFiler", componentFactory.getFilter(), "myFilterInvokerConfig", new FilterInvokerConfiguration())
+                .filter("myFiler", componentFactory.getFilter(), "myFilterPoJo", new FilterInvokerConfiguration())
                 .converter("File Converter", componentFactory.getSourceFileConverter())
                 .producer("JMS Producer", componentFactory.getJmsProducer()).build();
 
