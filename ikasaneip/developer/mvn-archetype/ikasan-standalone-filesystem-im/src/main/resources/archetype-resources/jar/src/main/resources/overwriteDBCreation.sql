ca56fbcdefd2504db90e0148642b783dccb7140d
mitcje <jeff@ikasan.org>
2018-01-03 19:15:15 +0000

    IKASAN-1281 - BugFix and cleanup on archetypes for builder pattern

---
+++ ikasaneip/developer/mvn-archetype/ikasan-standalone-filesystem-im/src/main/resources/archetype-resources/jar/src/main/resources/overwriteDBCreation.sql
@@ -0,0 +1,67 @@
+      INSERT INTO Users (Id, Username, Password, Enabled, FirstName, Surname,PreviousAccess )
+					VALUES (1,'admin', 'd033e22ae348aeb5660fc2140aec35850c4da997', 1 , 'Admin', 'User',10);
+
+      INSERT INTO Users (Id, Username, Password, Enabled, FirstName, Surname,PreviousAccess )
+					VALUES (2,'api', 'd033e22ae348aeb5660fc2140aec35850c4da997', 1 , 'API', 'API',10);
+
+      INSERT INTO SecurityPrincipal (Name , PrincipalType, Description, CreatedDateTime ,UpdatedDateTime)
+        VALUES ( 'admin','Admin user', 'The administrator user' ,'1970-01-01 00:00:00','1970-01-01 00:00:00');
+
+       INSERT INTO SecurityPolicy (Name ,  Description, CreatedDateTime ,UpdatedDateTime )
+         VALUES ( 'WriteBlueConsole', 'Policy to do write BlueConsole','1970-01-01 00:00:00','1970-01-01 00:00:00' );
+
+
+			 INSERT
+			 INTO UserPrincipal ( UserId, PrincipalId )
+			 (SELECT u.Id, s.Id
+			  FROM Users u, SecurityPrincipal s
+			  WHERE
+			  	   u.Username = 'admin'
+			  	   and s.Name = 'admin'
+			  );
+
+			 INSERT
+			 INTO UserPrincipal ( UserId, PrincipalId )
+			 (SELECT u.Id, s.Id
+			  FROM Users u, SecurityPrincipal s
+			  WHERE
+			  	   u.Username = 'admin'
+			  	   and s.Name = 'user'
+			  );
+
+			 INSERT INTO PrincipalRole ( PrincipalId, RoleId )
+			 (SELECT s.Id, r.Id
+			 FROM
+			 	SecurityPrincipal s, SecurityRole r
+			 WHERE
+			 	s.Name = 'admin'
+			 	and r.Name = 'ADMIN'
+			 );
+
+
+			 INSERT INTO RolePolicy ( RoleId, PolicyId )
+			 (SELECT r.Id, p.Id
+			 FROM
+			 	SecurityRole r , SecurityPolicy p
+			 WHERE
+			 	r.Name = 'ADMIN'
+			 	and p.Name = 'ALL'
+			 );
+
+			INSERT INTO RolePolicy ( RoleId, PolicyId )
+			 (SELECT r.Id, p.Id
+			 FROM
+			 	SecurityRole r , SecurityPolicy p
+			 WHERE
+			 	r.Name = 'ADMIN'
+			 	and p.Name = 'ReadBlueConsole'
+			 );
+
+      INSERT INTO RolePolicy ( RoleId, PolicyId )
+			 (SELECT r.Id, p.Id
+			 FROM
+			 	SecurityRole r , SecurityPolicy p
+			 WHERE
+			 	r.Name = 'ADMIN'
+			 	and p.Name = 'WriteBlueConsole'
+			 );
