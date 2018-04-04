0bfa05725b5cef5e1b6d8ac24b9d5422ccfdc731
Andrzej Majewski <andr2ot@gmail.com>
2018-04-04 10:30:40 +0100

    IKASAN-1425 - Update overwriteDBCreation.sql scripts to make sure that Text fields of error and exclusion tables are represented correctly (#469)

--- ikasaneip/platform/ikasan-h2-standalone-persistence/src/main/resources/overwriteDBCreation.sql
+++ ikasaneip/platform/ikasan-h2-standalone-persistence/src/main/resources/overwriteDBCreation.sql
@@ -91,3 +91,8 @@
 			 );
 
     ALTER TABLE IkasanWiretap ALTER COLUMN PayloadContent TYPE text;
+    ALTER TABLE ErrorOccurrence ALTER COLUMN EventAsString TYPE text;
+    ALTER TABLE ErrorOccurrence ALTER COLUMN ErrorDetail TYPE text;
+    ALTER TABLE ErrorOccurrence ALTER COLUMN ErrorMessage TYPE text;
+    ALTER TABLE ErrorOccurrence ALTER COLUMN Event TYPE BLOB;
+    ALTER TABLE ExclusionEvent ALTER COLUMN Event TYPE BLOB;
