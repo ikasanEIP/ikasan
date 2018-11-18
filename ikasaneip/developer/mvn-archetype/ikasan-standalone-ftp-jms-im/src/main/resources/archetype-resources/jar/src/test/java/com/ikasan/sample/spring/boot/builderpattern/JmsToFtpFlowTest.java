5263d4320814684da97ca6c14c309d862bb921bf
Andrzej Majewski <andr2ot@gmail.com>
2018-11-18 20:16:12 +0000

    IKASAN-1483 - Update archetype ikasan-standalone-ftp-jms project to use @SpringBootApplication and @SpringBootTest

--- ikasaneip/developer/mvn-archetype/ikasan-standalone-ftp-jms-im/src/main/resources/archetype-resources/jar/src/test/java/com/ikasan/sample/spring/boot/builderpattern/JmsToFtpFlowTest.java
+++ ikasaneip/developer/mvn-archetype/ikasan-standalone-ftp-jms-im/src/main/resources/archetype-resources/jar/src/test/java/com/ikasan/sample/spring/boot/builderpattern/JmsToFtpFlowTest.java
@@ -43,43 +43,80 @@ package com.ikasan.sample.spring.boot.builderpattern;
 import org.apache.activemq.command.ActiveMQMapMessage;
 import org.apache.activemq.junit.EmbeddedActiveMQBroker;
 import org.ikasan.endpoint.ftp.producer.FtpProducerConfiguration;
+import org.ikasan.spec.flow.Flow;
+import org.ikasan.spec.module.Module;
 import org.ikasan.testharness.flow.ftp.FtpRule;
-import org.ikasan.testharness.flow.rule.IkasanStandaloneFlowTestRule;
+import org.ikasan.testharness.flow.rule.IkasanFlowTestRule;
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
-import org.junit.rules.RuleChain;
-import org.junit.rules.TestRule;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.jms.core.JmsTemplate;
 import org.springframework.jms.core.MessageCreator;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.util.SocketUtils;
 
+import javax.annotation.Resource;
 import javax.jms.JMSException;
 import javax.jms.MapMessage;
 import javax.jms.Message;
 import javax.jms.Session;
+
+import static org.junit.Assert.assertNotNull;
 
 /**
  * This test Ftp To Log Flow.
  *
  * @author Ikasan Development Team
  */
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = {Application.class},
+    webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_EACH_TEST_METHOD)
 public class JmsToFtpFlowTest
 {
     private static String SAMPLE_MESSAGE = "Hello world!";
 
-    public IkasanStandaloneFlowTestRule flowTestRule = new IkasanStandaloneFlowTestRule("${targetFlowName}",
-        Application.class);
+    @Resource
+    public Module moduleUnderTest;
 
-    public FtpRule ftp = new FtpRule("test", "test", null, 22999);
+    @Resource
+    public JmsTemplate jmsTemplate;
 
-    public EmbeddedActiveMQBroker broker = new EmbeddedActiveMQBroker();
+    public IkasanFlowTestRule flowTestRule = new IkasanFlowTestRule( );
 
-    @Rule public TestRule chain = RuleChain.outerRule(broker).around(ftp).around(flowTestRule);
+    public FtpRule ftp;
 
-    @Test public void test_file_upload() throws Exception
+    public EmbeddedActiveMQBroker broker;
+
+    @Before
+    public void setup(){
+        ftp  = new FtpRule("test","test",null,SocketUtils.findAvailableTcpPort(20000, 21000));
+        ftp.start();
+
+        broker = new EmbeddedActiveMQBroker();
+        broker.start();
+
+        flowTestRule.withFlow((Flow) moduleUnderTest.getFlow("Jms To Ftp Flow"));
+    }
+
+    @After
+    public void teardown()
+    {
+        flowTestRule.stopFlow();
+        broker.stop();
+        ftp.stop();
+
+    }
+
+    @Test
+    public void test_file_upload() throws Exception
     {
 
         // Prepare test data
-        JmsTemplate jmsTemplate = flowTestRule.getIkasanApplication().getBean(JmsTemplate.class);
         System.out.println("Sending a JMS message.[" + SAMPLE_MESSAGE + "]");
         jmsTemplate.send("ftp.private.jms.queue", new MessageCreator()
         {
@@ -93,17 +130,25 @@ public class JmsToFtpFlowTest
         });
 
         //Update Ftp Consumer config
-        FtpProducerConfiguration consumerConfiguration = flowTestRule
+        FtpProducerConfiguration configuration = flowTestRule
             .getComponentConfig("Ftp Producer", FtpProducerConfiguration.class);
-        consumerConfiguration.setOutputDirectory(ftp.getBaseDir());
+        configuration.setOutputDirectory(ftp.getBaseDir());
+        configuration.setOverwrite(true);
+        configuration.setRemotePort(ftp.getPort());
+
 
         //Setup component expectations
-        flowTestRule.consumer("Ftp Jms Consumer").converter("MapMessage to FTP Payload Converter").producer("Ftp Producer");
+        flowTestRule
+            .consumer("Ftp Jms Consumer")
+            .converter("MapMessage to FTP Payload Converter")
+            .producer("Ftp Producer");
 
         // start the flow and assert it runs
         flowTestRule.startFlow();
 
-        flowTestRule.sleep(5000L);
+        flowTestRule.sleep(2000L);
+
+        assertNotNull(ftp.getFile("generatedFtpProducertest.out"));
 
 
     }
