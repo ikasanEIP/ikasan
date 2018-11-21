ddeb7ee03ef5a29ccb8a391b38b5b4ad59620148
Andrzej Majewski <andr2ot@gmail.com>
2018-11-21 22:59:57 +0000

    IKASAN-1483 - Fix archetype ikasan-standalone-filesystem-im project
    (cherry picked from commit bb63a81)

--- ikasaneip/developer/mvn-archetype/ikasan-standalone-filesystem-im/src/main/resources/archetype-resources/jar/src/test/java/com/ikasan/sample/spring/boot/builderpattern/ApplicationTest.java
+++ ikasaneip/developer/mvn-archetype/ikasan-standalone-filesystem-im/src/main/resources/archetype-resources/jar/src/test/java/com/ikasan/sample/spring/boot/builderpattern/ApplicationTest.java
@@ -1,66 +1,64 @@
-/*
- * $Id$
- * $URL$
+/* 
+ * $Id: SchedulerFactoryTest.java 3629 2011-04-18 10:00:52Z mitcje $
+ * $URL: http://open.jira.com/svn/IKASAN/branches/ikasaneip-0.9.x/scheduler/src/test/java/org/ikasan/scheduler/SchedulerFactoryTest.java $
  *
  * ====================================================================
  * Ikasan Enterprise Integration Platform
- *
+ * 
  * Distributed under the Modified BSD License.
- * Copyright notice: The copyright for this software and a full listing
- * of individual contributors are as shown in the packaged copyright.txt
- * file.
- *
+ * Copyright notice: The copyright for this software and a full listing 
+ * of individual contributors are as shown in the packaged copyright.txt 
+ * file. 
+ * 
  * All rights reserved.
  *
- * Redistribution and use in source and binary forms, with or without
+ * Redistribution and use in source and binary forms, with or without 
  * modification, are permitted provided that the following conditions are met:
  *
- *  - Redistributions of source code must retain the above copyright notice,
+ *  - Redistributions of source code must retain the above copyright notice, 
  *    this list of conditions and the following disclaimer.
  *
- *  - Redistributions in binary form must reproduce the above copyright notice,
- *    this list of conditions and the following disclaimer in the documentation
+ *  - Redistributions in binary form must reproduce the above copyright notice, 
+ *    this list of conditions and the following disclaimer in the documentation 
  *    and/or other materials provided with the distribution.
  *
  *  - Neither the name of the ORGANIZATION nor the names of its contributors may
- *    be used to endorse or promote products derived from this software without
+ *    be used to endorse or promote products derived from this software without 
  *    specific prior written permission.
  *
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
- * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
- * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
- * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
- * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
- * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
- * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" 
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE 
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE 
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE 
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL 
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR 
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER 
  * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
- * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE 
  * USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  * ====================================================================
  */
 package com.ikasan.sample.spring.boot.builderpattern;
 
 import org.apache.activemq.junit.EmbeddedActiveMQBroker;
-import org.ikasan.component.endpoint.filesystem.messageprovider.FileConsumerConfiguration;
-import org.ikasan.component.endpoint.filesystem.producer.FileProducer;
-
 import org.ikasan.component.endpoint.filesystem.producer.FileProducerConfiguration;
 import org.ikasan.component.endpoint.jms.spring.consumer.SpringMessageConsumerConfiguration;
+import org.ikasan.spec.configuration.ConfigurationManagement;
 import org.ikasan.spec.configuration.ConfiguredResource;
 import org.ikasan.spec.flow.Flow;
 import org.ikasan.spec.module.Module;
 import org.ikasan.testharness.flow.jms.MessageListenerVerifier;
+import org.ikasan.testharness.flow.rule.IkasanFlowTestRule;
 import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.jms.config.JmsListenerEndpointRegistry;
 import org.springframework.jms.core.JmsTemplate;
 import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.junit4.SpringRunner;
+
 
 import javax.annotation.Resource;
 import javax.jms.TextMessage;
@@ -69,26 +67,20 @@ import java.io.IOException;
 import java.nio.file.FileSystems;
 import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.util.ArrayList;
-import java.util.List;
 
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
 /**
- * This test class supports the <code>Application</code> class.
+ * This test class supports the <code>SimpleExample</code> class.
  *
  * @author Ikasan Development Team
  */
 @RunWith(SpringRunner.class)
 @SpringBootTest(classes = {Application.class},
-        webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+    webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class ApplicationTest
 {
-    /** logger */
-    private static Logger logger = LoggerFactory.getLogger(ApplicationTest.class);
-
     private static String FILE_PRODUCER_FILE_NAME = "testProducer.out";
 
     private static String FILE_CONSUMER_FILE_NAME = "testConsumer.txt";
@@ -96,7 +88,10 @@ public class ApplicationTest
     private EmbeddedActiveMQBroker broker;
 
     @Resource
-    private Module<Flow> moduleUnderTest;
+    private Module moduleUnderTest;
+
+    @Resource
+    private ConfigurationManagement configurationManagement;
 
     @Resource
     private JmsListenerEndpointRegistry registry;
@@ -132,19 +127,14 @@ public class ApplicationTest
         // create file to be consumed
         String content = "Hello World !!";
         Files.write(Paths.get(FILE_CONSUMER_FILE_NAME), content.getBytes());
-        List filenames = new ArrayList<String>();
-        filenames.add(FILE_CONSUMER_FILE_NAME);
 
-        Flow flow = moduleUnderTest.getFlow("sourceFileFlow");
 
-        ConfiguredResource<FileConsumerConfiguration> configuredResource = ((ConfiguredResource)flow.getFlowElement("File Consumer").getFlowComponent());
-        FileConsumerConfiguration configuration = configuredResource.getConfiguration();
-        configuration.setFilenames(filenames);
-        configuration.setCronExpression("*/1 * * * * ?");
+        ConfiguredResource configuredResource = (ConfiguredResource)flowTestRule.getComponent("File Consumer");
+        Object configuration = configurationManagement.createConfiguration(configuredResource);
+        configurationManagement.saveConfiguration(configuration);
 
-        // Get MessageListenerVerifier and start the listener
+        // Get MessageListenerVerifier and start the listner
         final MessageListenerVerifier messageListenerVerifierTarget = new MessageListenerVerifier(broker.getVmURL(), "jms.topic.test", registry);
-
         messageListenerVerifierTarget.start();
 
         //Setup component expectations
@@ -161,25 +151,10 @@ public class ApplicationTest
         // wait for a brief while to let the flow complete
         flowTestRule.sleep(2000L);
 
-        // start flow
-        flow.start();
-        assertEquals("running", flow.getState());
-
-        // check for published event
-        int count = 0;
-        while (messageListenerVerifierTarget.getCaptureResults().size() == 0)
-        {
-            pause(200);
-            assertFalse("Timed out waiting for event", count++ == 10);
-        }
-
-        flow.stop();
-        assertEquals("stopped", flow.getState());
-
         // Set expectation
         assertTrue(messageListenerVerifierTarget.getCaptureResults().size()>=1);
         assertEquals(((TextMessage)messageListenerVerifierTarget.getCaptureResults().get(0)).getText(),
-                FILE_CONSUMER_FILE_NAME);
+            FILE_CONSUMER_FILE_NAME);
     }
 
     @Test
@@ -191,36 +166,30 @@ public class ApplicationTest
 
         // Prepare test data
         String message = "Random Text";
-        logger.info("Sending a JMS message.[" + message + "]");
+        System.out.println("Sending a JMS message.[" + message + "]");
         jmsTemplate.convertAndSend("private.file.queue.test", message);
 
-        // get targetFileFlow from context
-        Flow flow = moduleUnderTest.getFlow("targetFileFlow");
 
         // update producer with file producer name
-        FileProducerConfiguration producerConfiguration = ((FileProducer) flow.getFlowElement("File Producer")
-                .getFlowComponent()).getConfiguration();
+        FileProducerConfiguration producerConfiguration =  flowTestRule.getComponentConfig("File Producer",FileProducerConfiguration.class);
         producerConfiguration.setFilename(FILE_PRODUCER_FILE_NAME);
 
-        // update flow consumer with file producer name
-        SpringMessageConsumerConfiguration jmsConfiguration = ((ConfiguredResource<SpringMessageConsumerConfiguration>)flow.getFlowElement("JMS Consumer").getFlowComponent()).getConfiguration();
+        // update flow consumer  with file producer name
+        SpringMessageConsumerConfiguration jmsConfiguration = flowTestRule.getComponentConfig("JMS Consumer",SpringMessageConsumerConfiguration.class);
         jmsConfiguration.setDestinationJndiName("private.file.queue.test");
 
         //Setup component expectations
         flowTestRule.consumer("JMS Consumer")
             .producer("File Producer");
 
-        // give flow time
-        pause(2000);
-        assertEquals("running", flow.getState());
-        flow.stop();
-        assertEquals("stopped", flow.getState());
+        flowTestRule.startFlow();
+        flowTestRule.sleep(2000L);
 
         File result = FileSystems.getDefault().getPath(FILE_PRODUCER_FILE_NAME).toFile();
 
         assertTrue("File does not exist.", result.exists());
         assertEquals("Generated file, has different content.", message,
-                new String(Files.readAllBytes(result.toPath())));
+            new String(Files.readAllBytes(result.toPath())));
     }
 
 }
