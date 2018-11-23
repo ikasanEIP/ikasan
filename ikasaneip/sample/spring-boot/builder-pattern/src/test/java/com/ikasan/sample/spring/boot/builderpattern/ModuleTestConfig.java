024a6476da4cc07357e53102fa77e0d865da41a3
mitcje <jeff@ikasan.org>
2018-11-23 17:07:19 +0000

    IKASAN-1483 Update archetypes for spring boot auto-configuration. jms-im.

--- ikasaneip/sample/spring-boot/builder-pattern/src/test/java/com/ikasan/sample/spring/boot/builderpattern/ModuleTestConfig.java
+++ ikasaneip/sample/spring-boot/builder-pattern/src/test/java/com/ikasan/sample/spring/boot/builderpattern/ModuleTestConfig.java
@@ -1,16 +1,10 @@
 package com.ikasan.sample.spring.boot.builderpattern;
 
 import org.apache.activemq.ActiveMQConnectionFactory;
-import org.apache.activemq.junit.EmbeddedActiveMQBroker;
-import org.ikasan.testharness.flow.jms.MessageListenerVerifier;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.DependsOn;
-import org.springframework.jms.config.JmsListenerEndpointRegistry;
 import org.springframework.jms.core.JmsTemplate;
-
-import javax.annotation.Resource;
 
 @Configuration
 public class ModuleTestConfig
@@ -25,5 +19,8 @@ public class ModuleTestConfig
         return jmsTemplate;
     }
 
-
+    @Bean String brokerUrl()
+    {
+        return brokerUrl;
+    }
 }
