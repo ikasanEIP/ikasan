ea23208aa01f3289e0f5f2ea5e75c9c8243b6151
Andrzej Majewski <andr2ot@gmail.com>
2018-12-24 16:02:22 +0000

    IKASAN-1506 - cleanup hsql and jboss-logging and upgrade commons-lang
    (cherry picked from commit 710d5c0)

--- ikasaneip/component/endpoint/filetransfer/connector-basefiletransfer/src/main/java/org/ikasan/connector/basefiletransfer/outbound/command/ChunkingRetrieveFileCommand.java
+++ ikasaneip/component/endpoint/filetransfer/connector-basefiletransfer/src/main/java/org/ikasan/connector/basefiletransfer/outbound/command/ChunkingRetrieveFileCommand.java
@@ -48,7 +48,7 @@ import java.util.List;
 
 import javax.resource.ResourceException;
 
-import org.apache.commons.lang.builder.ToStringBuilder;
+import org.apache.commons.lang3.builder.ToStringBuilder;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.ikasan.connector.base.command.ExecutionContext;
