cdb46b0583b3bcf77fbc84c5f840956c2d972280
mitcje <jeff@ikasan.org>
2019-03-17 01:08:36 +0000

    IKASAN-1940 - DB Archetype and minor changes to accommodate better lifecycle controls asssociated with DB archetype.

---
+++ ikasaneip/developer/mvn-archetype/ikasan-standalone-db-jms-im/src/main/resources/archetype-resources/jar/src/main/java/com/ikasan/sample/spring/boot/builderpattern/ComponentFactory.java
@@ -0,0 +1,291 @@
+/*
+ * $Id$
+ * $URL$
+ *
+ * ====================================================================
+ * Ikasan Enterprise Integration Platform
+ *
+ * Distributed under the Modified BSD License.
+ * Copyright notice: The copyright for this software and a full listing
+ * of individual contributors are as shown in the packaged copyright.txt
+ * file.
+ *
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ *  - Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer.
+ *
+ *  - Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ *
+ *  - Neither the name of the ORGANIZATION nor the names of its contributors may
+ *    be used to endorse or promote products derived from this software without
+ *    specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE
+ * USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ * ====================================================================
+ */
+package com.ikasan.sample.spring.boot.builderpattern;
+
+import com.ikasan.sample.person.PersonMessageProvider;
+import com.ikasan.sample.person.model.Person;
+import com.ikasan.sample.person.service.PersonService;
+import org.apache.activemq.ActiveMQXAConnectionFactory;
+import org.ikasan.builder.BuilderFactory;
+import org.ikasan.builder.OnException;
+import org.ikasan.exceptionResolver.ExceptionResolver;
+import org.ikasan.spec.component.endpoint.Consumer;
+import org.ikasan.spec.component.endpoint.EndpointException;
+import org.ikasan.spec.component.endpoint.Producer;
+import org.ikasan.spec.component.filter.Filter;
+import org.ikasan.spec.component.filter.FilterException;
+import org.ikasan.spec.component.transformation.Converter;
+import org.ikasan.spec.component.splitting.Splitter;
+import org.ikasan.spec.component.transformation.TransformationException;
+import org.ikasan.spec.configuration.ConfiguredResource;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.ImportResource;
+
+import javax.annotation.Resource;
+import javax.jms.DeliveryMode;
+import javax.jms.Session;
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.springframework.jms.listener.DefaultMessageListenerContainer.CACHE_CONNECTION;
+
+/**
+ * Sample component factory.
+ *
+ * @author Ikasan Development Team
+ */
+@Configuration
+@ImportResource( {
+        "classpath:ikasan-transaction-pointcut-quartz.xml",
+        "classpath:personDB-conf.xml",
+        "classpath:h2-datasource-conf.xml"
+} )
+public class ComponentFactory
+{
+    @Resource
+    private BuilderFactory builderFactory;
+
+    @Value("${db.consumer.cronExpression}")
+    String cronExpression;
+
+//    @Value("${db.consumer.scheduledGroupName}")
+//    String scheduledGroupName;
+//
+//    @Value("${db.consumer.scheduledName}")
+//    String scheduledName;
+//
+    @Value("${db.consumer.configuredResourceId}")
+    String dbConsumerConfiguredResourceId;
+
+    @Value("${jms.producer.configuredResourceId}")
+    String jmsProducerConfiguredResourceId;
+
+    @Value("${jms.provider.url}")
+    private String jmsProviderUrl;
+
+    @Resource
+    PersonMessageProvider personMessageProvider;
+
+    @Resource
+    PersonService personService;
+
+    /**
+     * Return an instance of a configured file consumer
+     * @return
+     */
+    Consumer getDBConsumer()
+    {
+        return builderFactory.getComponentBuilder().scheduledConsumer()
+                .setCronExpression(cronExpression)
+              //  .setScheduledJobGroupName(scheduledGroupName)
+              //  .setScheduledJobName(scheduledName)
+                .setConfiguredResourceId(dbConsumerConfiguredResourceId)
+                .setMessageProvider(personMessageProvider)
+                .build();
+    }
+
+    /**
+     * Return an instance of a configured file consumer
+     * @return
+     */
+    Producer getDBProducer()
+    {
+        return new DBProducer(personService);
+    }
+
+    Consumer getJmsConsumer()
+    {
+        ActiveMQXAConnectionFactory connectionFactory = new ActiveMQXAConnectionFactory(jmsProviderUrl);
+
+       return builderFactory.getComponentBuilder().jmsConsumer()
+                .setConnectionFactory(connectionFactory)
+                .setDestinationJndiName("jms.topic.test")
+                .setDurableSubscriptionName("testDurableSubscription")
+                .setDurable(true)
+                .setAutoContentConversion(true)
+                .setAutoSplitBatch(true)
+                .setBatchMode(false)
+                .setBatchSize(1)
+                .setCacheLevel(CACHE_CONNECTION)
+                .setConcurrentConsumers(1)
+                .setMaxConcurrentConsumers(1)
+                .setSessionAcknowledgeMode(Session.SESSION_TRANSACTED)
+                .setSessionTransacted(true)
+                .setPubSubDomain(false)
+                .build();
+    }
+
+    Converter getObjectToXmlStringConverter()
+    {
+        return builderFactory.getComponentBuilder().objectToXmlStringConverter()
+                .setObjectClass(Person.class)
+                .build();
+    }
+
+    Converter getXmlToObjectConverter()
+    {
+        return builderFactory.getComponentBuilder().xmlStringToObjectConverter()
+                .setClassToBeBound(com.ikasan.sample.person.model.Person.class)
+                .build();
+    }
+
+    Filter getFilter()
+    {
+        MyFilter myFilter = new MyFilter();
+        myFilter.setConfiguredResourceId("myFilterPoJo");
+        myFilter.setConfiguration( new MyFilterConfiguration() );
+        return myFilter;
+    }
+
+    Producer getJmsProducer()
+    {
+        ActiveMQXAConnectionFactory connectionFactory = new ActiveMQXAConnectionFactory(jmsProviderUrl);
+
+        return builderFactory.getComponentBuilder().jmsProducer()
+                .setConfiguredResourceId(jmsProducerConfiguredResourceId)
+                .setDestinationJndiName("jms.topic.test")
+                .setConnectionFactory(connectionFactory)
+                .setSessionAcknowledgeMode(Session.SESSION_TRANSACTED)
+                .setSessionTransacted(true)
+                .setPubSubDomain(false)
+                .setDeliveryPersistent(true)
+                .setDeliveryMode(DeliveryMode.PERSISTENT)
+                .setExplicitQosEnabled(true)
+                .setMessageIdEnabled(true)
+                .setMessageTimestampEnabled(true)
+                .build();
+    }
+
+    ExceptionResolver getSourceFlowExceptionResolver()
+    {
+        return builderFactory.getExceptionResolverBuilder().addExceptionToAction(TransformationException.class, OnException.excludeEvent()).build();
+    }
+
+    Splitter getListSplitter()
+    {
+        return builderFactory.getComponentBuilder().listSplitter().build();
+    }
+
+    class DBProducer implements Producer<Person>
+    {
+        /** required service */
+        PersonService personService;
+
+        /**
+         * Constructor
+         * @param personService
+         */
+        DBProducer(PersonService personService)
+        {
+            this.personService = personService;
+            if(personService == null)
+            {
+                throw new IllegalArgumentException("personService cannot be 'null'");
+            }
+        }
+
+        @Override
+        public void invoke(Person person) throws EndpointException
+        {
+            personService.update(person);
+        }
+    }
+
+    class MyFilter implements Filter<List<Person>>, ConfiguredResource<MyFilterConfiguration>
+    {
+        private final Logger logger = LoggerFactory.getLogger(MyFilter.class);
+
+        String configuredResourceId;
+        MyFilterConfiguration configuration;
+
+        @Override
+        public String getConfiguredResourceId() {
+            return configuredResourceId;
+        }
+
+        @Override
+        public void setConfiguredResourceId(String configuredResourceId) {
+            this.configuredResourceId = configuredResourceId;
+        }
+
+        @Override
+        public MyFilterConfiguration getConfiguration() {
+            return configuration;
+        }
+
+        @Override
+        public void setConfiguration(MyFilterConfiguration configuration) {
+            this.configuration = configuration;
+        }
+
+        @Override
+        public List<Person> filter(List<Person> people) throws FilterException
+        {
+            List<Person> peopleOldEnough = new ArrayList<Person>();
+
+            for(Person person:people)
+            {
+                try
+                {
+                    if(configuration.ageRestriction < person.getAge())
+                    {
+                        peopleOldEnough.add(person);
+                    }
+                }
+                catch(Exception e)
+                {
+                    logger.info("Unable to calculate age for " + person.getName(), e);
+                }
+            }
+
+            if(peopleOldEnough.size() > 0)
+            {
+                return peopleOldEnough;
+            }
+
+            return null;
+        }
+    }
+
+}
---
+++ ikasaneip/developer/mvn-archetype/ikasan-standalone-hibernate-jms-im/src/main/resources/archetype-resources/jar/src/test/java/com/ikasan/sample/person/PersonMessageProviderTest.java
@@ -0,0 +1,80 @@
+package com.ikasan.sample.person;
+
+import com.ikasan.sample.person.dao.PersonDao;
+import com.ikasan.sample.person.model.Person;
+import org.jmock.Mockery;
+import org.jmock.lib.legacy.ClassImposteriser;
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.quartz.JobExecutionContext;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import javax.annotation.Resource;
+import java.util.List;
+
+/**
+ * This test class supports the <code>PersonMessageProvider</code> class.
+ *
+ * @author Ikasan Development Team
+ */
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringBootTest(classes = { com.ikasan.sample.spring.boot.builderpattern.Application.class},
+        webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@ContextConfiguration(locations = {
+        "/transaction-pointcut-components-on-test.xml"
+})
+public class PersonMessageProviderTest
+{
+    @Resource
+    PersonMessageProvider personMessageProvider;
+
+    /**
+     * Mockery for mocking concrete classes
+     */
+    private final Mockery mockery = new Mockery()
+    {
+        {
+            setImposteriser(ClassImposteriser.INSTANCE);
+        }
+    };
+
+    private JobExecutionContext jobExecutionContext = mockery.mock(JobExecutionContext.class);
+
+    @Resource
+    PersonDao personDao;
+
+    @Before
+    public void setup()
+    {
+        Person person = new Person();
+        person.setId(1);
+        person.setName("ikasan");
+        person.setDobDayOfMonth(6);
+        person.setDobMonthOfYear(7);
+        person.setDobYear(2005);
+        personDao.saveOrUpdate(person);
+    }
+
+    @After
+    public void teardown()
+    {
+        List<Person> persons = personDao.findAll();
+        for(Person person:persons)
+        {
+            personDao.delete(person);
+        }
+    }
+
+    @Test
+    public void test_invoke()
+    {
+        List<Person> persons = personMessageProvider.invoke(jobExecutionContext);
+        Assert.assertTrue("There should be one person ", persons.size() == 1);
+    }
+
+}
