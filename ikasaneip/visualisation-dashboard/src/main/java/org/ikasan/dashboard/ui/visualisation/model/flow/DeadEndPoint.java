fe5118644f284c85627db1f3784ded0e0c3a3e54
Michael Stewart <mick_stewart73@hotmail.com>
2019-07-17 21:04:30 +0100

    IKASAN-1349 general development

--- ikasaneip/visualisation-dashboard/src/main/java/org/ikasan/dashboard/ui/visualisation/model/flow/DeadEndPoint.java
+++ ikasaneip/visualisation-dashboard/src/main/java/org/ikasan/dashboard/ui/visualisation/model/flow/DeadEndPoint.java
@@ -7,7 +7,7 @@ import org.ikasan.vaadin.visjs.network.util.Shape;
 /**
  * Created by stewmi on 07/11/2018.
  */
-public class DeadEndPoint extends Node implements SingleTransition
+public class DeadEndPoint extends Node implements SingleTransition, Endpoint
 {
 	public static final String IMAGE = "frontend/images/dead-end-point.png";
 
