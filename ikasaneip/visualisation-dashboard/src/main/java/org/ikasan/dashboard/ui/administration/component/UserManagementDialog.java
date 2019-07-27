0b31019f40b8038558b1c8e760c53220603e5f6c
Michael Stewart <mick@humeenterprises.co.uk>
2019-07-27 15:32:32 +0100

    IKASAN-1349 sorting out user management panel

--- ikasaneip/visualisation-dashboard/src/main/java/org/ikasan/dashboard/ui/administration/component/UserManagementDialog.java
+++ ikasaneip/visualisation-dashboard/src/main/java/org/ikasan/dashboard/ui/administration/component/UserManagementDialog.java
@@ -1,6 +1,5 @@
 package org.ikasan.dashboard.ui.administration.component;
 
-import com.github.appreciated.css.grid.GridLayoutComponent;
 import com.github.appreciated.css.grid.sizes.Flex;
 import com.github.appreciated.layout.FluentGridLayout;
 import com.vaadin.flow.component.button.Button;
@@ -13,14 +12,21 @@ import com.vaadin.flow.component.orderedlayout.FlexComponent;
 import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.component.textfield.TextField;
+import org.ikasan.dashboard.ui.util.SecurityConstants;
 import org.ikasan.security.model.User;
+import org.ikasan.security.service.SecurityService;
 import org.ikasan.security.service.UserService;
+import org.ikasan.security.service.authentication.IkasanAuthentication;
+import org.ikasan.systemevent.service.SystemEventService;
+import org.springframework.security.core.context.SecurityContextHolder;
 
 
 public class UserManagementDialog extends Dialog
 {
     private User user;
     private UserService userService;
+    private SecurityService securityService;
+    private SystemEventService systemEventService;
 
     public UserManagementDialog(User user, UserService userService)
     {
@@ -43,7 +49,7 @@ public class UserManagementDialog extends Dialog
         FluentGridLayout layout = new FluentGridLayout()
             .withTemplateRows(new Flex(1), new Flex(1.25), new Flex(1.25))
             .withTemplateColumns(new Flex(1), new Flex(1), new Flex(1))
-            .withRowAndColumn(initUserForm(), 1,1, 1, 2)
+            .withRowAndColumn(initUserForm(), 1,1, 1, 3)
             .withRowAndColumn(createLastAccessGrid(), 2,3, 3, 3)
             .withRowAndColumn(createRolesAccessGrid(), 2,1,  2, 3)
             .withRowAndColumn(createSecurityChangesGrid(), 3, 3)
@@ -64,7 +70,6 @@ public class UserManagementDialog extends Dialog
 
         grid.setClassName("my-grid");
         grid.addColumn(User::getUsername).setKey("username").setHeader("Username").setSortable(true);
-
         grid.addColumn(User::getFirstName).setKey("firstname").setHeader("First Name").setSortable(true);
         grid.addColumn(User::getSurname).setKey("surname").setHeader("Surname").setSortable(true);
         grid.addColumn(User::getEmail).setKey("email").setHeader("Email").setSortable(true);
@@ -75,6 +80,7 @@ public class UserManagementDialog extends Dialog
 
         VerticalLayout layout = new VerticalLayout();
         layout.add(dashboardActivityLabel, grid);
+
         return layout;
     }
 
@@ -96,6 +102,19 @@ public class UserManagementDialog extends Dialog
         grid.setSizeFull();
 
         Button addRoleButton = new Button("Add role");
+
+        IkasanAuthentication authentication = (IkasanAuthentication) SecurityContextHolder.getContext().getAuthentication();
+
+        if(authentication.hasGrantedAuthority(SecurityConstants.ALL_AUTHORITY) ||
+            authentication.hasGrantedAuthority(SecurityConstants.USER_ADMINISTRATION_ADMIN)
+            || authentication.hasGrantedAuthority(SecurityConstants.USER_ADMINISTRATION_WRITE))
+        {
+            addRoleButton.setVisible(true);
+        }
+        else
+        {
+            addRoleButton.setVisible(false);
+        }
 
         HorizontalLayout labelLayout = new HorizontalLayout();
         labelLayout.setWidthFull();
@@ -160,28 +179,25 @@ public class UserManagementDialog extends Dialog
 
     private VerticalLayout initUserForm()
     {
-        H3 userProfileLabel = new H3("User Profile");
+        H3 userProfileLabel = new H3("User Profile - " + this.user.getUsername());
 
         FormLayout formLayout = new FormLayout();
-        TextField usernameTf = new TextField("Username");
-        usernameTf.setWidth("400px");
-        formLayout.add(usernameTf, new Div());
 
         TextField firstnameTf = new TextField("First name");
-        firstnameTf.setWidth("400px");
+        firstnameTf.setValue(this.user.getFirstName());
         formLayout.add(firstnameTf);
 
         TextField surnameTf = new TextField("Surname");
-        surnameTf.setWidth("400px");
+        surnameTf.setValue(this.user.getSurname());
         formLayout.add(surnameTf);
 
         TextField departmentTf = new TextField("Department");
-        departmentTf.setWidth("400px");
+        departmentTf.setValue(this.user.getDepartment() == null ? "" : this.user.getDepartment());
         formLayout.add(departmentTf);
 
         TextField emailTf = new TextField("Email");
-        emailTf.setWidth("400px");
         formLayout.add(emailTf);
+        emailTf.setValue(this.user.getEmail()== null ? "" : this.user.getEmail());
         formLayout.setSizeFull();
 
         Div result = new Div();
