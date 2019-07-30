6a2b86ce35678315510bcaccbdea9e26f6153fa9
Andrzej Majewski <andr2ot@gmail.com>
2019-07-30 22:30:22 +0100

    IKASAN-1604 - enable Basic security on REST calls

--- ikasaneip/webconsole/jar/src/main/java/org/ikasan/web/WebSecurityConfig.java
+++ ikasaneip/webconsole/jar/src/main/java/org/ikasan/web/WebSecurityConfig.java
@@ -5,6 +5,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -12,6 +13,7 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 @EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter
 {
     @Autowired
@@ -73,6 +75,8 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter
     @Override protected void configure(HttpSecurity http) throws Exception
     {
         http
+            .httpBasic()
+            .and()
             .authorizeRequests()
             .antMatchers("/login.jsp").permitAll()
             .antMatchers("/css/**","/images/**","/js/**").permitAll()
