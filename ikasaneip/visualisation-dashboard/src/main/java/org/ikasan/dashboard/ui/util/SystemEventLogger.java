036ce7f033573d5796b04b63c1720647f3c6946e
Michael Stewart <mick_stewart73@hotmail.com>
2019-09-15 20:54:47 +0100

    IKASAN-1651 further work to enhance the migrated security functionality.

--- ikasaneip/visualisation-dashboard/src/main/java/org/ikasan/dashboard/ui/util/SystemEventLogger.java
+++ ikasaneip/visualisation-dashboard/src/main/java/org/ikasan/dashboard/ui/util/SystemEventLogger.java
@@ -1,11 +1,16 @@
 package org.ikasan.dashboard.ui.util;
 
+import org.ikasan.dashboard.ui.administration.view.UserManagementView;
 import org.ikasan.security.service.authentication.IkasanAuthentication;
 import org.ikasan.systemevent.service.SystemEventService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.security.core.context.SecurityContextHolder;
 
 public class SystemEventLogger
 {
+    private Logger logger = LoggerFactory.getLogger(SystemEventLogger.class);
+
     private SystemEventService systemEventService;
 
     public SystemEventLogger(SystemEventService systemEventService)
@@ -17,9 +22,16 @@ public class SystemEventLogger
         }
     }
 
-    public void logEvent(String subject, String action)
+    public void logEvent(String subject, String action, String actor)
     {
         IkasanAuthentication ikasanAuthentication = (IkasanAuthentication) SecurityContextHolder.getContext().getAuthentication();
-        systemEventService.logSystemEvent(subject, action, ikasanAuthentication.getName());
+        action += " - Performed by [" + ikasanAuthentication.getName() + "]";
+        if(actor == null)
+        {
+            actor = ikasanAuthentication.getName();
+        }
+        logger.debug("Logging system event [{}], [{}], [{}]", subject, action, actor);
+        systemEventService.logSystemEvent(subject, action, actor);
+        logger.debug("Sucessfully logged system event [{}], [{}], [{}]", subject, action, actor);
     }
 }
