d9a8baf36b5f2b74cae0ae4199d5a3adaff20cc7
Michael Stewart <mick@humeenterprises.co.uk>
2019-10-13 07:57:26 +0200

    IKASAN-1642 sorting out unit test.

--- ikasaneip/spec/service/dashboard-client/src/main/java/org/ikasan/spec/dashboard/DashboardRestService.java
+++ ikasaneip/spec/service/dashboard-client/src/main/java/org/ikasan/spec/dashboard/DashboardRestService.java
@@ -3,11 +3,11 @@ package org.ikasan.spec.dashboard;
 public interface DashboardRestService<T>
 {
 
-    String MODULE_NAME_PROPERTY="module.name";
-    String DASHBOARD_BASE_URL_PROPERTY="ikasan.dashboard.extract.base.url";
-    String DASHBOARD_USERNAME_PROPERTY="ikasan.dashboard.extract.username";
-    String DASHBOARD_PASSWORD_PROPERTY="ikasan.dashboard.extract.password";
-    String DASHBOARD_EXTRACT_ENABLED_PROPERTY ="ikasan.dashboard.extract.enabled";
+    public static final String MODULE_NAME_PROPERTY="module.name";
+    public static final String DASHBOARD_BASE_URL_PROPERTY="ikasan.dashboard.extract.base.url";
+    public static final String DASHBOARD_USERNAME_PROPERTY="ikasan.dashboard.extract.username";
+    public static final String DASHBOARD_PASSWORD_PROPERTY="ikasan.dashboard.extract.password";
+    public static final String DASHBOARD_EXTRACT_ENABLED_PROPERTY ="ikasan.dashboard.extract.enabled";
 
     boolean publish(T t);
 
