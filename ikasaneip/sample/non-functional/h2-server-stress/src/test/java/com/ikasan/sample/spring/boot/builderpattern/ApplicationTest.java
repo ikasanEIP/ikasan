693a8929ffaa754943d7a5cb8a13de3fb014a7f6
Andrzej Majewski <andr2ot@gmail.com>
2019-10-16 09:13:56 +0100

    IKASAN-1650 - add -ifNotExists

--- ikasaneip/sample/non-functional/h2-server-stress/src/test/java/com/ikasan/sample/spring/boot/builderpattern/ApplicationTest.java
+++ ikasaneip/sample/non-functional/h2-server-stress/src/test/java/com/ikasan/sample/spring/boot/builderpattern/ApplicationTest.java
@@ -127,7 +127,7 @@ public class ApplicationTest
     public static void setup() throws SQLException
     {
         // TODO can we use a random port and tie back to the application.properties url?
-        server = Server.createTcpServer("-tcpPort", "9092", "-tcpAllowOthers").start();
+        server = Server.createTcpServer("-tcpPort", "9092", "-tcpAllowOthers","-ifNotExists").start();
     }
 
     @Before
