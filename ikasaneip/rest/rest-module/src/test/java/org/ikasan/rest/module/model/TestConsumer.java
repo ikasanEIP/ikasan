76a2fc4e9b23a8015b91db6d272f7993151b0dae
Andrzej Majewski <andr2ot@gmail.com>
2020-01-02 17:00:42 +0000

    IKASAN-1704 - Update Scheduler REST controller, and provide tracing action

---
+++ ikasaneip/rest/rest-module/src/test/java/org/ikasan/rest/module/model/TestConsumer.java
@@ -0,0 +1,42 @@
+package org.ikasan.rest.module.model;
+
+import org.ikasan.spec.component.endpoint.Consumer;
+
+public class TestConsumer implements Consumer
+{
+    @Override
+    public void setListener(Object o)
+    {
+
+    }
+
+    @Override
+    public void setEventFactory(Object o)
+    {
+
+    }
+
+    @Override
+    public Object getEventFactory()
+    {
+        return null;
+    }
+
+    @Override
+    public void start()
+    {
+
+    }
+
+    @Override
+    public boolean isRunning()
+    {
+        return false;
+    }
+
+    @Override
+    public void stop()
+    {
+
+    }
+}
