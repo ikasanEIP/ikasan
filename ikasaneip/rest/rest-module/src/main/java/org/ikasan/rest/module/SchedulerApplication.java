4f99c1274b6292bb6897ec2f73ab955d10324584
Andrzej Majewski <andr2ot@gmail.com>
2020-01-02 17:00:42 +0000

    IKASAN-1704 - Update Scheduler REST controller, and provide tracing action

--- ikasaneip/rest/rest-module/src/main/java/org/ikasan/rest/module/SchedulerApplication.java
+++ ikasaneip/rest/rest-module/src/main/java/org/ikasan/rest/module/SchedulerApplication.java
@@ -9,14 +9,18 @@ import org.ikasan.spec.flow.FlowConfiguration;
 import org.ikasan.spec.flow.FlowElement;
 import org.ikasan.spec.module.Module;
 import org.ikasan.spec.module.ModuleContainer;
-import org.ikasan.spec.resubmission.ResubmissionService;
+import org.ikasan.spec.systemevent.SystemEventService;
 import org.quartz.*;
 import org.quartz.impl.matchers.GroupMatcher;
+import org.springframework.aop.framework.Advised;
+import org.springframework.aop.support.AopUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -35,11 +39,17 @@ import static org.quartz.TriggerBuilder.newTrigger;
 @RestController
 public class SchedulerApplication
 {
+    public static final String MANUAL_TRIGGER_OF_SCHEDULED_FLOW_SYSTEM_EVENT_ACTION = "Manual Trigger of flow "
+        + "requested";
+
     @Autowired
     private Scheduler platformScheduler;
 
     @Autowired
     private ModuleContainer moduleContainer;
+
+    @Autowired
+    private SystemEventService systemEventService;
 
     /**
      * Gets All Triggers
@@ -118,15 +128,38 @@ public class SchedulerApplication
 
                 FlowConfiguration flowConfiguration = flow.getFlowConfiguration();
                 FlowElement<Consumer> flowConfigurationConsumerFlowElement = flowConfiguration.getConsumerFlowElement();
+
                 if ( flowConfigurationConsumerFlowElement != null
-                    && flowConfigurationConsumerFlowElement.getFlowComponent() != null
-                    && flowConfigurationConsumerFlowElement.getFlowComponent() instanceof ScheduledConsumer )
+                    && flowConfigurationConsumerFlowElement.getFlowComponent() != null )
                 {
-                    ScheduledConsumer consumer = (ScheduledConsumer) flowConfigurationConsumerFlowElement
-                        .getFlowComponent();
-                    JobDetail jobDetail = ((ScheduledComponent<JobDetail>) consumer).getJobDetail();
-                    Trigger trigger = newTrigger().withIdentity("name", "group").forJob(jobDetail).build();
-                    consumer.scheduleAsEagerTrigger(trigger, 0);
+
+                    ScheduledConsumer consumer;
+                    if ( (AopUtils.isJdkDynamicProxy(flowConfigurationConsumerFlowElement.getFlowComponent())) )
+                    {
+                        consumer = getTargetObject(flowConfigurationConsumerFlowElement.getFlowComponent(),
+                            ScheduledConsumer.class
+                                                  );
+                    }
+                    else if ( flowConfigurationConsumerFlowElement.getFlowComponent() instanceof ScheduledConsumer )
+                    {
+
+                        consumer = (ScheduledConsumer) flowConfigurationConsumerFlowElement.getFlowComponent();
+
+                    }
+                    else
+                    {
+                        return new ResponseEntity(new ErrorDto(
+                            "Consumer of given module[" + moduleName + "] flow [" + flowName
+                                + "] is not of ScheduledConsumer type and cannot be triggered using this API."),
+                            HttpStatus.BAD_REQUEST
+                        );
+
+                    }
+                    //log the request
+                    this.systemEventService.logSystemEvent(moduleName + "." + flowName,
+                        MANUAL_TRIGGER_OF_SCHEDULED_FLOW_SYSTEM_EVENT_ACTION, getUser()
+                                                          );
+                    triggerScheduledConsumer(consumer);
 
                 }
 
@@ -142,5 +175,43 @@ public class SchedulerApplication
         {
             return new ResponseEntity(new ErrorDto(e.getMessage()), HttpStatus.BAD_REQUEST);
         }
+    }
+
+    private void triggerScheduledConsumer(ScheduledConsumer consumer) throws SchedulerException
+    {
+
+        JobDetail jobDetail = ((ScheduledComponent<JobDetail>) consumer).getJobDetail();
+        Trigger trigger = newTrigger().withIdentity("name", "group").forJob(jobDetail).build();
+        consumer.scheduleAsEagerTrigger(trigger, 0);
+    }
+
+    private <T> T getTargetObject(Object proxy, Class<T> targetClass)
+    {
+        try
+        {
+            if ( AopUtils.isJdkDynamicProxy(proxy) )
+            {
+                return (T) ((Advised) proxy).getTargetSource().getTarget();
+            }
+            else
+            {
+                return (T) proxy; // expected to be cglib proxy then, which is simply a specialized class
+            }
+        }
+        catch (Exception e)
+        {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private String getUser()
+    {
+        String user = "unknown";
+        SecurityContext context = SecurityContextHolder.getContext();
+        if ( context != null )
+        {
+            user = context.getAuthentication().getPrincipal().toString();
+        }
+        return user;
     }
 }
