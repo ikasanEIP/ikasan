a3a9538a100c61e604cfe493c872b72f79ce77a5
Andrzej Majewski <andr2ot@gmail.com>
2020-04-29 22:00:07 +0100

    IKASAN-1751 -  suppress h2 create-drop errors

---
+++ ikasaneip/configuration-service/src/test/java/org/ikasan/configurationService/service/TestConfiguration.java
@@ -0,0 +1,16 @@
+package org.ikasan.configurationService.service;
+
+import org.ikasan.transaction.IkasanTransactionConfiguration;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+import org.springframework.context.annotation.ImportResource;
+
+@Configuration
+@ImportResource(locations = {
+    "/configuration-service-conf.xml", "/h2-datasource-conf.xml",
+    "/substitute-components.xml"
+})
+@Import({ IkasanTransactionConfiguration.class})
+public class TestConfiguration
+{
+}
