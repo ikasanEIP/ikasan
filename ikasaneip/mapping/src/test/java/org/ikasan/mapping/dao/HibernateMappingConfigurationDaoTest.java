1a8f2b1af0e568b80966bc9a3331ebb3a53ee0b8
Andrzej Majewski <andr2ot@gmail.com>
2020-04-29 22:00:07 +0100

    IKASAN-1751 -  suppress h2 create-drop errors

--- ikasaneip/mapping/src/test/java/org/ikasan/mapping/dao/HibernateMappingConfigurationDaoTest.java
+++ ikasaneip/mapping/src/test/java/org/ikasan/mapping/dao/HibernateMappingConfigurationDaoTest.java
@@ -67,8 +67,7 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 @SuppressWarnings("unqualified-field-access")
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(locations={
-        "/mapping-conf.xml",
-        "/hsqldb-config.xml",
+        "/mapping-conf.xml", "/h2-config.xml",
         "/substitute-components.xml",
         "/mock-components.xml"
 })
