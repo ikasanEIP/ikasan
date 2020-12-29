dc02f417deda4d84814a0452f1651295e22b405d
mitcje <jeff@ikasan.org>
2020-12-29 23:28:40 +0000

    feature/IKASAN-1734 CLI addition for Ikasan Shell - WIP

--- ikasaneip/cli/shell/src/main/java/org/ikasan/cli/shell/IkasanShell.java
+++ ikasaneip/cli/shell/src/main/java/org/ikasan/cli/shell/IkasanShell.java
@@ -1,5 +1,6 @@
 package org.ikasan.cli.shell;
 
+import org.ikasan.cli.shell.noninteractive.Command;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -14,22 +15,13 @@ import java.util.List;
 @SpringBootApplication
 public class IkasanShell
 {
-    static List<String> supportedScriptCommands = new ArrayList<>(List.of(
-        "start", "start-h2", "start-module",
-        "stop", "stop-h2", "stop-module",
-        "ps"
-        ));
-
     public static void main(String[] args)
     {
-        // append any args for scripting with an "@"
+        // deal with any non-interactive commands passed as args
         List<String> scriptArgs = new ArrayList<>();
         for(String arg:args)
         {
-            if(supportedScriptCommands.contains(arg.toLowerCase()))
-            {
-                scriptArgs.add("@" + arg);
-            }
+            scriptArgs.add( Command.getNonInterative(arg) );
         }
 
         SpringApplication.run( IkasanShell.class, scriptArgs.toArray(String[]::new) );
---
+++ ikasaneip/developer/mvn-archetype/ikasan-standalone-ftp-jms-im/src/main/resources/archetype-resources/bin/env.bat
@@ -0,0 +1,1 @@
+echo "No additional environment settings."
