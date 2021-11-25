f9d03192ed352252f6ab6164f57dba36570e076e
Michael Stewart <mick@humeenterprises.co.uk>
2021-11-25 22:44:48 +0100

    IKASAN-1912 poc for events between dashboard and agents

---
+++ ikasaneip/spec/service/scheduled/src/main/java/org/ikasan/spec/scheduled/ContextParameter.java
@@ -0,0 +1,12 @@
+package org.ikasan.spec.scheduled;
+
+public interface ContextParameter {
+
+    public String getName();
+
+    public void setName(String name);
+
+    public String getType();
+
+    public void setType(String type);
+}
