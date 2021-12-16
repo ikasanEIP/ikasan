a64b90023fa73baa87c261bdac0ea61c778dd988
Michael Stewart <mick@humeenterprises.co.uk>
2021-12-16 10:17:26 +0100

    IKASAN-1912 tidying up the ootb scheduler agent

---
+++ ikasaneip/ootb/module/scheduler-agent/jar/src/main/java/org/ikasan/ootb/scheduler/agent/module/component/converter/configuration/ContextualisedConverterConfiguration.java
@@ -0,0 +1,14 @@
+package org.ikasan.ootb.scheduler.agent.module.component.converter.configuration;
+
+public class ContextualisedConverterConfiguration {
+
+    private String contextId;
+
+    public String getContextId() {
+        return contextId;
+    }
+
+    public void setContextId(String contextId) {
+        this.contextId = contextId;
+    }
+}
