04cac94059821874ff46e0b68c923af74134540f
Michael Stewart <mick@humeenterprises.co.uk>
2021-12-22 18:44:44 +0100

    IKASAN-1912 breaking out more interfaces into the spec for the scheduler and a small amount of refactoring

---
+++ ikasaneip/spec/service/scheduled/src/main/java/org/ikasan/spec/scheduled/context/model/ContextDependency.java
@@ -0,0 +1,22 @@
+package org.ikasan.spec.scheduled.context.model;
+
+import java.util.List;
+
+public interface ContextDependency {
+
+    String getContextIdentifier();
+
+    void setContextIdentifier(String contextIdentifier);
+
+    String getContextDependencyName();
+
+    void setContextDependencyName(String contextDependencyName);
+
+    LogicalGrouping getLogicalGrouping();
+
+    void setLogicalGrouping(LogicalGrouping logicalGrouping);
+
+    List<ContextDependency> getContextDependencies();
+
+    void setContextDependencies(List<ContextDependency> contextDependencies);
+}
