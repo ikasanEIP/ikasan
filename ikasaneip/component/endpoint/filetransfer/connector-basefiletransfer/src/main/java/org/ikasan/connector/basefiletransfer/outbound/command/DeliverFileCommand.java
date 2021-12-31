96b5ff342ad323e48fd76ab36e9282547ca52313
Kieron Edwards <kieron.edwards@uk.mizuho-sc.com>
2021-12-31 02:29:53 +0000

    IKASAN-2032 - Add dontUseFileRename to SftpProducerConfiguraiton, update DeliverFileCommand to not rename if this is set.

--- ikasaneip/component/endpoint/filetransfer/connector-basefiletransfer/src/main/java/org/ikasan/connector/basefiletransfer/outbound/command/DeliverFileCommand.java
+++ ikasaneip/component/endpoint/filetransfer/connector-basefiletransfer/src/main/java/org/ikasan/connector/basefiletransfer/outbound/command/DeliverFileCommand.java
@@ -1,42 +1,40 @@
 /*
- * $Id:$
- * $URL:$
- * 
- * ====================================================================
- * Ikasan Enterprise Integration Platform
- * 
- * Distributed under the Modified BSD License.
- * Copyright notice: The copyright for this software and a full listing 
- * of individual contributors are as shown in the packaged copyright.txt 
- * file. 
- * 
- * All rights reserved.
+ *  ====================================================================
+ *  Ikasan Enterprise Integration Platform
  *
- * Redistribution and use in source and binary forms, with or without 
- * modification, are permitted provided that the following conditions are met:
+ *  Distributed under the Modified BSD License.
+ *  Copyright notice: The copyright for this software and a full listing
+ *  of individual contributors are as shown in the packaged copyright.txt
+ *  file.
  *
- *  - Redistributions of source code must retain the above copyright notice, 
- *    this list of conditions and the following disclaimer.
+ *  All rights reserved.
  *
- *  - Redistributions in binary form must reproduce the above copyright notice, 
- *    this list of conditions and the following disclaimer in the documentation 
- *    and/or other materials provided with the distribution.
+ *  Redistribution and use in source and binary forms, with or without
+ *  modification, are permitted provided that the following conditions are met:
  *
- *  - Neither the name of the ORGANIZATION nor the names of its contributors may
- *    be used to endorse or promote products derived from this software without 
- *    specific prior written permission.
+ *   - Redistributions of source code must retain the above copyright notice,
+ *     this list of conditions and the following disclaimer.
  *
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" 
- * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE 
- * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE 
- * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE 
- * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL 
- * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR 
- * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER 
- * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
- * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE 
- * USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- * ====================================================================
+ *   - Redistributions in binary form must reproduce the above copyright notice,
+ *     this list of conditions and the following disclaimer in the documentation
+ *     and/or other materials provided with the distribution.
+ *
+ *   - Neither the name of the ORGANIZATION nor the names of its contributors may
+ *     be used to endorse or promote products derived from this software without
+ *     specific prior written permission.
+ *
+ *  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ *  AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ *  IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ *  DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+ *  FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ *  DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ *  SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ *  CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ *  OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE
+ *  USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *  ====================================================================
+ *
  */
 package org.ikasan.connector.basefiletransfer.outbound.command;
 
@@ -44,6 +42,7 @@ import java.io.InputStream;
 
 import javax.resource.ResourceException;
 
+import org.apache.commons.lang3.StringUtils;
 import org.apache.commons.lang3.builder.ToStringBuilder;
 import org.ikasan.filetransfer.util.FileUtil;
 import org.slf4j.Logger;
@@ -57,6 +56,8 @@ import org.ikasan.connector.basefiletransfer.net.ClientListEntry;
 import org.ikasan.connector.basefiletransfer.net.FileTransferClient;
 import org.ikasan.connector.listener.TransactionCommitException;
 
+import static org.slf4j.LoggerFactory.getLogger;
+
 /**
  * Delivers a specified payload to a remote directory
  * 
@@ -65,7 +66,7 @@ import org.ikasan.connector.listener.TransactionCommitException;
 public class DeliverFileCommand extends AbstractBaseFileTransferTransactionalResourceCommand
 {
     /** The logger instance. */
-    private static Logger logger = LoggerFactory.getLogger(DeliverFileCommand.class);
+    private static Logger logger = getLogger(DeliverFileCommand.class);
 
     /** we are dealing with pathnames so make sure we stay platform independent */
     final String FILE_SEPARATOR = System.getProperty("file.separator");
@@ -267,6 +268,7 @@ public class DeliverFileCommand extends AbstractBaseFileTransferTransactionalRes
         return false;
     }
 
+
     /**
      * Commit if file delivery is successful: rename file from its temporary name to actual name.
      * This method will not check for duplicate file names - it simply overwrites anything of the same
@@ -282,7 +284,7 @@ public class DeliverFileCommand extends AbstractBaseFileTransferTransactionalRes
         try
         {
             boolean changeDirectory = false;
-            
+            boolean noRename = this.fileName.equals(this.tempFileName);
             logger.info("commit called on this command:" + this + "]"); //$NON-NLS-1$ //$NON-NLS-2$);
             FileTransferClient client = getClient();
             client.ensureConnection();
@@ -304,7 +306,7 @@ public class DeliverFileCommand extends AbstractBaseFileTransferTransactionalRes
                  * and overwrite flag is false is handled at the execute stage; an exception would be thrown
                  * and we never reach this stage.
                  */
-                if (this.overwriteExisting && this.fileExists(this.fileName))
+                if (this.overwriteExisting && this.fileExists(this.fileName) && !noRename)
                 {
                     logger.debug("Deleting existing file of the same name as the one we are delivering [" //$NON-NLS-1$
                             + this.fileName + "]");
@@ -315,9 +317,11 @@ public class DeliverFileCommand extends AbstractBaseFileTransferTransactionalRes
             {
                 logger.warn(e.getMessage(),e);
             }
-
-            renameFile(this.tempFileName, this.fileName);
-
+            if (!noRename) {
+                renameFile(this.tempFileName, this.fileName);
+            } else {
+                logger.info("tempFileName and fileName are the same so will NOT rename file");
+            }
             if(changeDirectory)
             {
                 changeDirectory(originalDirectory);
