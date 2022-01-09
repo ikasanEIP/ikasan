5ff4bfadbf35250c602916dd3e0f190c282ba99d
Andrzej Majewski <andr2ot@gmail.com>
2022-01-09 20:07:53 +0000

    IKASAN-2038 - Add SftpChunkingToJavaFlow

---
+++ ikasaneip/sample/spring-boot/sftp-jms/src/main/java/com/ikasan/sample/spring/boot/builderpattern/SftpChunkingToJmsFlow.java
@@ -0,0 +1,40 @@
+package com.ikasan.sample.spring.boot.builderpattern;
+
+import org.ikasan.builder.BuilderFactory;
+import org.ikasan.component.converter.filetransfer.PayloadToMapConverter;
+import org.ikasan.spec.component.endpoint.Consumer;
+import org.ikasan.spec.component.endpoint.Producer;
+import org.ikasan.spec.flow.Flow;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.annotation.Resource;
+
+@Configuration("SftpChunkingToJmsFlowFactory")
+public class SftpChunkingToJmsFlow
+{
+    @Resource
+    private BuilderFactory builderFactory;
+
+    @Value("${module.name}")
+    private String moduleName;
+
+    @Resource
+    private Consumer sftpChunkingConsumer;
+
+    @Resource
+    private Producer jmsChunkingProducer;
+
+    @Bean
+    public Flow sftpChunkingToJmsFlow()
+    {
+
+        return builderFactory.getModuleBuilder(moduleName)
+                             .getFlowBuilder("Sftp Chunking To Jms Flow").withDescription("Sftp Chunking to Jms")
+                             .consumer("Sftp Chunking Consumer", sftpChunkingConsumer)
+                             .converter("Sftp Payload to Map Converter", new PayloadToMapConverter())
+                             .producer("Sftp Chunking Jms Producer", jmsChunkingProducer).build();
+    }
+
+}
---
+++ ikasaneip/sample/spring-boot/sftp-jms/src/main/java/com/ikasan/sample/spring/boot/builderpattern/SftpChunkingToJmsFlowComponentFactory.java
@@ -0,0 +1,63 @@
+package com.ikasan.sample.spring.boot.builderpattern;
+
+import org.apache.activemq.ActiveMQXAConnectionFactory;
+import org.ikasan.builder.BuilderFactory;
+import org.ikasan.component.endpoint.jms.spring.producer.SpringMessageProducerConfiguration;
+import org.ikasan.endpoint.sftp.consumer.SftpConsumerConfiguration;
+import org.ikasan.spec.component.endpoint.Consumer;
+import org.ikasan.spec.component.endpoint.Producer;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.annotation.Resource;
+import javax.jms.ConnectionFactory;
+
+@Configuration
+public class SftpChunkingToJmsFlowComponentFactory
+{
+    @Resource
+    private BuilderFactory builderFactory;
+
+    @Value("${jms.provider.url}")
+    private String brokerUrl;
+
+    @Bean
+    @ConfigurationProperties(prefix = "sftp.chunking.to.jms.flow.sftp.consumer")
+    public SftpConsumerConfiguration sftpChunkingConsumerConfiguration()
+    {
+        return new SftpConsumerConfiguration();
+    }
+
+    @Bean
+    public Consumer sftpChunkingConsumer(SftpConsumerConfiguration sftpChunkingConsumerConfiguration)
+    {
+        return builderFactory.getComponentBuilder()
+                             .sftpConsumer()
+                             .setConfiguration(sftpChunkingConsumerConfiguration)
+                             .setConfiguredResourceId("SftpChunkingConsumer")
+                             .setScheduledJobGroupName("SftpChunkingConsumer")
+                             .setScheduledJobName("SftpChunkingConsumer").build();
+    }
+
+    @Bean
+    @ConfigurationProperties(prefix = "sftp.chunking.to.jms.flow.jms.producer")
+
+    public SpringMessageProducerConfiguration jmsChunkingProducerConfiguration()
+    {
+        return new SpringMessageProducerConfiguration();
+    }
+
+
+    @Bean
+    public Producer jmsChunkingProducer(SpringMessageProducerConfiguration jmsChunkingProducerConfiguration)
+    {
+        ConnectionFactory producerConnectionFactory = new ActiveMQXAConnectionFactory(brokerUrl);
+
+        return builderFactory.getComponentBuilder().jmsProducer().setConnectionFactory(producerConnectionFactory)
+                             .setConfiguration(jmsChunkingProducerConfiguration)
+                             .setConfiguredResourceId("sftpChunkingJmsProducer").build();
+    }
+
+}
