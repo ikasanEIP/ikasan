f3d6d24a26b7e0971dbab0bc61f4e063287f997c
Andrzej Majewski <andr2ot@gmail.com>
2022-01-09 15:53:41 +0000

    IKASAN-2038 - refactor existing SFTP flows to use ConfigurationProperties

---
+++ ikasaneip/sample/spring-boot/sftp-jms/src/main/java/com/ikasan/sample/spring/boot/builderpattern/SftpToJmsFlow.java
@@ -0,0 +1,40 @@
+package com.ikasan.sample.spring.boot.builderpattern;
+
+import org.ikasan.builder.BuilderFactory;
+import org.ikasan.component.converter.filetransfer.PayloadToMapConverter;
+import org.ikasan.spec.component.endpoint.Consumer;
+import org.ikasan.spec.component.endpoint.Producer;
+import org.ikasan.spec.flow.Flow;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.annotation.Resource;
+
+@Configuration("SftpToJmsFlowFactory")
+public class SftpToJmsFlow
+{
+    @Resource
+    private BuilderFactory builderFactory;
+
+    @Value("${module.name}")
+    private String moduleName;
+
+    @Resource
+    private Consumer sftpConsumer;
+
+    @Resource
+    private Producer jmsProducer;
+
+    @Bean
+    public Flow sftpToJmsFlow()
+    {
+
+        return builderFactory.getModuleBuilder(moduleName)
+                             .getFlowBuilder("Sftp To Jms Flow").withDescription("Sftp to Jms")
+                             .consumer("Sftp Consumer", sftpConsumer)
+                             .converter("Sftp Payload to Map Converter", new PayloadToMapConverter())
+                             .producer("Sftp Jms Producer", jmsProducer).build();
+    }
+
+}
