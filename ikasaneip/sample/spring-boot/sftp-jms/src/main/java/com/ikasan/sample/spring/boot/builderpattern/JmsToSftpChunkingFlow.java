8707a4f8ca45a3835116d604d33357301cfa2657
Andrzej Majewski <andr2ot@gmail.com>
2022-01-09 22:47:17 +0000

    IKASAN-2038 - Add JmsToSftpChunkingFlow

---
+++ ikasaneip/sample/spring-boot/sftp-jms/src/main/java/com/ikasan/sample/spring/boot/builderpattern/JmsToSftpChunkingFlow.java
@@ -0,0 +1,44 @@
+package com.ikasan.sample.spring.boot.builderpattern;
+
+import org.ikasan.builder.BuilderFactory;
+import org.ikasan.component.converter.filetransfer.MapMessageToPayloadConverter;
+import org.ikasan.spec.component.endpoint.Consumer;
+import org.ikasan.spec.component.endpoint.Producer;
+import org.ikasan.spec.flow.Flow;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.annotation.Resource;
+
+@Configuration("JmsToSftpChunkingFlowFactory")
+public class JmsToSftpChunkingFlow
+{
+    @Resource
+    private BuilderFactory builderFactory;
+
+    @Value("${module.name}")
+    private String moduleName;
+
+    @Resource
+    private Consumer jmsSftpChunkingConsumer;
+
+    @Resource
+    private Producer sftpChunkingProducer;
+
+    @Bean
+    public Flow jmsToSftpChunkingFlow()
+    {
+
+        return builderFactory.getModuleBuilder(moduleName)
+                             .getFlowBuilder("Jms To Sftp Chunking Flow")
+            .withDescription("Receives Map Jms message Representing Chunked SFTP content sends it to sftp as file")
+            .consumer("Sftp Jms Consumer", jmsSftpChunkingConsumer)
+            .converter("MapMessage to SFTP Payload Converter",new MapMessageToPayloadConverter())
+            .producer("Sftp Producer", sftpChunkingProducer)
+            .build();
+
+
+    }
+
+}
