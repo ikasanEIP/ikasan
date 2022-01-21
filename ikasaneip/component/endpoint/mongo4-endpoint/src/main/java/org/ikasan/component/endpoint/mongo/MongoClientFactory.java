00f9a0354993381439731d4aee3c080245a6467d
Alex Mihai <Alex.Mihai@uk.mizuho-sc.com>
2022-01-21 17:34:40 +0200

    IKASAN-2045 - Ikasan-mongo-endpoint changes to support connecting to mongo instances in the cloud using driver version 3.12.10 - add module for mongo driver 4.4.x

--- ikasaneip/component/endpoint/mongo4-endpoint/src/main/java/org/ikasan/component/endpoint/mongo/MongoClientFactory.java
+++ ikasaneip/component/endpoint/mongo4-endpoint/src/main/java/org/ikasan/component/endpoint/mongo/MongoClientFactory.java
@@ -1,8 +1,7 @@
 package org.ikasan.component.endpoint.mongo;
 
-import com.mongodb.MongoClient;
-import com.mongodb.MongoClientOptions;
-import com.mongodb.MongoClientURI;
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoClients;
 
 import java.util.List;
 
@@ -11,6 +10,7 @@ import java.util.List;
  */
 public class MongoClientFactory
 {
+
     private MongoClientFactory()
     {
         // no instances
@@ -28,11 +28,7 @@ public class MongoClientFactory
     {
         configuration.validate();
 
-        String url = buildUrl(configuration);
-
-        MongoClientOptions.Builder mongoClientOptionsBuilder = mongoClientOptionsBuilder(configuration);
-
-        return new MongoClient(new MongoClientURI(url, mongoClientOptionsBuilder));
+        return MongoClients.create(buildUrl(configuration));
     }
 
     private static String buildUrl(MongoClientConfiguration configuration)
@@ -59,121 +55,28 @@ public class MongoClientFactory
 
         url += String.join(",", connectionUrls) + "/" + configuration.getDatabaseName();
 
+        url += "?ssl=" + falseIfNull(configuration.getSslEnabled());
+
         if (configuration.isAuthenticated() != null && configuration.isAuthenticated())
         {
-            url += "?authSource=" + getAuthDatabase(configuration);
+            url += "&authSource=" + getAuthDatabase(configuration);
         }
 
         return url;
     }
 
+    private static boolean falseIfNull(Boolean bool) {
+        if (bool == null) {
+            return false;
+        }
+
+        return bool;
+    }
+
     private static String getAuthDatabase(MongoClientConfiguration configuration)
     {
         return configuration.getAuthDatabaseName() != null ?
-                configuration.getAuthDatabaseName() :
-                configuration.getDatabaseName();
-    }
-
-    /**
-     * Mongo option builder method
-     */
-    private static MongoClientOptions.Builder mongoClientOptionsBuilder(MongoClientConfiguration configuration)
-    {
-        MongoClientOptions.Builder builder = MongoClientOptions.builder();
-        if (configuration.getLocalThreshold() != null)
-        {
-            builder.localThreshold(configuration.getLocalThreshold());
-        }
-        if (configuration.getAlwaysUseMBeans() != null)
-        {
-            builder.alwaysUseMBeans(configuration.getAlwaysUseMBeans());
-        }
-        if (configuration.getConnectionsPerHost() != null)
-        {
-            builder.connectionsPerHost(configuration.getConnectionsPerHost());
-        }
-        if (configuration.getConnectionTimeout() != null)
-        {
-            builder.connectTimeout(configuration.getConnectionTimeout());
-        }
-        if (configuration.getCursorFinalizerEnabled() != null)
-        {
-            builder.cursorFinalizerEnabled(configuration.getCursorFinalizerEnabled());
-        }
-        if (configuration.getDescription() != null)
-        {
-            builder.description(configuration.getDescription());
-        }
-        if (configuration.getMinHeartbeatFrequency() != null)
-        {
-            builder.minHeartbeatFrequency(configuration.getMinHeartbeatFrequency());
-        }
-        if (configuration.getHeartbeatConnectTimeout() != null)
-        {
-            builder.heartbeatConnectTimeout(configuration.getHeartbeatConnectTimeout());
-        }
-        if (configuration.getHeartbeatFrequency() != null)
-        {
-            builder.heartbeatFrequency(configuration.getHeartbeatFrequency());
-        }
-        if (configuration.getHeartbeatSocketTimeout() != null)
-        {
-            builder.heartbeatSocketTimeout(configuration.getHeartbeatSocketTimeout());
-        }
-        if (configuration.getLegacyDefaults() != null && configuration.getLegacyDefaults())
-        {
-            builder.legacyDefaults();
-        }
-        if (configuration.getMaxConnectionIdleTime() != null)
-        {
-            builder.maxConnectionIdleTime(configuration.getMaxConnectionIdleTime());
-        }
-        if (configuration.getMaxConnectionLifeTime() != null)
-        {
-            builder.maxConnectionLifeTime(configuration.getMaxConnectionLifeTime());
-        }
-        if (configuration.getMaxWaitTime() != null)
-        {
-            builder.maxWaitTime(configuration.getMaxWaitTime());
-        }
-        if (configuration.getSocketTimeout() != null)
-        {
-            builder.socketTimeout(configuration.getSocketTimeout());
-        }
-        if (configuration.getMinConnectionsPerHost() != null)
-        {
-            builder.minConnectionsPerHost(configuration.getMinConnectionsPerHost());
-        }
-        if (configuration.getRequiredReplicaSetName() != null)
-        {
-            builder.requiredReplicaSetName(configuration.getRequiredReplicaSetName());
-        }
-        if (configuration.getSocketKeepAlive() != null)
-        {
-            builder.socketKeepAlive(configuration.getSocketKeepAlive());
-        }
-        if (configuration.getThreadsAllowedToBlockForConnectionMultiplier() != null)
-        {
-            builder.threadsAllowedToBlockForConnectionMultiplier(configuration
-                    .getThreadsAllowedToBlockForConnectionMultiplier());
-        }
-        if (configuration.getReadPreference() != null)
-        {
-            builder.readPreference(configuration.getReadPreference());
-        }
-        if (configuration.getWriteConcern() != null)
-        {
-            builder.writeConcern(configuration.getWriteConcern());
-        }
-        if (configuration.getSslEnabled() != null)
-        {
-            builder.sslEnabled(configuration.getSslEnabled());
-        }
-        if (configuration.getSslInvalidHostNameAllowed() != null)
-        {
-            builder.sslInvalidHostNameAllowed(configuration.getSslInvalidHostNameAllowed());
-        }
-
-        return builder;
+            configuration.getAuthDatabaseName() :
+            configuration.getDatabaseName();
     }
 }
