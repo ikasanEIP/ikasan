3c3c48b464e56092571a5b62d7c14734beab648c
Michael Stewart <mick@humeenterprises.co.uk>
2022-01-28 19:26:48 +0000

    IKASAN-1912 expanding jobs to contain all component configurations relevant to the endpoint they are leveraging.

--- ikasaneip/spec/service/scheduled/src/main/java/org/ikasan/spec/scheduled/job/model/QuartzScheduleDrivenJob.java
+++ ikasaneip/spec/service/scheduled/src/main/java/org/ikasan/spec/scheduled/job/model/QuartzScheduleDrivenJob.java
@@ -1,5 +1,7 @@
 package org.ikasan.spec.scheduled.job.model;
 
+import java.util.Map;
+
 public interface QuartzScheduleDrivenJob extends SchedulerJob {
 
     String getJobGroup();
@@ -13,4 +15,28 @@ public interface QuartzScheduleDrivenJob extends SchedulerJob {
     String getTimeZone();
 
     void setTimeZone(String timeZone);
+
+    Map<String, String> getPassthroughProperties();
+
+    void setPassthroughProperties(Map<String, String> passthroughProperties);
+
+    boolean isEager();
+
+    int getMaxEagerCallbacks();
+
+    void setMaxEagerCallbacks(int maxEagerCallbacks);
+
+    void setEager(boolean eager);
+
+    void setIgnoreMisfire(boolean ignoreMisfire);
+
+    boolean isIgnoreMisfire();
+
+    long getRecoveryTolerance();
+
+    void setRecoveryTolerance(long recoveryTolerance);
+
+    boolean isPersistentRecovery();
+
+    void setPersistentRecovery(boolean persistentRecovery);
 }
