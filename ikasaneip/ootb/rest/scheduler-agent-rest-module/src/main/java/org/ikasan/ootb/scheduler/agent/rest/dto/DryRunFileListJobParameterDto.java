61f7a84dcc51c25ccbf5ba0e23b02bfa0010ee6a
John Otaegui <johnot@gmail.com>
2022-03-03 15:50:13 +1100

    IKASAN-2067 add in rest points for accepting dry run and list of files to dry run.

---
+++ ikasaneip/ootb/rest/scheduler-agent-rest-module/src/main/java/org/ikasan/ootb/scheduler/agent/rest/dto/DryRunFileListJobParameterDto.java
@@ -0,0 +1,35 @@
+package org.ikasan.ootb.scheduler.agent.rest.dto;
+
+import org.apache.commons.lang3.builder.ToStringBuilder;
+import org.apache.commons.lang3.builder.ToStringStyle;
+import org.ikasan.spec.scheduled.event.model.DryRunFileListJobParameter;
+
+public class DryRunFileListJobParameterDto implements DryRunFileListJobParameter {
+    private String jobName;
+    private String fileName;
+
+    @Override
+    public void setJobName(String jobName) {
+        this.jobName = jobName;
+    }
+
+    @Override
+    public String getJobName() {
+        return jobName;
+    }
+
+    @Override
+    public void setFileName(String fileName) {
+        this.fileName = fileName;
+            }
+
+    @Override
+    public String getFileName() {
+        return fileName;
+    }
+
+    @Override
+    public String toString() {
+        return ToStringBuilder.reflectionToString(this, ToStringStyle.SHORT_PREFIX_STYLE);
+    }
+}
