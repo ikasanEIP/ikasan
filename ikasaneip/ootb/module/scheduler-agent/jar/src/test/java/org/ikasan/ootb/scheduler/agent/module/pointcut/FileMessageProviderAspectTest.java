dd986da9ea7a60d3294d611c82fa4c6a15db7016
John Otaegui <johnot@gmail.com>
2022-03-03 22:34:27 +1100

    IKASAN-2067 fix tests to use primary bean and use test config. More WIP.

--- ikasaneip/ootb/module/scheduler-agent/jar/src/test/java/org/ikasan/ootb/scheduler/agent/module/pointcut/FileMessageProviderAspectTest.java
+++ ikasaneip/ootb/module/scheduler-agent/jar/src/test/java/org/ikasan/ootb/scheduler/agent/module/pointcut/FileMessageProviderAspectTest.java
@@ -3,31 +3,49 @@ package org.ikasan.ootb.scheduler.agent.module.pointcut;
 import static org.mockito.Mockito.*;
 
 import org.aspectj.lang.ProceedingJoinPoint;
+import org.ikasan.ootb.scheduler.agent.module.configuration.SchedulerAgentConfiguredModuleConfiguration;
+import org.ikasan.spec.configuration.ConfiguredResource;
+import org.ikasan.spec.flow.Flow;
+import org.ikasan.spec.module.Module;
+import org.ikasan.spec.module.ModuleService;
 import org.junit.Before;
 import org.junit.Ignore;
 import org.junit.Test;
-import org.junit.runner.RunWith;
 import org.mockito.Mock;
-import org.mockito.junit.MockitoJUnitRunner;
 import org.springframework.test.util.ReflectionTestUtils;
 
-@RunWith(MockitoJUnitRunner.class)
 public class FileMessageProviderAspectTest {
 
     @Mock
+    private ModuleService moduleService;
+
+    @Mock
+    private Module module;
+
+    @Mock
     private ProceedingJoinPoint proceedingJoinPoint;
+
+    @Mock
+    private SchedulerAgentConfiguredModuleConfiguration schedulerAgentConfiguredModuleConfiguration;
+
+    @Mock
+    private ConfiguredResource<SchedulerAgentConfiguredModuleConfiguration> configuredModule;
 
     private FileMessageProviderAspect fileMessageProviderAspect;
 
     @Before
     public void setUp() {
         fileMessageProviderAspect = new FileMessageProviderAspect();
+        ReflectionTestUtils.setField(fileMessageProviderAspect, "moduleName", "scheduler-agent");
+        ReflectionTestUtils.setField(fileMessageProviderAspect, "moduleService", moduleService);
     }
 
     @Test
     @Ignore
     public void shouldRunProceedingJoinPointIfNotInDryRunMode() throws Throwable {
-        ReflectionTestUtils.setField(fileMessageProviderAspect, "dryRunMode", false);
+        when(moduleService.getModule("scheduler-agent")).thenReturn(module);
+        when(((ConfiguredResource<SchedulerAgentConfiguredModuleConfiguration>) (Module<Flow>) moduleService.getModule("scheduler-agent")).getConfiguration()).thenReturn(schedulerAgentConfiguredModuleConfiguration);
+        when(schedulerAgentConfiguredModuleConfiguration.isDryRunMode()).thenReturn(false);
 
         fileMessageProviderAspect.fileMessageProviderInvoke(proceedingJoinPoint);
 
