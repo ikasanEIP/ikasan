3e0111489936baec9d6622ea274c63ab9eead33e
John Otaegui <johnot@gmail.com>
2022-03-04 22:24:27 +1100

    IKASAN-2067 make aspect more robus and dry service run simpler. More tests.

--- ikasaneip/ootb/module/scheduler-agent/jar/src/test/java/org/ikasan/ootb/scheduler/agent/module/pointcut/FileMessageProviderAspectTest.java
+++ ikasaneip/ootb/module/scheduler-agent/jar/src/test/java/org/ikasan/ootb/scheduler/agent/module/pointcut/FileMessageProviderAspectTest.java
@@ -1,14 +1,17 @@
 package org.ikasan.ootb.scheduler.agent.module.pointcut;
 
 import static org.mockito.Mockito.*;
+import static org.quartz.TriggerBuilder.newTrigger;
 
 import org.aspectj.lang.ProceedingJoinPoint;
+import org.ikasan.serialiser.model.JobExecutionContextDefaultImpl;
 import org.ikasan.spec.scheduled.dryrun.DryRunModeService;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.MockitoJUnitRunner;
+import org.quartz.Trigger;
 
 @RunWith(MockitoJUnitRunner.class)
 public class FileMessageProviderAspectTest {
@@ -28,16 +31,25 @@ public class FileMessageProviderAspectTest {
 
         fileMessageProviderAspect.fileMessageProviderInvoke(proceedingJoinPoint);
 
+        verify(dryRunModeService).getDryRunMode();
         verify(proceedingJoinPoint, times(1)).proceed();
         verifyNoMoreInteractions(proceedingJoinPoint);
     }
 
     @Test
     public void shouldRunProceedingJoinPointInDryRunMode() throws Throwable {
+        JobExecutionContextDefaultImpl context = new JobExecutionContextDefaultImpl();
+        Trigger trigger = newTrigger().withIdentity("Job Name", "Job Group").build();
+        context.setTrigger(trigger);
+        Object[] args = {context};
+
         when(dryRunModeService.getDryRunMode()).thenReturn(true);
+        when(proceedingJoinPoint.getArgs()).thenReturn(args);
 
         fileMessageProviderAspect.fileMessageProviderInvoke(proceedingJoinPoint);
 
+        verify(dryRunModeService).getDryRunMode();
+        verify(proceedingJoinPoint).getArgs();
         verifyNoMoreInteractions(proceedingJoinPoint);
     }
 }
