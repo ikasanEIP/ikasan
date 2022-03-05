a7363be5aa577658afbc8dce9c384415d6b9c2ce
Michael Stewart <mick@humeenterprises.co.uk>
2022-03-05 13:22:06 +0000

    IKASAN-2069 adding child context ids to jobs

--- ikasaneip/spec/service/scheduled/src/main/java/org/ikasan/spec/scheduled/job/model/SchedulerJob.java
+++ ikasaneip/spec/service/scheduled/src/main/java/org/ikasan/spec/scheduled/job/model/SchedulerJob.java
@@ -1,27 +1,102 @@
 package org.ikasan.spec.scheduled.job.model;
 
+import java.util.List;
+
 public interface SchedulerJob {
+
+    /**
+     * Get the parent context id that this job belongs to.
+     * @return
+     */
     String getContextId();
 
+    /**
+     * Set the parent context id that this job belongs to.
+     * @param contextId
+     */
     void setContextId(String contextId);
 
+    /**
+     * Get the child context ids that this job belongs to. Contexts can appear within contexts.
+     *
+     * @return
+     */
+    List<String> getChildContextIds();
+
+    /**
+     * Set the child context ids that this job belongs to. Contexts can appear within contexts.
+     *
+     * @param contextIds
+     */
+    void setChildContextIds(List<String> contextIds);
+
+    /**
+     * Get the unique identifier for this job.
+     *
+     * @return
+     */
     String getIdentifier();
 
+    /**
+     * Set the unique identifier for this job.
+     *
+     * @param jobIdentifier
+     */
     void setIdentifier(String jobIdentifier);
 
+    /**
+     * Get the name of this agent that the job runs on.
+     *
+     * @return
+     */
     String getAgentName();
 
+    /**
+     * Set the name of the agent that this job runs on.
+     *
+     * @param agentName
+     */
     void setAgentName(String agentName);
 
+    /**
+     * Get the name of this job.
+     *
+     * @return
+     */
     String getJobName();
 
+    /**
+     * Set the name of this job.
+     *
+     * @param jobName
+     */
     void setJobName(String jobName);
 
+    /**
+     * Get the description of this job.
+     *
+     * @return
+     */
     String getJobDescription();
 
+    /**
+     * Set the description of this job.
+     *
+     * @param jobDescription
+     */
     void setJobDescription(String jobDescription);
 
+    /**
+     * Get the start up control type of this job.
+     *
+     * @return
+     */
     String getStartupControlType();
 
+    /**
+     * Set the start up control of this job.
+     *
+     * @param startupControlType
+     */
     void setStartupControlType(String startupControlType);
 }
