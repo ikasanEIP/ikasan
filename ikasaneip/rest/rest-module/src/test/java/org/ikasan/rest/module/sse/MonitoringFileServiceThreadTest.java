a2ddf488a6e5ab284ef317f819074c52c4fc30ca
John Otaegui <john@waddle.com.au>
2022-03-07 22:27:41 +1100

    IKASAN-2072 another branch to make merge easier

--- ikasaneip/rest/rest-module/src/test/java/org/ikasan/rest/module/sse/MonitoringFileServiceThreadTest.java
+++ ikasaneip/rest/rest-module/src/test/java/org/ikasan/rest/module/sse/MonitoringFileServiceThreadTest.java
@@ -7,30 +7,33 @@ import static org.mockito.Mockito.*;
 
 import java.io.File;
 import java.io.IOException;
-import java.nio.channels.FileChannel;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Paths;
-import java.nio.file.StandardOpenOption;
 import java.util.List;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.AtomicLong;
 
 import org.apache.commons.io.FileUtils;
+import org.junit.After;
 import org.junit.Before;
-import org.junit.Ignore;
 import org.junit.Test;
 import org.springframework.test.util.ReflectionTestUtils;
 import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
 
 public class MonitoringFileServiceThreadTest {
 
-    private SseEmitter sseEmitter = mock(SseEmitter.class);
+    private final SseEmitter sseEmitter = mock(SseEmitter.class);
 
-    private final String sampleLogFileStr = "src/test/resources/data/log.sample";
+    private final String sampleLogFileStr = "target/tmp/data/log.sample";
 
     @Before
     public void setup() throws IOException {
-        FileChannel.open(Paths.get(sampleLogFileStr), StandardOpenOption.WRITE).truncate(0).close();
+        FileUtils.write(new File(sampleLogFileStr), "", StandardCharsets.UTF_8);
+    }
+
+    @After
+    public void tearDown() throws IOException {
+        FileUtils.forceDelete(new File(sampleLogFileStr));
     }
 
     @Test
@@ -46,36 +49,32 @@ public class MonitoringFileServiceThreadTest {
     }
 
     @Test
-    @Ignore
     public void test_monitoringService_monitorsForMessages_should_reset_atomic_count_if_file_deleted_and_send_new_messages() throws Exception {
-        // create new file
-        String pathname = "src/test/resources/data/log1.sample";
-        File newFile = new File(pathname);
-        FileUtils.write(newFile, "111", StandardCharsets.UTF_8);
 
-        // start the thread
-        MonitoringFileServiceThread monitoringFileService = new MonitoringFileServiceThread(pathname, sseEmitter, 100, 300000);
+        FileUtils.writeLines(Paths.get(sampleLogFileStr).toFile(), List.of("111"), true);
+        MonitoringFileServiceThread monitoringFileService = new MonitoringFileServiceThread(sampleLogFileStr, sseEmitter, 100, 300000);
         monitoringFileService.start();
 
         // verify has sent message 1
-        verifySseEmiterAndCounter(1, 3, monitoringFileService);
+        verifySseEmiterAndCounter(1, 4, monitoringFileService);
 
         // add another line
-        FileUtils.writeLines(Paths.get(pathname).toFile(), List.of("222"), true);
+        FileUtils.writeLines(Paths.get(sampleLogFileStr).toFile(), List.of("222"), true);
 
         // verify has sent message 2
-        verifySseEmiterAndCounter(2, 7, monitoringFileService);
+        verifySseEmiterAndCounter(2, 8, monitoringFileService);
 
         // recreate the file
-        FileUtils.forceDelete(new File(pathname));
-        newFile = new File(pathname);
-        FileUtils.write(newFile, "", StandardCharsets.UTF_8);
+        FileUtils.forceDelete(new File(sampleLogFileStr));
+        // this sleep is so we can run the tests on macs - takes a while for the delete to propagate events
+        Thread.sleep(15000);
 
+        FileUtils.write(new File(sampleLogFileStr), "", StandardCharsets.UTF_8);
         // wait until counter is reset to 0
         verifySseEmiterAndCounter(2, 0, monitoringFileService);
 
         // add another line
-        FileUtils.writeLines(Paths.get(pathname).toFile(), List.of("333"), true);
+        FileUtils.writeLines(Paths.get(sampleLogFileStr).toFile(), List.of("333"), true);
 
         // verify has sent message 3
         verifySseEmiterAndCounter(3, 4, monitoringFileService);
@@ -83,13 +82,9 @@ public class MonitoringFileServiceThreadTest {
         assertEquals(monitoringFileService.getState(), Thread.State.TIMED_WAITING);
 
         monitoringFileService.interrupt();
-
-        // clean up
-        FileUtils.forceDelete(new File(pathname));
     }
 
     @Test
-    @Ignore
     public void test_monitoringService_monitors_for_new_messages() throws IOException {
 
         MonitoringFileServiceThread monitoringFileService = new MonitoringFileServiceThread(sampleLogFileStr, sseEmitter, 100, 300000);
@@ -133,9 +128,10 @@ public class MonitoringFileServiceThreadTest {
 
     private void verifySseEmiterAndCounter(int seeEmitterCount, int fileMessageCounter, Thread service) {
         with().pollInterval(1, TimeUnit.SECONDS).and().with().pollDelay(1, TimeUnit.SECONDS).await()
-            .atMost(5, TimeUnit.SECONDS).untilAsserted(() -> {
+            .atMost(30, TimeUnit.SECONDS).untilAsserted(() -> {
                 verify(sseEmitter, times(seeEmitterCount)).send(any(SseEmitter.SseEventBuilder.class));
-                assertEquals(fileMessageCounter, ((AtomicLong) ReflectionTestUtils.getField(service, "counter")).get());
+                long counter = ((AtomicLong) ReflectionTestUtils.getField(service, "counter")).get();
+                assertEquals(fileMessageCounter, counter);
             });
     }
 }
