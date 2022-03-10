0167015e6a71fb5c8eeda2bb67f798c635e2de68
gulser <Serkan.Guler2@uk.mizuho-sc.com>
2022-03-10 13:54:04 +0000

    IKASAN-2075 initial commit

---
+++ ikasaneip/ootb/module/scheduler-agent/jar/src/main/java/org/ikasan/ootb/scheduler/agent/module/configuration/HousekeepLogFilesProcessConfiguration.java
@@ -0,0 +1,59 @@
+package org.ikasan.ootb.scheduler.agent.module.configuration;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class HousekeepLogFilesProcessConfiguration {
+
+    @Value( "${housekeep.log.files.process.log-folder}" )
+    private String logFolder;
+    @Value( "${housekeep.log.files.process.ttl}" )
+    private int timeToLive = 30;
+    @Value( "${housekeep.log.files.process.should-archive}" )
+    private boolean shouldArchive = false;
+    @Value( "${housekeep.log.files.process.should-move}" )
+    private boolean shouldMove = false;
+    @Value( "${housekeep.log.files.process.move-folder}" )
+    private String folderToMove;
+
+    public String getLogFolder() {
+        return logFolder;
+    }
+
+    public void setLogFolder(String logFolder) {
+        this.logFolder = logFolder;
+    }
+
+    public int getTimeToLive() {
+        return timeToLive;
+    }
+
+    public void setTimeToLive(int timeToLive) {
+        this.timeToLive = timeToLive;
+    }
+
+    public boolean isShouldArchive() {
+        return shouldArchive;
+    }
+
+    public void setShouldArchive(boolean shouldArchive) {
+        this.shouldArchive = shouldArchive;
+    }
+
+    public boolean isShouldMove() {
+        return shouldMove;
+    }
+
+    public void setShouldMove(boolean shouldMove) {
+        this.shouldMove = shouldMove;
+    }
+
+    public String getFolderToMove() {
+        return folderToMove;
+    }
+
+    public void setFolderToMove(String folderToMove) {
+        this.folderToMove = folderToMove;
+    }
+}
