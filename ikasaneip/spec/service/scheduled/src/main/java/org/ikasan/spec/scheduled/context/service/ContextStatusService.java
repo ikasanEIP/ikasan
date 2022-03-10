e889e6eb6355aef36d52bc1d9c802b8e36809a77
John Otaegui <john@waddle.com.au>
2022-03-11 09:35:35 +1100

    IKSAN-2076 MSM-158 interface for context status service.

---
+++ ikasaneip/spec/service/scheduled/src/main/java/org/ikasan/spec/scheduled/context/service/ContextStatusService.java
@@ -0,0 +1,9 @@
+package org.ikasan.spec.scheduled.context.service;
+
+public interface ContextStatusService {
+
+    String getContextStatus(String instanceName, String contextName);
+
+    String getContextStatusForJob(String instanceName, String contextName, String agentName, String jobName);
+
+}
