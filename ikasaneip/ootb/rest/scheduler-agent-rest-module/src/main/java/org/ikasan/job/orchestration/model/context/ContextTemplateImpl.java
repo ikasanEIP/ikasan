ad85f58d6cabd869c02819c9cb8d018a52893923
John Otaegui <john@waddle.com.au>
2022-03-24 02:32:09 +1100

    IKASAN-2079 Initial cur of job lock cache for dashboard interfaces.

--- ikasaneip/ootb/rest/scheduler-agent-rest-module/src/main/java/org/ikasan/job/orchestration/model/context/ContextTemplateImpl.java
+++ ikasaneip/ootb/rest/scheduler-agent-rest-module/src/main/java/org/ikasan/job/orchestration/model/context/ContextTemplateImpl.java
@@ -2,8 +2,8 @@ package org.ikasan.job.orchestration.model.context;
 
 import org.ikasan.spec.scheduled.context.model.ContextParameter;
 import org.ikasan.spec.scheduled.context.model.ContextTemplate;
+import org.ikasan.spec.scheduled.job.model.JobLock;
 import org.ikasan.spec.scheduled.job.model.SchedulerJob;
 
-public class ContextTemplateImpl extends ContextImpl<ContextTemplate, ContextParameter, SchedulerJob> implements ContextTemplate {
-
+public class ContextTemplateImpl extends ContextImpl<ContextTemplate, ContextParameter, SchedulerJob, JobLock> implements ContextTemplate {
 }
