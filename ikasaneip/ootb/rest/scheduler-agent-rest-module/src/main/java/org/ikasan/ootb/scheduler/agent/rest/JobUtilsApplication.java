beae4b66a97b2556e612ed32ec2505d7e9b1d1cc
gulser <Serkan.Guler2@uk.mizuho-sc.com>
2022-03-28 13:14:48 +0100

    IKASAN-2080 adding new endpoint to kill a pid

---
+++ ikasaneip/ootb/rest/scheduler-agent-rest-module/src/main/java/org/ikasan/ootb/scheduler/agent/rest/JobUtilsApplication.java
@@ -0,0 +1,54 @@
+package org.ikasan.ootb.scheduler.agent.rest;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
+
+@RequestMapping("/rest/jobUtils")
+@RestController
+public class JobUtilsApplication
+{
+    /** logger */
+    private static Logger logger = LoggerFactory.getLogger(JobUtilsApplication.class);
+
+
+    /**
+     * Allows to kill a job by a given pid.
+     */
+    @RequestMapping(method = RequestMethod.GET,
+                    value = "/kill/{pid}",
+                    produces = { MediaType.APPLICATION_JSON_VALUE })
+    @PreAuthorize("hasAnyAuthority('ALL','WebServiceAdmin')")
+    public ResponseEntity killPid(@PathVariable("pid") long pid, @RequestParam(defaultValue = "false") boolean destroy)
+    {
+        try
+        {
+            Optional<ProcessHandle> processHandleOptional = ProcessHandle.of(pid);
+
+            if (processHandleOptional.isEmpty()) {
+                return new ResponseEntity("pid not found!", HttpStatus.BAD_REQUEST);
+            }
+            else {
+                boolean result = destroy ? processHandleOptional.get().destroyForcibly() : processHandleOptional.get().destroy();
+                if (result) {
+                    return new ResponseEntity(HttpStatus.OK);
+                }
+                else {
+                    return new ResponseEntity("could not kill the pid!", HttpStatus.BAD_REQUEST);
+                }
+            }
+
+        }
+        catch (Exception e)
+        {
+            return new ResponseEntity(e, HttpStatus.BAD_REQUEST);
+        }
+    }
+
+}
