ba9bea273d9fcc222133b4911fec3b8b878d7f09
Michael Stewart <mick@humeenterprises.co.uk>
2022-03-30 12:04:18 +0200

    IKASAN-2079 moving job lock to context namespace. Tidy up a couple of files.

--- ikasaneip/ootb/rest/scheduler-agent-rest-module/src/main/java/org/ikasan/job/orchestration/model/context/ContextTemplateImpl.java
+++ ikasaneip/ootb/rest/scheduler-agent-rest-module/src/main/java/org/ikasan/job/orchestration/model/context/ContextTemplateImpl.java
@@ -2,7 +2,7 @@ package org.ikasan.job.orchestration.model.context;
 
 import org.ikasan.spec.scheduled.context.model.ContextParameter;
 import org.ikasan.spec.scheduled.context.model.ContextTemplate;
-import org.ikasan.spec.scheduled.job.model.JobLock;
+import org.ikasan.spec.scheduled.context.model.JobLock;
 import org.ikasan.spec.scheduled.job.model.SchedulerJob;
 
 public class ContextTemplateImpl extends ContextImpl<ContextTemplate, ContextParameter, SchedulerJob, JobLock> implements ContextTemplate {
