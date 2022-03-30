c3cb9a34480ca8d51c68219e73361c6fa5feabdf
Michael Stewart <mick@humeenterprises.co.uk>
2022-03-30 12:04:18 +0200

    IKASAN-2079 moving job lock to context namespace. Tidy up a couple of files.

--- ikasaneip/spec/service/scheduled/src/main/java/org/ikasan/spec/scheduled/instance/model/JobLockInstance.java
+++ ikasaneip/spec/service/scheduled/src/main/java/org/ikasan/spec/scheduled/instance/model/JobLockInstance.java
@@ -1,6 +1,6 @@
 package org.ikasan.spec.scheduled.instance.model;
 
-import org.ikasan.spec.scheduled.job.model.JobLock;
+import org.ikasan.spec.scheduled.context.model.JobLock;
 
 public interface JobLockInstance extends JobLock {
 
