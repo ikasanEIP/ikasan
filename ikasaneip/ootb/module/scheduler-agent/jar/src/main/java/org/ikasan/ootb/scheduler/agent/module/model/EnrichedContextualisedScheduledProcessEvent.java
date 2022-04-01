a03fae3fdad551a28561abb3a510007a0495bb0a
gulser <Serkan.Guler2@uk.mizuho-sc.com>
2022-04-01 10:55:41 +0100

    IKASAN-2081 fixing the flow and the test

--- ikasaneip/ootb/module/scheduler-agent/jar/src/main/java/org/ikasan/ootb/scheduler/agent/module/model/EnrichedContextualisedScheduledProcessEvent.java
+++ ikasaneip/ootb/module/scheduler-agent/jar/src/main/java/org/ikasan/ootb/scheduler/agent/module/model/EnrichedContextualisedScheduledProcessEvent.java
@@ -1,15 +1,19 @@
 package org.ikasan.ootb.scheduler.agent.module.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import org.ikasan.ootb.scheduled.model.ContextualisedScheduledProcessEventImpl;
 
 public class EnrichedContextualisedScheduledProcessEvent extends ContextualisedScheduledProcessEventImpl {
 
+    @JsonIgnore
     private Process process;
 
+    @JsonIgnore
     public Process getProcess() {
         return process;
     }
 
+    @JsonIgnore
     public void setProcess(Process process) {
         this.process = process;
     }
