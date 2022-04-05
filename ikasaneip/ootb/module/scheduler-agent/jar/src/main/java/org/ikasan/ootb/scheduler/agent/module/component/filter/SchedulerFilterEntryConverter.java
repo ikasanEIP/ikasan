56fe41a9ca5407707238a10b4fb9581a3f5d9118
Michael Stewart <mick@humeenterprises.co.uk>
2022-04-05 10:24:23 +0100

    IKASAN-2064 finalising a bunch of unit tests.

--- ikasaneip/ootb/module/scheduler-agent/jar/src/main/java/org/ikasan/ootb/scheduler/agent/module/component/filter/SchedulerFilterEntryConverter.java
+++ ikasaneip/ootb/module/scheduler-agent/jar/src/main/java/org/ikasan/ootb/scheduler/agent/module/component/filter/SchedulerFilterEntryConverter.java
@@ -4,23 +4,42 @@ import org.ikasan.filter.duplicate.model.DefaultFilterEntry;
 import org.ikasan.filter.duplicate.model.FilterEntry;
 import org.ikasan.filter.duplicate.model.FilterEntryConverter;
 import org.ikasan.filter.duplicate.model.FilterEntryConverterException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.io.File;
 import java.util.List;
 
 public class SchedulerFilterEntryConverter implements FilterEntryConverter<List<File>> {
 
+    /** logger */
+    private static Logger logger = LoggerFactory.getLogger(SchedulerFilterEntryConverter.class);
+
     private String configuredResourceId;
     private int filterTimeToLive;
 
     public SchedulerFilterEntryConverter(String configuredResourceId, int filterTimeToLive) {
         this.configuredResourceId = configuredResourceId;
+        if(this.configuredResourceId == null) {
+            throw new IllegalArgumentException("configuredResourceId cannot be null!");
+        }
         this.filterTimeToLive = filterTimeToLive;
     }
 
     @Override
-    public FilterEntry convert(List<File> message) throws FilterEntryConverterException {
-        Integer criteria = message.get(0).getName().hashCode();
+    public FilterEntry convert(List<File> files) throws FilterEntryConverterException {
+        if(files == null || files.isEmpty()) {
+            throw new FilterEntryConverterException("Received a null or empty file list!");
+        }
+
+        if(files.size() > 1) {
+            StringBuffer filenames = new StringBuffer();
+            files.forEach(file -> filenames.append(file.getName()).append(" "));
+
+            logger.info("Received multiple files {}. Expecting only one.", filenames.toString());
+        }
+
+        Integer criteria = files.get(0).getName().hashCode();
         return new DefaultFilterEntry(criteria, configuredResourceId, filterTimeToLive);
     }
 }
