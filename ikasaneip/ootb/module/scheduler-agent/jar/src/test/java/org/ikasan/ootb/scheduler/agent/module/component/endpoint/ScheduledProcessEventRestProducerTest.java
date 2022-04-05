ce48a449c9ff5b46d833ea680668534d3a5dd469
Michael Stewart <mick@humeenterprises.co.uk>
2022-04-05 10:24:23 +0100

    IKASAN-2064 finalising a bunch of unit tests.

--- ikasaneip/ootb/module/scheduler-agent/jar/src/test/java/org/ikasan/ootb/scheduler/agent/module/component/endpoint/ScheduledProcessEventRestProducerTest.java
+++ ikasaneip/ootb/module/scheduler-agent/jar/src/test/java/org/ikasan/ootb/scheduler/agent/module/component/endpoint/ScheduledProcessEventRestProducerTest.java
@@ -1,4 +1,51 @@
 package org.ikasan.ootb.scheduler.agent.module.component.endpoint;
 
+import org.ikasan.spec.component.endpoint.EndpointException;
+import org.ikasan.spec.dashboard.DashboardRestService;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
+
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.when;
+
+@RunWith(MockitoJUnitRunner.class)
 public class ScheduledProcessEventRestProducerTest {
+
+    @Mock
+    DashboardRestService dashboardRestService;
+
+    @Test(expected = IllegalArgumentException.class)
+    public void test_exception_constructor_null_dashboard_rest_service() {
+        new ScheduledProcessEventRestProducer(null);
+    }
+
+    @Test
+    public void test_invoke_success() {
+        when(dashboardRestService.publish(anyString())).thenReturn(true);
+
+        ScheduledProcessEventRestProducer producer = new ScheduledProcessEventRestProducer(dashboardRestService);
+
+        producer.invoke("payload");
+    }
+
+    @Test(expected = EndpointException.class)
+    public void test_invoke_failure() {
+        when(dashboardRestService.publish(anyString())).thenReturn(false);
+
+        ScheduledProcessEventRestProducer producer = new ScheduledProcessEventRestProducer(dashboardRestService);
+
+        producer.invoke("payload");
+    }
+
+    @Test(expected = EndpointException.class)
+    public void test_invoke_runtime_exception() {
+        when(dashboardRestService.publish(anyString())).thenThrow(new RuntimeException("error!"));
+
+        ScheduledProcessEventRestProducer producer = new ScheduledProcessEventRestProducer(dashboardRestService);
+
+        producer.invoke("payload");
+    }
 }
