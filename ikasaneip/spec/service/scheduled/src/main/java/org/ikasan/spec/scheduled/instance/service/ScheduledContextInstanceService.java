52a51e33098cbe682f3a88525c341f38b1eec44e
John Otaegui <john@waddle.com.au>
2022-04-07 14:54:28 +1000

    IKASAN-2090: Initial cut of api changes in core

--- ikasaneip/spec/service/scheduled/src/main/java/org/ikasan/spec/scheduled/instance/service/ScheduledContextInstanceService.java
+++ ikasaneip/spec/service/scheduled/src/main/java/org/ikasan/spec/scheduled/instance/service/ScheduledContextInstanceService.java
@@ -1,10 +1,11 @@
 package org.ikasan.spec.scheduled.instance.service;
 
+import java.util.List;
+
 import org.ikasan.spec.scheduled.instance.model.InstanceStatus;
+import org.ikasan.spec.scheduled.instance.model.ScheduledContextInstanceAuditRecord;
 import org.ikasan.spec.scheduled.instance.model.ScheduledContextInstanceRecord;
 import org.ikasan.spec.search.SearchResults;
-
-import java.util.List;
 
 public interface ScheduledContextInstanceService {
 
@@ -30,4 +31,30 @@ public interface ScheduledContextInstanceService {
      * @return
      */
     SearchResults<ScheduledContextInstanceRecord> getScheduledContextInstancesByStatus(List<InstanceStatus> instanceStatuses);
+
+    /**
+     * Save a scheduled context instance audit record.
+     *
+     * @param scheduledContextInstanceAuditRecord
+     */
+    void saveAudit(ScheduledContextInstanceAuditRecord scheduledContextInstanceAuditRecord);
+
+    /**
+     * Get scheduled context instance audit records.
+     *
+     * @param limit
+     * @param offset
+     * @return
+     */
+    SearchResults<ScheduledContextInstanceAuditRecord> findAllAuditRecords(int limit, int offset);
+
+    /**
+     * Get all scheduled context instance audit records for a context id.
+     *
+     * @param contextId
+     * @param limit
+     * @param offset
+     * @return
+     */
+    SearchResults<ScheduledContextInstanceAuditRecord> findAllAuditRecordsByContextId(String contextId, int limit, int offset);
 }
