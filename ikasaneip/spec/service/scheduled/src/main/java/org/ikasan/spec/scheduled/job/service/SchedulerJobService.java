e7f8b1b255a428ff7dd59ff18a69488a96034a4d
Michael Stewart <mick@humeenterprises.co.uk>
2022-04-17 08:09:55 +0100

    IKASAN-1999 adding spec service interfaces

--- ikasaneip/spec/service/scheduled/src/main/java/org/ikasan/spec/scheduled/job/service/SchedulerJobService.java
+++ ikasaneip/spec/service/scheduled/src/main/java/org/ikasan/spec/scheduled/job/service/SchedulerJobService.java
@@ -8,10 +8,37 @@ import java.util.List;
 
 public interface SchedulerJobService<T extends SchedulerJobRecord> {
 
+    /**
+     * Find jobs that are associated with an agent.
+     *
+     * @param agent
+     * @param limit
+     * @param offset
+     * @return
+     */
     SearchResults<? extends T> findByAgent(String agent, int limit, int offset);
 
+    /**
+     * Find a job by context and job name.
+     *
+     * @param contextId
+     * @param jobName
+     * @return
+     */
+    T findByContextIdAndJobName(String contextId, String jobName);
+
+    /**
+     * Delete an individual job record.
+     *
+     * @param record
+     */
     void delete(T record);
 
+    /**
+     * Delete all jobs associated with an agent
+     *
+     * @param agentName
+     */
     void deleteByAgentName(String agentName);
 
     /**
@@ -55,15 +82,44 @@ public interface SchedulerJobService<T extends SchedulerJobRecord> {
      */
     void saveQuartzScheduledJob(QuartzScheduleDrivenJob quartzScheduleDrivenJob);
 
+    /**
+     * Save a list of FileEventDrivenJobRecord.
+     *
+     * @param fileEventDrivenJobRecords
+     */
     public void saveFileEventDrivenJobRecords(List<FileEventDrivenJobRecord> fileEventDrivenJobRecords);
 
+    /**
+     * Save a list of InternalEventDrivenJobRecord
+     *
+     * @param internalEventDrivenJobRecord
+     */
     public void saveInternalEventDrivenJobRecords(List<InternalEventDrivenJobRecord> internalEventDrivenJobRecord);
 
+    /**
+     * Save a list of QuartzScheduleDrivenJobRecord.
+     *
+     * @param quartzScheduleDrivenJobRecord
+     */
     public void saveQuartzScheduledJobRecords(List<QuartzScheduleDrivenJobRecord> quartzScheduleDrivenJobRecord);
 
+    /**
+     * Save a list of InternalEventDrivenJob.
+     *
+     * @param quartzScheduleDrivenJobs
+     */
     public void saveInternalEventDrivenJobs(List<InternalEventDrivenJob> quartzScheduleDrivenJobs);
 
+    /**
+     * Save a list of QuartzScheduleDrivenJob.
+     * @param quartzScheduleDrivenJobs
+     */
     public void saveQuartzScheduledJobs(List<QuartzScheduleDrivenJob> quartzScheduleDrivenJobs);
 
+    /**
+     * Save a list of FileEventDrivenJob.
+     *
+     * @param quartzScheduleDrivenJobs
+     */
     public void saveFileEventDrivenJobs(List<FileEventDrivenJob> quartzScheduleDrivenJobs);
 }
