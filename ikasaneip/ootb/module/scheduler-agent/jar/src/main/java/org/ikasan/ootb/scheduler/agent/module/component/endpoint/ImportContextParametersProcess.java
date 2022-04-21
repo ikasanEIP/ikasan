9e8be7e76f62e90d46dacdaa0193922e9fba6b66
gulser <serkan.guler2@uk.mizuho-sc.com>
2022-04-21 14:48:34 +0100

    IKASAN-2097 adding a new ContextParametersCache and a new ImportContextParametersFlow

--- ikasaneip/ootb/module/scheduler-agent/jar/src/main/java/org/ikasan/ootb/scheduler/agent/module/component/endpoint/ImportContextParametersProcess.java
+++ ikasaneip/ootb/module/scheduler-agent/jar/src/main/java/org/ikasan/ootb/scheduler/agent/module/component/endpoint/ImportContextParametersProcess.java
@@ -4,8 +4,6 @@ import org.ikasan.dashboard.ContextParametersRestServiceImpl;
 import org.ikasan.ootb.scheduler.agent.module.component.cache.ContextParametersCache;
 import org.ikasan.spec.component.endpoint.EndpointException;
 import org.ikasan.spec.component.endpoint.Producer;
-import org.ikasan.spec.management.ManagedResource;
-import org.ikasan.spec.management.ManagedResourceRecoveryManager;
 import org.ikasan.spec.scheduled.instance.model.ContextParameterInstance;
 import org.springframework.core.env.Environment;
 import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
@@ -14,7 +12,7 @@ import javax.annotation.Resource;
 import java.util.List;
 import java.util.Map;
 
-public class ImportContextParametersProcess<T> implements Producer<T>, ManagedResource {
+public class ImportContextParametersProcess<T> implements Producer<T> {
 
     @Resource
     private Environment environment;
@@ -24,7 +22,9 @@ public class ImportContextParametersProcess<T> implements Producer<T>, ManagedRe
     @Override
     public void invoke(T payload) throws EndpointException {
 
-        importContextParameters();
+        Map<String, List<ContextParameterInstance>> contextParameters = getService().getAll();
+
+        ContextParametersCache.instance().put(contextParameters);
     }
 
     public ContextParametersRestServiceImpl getService() {
@@ -38,37 +38,5 @@ public class ImportContextParametersProcess<T> implements Producer<T>, ManagedRe
 
     public void setService(ContextParametersRestServiceImpl service) {
         this.service = service;
-    }
-
-    private void importContextParameters() {
-        Map<String, List<ContextParameterInstance>> contextParameters = getService().getAll();
-
-        ContextParametersCache.instance().put(contextParameters);
-    }
-
-    @Override
-    public void startManagedResource() {
-        // on module startup, we import all context parameters
-        importContextParameters();
-    }
-
-    @Override
-    public void stopManagedResource() {
-
-    }
-
-    @Override
-    public void setManagedResourceRecoveryManager(ManagedResourceRecoveryManager managedResourceRecoveryManager) {
-
-    }
-
-    @Override
-    public boolean isCriticalOnStartup() {
-        return false;
-    }
-
-    @Override
-    public void setCriticalOnStartup(boolean criticalOnStartup) {
-
     }
 }
