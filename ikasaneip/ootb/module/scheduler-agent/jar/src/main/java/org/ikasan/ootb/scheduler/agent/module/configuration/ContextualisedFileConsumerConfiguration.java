1ed863a9d28eb2f82d7e436c84d9fed28eb1c578
gulser <Serkan.Guler2@uk.mizuho-sc.com>
2022-04-27 15:51:43 +0100

    IKASAN-2097 adding ContextualisedFileMessageProvider

---
+++ ikasaneip/ootb/module/scheduler-agent/jar/src/main/java/org/ikasan/ootb/scheduler/agent/module/configuration/ContextualisedFileConsumerConfiguration.java
@@ -0,0 +1,16 @@
+package org.ikasan.ootb.scheduler.agent.module.configuration;
+
+import org.ikasan.component.endpoint.filesystem.messageprovider.FileConsumerConfiguration;
+
+public class ContextualisedFileConsumerConfiguration extends FileConsumerConfiguration {
+
+    private String contextId;
+
+    public String getContextId() {
+        return contextId;
+    }
+
+    public void setContextId(String contextId) {
+        this.contextId = contextId;
+    }
+}
