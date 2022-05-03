6795a6c32e4836a9e1e132f1bc924ecd96974d17
Michael Stewart <mick@humeenterprises.co.uk>
2022-05-03 07:49:24 +0100

    IKASAN-1999 adding proper toString method so that can be wiretapped.

--- ikasaneip/ootb/module/scheduler-agent/jar/src/main/java/org/ikasan/ootb/scheduler/agent/module/model/EnrichedContextualisedScheduledProcessEvent.java
+++ ikasaneip/ootb/module/scheduler-agent/jar/src/main/java/org/ikasan/ootb/scheduler/agent/module/model/EnrichedContextualisedScheduledProcessEvent.java
@@ -52,4 +52,39 @@ public class EnrichedContextualisedScheduledProcessEvent extends ContextualisedS
     public List<ContextParameterInstance> getContextParameters() {
         return this.contextParameters;
     }
+
+    @Override
+    public String toString() {
+        final StringBuffer sb = new StringBuffer("EnrichedContextualisedScheduledProcessEvent{");
+        sb.append("id=").append(id);
+        sb.append(", agentName='").append(agentName).append('\'');
+        sb.append(", agentHostname='").append(agentHostname).append('\'');
+        sb.append(", jobName='").append(jobName).append('\'');
+        sb.append(", jobGroup='").append(jobGroup).append('\'');
+        sb.append(", jobDescription='").append(jobDescription).append('\'');
+        sb.append(", commandLine='").append(commandLine).append('\'');
+        sb.append(", returnCode=").append(returnCode);
+        sb.append(", successful=").append(successful);
+        sb.append(", outcome=").append(outcome);
+        sb.append(", resultOutput='").append(resultOutput).append('\'');
+        sb.append(", resultError='").append(resultError).append('\'');
+        sb.append(", pid=").append(pid);
+        sb.append(", user='").append(user).append('\'');
+        sb.append(", fireTime=").append(fireTime);
+        sb.append(", nextFireTime=").append(nextFireTime);
+        sb.append(", completionTime=").append(completionTime);
+        sb.append(", harvested=").append(harvested);
+        sb.append(", harvestedDateTime=").append(harvestedDateTime);
+        sb.append(", dryRun=").append(dryRun);
+        sb.append(", jobStarting=").append(jobStarting);
+        sb.append(", dryRunParameters=").append(dryRunParameters);
+        if(contextParameters != null && !contextParameters.isEmpty()) {
+            sb.append(", contextParameters=[");
+            contextParameters.forEach(param -> sb.append(param).append(","));
+            sb.deleteCharAt(sb.length() - 1);
+            sb.append("]");
+        }
+        sb.append('}');
+        return sb.toString();
+    }
 }
