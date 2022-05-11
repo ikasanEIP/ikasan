3352b561f3d9d7997b4176d21fcaa9b1a0508673
Michael Stewart <mick@humeenterprises.co.uk>
2022-05-11 12:32:51 +0100

    IKASAN-2105 general development

--- ikasaneip/spec/service/scheduled/src/main/java/org/ikasan/spec/scheduled/context/service/ScheduledContextService.java
+++ ikasaneip/spec/service/scheduled/src/main/java/org/ikasan/spec/scheduled/context/service/ScheduledContextService.java
@@ -7,11 +7,55 @@ import java.util.List;
 
 public interface ScheduledContextService {
 
+    /**
+     * Find all ScheduledContextRecords
+     *
+     * @return SearchResults containing a List of ScheduledContextRecord and associated meta data.
+     */
     SearchResults<? extends ScheduledContextRecord> findAll();
 
+    /**
+     * Find all ScheduledContextRecords with limit and offset. Used for result paging.
+     *
+     * @param limit
+     * @param offset
+     *
+     * @return SearchResults containing a List of ScheduledContextRecord and associated meta data.
+     */
+    SearchResults<? extends ScheduledContextRecord> findAll(int limit, int offset);
+
+    /**
+     * Find all ScheduledContextRecords containing keyword with limit and offset. Used for result paging.
+     *
+     * @param keyword
+     * @param limit
+     * @param offset
+     *
+     * @return SearchResults containing a List of ScheduledContextRecord and associated meta data.
+     */
+    SearchResults<? extends ScheduledContextRecord> findByKeyword(String keyword, int limit, int offset);
+
+    /**
+     * Find ScheduledContextRecord by id.
+     * @param id
+     *
+     * @return matching ScheduledContextRecord or null if not found
+     */
     ScheduledContextRecord findById(String id);
 
+    /**
+     * Find ScheduledContextRecord by name
+     *
+     * @param name
+     *
+     * @return matching ScheduledContextRecord or null if not found
+     */
     ScheduledContextRecord findByName(String name);
 
+    /**
+     * Save a ScheduledContextRecordß
+     *
+     * @param scheduledContextRecord
+     */
     void save(ScheduledContextRecord scheduledContextRecord);
 }
