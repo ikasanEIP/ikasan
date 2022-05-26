64dbe54f2e1ce227f738bed90524ce00a690314e
Michael Stewart <mick@humeenterprises.co.uk>
2022-05-26 18:34:08 +0100

    IKASAN-1999 persistence for scheduler job instances

---
+++ ikasaneip/spec/service/scheduled/src/main/java/org/ikasan/spec/scheduled/instance/dao/SchedulerJobInstanceDao.java
@@ -0,0 +1,62 @@
+package org.ikasan.spec.scheduled.instance.dao;
+
+import org.ikasan.spec.scheduled.instance.model.*;
+import org.ikasan.spec.search.SearchResults;
+
+import java.util.List;
+
+public interface SchedulerJobInstanceDao {
+
+    /**
+     * Get a scheduled context instance record by id.
+     *
+     * @param id
+     * @return
+     */
+    SchedulerJobInstanceRecord findById(String id);
+
+    /**
+     * Save a scheduled context instance record.
+     *
+     * @param scheduledContextInstanceRecord
+     */
+    void save(SchedulerJobInstanceRecord scheduledContextInstanceRecord);
+
+    /**
+     * Get scheduler job instances by context instance id
+     *
+     * @param contextInstanceId
+     * @param limit
+     * @param offset
+     * @param sortField
+     * @param sortDirection
+     * @return
+     */
+    SearchResults<SchedulerJobInstanceRecord> getSchedulerJobInstancesByContextInstanceId(String contextInstanceId, int limit, int offset, String sortField, String sortDirection);
+
+    /**
+     * Get scheduler job instances by context name
+     *
+     * @param contextName
+     * @param limit
+     * @param offset
+     * @param sortField
+     * @param sortDirection
+     * @return
+     */
+    SearchResults<SchedulerJobInstanceRecord> getSchedulerJobInstancesByContextName(String contextName, int limit, int offset, String sortField, String sortDirection);
+
+
+    /**
+     * Get scheduled context instance records by filter with limit and offset.
+     *
+     * @param filter
+     * @param limit
+     * @param offset
+     * @param sortField
+     * @param sortDirection
+     * @return
+     */
+    SearchResults<SchedulerJobInstanceRecord> getScheduledContextInstancesByFilter(SchedulerJobInstanceSearchFilter filter, int limit, int offset, String sortField, String sortDirection);
+
+}
