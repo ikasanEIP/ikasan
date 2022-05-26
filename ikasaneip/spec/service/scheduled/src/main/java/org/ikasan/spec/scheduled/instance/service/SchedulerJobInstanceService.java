08001179c4e687fb68e36c3ffcb6acaef33edc3a
Michael Stewart <mick@humeenterprises.co.uk>
2022-05-26 18:34:08 +0100

    IKASAN-1999 persistence for scheduler job instances

---
+++ ikasaneip/spec/service/scheduled/src/main/java/org/ikasan/spec/scheduled/instance/service/SchedulerJobInstanceService.java
@@ -0,0 +1,61 @@
+package org.ikasan.spec.scheduled.instance.service;
+
+import org.ikasan.spec.scheduled.instance.model.SchedulerJobInstanceRecord;
+import org.ikasan.spec.scheduled.instance.model.SchedulerJobInstanceSearchFilter;
+import org.ikasan.spec.search.SearchResults;
+
+public interface SchedulerJobInstanceService {
+
+    /**
+     * Get a scheduled context instance record by id.
+     *
+     * @param id
+     * @return
+     */
+    SchedulerJobInstanceRecord findById(String id);
+
+    /**
+     * Save a scheduled context instance record.
+     *
+     * @param scheduledContextInstanceRecord
+     */
+    void save(SchedulerJobInstanceRecord scheduledContextInstanceRecord);
+
+    /**
+     * Get scheduler job instances by context instance id
+     *
+     * @param contextInstanceId
+     * @param limit
+     * @param offset
+     * @param sortField
+     * @param sortDirection
+     * @return
+     */
+    SearchResults<SchedulerJobInstanceRecord> getSchedulerJobInstancesByContextInstanceId(String contextInstanceId, int limit, int offset, String sortField, String sortDirection);
+
+    /**
+     * Get scheduler job instances by context name
+     *
+     * @param contextName
+     * @param limit
+     * @param offset
+     * @param sortField
+     * @param sortDirection
+     * @return
+     */
+    SearchResults<SchedulerJobInstanceRecord> getSchedulerJobInstancesByContextName(String contextName, int limit, int offset, String sortField, String sortDirection);
+
+
+    /**
+     * Get scheduled context instance records by filter with limit and offset.
+     *
+     * @param filter
+     * @param limit
+     * @param offset
+     * @param sortField
+     * @param sortDirection
+     * @return
+     */
+    SearchResults<SchedulerJobInstanceRecord> getScheduledContextInstancesByFilter(SchedulerJobInstanceSearchFilter filter, int limit, int offset, String sortField, String sortDirection);
+
+}
