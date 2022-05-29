e34eae1f0c6877872bfdb4e66b4c78df5a6a032e
Michael Stewart <mick@humeenterprises.co.uk>
2022-05-29 21:38:34 +0100

    IKASAN-1999 more contracts for job instances

---
+++ ikasaneip/spec/service/scheduled/src/main/java/org/ikasan/spec/scheduled/instance/model/InternalEventDrivenJobInstance.java
@@ -0,0 +1,36 @@
+package org.ikasan.spec.scheduled.instance.model;
+
+import org.ikasan.spec.scheduled.context.model.ContextParameter;
+import org.ikasan.spec.scheduled.job.model.SchedulerJob;
+
+import java.util.List;
+
+public interface InternalEventDrivenJobInstance extends SchedulerJobInstance {
+    List<String> getSuccessfulReturnCodes();
+
+    void setSuccessfulReturnCodes(List<String> successfulReturnCodes);
+
+    String getWorkingDirectory();
+
+    void setWorkingDirectory(String workingDirectory);
+
+    String getCommandLine();
+
+    void setCommandLine(String commandLine);
+
+    long getMinExecutionTime();
+
+    void setMinExecutionTime(long minExecutionTime);
+
+    long getMaxExecutionTime();
+
+    void setMaxExecutionTime(long maxExecutionTime);
+
+    List<ContextParameter> getContextParameters();
+
+    void setContextParameters(List<ContextParameter> contextParameters);
+
+    List<Integer> getDaysOfWeekToRun();
+
+    void setDaysOfWeekToRun(List<Integer> daysOfWeekToRun);
+}
