fd4565b0789a15a53ff188c2a57456f98b6ea6ba
Michael Stewart <mick@humeenterprises.co.uk>
2022-05-31 20:24:56 +0100

    IKASAN-1999 adding serializable interface to all scheduler events

--- ikasaneip/spec/service/scheduled/src/main/java/org/ikasan/spec/scheduled/event/model/SchedulerJobInstanceStateChangeEvent.java
+++ ikasaneip/spec/service/scheduled/src/main/java/org/ikasan/spec/scheduled/event/model/SchedulerJobInstanceStateChangeEvent.java
@@ -3,7 +3,9 @@ package org.ikasan.spec.scheduled.event.model;
 import org.ikasan.spec.scheduled.instance.model.ContextInstance;
 import org.ikasan.spec.scheduled.instance.model.SchedulerJobInstance;
 
-public interface SchedulerJobInstanceStateChangeEvent extends StateChangeEvent {
+import java.io.Serializable;
+
+public interface SchedulerJobInstanceStateChangeEvent extends StateChangeEvent, Serializable {
 
     SchedulerJobInstance getSchedulerJobInstance();
 
