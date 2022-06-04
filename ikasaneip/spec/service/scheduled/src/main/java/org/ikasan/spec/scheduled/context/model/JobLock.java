16f9109d0faba2ae339306f2f47b7a3e30237399
Michael Stewart <mick@humeenterprises.co.uk>
2022-06-04 22:17:28 +0100

    IKASAN-2110 refactoring the job lock cache and locking mechanism in an attempt to simplify and rectify inconsistent behaviour.

--- ikasaneip/spec/service/scheduled/src/main/java/org/ikasan/spec/scheduled/context/model/JobLock.java
+++ ikasaneip/spec/service/scheduled/src/main/java/org/ikasan/spec/scheduled/context/model/JobLock.java
@@ -4,15 +4,48 @@ import org.ikasan.spec.scheduled.job.model.SchedulerJob;
 
 import java.io.Serializable;
 import java.util.List;
+import java.util.Map;
 
 public interface JobLock extends Serializable {
 
+    /**
+     * Set the name of the job lock.
+     *
+     * @param name
+     */
     void setName(String name);
+
+    /**
+     * Get the name of the job lock.
+     *
+     * @return
+     */
     String getName();
 
+    /**
+     * Set the name of the lock count.
+     *
+     * @param lockCount
+     */
     void setLockCount(long lockCount);
+
+    /**
+     * Get the name of the lock count.
+     * @return
+     */
     long getLockCount();
 
-    void setJobs(List<SchedulerJob> jobs);
-    List<SchedulerJob> getJobs();
+    /**
+     * Set the scheduler jobs.
+     *
+     * @param jobs
+     */
+    void setJobs(Map<String, List<SchedulerJob>> jobs);
+
+    /**
+     * Get the scheduler jobs.
+     *
+     * @return
+     */
+    Map<String, List<SchedulerJob>> getJobs();
 }
