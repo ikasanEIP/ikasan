7e237467a56cbca48197344f47c098a8ceda44c0
Michael Stewart <mick@humeenterprises.co.uk>
2022-06-07 15:14:18 +0100

    IKASAN-2108 refactoring cache to have a single collection.

--- ikasaneip/spec/service/scheduled/src/main/java/org/ikasan/spec/scheduled/context/model/JobLockCache.java
+++ ikasaneip/spec/service/scheduled/src/main/java/org/ikasan/spec/scheduled/context/model/JobLockCache.java
@@ -1,24 +1,24 @@
 package org.ikasan.spec.scheduled.context.model;
 
-import java.io.Serializable;
-import java.util.List;
-
 import org.ikasan.spec.scheduled.event.model.ContextualisedSchedulerJobInitiationEvent;
 import org.ikasan.spec.scheduled.event.model.SchedulerJobInitiationEvent;
-import org.ikasan.spec.scheduled.job.model.SchedulerJob;
-import org.ikasan.spec.scheduled.joblock.model.JobLockCacheData;
 import org.ikasan.spec.scheduled.joblock.model.JobLockCacheRecord;
 import org.ikasan.spec.scheduled.joblock.service.JobLockCacheService;
+
+import java.io.Serializable;
+import java.util.List;
 
 public interface JobLockCache extends Serializable {
 
     /**
+     * Add locks to the cache
      *
      * @param jobLocks
      */
     void addLocks(List<JobLock> jobLocks);
 
     /**
+     * Lock a job based on its identifier.
      *
      * @param jobIdentifier
      * @param contextName
@@ -27,6 +27,7 @@ public interface JobLockCache extends Serializable {
     boolean lock(String jobIdentifier, String contextName);
 
     /**
+     * Release a job basd upon its identifier.
      *
      * @param jobIdentifier
      * @param contextName
@@ -35,6 +36,7 @@ public interface JobLockCache extends Serializable {
     boolean release(String jobIdentifier, String contextName);
 
     /**
+     * Determine if a job participates in a lock.
      *
      * @param jobIdentifier
      * @param contextName
@@ -43,6 +45,7 @@ public interface JobLockCache extends Serializable {
     boolean doesJobParticipateInLock(String jobIdentifier, String contextName);
 
     /**
+     * Determine if a lock that a job is part of is currently locked.
      *
      * @param jobIdentifier
      * @param contextName
@@ -51,6 +54,7 @@ public interface JobLockCache extends Serializable {
     boolean locked(String jobIdentifier, String contextName);
 
     /**
+     * Determine if a job is currently holding a lock.
      *
      * @param jobIdentifier
      * @param contextName
@@ -59,11 +63,12 @@ public interface JobLockCache extends Serializable {
     boolean hasLock(String jobIdentifier, String contextName);
 
     /**
-     *
+     * Reset the job lock cache. All data will be removed from the cache!
      */
     void reset();
 
     /**
+     * Reset an individual lock based upon its name.
      *
      * @param lockName
      * @return
@@ -71,12 +76,14 @@ public interface JobLockCache extends Serializable {
     boolean resetLock(String lockName);
 
     /**
+     * Set the underlying persistence service.
      *
      * @param jobLockCacheService
      */
     void setJobLockCacheService(JobLockCacheService jobLockCacheService);
 
     /**
+     * Add an event to a locks underlying wait queue.
      *
      * @param jobIdentifier
      * @param contextName
@@ -85,6 +92,7 @@ public interface JobLockCache extends Serializable {
     void addQueuedSchedulerJobInitiationEvent(String jobIdentifier, String contextName, SchedulerJobInitiationEvent event);
 
     /**
+     * Get the next job in the wait queue.
      *
      * @return
      */
--- ikasaneip/spec/service/scheduled/src/main/java/org/ikasan/spec/scheduled/context/model/JobLockHolder.java
+++ ikasaneip/spec/service/scheduled/src/main/java/org/ikasan/spec/scheduled/context/model/JobLockHolder.java
@@ -1,36 +1,92 @@
 package org.ikasan.spec.scheduled.context.model;
 
+import org.ikasan.spec.scheduled.event.model.ContextualisedSchedulerJobInitiationEvent;
+import org.ikasan.spec.scheduled.job.model.SchedulerJob;
+
 import java.io.Serializable;
 import java.util.List;
 import java.util.Map;
 import java.util.Queue;
 import java.util.Set;
 
-import org.ikasan.spec.scheduled.event.model.ContextualisedSchedulerJobInitiationEvent;
-import org.ikasan.spec.scheduled.event.model.SchedulerJobInitiationEvent;
-import org.ikasan.spec.scheduled.job.model.SchedulerJob;
-
 public interface JobLockHolder extends Serializable {
 
+    /**
+     * Get the job lock name.
+     *
+     * @return
+     */
     String getLockName();
 
+    /**
+     * et the job lock name.
+     *
+     * @param lockName
+     */
     void setLockName(String lockName);
 
+    /**
+     * Get the lock count
+     *
+     * @return
+     */
     long getLockCount();
 
+    /**
+     * Set the lock count
+     *
+     * @param lockCount
+     */
     void setLockCount(long lockCount);
 
+    /**
+     * Get the jobs associated with the lock
+     *
+     * @return
+     */
     Map<String, List<SchedulerJob>> getSchedulerJobs();
 
+    /**
+     * Add jobs to the lock.
+     *
+     * @param contextName
+     * @param jobs
+     */
     void addSchedulerJobs(String contextName, List<SchedulerJob> jobs);
 
+    /**
+     * Get the job identifier/s holding the lock.
+     *
+     * @return
+     */
     Set<String> getLockHolders();
 
+    /**
+     * Set the identifier of a job holding the lock.
+     *
+     * @param jobIdentifier
+     */
     void addLockHolder(String jobIdentifier);
 
+    /**
+     * Remove a job holding the lock based on its identifier.
+     *
+     * @param jobIdentifier
+     * @return
+     */
     boolean removeLockHolder(String jobIdentifier);
 
+    /**
+     * Get the queued jobs waiting for the lock to be released.
+     *
+     * @return
+     */
     Queue<ContextualisedSchedulerJobInitiationEvent> getSchedulerJobInitiationEventWaitQueue();
 
+    /**
+     * Set the queued jobs waiting for the lock to be released.
+     *
+     * @param contextualisedSchedulerJobInitiationEventQueue
+     */
     void setSchedulerJobInitiationEventWaitQueue(Queue<ContextualisedSchedulerJobInitiationEvent> contextualisedSchedulerJobInitiationEventQueue);
 }
