6835d0e57fa55a3cc278a6f986dc00e057cfbfb7
gulser <Serkan.Guler2@uk.mizuho-sc.com>
2022-06-07 14:27:04 +0100

    IKASAN-2107 adding notification interfaces

---
+++ ikasaneip/spec/service/scheduled/src/main/java/org/ikasan/spec/scheduled/notification/dao/EmailNotificationDetailsDao.java
@@ -0,0 +1,16 @@
+package org.ikasan.spec.scheduled.notification.dao;
+
+import org.ikasan.spec.search.SearchResults;
+
+import java.util.List;
+
+public interface EmailNotificationDetailsDao<T> {
+
+    SearchResults<T> findAll(int limit, int offset);
+
+    T findByJobNameAndMonitorType(String jobName, String monitorType);
+
+    void save(T var1);
+
+    void save(List<T> var1);
+}
