1ef58cf819260fa6192faa833de4eedf444f39e0
John Otaegui <johnot@gmail.com>
2022-06-16 20:10:00 +1000

    IAKSAN-2097 add feature flag to turn on life cyle recovery.

--- ikasaneip/ootb/module/scheduler-agent/jar/src/main/java/org/ikasan/ootb/scheduler/agent/module/boot/recovery/AgentInstanceRecoveryManager.java
+++ ikasaneip/ootb/module/scheduler-agent/jar/src/main/java/org/ikasan/ootb/scheduler/agent/module/boot/recovery/AgentInstanceRecoveryManager.java
@@ -10,16 +10,20 @@ import org.ikasan.spec.dashboard.ContextInstanceRestService;
 public class AgentInstanceRecoveryManager {
     private final ContextInstanceRestService contextInstanceRestService;
     private final long minutesToKeepRetrying;
+    private boolean agentRecoveryActive;
 
     Executor executor = Executors.newSingleThreadExecutor();
 
-    public AgentInstanceRecoveryManager(ContextInstanceRestService contextInstanceRestService, long minutesToKeepRetrying) {
+    public AgentInstanceRecoveryManager(ContextInstanceRestService contextInstanceRestService, long minutesToKeepRetrying, boolean agentRecoveryActive) {
         this.contextInstanceRestService = contextInstanceRestService;
         this.minutesToKeepRetrying = minutesToKeepRetrying;
+        this.agentRecoveryActive = agentRecoveryActive;
     }
 
     @PostConstruct
     public void init() {
-        executor.execute(new AgentRecoveryRunnable(this.contextInstanceRestService, this.minutesToKeepRetrying));
+        if (agentRecoveryActive) {
+            executor.execute(new AgentRecoveryRunnable(this.contextInstanceRestService, this.minutesToKeepRetrying));
+        }
     }
 }
