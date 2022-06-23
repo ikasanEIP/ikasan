7d65426faf2a1e8ce00334507362c6ae5a1ece11
Michael Stewart <mick@humeenterprises.co.uk>
2022-06-23 16:00:32 +0200

    IKASAN-2117 adding job dry run capability to the move file broker.

--- ikasaneip/ootb/module/scheduler-agent/jar/src/main/java/org/ikasan/ootb/scheduler/agent/module/component/broker/configuration/MoveFileBrokerConfiguration.java
+++ ikasaneip/ootb/module/scheduler-agent/jar/src/main/java/org/ikasan/ootb/scheduler/agent/module/component/broker/configuration/MoveFileBrokerConfiguration.java
@@ -3,6 +3,7 @@ package org.ikasan.ootb.scheduler.agent.module.component.broker.configuration;
 public class MoveFileBrokerConfiguration {
 
     private String moveDirectory;
+    private String jobName;
 
     public String getMoveDirectory() {
         return moveDirectory;
@@ -10,5 +11,13 @@ public class MoveFileBrokerConfiguration {
 
     public void setMoveDirectory(String moveDirectory) {
         this.moveDirectory = moveDirectory;
+    }
+
+    public String getJobName() {
+        return jobName;
+    }
+
+    public void setJobName(String jobName) {
+        this.jobName = jobName;
     }
 }
