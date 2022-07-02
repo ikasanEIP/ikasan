da246eb6399ca939ba096cd3e32cb1bc6400751c
Michael Stewart <mick@humeenterprises.co.uk>
2022-07-02 07:26:32 +0200

    IKASAN-2123 adding context profile spec

---
+++ ikasaneip/spec/service/scheduled/src/main/java/org/ikasan/spec/scheduled/profile/model/ContextProfileRecord.java
@@ -0,0 +1,43 @@
+package org.ikasan.spec.scheduled.profile.model;
+
+import java.util.List;
+
+public interface ContextProfileRecord {
+    String SYSTEM_OWNER = "SYSTEM_OWNER";
+
+    String getProfileName();
+
+    void setProfileName(String profileName);
+
+    String getContextName();
+
+    void setContextName(String contextName);
+
+    String getOwner();
+
+    void setOwner(String owner);
+
+    ContextProfile getContextProfile();
+
+    void setContextProfile(ContextProfile contextProfile);
+
+    List<String> getAccessRoles();
+
+    void setAccessRoles(List<String> accessRoles);
+
+    List<String> getAccessUsers();
+
+    void setAccessUsers(List<String> accessUsers);
+
+    long getCreatedDateTime();
+
+    void setCreatedDateTime(long createdDateTime);
+
+    long getModifiedDateTime();
+
+    void setModifiedDateTime(long createdDateTime);
+
+    String getModifiedBy();
+
+    void setModifiedBy(String modifiedBy);
+}
