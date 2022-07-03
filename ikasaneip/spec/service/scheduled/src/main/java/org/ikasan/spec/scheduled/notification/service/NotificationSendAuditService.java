f90227b9a57ac9b8a1e938559ba5761d25341948
gulser <Serkan.Guler2@uk.mizuho-sc.com>
2022-07-03 23:34:39 +0100

    IKASAN-2107 adding contextName to NotificationSendAudit

--- ikasaneip/spec/service/scheduled/src/main/java/org/ikasan/spec/scheduled/notification/service/NotificationSendAuditService.java
+++ ikasaneip/spec/service/scheduled/src/main/java/org/ikasan/spec/scheduled/notification/service/NotificationSendAuditService.java
@@ -2,7 +2,7 @@ package org.ikasan.spec.scheduled.notification.service;
 
 public interface NotificationSendAuditService<T> {
 
-    T find(String contextInstanceId, String jobName, String monitorType, String notifierType);
+    T find(String contextInstanceId, String contextName, String jobName, String monitorType, String notifierType);
 
     void save(T var1);
 }
