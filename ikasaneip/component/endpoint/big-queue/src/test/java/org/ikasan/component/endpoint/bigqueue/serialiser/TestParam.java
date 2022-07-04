1c5de0f6e333f51c03679264332315da87b5525f
John Otaegui <johnot@gmail.com>
2022-07-04 18:33:13 +1000

    IKASAN-2121 adding big queue message to big queues for easier management of queues. Bug fixxes.

---
+++ ikasaneip/component/endpoint/big-queue/src/test/java/org/ikasan/component/endpoint/bigqueue/serialiser/TestParam.java
@@ -0,0 +1,48 @@
+package org.ikasan.component.endpoint.bigqueue.serialiser;
+
+import org.apache.commons.lang3.builder.EqualsBuilder;
+import org.apache.commons.lang3.builder.HashCodeBuilder;
+import org.apache.commons.lang3.builder.ToStringBuilder;
+import org.apache.commons.lang3.builder.ToStringStyle;
+
+public class TestParam {
+    private String name;
+    private long value;
+
+    public TestParam() {}
+
+    public TestParam(String name, long value) {
+        this.name = name;
+        this.value = value;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public long getValue() {
+        return value;
+    }
+
+    public void setValue(long value) {
+        this.value = value;
+    }
+    @Override
+    public String toString() {
+        return ToStringBuilder.reflectionToString(this, ToStringStyle.SHORT_PREFIX_STYLE);
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        return EqualsBuilder.reflectionEquals(this, other);
+    }
+
+    @Override
+    public int hashCode() {
+        return HashCodeBuilder.reflectionHashCode(this);
+    }
+}
