91dacd49905342ffedb4d76f9e4cece5bef17c3e
gulser <Serkan.Guler2@uk.mizuho-sc.com>
2022-07-05 10:16:44 +0100

    IKASAN-2107 adding new EmailNotificationTemplateParameters

--- ikasaneip/spec/service/scheduled/src/main/java/org/ikasan/spec/scheduled/notification/model/EmailNotificationTemplateParameters.java
+++ ikasaneip/spec/service/scheduled/src/main/java/org/ikasan/spec/scheduled/notification/model/EmailNotificationTemplateParameters.java
@@ -2,7 +2,8 @@ package org.ikasan.spec.scheduled.notification.model;
 
 public enum EmailNotificationTemplateParameters {
 
-    EMAIL_BODY_LINK,
+    EMAIL_BODY_LINK_1,
+    EMAIL_BODY_LINK_2,
     EMAIL_BODY_TEXT,
     EMAIL_SUBJECT_TEXT,
     EMAIL_SUBJECT_LINK;
