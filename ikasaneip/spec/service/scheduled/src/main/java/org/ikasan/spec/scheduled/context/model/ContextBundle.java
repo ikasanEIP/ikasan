d57e51812d4d762d3b2c68a434ffc79978a56d7a
Michael Stewart <mick@humeenterprises.co.uk>
2022-07-06 06:08:27 +0200

    IKASAN-2123 adding context bundle spec and refining the context provision service.

---
+++ ikasaneip/spec/service/scheduled/src/main/java/org/ikasan/spec/scheduled/context/model/ContextBundle.java
@@ -0,0 +1,15 @@
+package org.ikasan.spec.scheduled.context.model;
+
+import org.ikasan.spec.scheduled.job.model.SchedulerJob;
+import org.ikasan.spec.scheduled.profile.model.ContextProfileRecord;
+
+import java.util.List;
+
+public interface ContextBundle {
+
+    ContextTemplate getContextTemplate();
+
+    List<SchedulerJob> getSchedulerJobs();
+
+    List<ContextProfileRecord> getContextProfiles();
+}
