9c81f7c0d17f0e5daa47ec6cdf4d78d954754556
gulser <Serkan.Guler2@uk.mizuho-sc.com>
2022-07-26 09:30:11 +0100

    IKASAN-2107 adding JobRunningTimesMonitor support

--- ikasaneip/spec/service/scheduled/src/main/java/org/ikasan/spec/scheduled/notification/model/EmailNotificationTemplateParameters.java
+++ ikasaneip/spec/service/scheduled/src/main/java/org/ikasan/spec/scheduled/notification/model/EmailNotificationTemplateParameters.java
@@ -5,6 +5,7 @@ public enum EmailNotificationTemplateParameters {
     EMAIL_BODY_LINK_1,
     EMAIL_BODY_LINK_2,
     EMAIL_BODY_TEXT,
+    EMAIL_BODY_MESSAGE_FROM_MONITOR,
     EMAIL_SUBJECT_TEXT,
     EMAIL_SUBJECT_LINK;
 }
