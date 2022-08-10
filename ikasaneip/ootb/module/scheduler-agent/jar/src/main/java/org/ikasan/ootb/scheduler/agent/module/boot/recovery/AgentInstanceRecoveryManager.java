b0728dd48f332cd5876c3162d9e85afc423378b5
John Otaegui <johnot@gmail.com>
2022-08-10 13:51:53 +1000

    IKASAN-2129 bug fixes and enhancements to core.

--- ikasaneip/ootb/module/scheduler-agent/jar/src/main/java/org/ikasan/ootb/scheduler/agent/module/boot/recovery/AgentInstanceRecoveryManager.java
+++ ikasaneip/ootb/module/scheduler-agent/jar/src/main/java/org/ikasan/ootb/scheduler/agent/module/boot/recovery/AgentInstanceRecoveryManager.java
@@ -6,24 +6,33 @@ import java.util.concurrent.Executors;
 import javax.annotation.PostConstruct;
 
 import org.ikasan.spec.dashboard.ContextInstanceRestService;
+import org.ikasan.spec.module.ModuleService;
 
 public class AgentInstanceRecoveryManager {
     private final ContextInstanceRestService contextInstanceRestService;
     private final long minutesToKeepRetrying;
     private boolean agentRecoveryActive;
+    private String moduleName;
+    private ModuleService moduleService;
 
     Executor executor = Executors.newSingleThreadExecutor();
 
-    public AgentInstanceRecoveryManager(ContextInstanceRestService contextInstanceRestService, long minutesToKeepRetrying, boolean agentRecoveryActive) {
+    public AgentInstanceRecoveryManager(ContextInstanceRestService contextInstanceRestService,
+                                        long minutesToKeepRetrying,
+                                        boolean agentRecoveryActive,
+                                        String moduleName,
+                                        ModuleService moduleService) {
         this.contextInstanceRestService = contextInstanceRestService;
         this.minutesToKeepRetrying = minutesToKeepRetrying;
         this.agentRecoveryActive = agentRecoveryActive;
+        this.moduleName = moduleName;
+        this.moduleService = moduleService;
     }
 
     @PostConstruct
     public void init() {
         if (agentRecoveryActive) {
-            executor.execute(new AgentRecoveryRunnable(this.contextInstanceRestService, this.minutesToKeepRetrying));
+            executor.execute(new AgentRecoveryRunnable(this.contextInstanceRestService, this.minutesToKeepRetrying, this.moduleName, this.moduleService));
         }
     }
 }
