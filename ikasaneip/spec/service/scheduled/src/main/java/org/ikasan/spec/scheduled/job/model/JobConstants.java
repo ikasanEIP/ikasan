86353733ef9b67f57fefef38edbb1ced9d42659c
Tan Richard <Richard.Tan@uk.mizuho-sc.com>
2022-09-21 10:55:40 +0100

    IKASAN-2137 - moved JobConstants from ikasan-dashboard project->ikasan-solr-client to specs

---
+++ ikasaneip/spec/service/scheduled/src/main/java/org/ikasan/spec/scheduled/job/model/JobConstants.java
@@ -0,0 +1,11 @@
+package org.ikasan.spec.scheduled.job.model;
+
+public interface JobConstants {
+    String FILE_EVENT_DRIVEN_JOB = "fileEventDrivenJob";
+    String INTERNAL_EVENT_DRIVEN_JOB = "internalEventDrivenJob";
+    String QUARTZ_SCHEDULE_DRIVEN_JOB = "quartzScheduleDrivenJob";
+
+    String FILE_EVENT_DRIVEN_JOB_INSTANCE = "fileEventDrivenJobInstance";
+    String INTERNAL_EVENT_DRIVEN_JOB_INSTANCE = "internalEventDrivenJobInstance";
+    String QUARTZ_SCHEDULE_DRIVEN_JOB_INSTANCE = "quartzScheduleDrivenJobInstance";
+}
