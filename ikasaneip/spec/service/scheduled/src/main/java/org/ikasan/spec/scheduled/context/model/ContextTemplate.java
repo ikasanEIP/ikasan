28a75cd04ced6c5f524017fbda344424192df2fc
Michael Stewart <mick@humeenterprises.co.uk>
2022-10-02 07:36:42 +0100

    IKASAN-2138 general development to disable context templates.

--- ikasaneip/spec/service/scheduled/src/main/java/org/ikasan/spec/scheduled/context/model/ContextTemplate.java
+++ ikasaneip/spec/service/scheduled/src/main/java/org/ikasan/spec/scheduled/context/model/ContextTemplate.java
@@ -6,4 +6,17 @@ import java.io.Serializable;
 
 public interface ContextTemplate extends Context<ContextTemplate, ContextParameter, SchedulerJob, JobLock>, Serializable {
 
+    /**
+     * Set context template to disabled.
+     *
+     * @param disabled
+     */
+    void setDisabled(boolean disabled);
+
+    /**
+     * Determine if the context template is disabled.
+     *
+     * @return
+     */
+    boolean isDisabled();
 }
