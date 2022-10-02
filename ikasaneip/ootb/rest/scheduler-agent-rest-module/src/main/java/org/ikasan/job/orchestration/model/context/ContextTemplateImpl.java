60ff7352f3d218fc4ce4a56ec6ef9bcd0a7bb349
Michael Stewart <mick@humeenterprises.co.uk>
2022-10-02 07:36:42 +0100

    IKASAN-2138 general development to disable context templates.

--- ikasaneip/ootb/rest/scheduler-agent-rest-module/src/main/java/org/ikasan/job/orchestration/model/context/ContextTemplateImpl.java
+++ ikasaneip/ootb/rest/scheduler-agent-rest-module/src/main/java/org/ikasan/job/orchestration/model/context/ContextTemplateImpl.java
@@ -6,4 +6,15 @@ import org.ikasan.spec.scheduled.context.model.JobLock;
 import org.ikasan.spec.scheduled.job.model.SchedulerJob;
 
 public class ContextTemplateImpl extends ContextImpl<ContextTemplate, ContextParameter, SchedulerJob, JobLock> implements ContextTemplate {
+    private boolean disabled = false;
+
+    @Override
+    public boolean isDisabled() {
+        return disabled;
+    }
+
+    @Override
+    public void setDisabled(boolean disabled) {
+        this.disabled = disabled;
+    }
 }
