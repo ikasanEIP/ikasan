f4b2a72af30a983770f578a77fd95a88c3bce7d4
Michael Stewart <mick@humeenterprises.co.uk>
2022-10-12 16:30:22 +0100

    IKASAN-2145 spec changes to support operator monitoring dashboard

--- ikasaneip/spec/service/scheduled/src/main/java/org/ikasan/spec/scheduled/instance/model/InstanceStatus.java
+++ ikasaneip/spec/service/scheduled/src/main/java/org/ikasan/spec/scheduled/instance/model/InstanceStatus.java
@@ -1,15 +1,72 @@
 package org.ikasan.spec.scheduled.instance.model;
 
 public enum InstanceStatus {
-    RUNNING,
-    SKIPPED_RUNNING,
-    COMPLETE,
-    SKIPPED_COMPLETE,
-    WAITING,
-    ERROR,
-    SKIPPED,
-    ON_HOLD,
-    RELEASED,
-    ENDED,
-    LOCK_QUEUED;
+    RUNNING {
+        @Override
+        public String getTranslationLabel() {
+            return "status.RUNNING";
+        }
+    },
+    SKIPPED_RUNNING{
+        @Override
+        public String getTranslationLabel() {
+            return "status.SKIPPED";
+        }
+    },
+    COMPLETE{
+        @Override
+        public String getTranslationLabel() {
+            return "status.COMPLETE";
+        }
+    },
+    SKIPPED_COMPLETE{
+        @Override
+        public String getTranslationLabel() {
+            return "status.SKIPPED";
+        }
+    },
+    WAITING{
+        @Override
+        public String getTranslationLabel() {
+            return "status.WAITING";
+        }
+    },
+    ERROR{
+        @Override
+        public String getTranslationLabel() {
+            return "status.ERROR";
+        }
+    },
+    SKIPPED{
+        @Override
+        public String getTranslationLabel() {
+            return "status.SKIPPED";
+        }
+    },
+    ON_HOLD{
+        @Override
+        public String getTranslationLabel() {
+            return "status.ON_HOLD";
+        }
+    },
+    RELEASED{
+        @Override
+        public String getTranslationLabel() {
+            return "status.RELEASED";
+        }
+    },
+    ENDED{
+        @Override
+        public String getTranslationLabel() {
+            return "status.ENDED";
+        }
+    },
+    LOCK_QUEUED{
+        @Override
+        public String getTranslationLabel() {
+            return "status.LOCK_QUEUED";
+        }
+    };
+
+    public abstract String getTranslationLabel();
 }
