dc7d92b731e1dd7b00867c855cc7687ef0227614
Michael Stewart <mick@humeenterprises.co.uk>
2022-11-26 09:49:45 +0000

    IKASAN-2164 finalising the security implementation and associated refactoring.

--- ikasaneip/spec/service/scheduled/src/main/java/org/ikasan/spec/scheduled/instance/model/SchedulerJobInstanceRecord.java
+++ ikasaneip/spec/service/scheduled/src/main/java/org/ikasan/spec/scheduled/instance/model/SchedulerJobInstanceRecord.java
@@ -2,55 +2,213 @@ package org.ikasan.spec.scheduled.instance.model;
 
 public interface SchedulerJobInstanceRecord {
 
+    /**
+     * Get the id.
+     *
+     * @return
+     */
     String getId();
 
+    /**
+     * Get the job type.
+     *
+     * @return
+     */
     String getType();
 
+    /**
+     * Get the job name.
+     *
+     * @return
+     */
     String getJobName();
 
+    /**
+     * Set the job name.
+     *
+     * @param jobName
+     */
     void setJobName(String jobName);
 
+    /**
+     * Get the context name.
+     *
+     * @return
+     */
     String getContextName();
 
+    /**
+     * Set the context name.
+     *
+     * @param contextName
+     */
     void setContextName(String contextName);
 
+    /**
+     * Get the child context name.
+     *
+     * @return
+     */
     String getChildContextName();
 
+    /**
+     * Set the child context name.
+     *
+     * @param childContextName
+     */
     void setChildContextName(String childContextName);
 
+    /**
+     * Get the context instance id.
+     *
+     * @return
+     */
     String getContextInstanceId();
 
+    /**
+     * Set the context instance id.
+     *
+     * @param contextInstanceId
+     */
     void setContextInstanceId(String contextInstanceId);
 
+    /**
+     * Get the scheduler job instance.
+     *
+     * @return
+     */
     SchedulerJobInstance getSchedulerJobInstance();
 
+    /**
+     * Set the scheduler job instance.
+     *
+     * @param schedulerJobInstance
+     */
     void setSchedulerJobInstance(SchedulerJobInstance schedulerJobInstance);
 
+    /**
+     * Get the status.
+     *
+     * @return
+     */
     String getStatus();
 
+    /**
+     * Set the status.
+     *
+     * @param status
+     */
     void setStatus(String status);
 
+    /**
+     * Set the flag to indicate that the job only targets
+     * its residing context.
+     *
+     * @param targetResidingContextOnly
+     */
     void setTargetResidingContextOnly(boolean targetResidingContextOnly);
 
+    /**
+     * Flag to indicate that the job targets its residing contect only.
+     * @return
+     */
     boolean isTargetResidingContextOnly();
 
+    /**
+     * Set the floag to indicate that the job participates in a lock.
+     *
+     * @param participatesInLock
+     */
     void setParticipatesInLock(boolean participatesInLock);
 
+    /**
+     * Flag to indicate if the job participates in a lock.
+     *
+     * @return
+     */
     boolean isParticipatesInLock();
 
+    /**
+     * Get the start time.
+     *
+     * @return
+     */
+    long getStartTime();
+
+    /**
+     * Set the start time.
+     *
+     * @return
+     */
+    void setStartTime(long endTime);
+
+    /**
+     * Get the end time.
+     *
+     * @return
+     */
+    long getEndTime();
+
+    /**
+     * Set the end time.
+     *
+     * @return
+     */
+    void setEndTime(long endTime);
+
+    /**
+     * Get the created time stamp of the job instance.
+     *
+     * @return
+     */
     long getTimestamp();
 
+    /**
+     * Set the created time stamp of the job instance.
+     *
+     * @param timestamp
+     */
     void setTimestamp(long timestamp);
 
+    /**
+     * Get the modified time stamp of the job instance.
+     *
+     * @return
+     */
     long getModifiedTimestamp();
 
+    /**
+     * Set the created time stamp of the job instance.
+     *
+     * @param timestamp
+     */
     void setModifiedTimestamp(long timestamp);
 
+    /**
+     * Get the user name of who modified the job.
+     *
+     * @return
+     */
     String getModifiedBy();
 
+    /**
+     * Set the user name of who modified the job.
+     *
+     * @param modifiedBy
+     */
     void setModifiedBy(String modifiedBy);
 
+    /**
+     * Get the username of who manually submitted the job.
+     *
+     * @return
+     */
     String getManuallySubmittedBy();
 
+    /**
+     * Set the username of who manually submitted the job.
+     *
+     * @param manuallySubmittedBy
+     */
     void setManuallySubmittedBy(String manuallySubmittedBy);
 }
