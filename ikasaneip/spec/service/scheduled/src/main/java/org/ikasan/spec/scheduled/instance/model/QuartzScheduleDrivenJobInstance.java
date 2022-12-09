7be2d1d1f005fefc03394a837ecdd301fb253870
Michael Stewart <mick@humeenterprises.co.uk>
2022-12-09 11:27:58 +0000

    IKASAN-2127 spec changes work in progress

--- ikasaneip/spec/service/scheduled/src/main/java/org/ikasan/spec/scheduled/instance/model/QuartzScheduleDrivenJobInstance.java
+++ ikasaneip/spec/service/scheduled/src/main/java/org/ikasan/spec/scheduled/instance/model/QuartzScheduleDrivenJobInstance.java
@@ -1,45 +1,10 @@
 package org.ikasan.spec.scheduled.instance.model;
 
+import org.ikasan.spec.scheduled.job.model.QuartzScheduleDrivenJob;
 import org.ikasan.spec.scheduled.job.model.SchedulerJob;
 
 import java.io.Serializable;
 import java.util.Map;
 
-public interface QuartzScheduleDrivenJobInstance extends SchedulerJobInstance, Serializable {
-
-    String getJobGroup();
-
-    void setJobGroup(String jobGroup);
-
-    String getCronExpression();
-
-    void setCronExpression(String cronExpression);
-
-    String getTimeZone();
-
-    void setTimeZone(String timeZone);
-
-    Map<String, String> getPassthroughProperties();
-
-    void setPassthroughProperties(Map<String, String> passthroughProperties);
-
-    boolean isEager();
-
-    int getMaxEagerCallbacks();
-
-    void setMaxEagerCallbacks(int maxEagerCallbacks);
-
-    void setEager(boolean eager);
-
-    void setIgnoreMisfire(boolean ignoreMisfire);
-
-    boolean isIgnoreMisfire();
-
-    long getRecoveryTolerance();
-
-    void setRecoveryTolerance(long recoveryTolerance);
-
-    boolean isPersistentRecovery();
-
-    void setPersistentRecovery(boolean persistentRecovery);
+public interface QuartzScheduleDrivenJobInstance extends SchedulerJobInstance, QuartzScheduleDrivenJob, Serializable {
 }
