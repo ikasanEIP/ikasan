b95795281e5fcfe2dee0b3ab1acf5d29053db39b
Michael Stewart <mick@humeenterprises.co.uk>
2022-12-23 08:43:45 +0000

    IKASAN-2178 adding repeatable flag to command execution jobs

--- ikasaneip/spec/service/scheduled/src/main/java/org/ikasan/spec/scheduled/job/model/InternalEventDrivenJob.java
+++ ikasaneip/spec/service/scheduled/src/main/java/org/ikasan/spec/scheduled/job/model/InternalEventDrivenJob.java
@@ -6,40 +6,127 @@ import java.io.Serializable;
 import java.util.List;
 
 public interface InternalEventDrivenJob extends SchedulerJob, Serializable {
+    /**
+     * Get the list of return codes that will be deemed successful.
+     *
+     * @return
+     */
     List<String> getSuccessfulReturnCodes();
 
+    /**
+     * Set the list of return codes that will be deemed successful.
+     * @param successfulReturnCodes
+     */
     void setSuccessfulReturnCodes(List<String> successfulReturnCodes);
 
+    /**
+     * Get the directory that the process will be executed from.
+     *
+     * @return
+     */
     String getWorkingDirectory();
 
+    /**
+     * Set the directory that the process will be executed from.
+     *
+     * @param workingDirectory
+     */
     void setWorkingDirectory(String workingDirectory);
 
+    /**
+     * Get the contents of the command that will be executed by the process.
+     *
+     * @return
+     */
     String getCommandLine();
 
+    /**
+     * Set the contents of the command that will be executed by the process.
+     * @param commandLine
+     */
     void setCommandLine(String commandLine);
 
+    /**
+     * Get the minimum execution time that the job is expected to run for.
+     *
+     * @return
+     */
     long getMinExecutionTime();
 
+    /**
+     * Set the minimum execution time that the job is expected to run for.
+     *
+     * @param minExecutionTime
+     */
     void setMinExecutionTime(long minExecutionTime);
 
+    /**
+     * Get the maximum execution time that the job is expected to run for.
+     *
+     * @return
+     */
     long getMaxExecutionTime();
 
+    /**
+     * Set the maximum execution time that the job is expected to run for.
+     *
+     * @param maxExecutionTime
+     */
     void setMaxExecutionTime(long maxExecutionTime);
 
+    /**
+     * Get the parameters that are passed to the process.
+     *
+     * @return
+     */
     List<ContextParameter> getContextParameters();
 
+    /**
+     * Set the parameters that are passed to the process.
+     *
+     * @param contextParameters
+     */
     void setContextParameters(List<ContextParameter> contextParameters);
 
+    /**
+     * Get the days of the week that the job will run. 0 -> 6 == Sunday -> Saturday
+     *
+     * @return
+     */
     List<Integer> getDaysOfWeekToRun();
 
+    /**
+     * Set the days of the week that the job will run. 0 -> 6 == Sunday -> Saturday
+     *
+     * @param daysOfWeekToRun
+     */
     void setDaysOfWeekToRun(List<Integer> daysOfWeekToRun);
 
+    /**
+     * Set flog to indicate that the job should only run in the context that it resides within.
+     *
+     * @param targetResidingContextOnly
+     */
     void setTargetResidingContextOnly(boolean targetResidingContextOnly);
 
+    /**
+     * Get flog to indicate that the job should only run in the context that it resides within.
+     * @return
+     */
     boolean isTargetResidingContextOnly();
 
+    /**
+     * Set flag to indicate that a job participates in a lock.
+     *
+     * @param participatesInLock
+     */
     void setParticipatesInLock(boolean participatesInLock);
 
+    /**
+     * Get flag to indicate that a job participates in a lock.
+     *
+     * @return
+     */
     boolean isParticipatesInLock();
 
     /**
@@ -58,4 +145,18 @@ public interface InternalEventDrivenJob extends SchedulerJob, Serializable {
      * @return
      */
     void setExecutionEnvironmentProperties(String executionEnvironmentProperties);
+
+    /**
+     * Set flag to indicate that a job can be repeated.
+     *
+     * @param jobRepeatable
+     */
+    void setJobRepeatable(boolean jobRepeatable);
+
+    /**
+     * Set flag to indicate that a job can be repeated.
+     *
+     * @return
+     */
+    boolean isJobRepeatable();
 }
