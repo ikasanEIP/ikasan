fc420f5c35553eca4849d71a9bcc914ce4b09aa1
davidhilton68 <davidhilton68@gmail.com>
2023-01-04 13:47:10 +0000

    IKASAN-2172 Update agent recovery to be driven by agentName and instance ID

--- ikasaneip/ootb/rest/scheduler-agent-rest-dashboard-client/src/main/java/org/ikasan/ootb/scheduler/agent/rest/client/context/ContextInstanceRestServiceImpl.java
+++ ikasaneip/ootb/rest/scheduler-agent-rest-dashboard-client/src/main/java/org/ikasan/ootb/scheduler/agent/rest/client/context/ContextInstanceRestServiceImpl.java
@@ -54,39 +54,67 @@ public class ContextInstanceRestServiceImpl extends AbstractRestServiceImpl impl
         this.mapper = ObjectMapperFactory.newInstance();
     }
 
+    // @Mick I don't think we need these just yet, remove ?
+//    @Override
+//    public Map<String, ContextInstance> getAll() {
+//        if (this.token == null) {
+//            authenticate(moduleName);
+//        }
+//        HttpHeaders headers = super.createHttpHeaders(moduleName);
+//        HttpEntity entity = new HttpEntity(headers);
+//        try {
+//            ResponseEntity<String> response = restTemplate.exchange(url + "/getAll", HttpMethod.GET, entity, String.class);
+//
+//            return this.mapper.readValue(response.getBody(), new TypeReference<>() {
+//            });
+//        } catch (RestClientException | JsonProcessingException e) {
+//            String message = "Issue getting all context instances for url [" + url + "]  with response [{" + e.getLocalizedMessage() + "}]";
+//            logger.error(message);
+//            throw new EndpointException(e);
+//        }
+//    }
+//
+//    @Override
+//    public Map<String, ContextInstance> getByContextInstanceId(String correlationId) {
+//        if (this.token == null) {
+//            authenticate(moduleName);
+//        }
+//        HttpHeaders headers = super.createHttpHeaders(moduleName);
+//        HttpEntity entity = new HttpEntity(headers);
+//        try {
+//            String urlTemplate = UriComponentsBuilder.fromHttpUrl(url + "/getByContextInstanceId")
+//                .queryParam("correlationId", "{correlationId}")
+//                .encode()
+//                .toUriString();
+//            Map<String, String> parameters = new HashMap<>() {{
+//                put("correlationId", correlationId);
+//            }};
+//
+//            ResponseEntity<String> response = restTemplate.exchange(urlTemplate, HttpMethod.GET, entity, String.class, parameters);
+//
+//            return this.mapper.readValue(response.getBody(), new TypeReference<>() {
+//            });
+//
+//        } catch (RestClientException | JsonProcessingException e) {
+//            String message = "Issue getting context instance for url [" + url + "]  with response [{" + e.getLocalizedMessage() + "}]";
+//            logger.error(message);
+//            throw new EndpointException(e);
+//        }
+//    }
     @Override
-    public Map<String, ContextInstance> getAll() {
+    public Map<String, ContextInstance> getByAgentName(String agentName) {
         if (this.token == null) {
             authenticate(moduleName);
         }
         HttpHeaders headers = super.createHttpHeaders(moduleName);
-        HttpEntity entity = new HttpEntity(headers);
+        HttpEntity<HttpHeaders> entity = new HttpEntity<>(headers);
         try {
-            ResponseEntity<String> response = restTemplate.exchange(url + "/getAll", HttpMethod.GET, entity, String.class);
-
-            return this.mapper.readValue(response.getBody(), new TypeReference<>() {
-            });
-        } catch (RestClientException | JsonProcessingException e) {
-            String message = "Issue getting all context instances for url [" + url + "]  with response [{" + e.getLocalizedMessage() + "}]";
-            logger.error(message);
-            throw new EndpointException(e);
-        }
-    }
-
-    @Override
-    public Map<String, ContextInstance> getByContextId(String correlationId) {
-        if (this.token == null) {
-            authenticate(moduleName);
-        }
-        HttpHeaders headers = super.createHttpHeaders(moduleName);
-        HttpEntity entity = new HttpEntity(headers);
-        try {
-            String urlTemplate = UriComponentsBuilder.fromHttpUrl(url + "/getByContextName")
-                .queryParam("contextName", "{contextName}")
+            String urlTemplate = UriComponentsBuilder.fromHttpUrl(url + "/getByAgentName")
+                .queryParam("agentName", "{agentName}")
                 .encode()
                 .toUriString();
             Map<String, String> parameters = new HashMap<>() {{
-                put("contextName", correlationId);
+                put("agentName", agentName);
             }};
 
             ResponseEntity<String> response = restTemplate.exchange(urlTemplate, HttpMethod.GET, entity, String.class, parameters);
