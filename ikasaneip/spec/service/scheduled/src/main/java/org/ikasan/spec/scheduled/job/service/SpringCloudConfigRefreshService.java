65db91e2c93a5e51ec6c98e05ae64bcb66c91048
Tan Richard <Richard.Tan@uk.mizuho-sc.com>
2023-01-13 12:01:08 +0000

    IKASAN-2181 - spec for SpringCloudConfigRefreshService

---
+++ ikasaneip/spec/service/scheduled/src/main/java/org/ikasan/spec/scheduled/job/service/SpringCloudConfigRefreshService.java
@@ -0,0 +1,18 @@
+package org.ikasan.spec.scheduled.job.service;
+
+/**
+ * Scheduler can host configuration properties from multiple repositories within a single Spring Cloud Config Service. 
+ * This service allows the dashboard to call the config service to refresh multiple repos that host these
+ * configuration. This is based on:
+ * https://docs.spring.io/spring-cloud-config/docs/current/reference/html/#_pattern_matching_and_multiple_repositories
+ */
+public interface SpringCloudConfigRefreshService {
+
+    /**
+     * Request config based on an applicationPattern from the available Spring Cloud Config Service.
+     * This will request it on the default profile.
+     * @param contextUrl url path to config service
+     * @param applicationPattern application patterned required for multi repository
+     */
+    void refreshConfigRepo(String contextUrl, String applicationPattern);
+}
