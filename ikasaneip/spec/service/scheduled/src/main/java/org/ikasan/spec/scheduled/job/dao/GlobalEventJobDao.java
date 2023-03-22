ad39b91a01a1ff55aad1bcaeaa1d491fe52632bd
Michael Stewart <mick@humeenterprises.co.uk>
2023-03-22 02:28:29 +0000

    IKASAN-2207 spec changes to support skipping global events

--- ikasaneip/spec/service/scheduled/src/main/java/org/ikasan/spec/scheduled/job/dao/GlobalEventJobDao.java
+++ ikasaneip/spec/service/scheduled/src/main/java/org/ikasan/spec/scheduled/job/dao/GlobalEventJobDao.java
@@ -3,6 +3,8 @@ package org.ikasan.spec.scheduled.job.dao;
 import org.ikasan.spec.scheduled.job.model.GlobalEventJobRecord;
 import org.ikasan.spec.search.SearchResults;
 
+import java.util.List;
+
 public interface GlobalEventJobDao <T extends GlobalEventJobRecord> {
 
     SearchResults<? extends T> findAll(int limit, int offset);
@@ -12,4 +14,23 @@ public interface GlobalEventJobDao <T extends GlobalEventJobRecord> {
     T findById(String id);
 
     void save(T record);
+
+    /**
+     * Set a InternalEventDrivenJobRecord to skip. If targetResidingContextOnly is set
+     * on the InternalEventDrivenJob the childContextNames contain the specific child
+     * contexts that the job will be skipped in.
+     *
+     * @param jobRecord
+     * @param childContextNames
+     * @param actor
+     */
+    void skip(T jobRecord, List<String> childContextNames, String actor);
+
+    /**
+     * Set a InternalEventDrivenJobRecord to enabled. Enabled is skipped == false.
+     *
+     * @param jobRecord
+     * @param actor
+     */
+    void enable(T jobRecord, String actor);
 }
