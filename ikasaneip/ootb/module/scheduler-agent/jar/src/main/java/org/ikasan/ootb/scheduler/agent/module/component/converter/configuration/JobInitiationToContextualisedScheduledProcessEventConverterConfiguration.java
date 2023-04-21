32c0c262760b4ccb1262e61f9137656ac49dc0b4
Michael Stewart <mick@humeenterprises.co.uk>
2023-04-21 06:10:11 +0100

    IKASAN-2227 add configurable option to hash scheduler agent process log file names.

---
+++ ikasaneip/ootb/module/scheduler-agent/jar/src/main/java/org/ikasan/ootb/scheduler/agent/module/component/converter/configuration/JobInitiationToContextualisedScheduledProcessEventConverterConfiguration.java
@@ -0,0 +1,14 @@
+package org.ikasan.ootb.scheduler.agent.module.component.converter.configuration;
+
+public class JobInitiationToContextualisedScheduledProcessEventConverterConfiguration {
+
+    private boolean hashProcessLogFilenames = false;
+
+    public boolean isHashProcessLogFilenames() {
+        return hashProcessLogFilenames;
+    }
+
+    public void setHashProcessLogFilenames(boolean hashProcessLogFilenames) {
+        this.hashProcessLogFilenames = hashProcessLogFilenames;
+    }
+}
