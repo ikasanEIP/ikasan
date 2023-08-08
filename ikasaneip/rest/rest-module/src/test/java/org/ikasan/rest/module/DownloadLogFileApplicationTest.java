9142664fa6f847aaeeb8ad8ef5856676fc80a833
Tan Richard <Richard.Tan@uk.mizuho-sc.com>
2023-08-08 15:09:28 +0100

    IKASAN-2270 - add parameter to limit the file size that can be downloaded

--- ikasaneip/rest/rest-module/src/test/java/org/ikasan/rest/module/DownloadLogFileApplicationTest.java
+++ ikasaneip/rest/rest-module/src/test/java/org/ikasan/rest/module/DownloadLogFileApplicationTest.java
@@ -56,7 +56,7 @@ public class DownloadLogFileApplicationTest {
 
         System.setProperty("user.dir", String.valueOf(Path.of(System.getProperty("user.dir"), "src", "test", "resources", "data")));
 
-        RequestBuilder requestBuilder = MockMvcRequestBuilders.get("/rest/logs/listLogFiles")
+        RequestBuilder requestBuilder = MockMvcRequestBuilders.get("/rest/logs/listLogFiles?maxFileSize=20971520")
             .accept(MediaType.APPLICATION_JSON_VALUE)
             .contentType(MediaType.APPLICATION_JSON_VALUE);
 
@@ -75,13 +75,30 @@ public class DownloadLogFileApplicationTest {
 
         System.setProperty("user.dir", String.valueOf(Path.of(System.getProperty("user.dir"), "src", "test", "resources", "data")));
 
-        RequestBuilder requestBuilder = MockMvcRequestBuilders.get("/rest/logs/listLogFiles")
+        RequestBuilder requestBuilder = MockMvcRequestBuilders.get("/rest/logs/listLogFiles?maxFileSize=20971520")
             .accept(MediaType.APPLICATION_JSON_VALUE);
 
         mockMvc.perform(requestBuilder)
             .andExpect(status().is(HttpStatus.BAD_REQUEST.value()))
             .andReturn();
 
+    }
+
+    @Test
+    @WithMockUser(authorities = "WebServiceAdmin")
+    public void listLogFilesTooBigTest() throws Exception {
+
+        System.setProperty("user.dir", String.valueOf(Path.of(System.getProperty("user.dir"), "src", "test", "resources", "data")));
+
+        RequestBuilder requestBuilder = MockMvcRequestBuilders.get("/rest/logs/listLogFiles?maxFileSize=20")
+            .accept(MediaType.APPLICATION_JSON_VALUE)
+            .contentType(MediaType.APPLICATION_JSON_VALUE);
+
+        MvcResult results = mockMvc.perform(requestBuilder)
+            .andExpect(status().is(HttpStatus.OK.value()))
+            .andReturn();
+        //application.log is over 20 bytes (24bytes), so do not return
+        Assert.assertTrue(results.getResponse().getContentAsString().contains("h2.log"));
     }
 
     @Test
@@ -100,7 +117,7 @@ public class DownloadLogFileApplicationTest {
             }
         }
 
-        RequestBuilder requestBuilder = MockMvcRequestBuilders.get("/rest/logs/downloadLogFile?fullFilePath="+fullPathName)
+        RequestBuilder requestBuilder = MockMvcRequestBuilders.get("/rest/logs/downloadLogFile?maxFileSize=20971520&fullFilePath="+fullPathName)
             .accept(MediaType.APPLICATION_OCTET_STREAM_VALUE)
             .contentType(MediaType.APPLICATION_OCTET_STREAM_VALUE);
 
@@ -111,6 +128,37 @@ public class DownloadLogFileApplicationTest {
             .andReturn();
 
         Assert.assertEquals(results.getResponse().getContentAsString(), "Some application logging");
+        Assert.assertEquals(results.getResponse().getContentType(), MediaType.APPLICATION_OCTET_STREAM_VALUE);
+        Assert.assertEquals(results.getResponse().getHeader("Content-Disposition"), "attachment;filename=application.log");
+    }
+
+    @Test
+    @WithMockUser(authorities = "WebServiceAdmin")
+    public void downloadLogFileTooBigTest() throws Exception {
+
+        Path path = Path.of(System.getProperty("user.dir"), "src", "test", "resources", "data", "logs");
+
+        File[] filesList = new File(path.toUri()).listFiles();
+        String fullPathName = "";
+        for (File file : filesList) {
+            if (file.getAbsoluteFile().isFile()) {
+                if (file.getName().equals("application.log")) {
+                    fullPathName = String.valueOf(file.getAbsoluteFile());
+                }
+            }
+        }
+
+        RequestBuilder requestBuilder = MockMvcRequestBuilders.get("/rest/logs/downloadLogFile?maxFileSize=20&fullFilePath="+fullPathName)
+            .accept(MediaType.APPLICATION_OCTET_STREAM_VALUE)
+            .contentType(MediaType.APPLICATION_OCTET_STREAM_VALUE);
+
+        MvcResult results = mockMvc.perform(requestBuilder)
+            .andExpect(request().asyncStarted())
+            .andDo(MvcResult::getAsyncResult)
+            .andExpect(status().is(HttpStatus.INTERNAL_SERVER_ERROR.value()))
+            .andReturn();
+
+        Assert.assertEquals(results.getResponse().getContentAsString(), "Not able to download the file for [application.log]. Log file maybe too big to download. Maximum size allowed is 20 bytes.");
         Assert.assertEquals(results.getResponse().getContentType(), MediaType.APPLICATION_OCTET_STREAM_VALUE);
         Assert.assertEquals(results.getResponse().getHeader("Content-Disposition"), "attachment;filename=application.log");
     }
@@ -130,7 +178,7 @@ public class DownloadLogFileApplicationTest {
             }
         }
 
-        RequestBuilder requestBuilder = MockMvcRequestBuilders.get("/rest/logs/downloadLogFile?fullFilePath="+fullPathName)
+        RequestBuilder requestBuilder = MockMvcRequestBuilders.get("/rest/logs/downloadLogFile?maxFileSize=20971520&fullFilePath="+fullPathName)
             .accept(MediaType.APPLICATION_OCTET_STREAM_VALUE)
             .contentType(MediaType.APPLICATION_OCTET_STREAM_VALUE);
 
@@ -140,7 +188,7 @@ public class DownloadLogFileApplicationTest {
             .andExpect(status().is(HttpStatus.INTERNAL_SERVER_ERROR.value()))
             .andReturn();
 
-        Assert.assertEquals(results.getResponse().getContentAsString(), "Not able to download the file for [some-random-file.txt]");
+        Assert.assertEquals(results.getResponse().getContentAsString(), "Not able to download the file for [some-random-file.txt]. Log file maybe too big to download. Maximum size allowed is 20971520 bytes.");
         Assert.assertEquals(results.getResponse().getContentType(), MediaType.APPLICATION_OCTET_STREAM_VALUE);
         Assert.assertEquals(results.getResponse().getHeader("Content-Disposition"), "attachment;filename=some-random-file.txt");
     }
@@ -162,7 +210,7 @@ public class DownloadLogFileApplicationTest {
             }
         }
 
-        RequestBuilder requestBuilder = MockMvcRequestBuilders.get("/rest/logs/downloadLogFile?fullFilePath="+fullPathName)
+        RequestBuilder requestBuilder = MockMvcRequestBuilders.get("/rest/logs/downloadLogFile?maxFileSize=20971520&fullFilePath="+fullPathName)
             .accept(MediaType.APPLICATION_OCTET_STREAM_VALUE)
             .contentType(MediaType.APPLICATION_OCTET_STREAM_VALUE);
 
@@ -194,7 +242,7 @@ public class DownloadLogFileApplicationTest {
             }
         }
 
-        RequestBuilder requestBuilder = MockMvcRequestBuilders.get("/rest/logs/downloadLogFile?fullFilePath="+fullPathName)
+        RequestBuilder requestBuilder = MockMvcRequestBuilders.get("/rest/logs/downloadLogFile?maxFileSize=20971520&fullFilePath="+fullPathName)
             .accept(MediaType.APPLICATION_OCTET_STREAM_VALUE)
             .contentType(MediaType.APPLICATION_OCTET_STREAM_VALUE);
 
