99e1c15fbfef40c0d282ca419147cc36fbe57afc
Michael Stewart <mick@humeenterprises.co.uk>
2023-10-12 10:24:01 +0100

    IKASAN-2307 updating filter to allow start and end time filtering along with context instance names.

--- ikasaneip/spec/service/scheduled/src/main/java/org/ikasan/spec/scheduled/instance/model/ContextInstanceSearchFilter.java
+++ ikasaneip/spec/service/scheduled/src/main/java/org/ikasan/spec/scheduled/instance/model/ContextInstanceSearchFilter.java
@@ -1,9 +1,15 @@
 package org.ikasan.spec.scheduled.instance.model;
 
+import java.util.List;
+
 public interface ContextInstanceSearchFilter {
     String getContextSearchFilter();
 
     public void setContextSearchFilter(String contextSearchFilter);
+
+    List<String> getContextInstanceNames();
+
+    public void setContextInstanceNames(List<String> contextInstanceNames);
 
     public String getContextInstanceId();
 
@@ -19,11 +25,27 @@ public interface ContextInstanceSearchFilter {
 
     public long getStartTime();
 
-    public void setStartTime(long createdTimestamp);
+    public void setStartTime(long timestamp);
+
+    public long getStartTimeStart();
+
+    public void setStartTimeStart(long timestamp);
+
+    public long getStartTimeEnd();
+
+    public void setStartTimeEnd(long timestamp);
 
     public long getEndTime();
 
-    public void setEndTime(long modifiedTimestamp);
+    public void setEndTime(long timestamp);
+
+    public long getEndTimeStart();
+
+    public void setEndTimeStart(long timestamp);
+
+    public long getEndTimeEnd();
+
+    public void setEndTimeEnd(long timestamp);
 
     public String getStatus();
 
