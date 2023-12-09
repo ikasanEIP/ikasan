8ba69168655ea79d7feec009daf00142521e7f22
Michael Stewart <mick@humeenterprises.co.uk>
2023-12-09 07:49:11 +0000

    IKASAN-2318 sorting out ikasan module - module

---
+++ ikasaneip/module/src/test/java/org/ikasan/module/IkasanModuleTestAutoConfiguration.java
@@ -0,0 +1,46 @@
+package org.ikasan.module;
+
+import jakarta.persistence.EntityManagerFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.ImportResource;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+import javax.sql.DataSource;
+import java.util.Properties;
+
+@Configuration
+@EnableTransactionManagement
+@ImportResource("/test-transaction.xml")
+public class IkasanModuleTestAutoConfiguration
+{
+    @Bean(name = {"ikasan.xads", "ikasan.ds"})
+    public DataSource ikasanDataSource() {
+        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+        dataSource.setDriverClassName("org.h2.Driver");
+        dataSource.setUrl("jdbc:h2:mem:testdb;DB_CLOSE_DELAY=-1");
+        dataSource.setUsername("sa");
+        dataSource.setPassword("sa");
+        return dataSource;
+    }
+
+    @Bean
+    public PlatformTransactionManager transactionManager(EntityManagerFactory moduleEntityManager) {
+        JpaTransactionManager transactionManager = new JpaTransactionManager();
+        transactionManager.setEntityManagerFactory(moduleEntityManager);
+        return transactionManager;
+    }
+
+
+    @Bean
+    Properties platformJpaProperties() {
+        Properties platformJpaProperties = new Properties();
+        platformJpaProperties.put("hibernate.show_sql", false);
+        platformJpaProperties.put("hibernate.hbm2ddl.auto", "create-drop");
+
+        return platformJpaProperties;
+    }
+}
