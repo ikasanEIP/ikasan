d74217381052e6b88b1701f75773cb266fd1e750
Michael Stewart <mick@humeenterprises.co.uk>
2024-02-22 09:03:28 +0000

    IKASAN-2332 adding strip to string comparison to see if eol characters are causing issues with travis build.

--- ikasaneip/platform/ikasan-migration/ikasan-h2-migration/src/test/java/org/ikasan/h2/migration/H2DbExtractPostProcessorTest.java
+++ ikasaneip/platform/ikasan-migration/ikasan-h2-migration/src/test/java/org/ikasan/h2/migration/H2DbExtractPostProcessorTest.java
@@ -1,6 +1,7 @@
 package org.ikasan.h2.migration;
 
 import org.apache.commons.io.FileUtils;
+import org.junit.Ignore;
 import org.junit.Test;
 import org.springframework.util.ResourceUtils;
 
@@ -10,7 +11,7 @@ import static org.junit.Assert.assertEquals;
 
 public class H2DbExtractPostProcessorTest {
     @Test
-    public void test() throws IOException {
+    public void test_post_process_sucess() throws IOException {
 
         H2DbExtractPostProcessor postProcessor
             = new H2DbExtractPostProcessor();
@@ -18,7 +19,7 @@ public class H2DbExtractPostProcessorTest {
             ResourceUtils.getFile("./target/post-processed.sql"));
 
         assertEquals("The files differ!",
-            FileUtils.readFileToString(ResourceUtils.getFile("classpath:migration/expected.sql"), "utf-8"),
-            FileUtils.readFileToString(ResourceUtils.getFile("./target/post-processed.sql"), "utf-8"));
+            FileUtils.readFileToString(ResourceUtils.getFile("classpath:migration/expected.sql"), "utf-8").strip(),
+            FileUtils.readFileToString(ResourceUtils.getFile("./target/post-processed.sql"), "utf-8").strip());
     }
 }
