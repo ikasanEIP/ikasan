5cb933663e1c3731cedcf8961a0dd516569ded24
Michael Stewart <mick@humeenterprises.co.uk>
2024-05-01 03:27:07 +0100

    IKASAN-2369 back filling db backup feature after testing.

--- ikasaneip/platform/ikasan-h2-backup/src/test/java/org/ikasan/backup/h2/service/H2DatabaseValidatorTest.java
+++ ikasaneip/platform/ikasan-h2-backup/src/test/java/org/ikasan/backup/h2/service/H2DatabaseValidatorTest.java
@@ -5,38 +5,21 @@ import org.ikasan.backup.h2.IkasanBackupAutoTestConfiguration;
 import org.ikasan.backup.h2.exception.H2DatabaseValidationException;
 import org.ikasan.backup.h2.exception.InvalidH2ConnectionUrlException;
 import org.ikasan.backup.h2.model.H2DatabaseBackup;
-import org.ikasan.backup.h2.persistence.model.H2DatabaseBackupManifest;
-import org.ikasan.backup.h2.persistence.service.H2DatabaseBackupManifestService;
 import org.ikasan.backup.h2.util.H2BackupUtils;
-import org.ikasan.backup.h2.util.H2ConnectionUrlUtils;
-import org.h2.tools.Server;
-import org.ikasan.spec.flow.Flow;
-import org.ikasan.spec.housekeeping.HousekeepingJob;
-import org.ikasan.spec.module.Module;
 import org.junit.After;
-import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.mockito.Mockito;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-import org.springframework.util.SocketUtils;
+import org.springframework.test.util.TestSocketUtils;
 
 import java.io.IOException;
-import java.nio.file.FileSystems;
-import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.sql.SQLException;
-import java.util.List;
-import java.util.concurrent.TimeUnit;
-import java.util.stream.Collectors;
 
-import static org.awaitility.Awaitility.with;
 import static org.junit.Assert.fail;
-import static org.mockito.Mockito.*;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(classes = {IkasanBackupAutoConfiguration.class, IkasanBackupAutoTestConfiguration.class})
@@ -46,7 +29,7 @@ public class H2DatabaseValidatorTest {
     private static final String DATABASE_DIRECTORY = Paths.get("./target","database-dir").toString();
     private static final String CORRUPT_DATABASE_DIRECTORY = Paths.get("./target","corrupt-database-dir").toString();
 
-    int port = SocketUtils.findAvailableTcpPort();
+    int port = TestSocketUtils.findAvailableTcpPort();
 
     @Before
     public void setup() throws IOException, SQLException {
@@ -72,7 +55,7 @@ public class H2DatabaseValidatorTest {
         h2DatabaseBackup.setDbBackupBaseDirectory(DATABASE_DIRECTORY);
 
         H2DatabaseValidator h2BackupService = new H2DatabaseValidator(h2DatabaseBackup);
-        h2BackupService.runDatabaseValidationTest("./src/test/resources/data/corrupt-db.zip", SocketUtils.findAvailableTcpPort());
+        h2BackupService.runDatabaseValidationTest("./src/test/resources/data/corrupt-db.zip", TestSocketUtils.findAvailableTcpPort());
     }
 
     @Test
@@ -88,7 +71,7 @@ public class H2DatabaseValidatorTest {
         H2DatabaseValidator h2BackupService = new H2DatabaseValidator(h2DatabaseBackup);
 
         try {
-            h2BackupService.runDatabaseValidationTest("./src/test/resources/data/esb-backup-20240321-06-11-00.zip", SocketUtils.findAvailableTcpPort());
+            h2BackupService.runDatabaseValidationTest("./src/test/resources/data/esb-backup-20240321-06-11-00.zip", TestSocketUtils.findAvailableTcpPort());
         }
         catch (Exception e) {
             fail(String.format("No exception should be thrown here! However the following exception occurred [%s]", e.getMessage()));
