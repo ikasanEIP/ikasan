8e55a134e5380876a41f37480a888e997b3a05c6
hidavi <David.Hilton3@uk.mizuho-sc.com>
2024-05-23 16:44:42 +0100

    IKASAN-2381 Support nohup style on Powershell.

--- ikasaneip/ootb/module/scheduler-agent/jar/src/main/java/org/ikasan/ootb/scheduler/agent/module/service/processtracker/dao/ProcessStatusDao.java
+++ ikasaneip/ootb/module/scheduler-agent/jar/src/main/java/org/ikasan/ootb/scheduler/agent/module/service/processtracker/dao/ProcessStatusDao.java
@@ -6,6 +6,8 @@ public interface ProcessStatusDao {
 
     String createCommandScript(String processIdentity, String scriptPostfix, String commandsToBeExecuted) throws IOException ;
 
+    String createCommandWrapperScript(String processIdentity, String scriptPostfix, String commandsToBeExecuted) throws IOException ;
+
     String getPersistedReturnCode(String processIdentity);
 
     void removeScriptAndResult(String processIdentity, String scriptPostfix) throws IOException ;
