cf0f8392a990063697843af8d1df2b812adff19a
Michael Stewart <mick@humeenterprises.co.uk>
2024-05-25 08:47:26 +0100

    IKASAN-2394 forget to revert value column name back and add keyword ignore to test DB url

--- ikasaneip/wiretap/src/test/java/org/ikasan/WiretapTestAutoConfiguration.java
+++ ikasaneip/wiretap/src/test/java/org/ikasan/WiretapTestAutoConfiguration.java
@@ -55,7 +55,7 @@ public class WiretapTestAutoConfiguration {
     public DataSource ikasanXaDataSource() {
         DriverManagerDataSource dataSource = new DriverManagerDataSource();
         dataSource.setDriverClassName("org.h2.Driver");
-        dataSource.setUrl("jdbc:h2:mem:testdb;DB_CLOSE_DELAY=-1");
+        dataSource.setUrl("jdbc:h2:mem:testdb;DB_CLOSE_DELAY=-1;NON_KEYWORDS=VALUE");
         dataSource.setUsername("sa");
         dataSource.setPassword("sa");
         return dataSource;
