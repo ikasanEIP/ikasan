04461661ae23f3e24bb913a9f38bb5b7d1dae843
Michael Stewart <mick@humeenterprises.co.uk>
2024-05-29 09:38:00 +0100

    IKASAN-2392 fixing inadvertently committed commented out unit test.

--- ikasaneip/platform/ikasan-h2-backup/src/test/java/org/ikasan/backup/h2/service/H2DatabaseBackupManifestServiceImplTest.java
+++ ikasaneip/platform/ikasan-h2-backup/src/test/java/org/ikasan/backup/h2/service/H2DatabaseBackupManifestServiceImplTest.java
@@ -49,40 +49,40 @@ public class H2DatabaseBackupManifestServiceImplTest {
         Assert.assertEquals(1234567890L, found.getFailureTimestamp());
     }
 
-//    @Test
-//    public void test_save_and_delete_success() {
-//        H2DatabaseBackupManifestService h2DatabaseBackupManifestService
-//                = H2DatabaseBackupManifestService.instance(PERSISTENCE_DIR, "ESB");
-//
-//        H2DatabaseBackupManifest h2DatabaseBackupManifest = new H2DatabaseBackupManifest();
-//        h2DatabaseBackupManifest.setBackupName("ESB");
-//        h2DatabaseBackupManifest.setFailureMessage("Error has occurred!");
-//        h2DatabaseBackupManifest.setRetryCount(1);
-//        h2DatabaseBackupManifest.setFailureTimestamp(1234567890L);
-//
-//        h2DatabaseBackupManifestService.save(h2DatabaseBackupManifest);
-//
-//        H2DatabaseBackupManifest found = h2DatabaseBackupManifestService.find();
-//
-//        Assert.assertNotNull(found);
-//
-//        h2DatabaseBackupManifestService.delete();
-//
-//        found = h2DatabaseBackupManifestService.find();
-//
-//        Assert.assertNull(found);
-//    }
-//
-//
-//    // Helper method to delete all files between tests to provide isolation
+    @Test
+    public void test_save_and_delete_success() {
+        H2DatabaseBackupManifestService h2DatabaseBackupManifestService
+                = H2DatabaseBackupManifestService.instance(PERSISTENCE_DIR, "ESB");
+
+        H2DatabaseBackupManifest h2DatabaseBackupManifest = new H2DatabaseBackupManifest();
+        h2DatabaseBackupManifest.setBackupName("ESB");
+        h2DatabaseBackupManifest.setFailureMessage("Error has occurred!");
+        h2DatabaseBackupManifest.setRetryCount(1);
+        h2DatabaseBackupManifest.setFailureTimestamp(1234567890L);
+
+        h2DatabaseBackupManifestService.save(h2DatabaseBackupManifest);
+
+        H2DatabaseBackupManifest found = h2DatabaseBackupManifestService.find();
+
+        Assert.assertNotNull(found);
+
+        h2DatabaseBackupManifestService.delete();
+
+        found = h2DatabaseBackupManifestService.find();
+
+        Assert.assertNull(found);
+    }
+
+
+    // Helper method to delete all files between tests to provide isolation
     private void deleteFilesInDirectory() {
-//        Path directoryPath = Paths.get(PERSISTENCE_DIR);
-//        File directory = directoryPath.toFile();
-//        if (directory.exists()) {
-//            for (File file : directory.listFiles()) {
-//                file.delete();
-//            }
-//        }
+        Path directoryPath = Paths.get(PERSISTENCE_DIR);
+        File directory = directoryPath.toFile();
+        if (directory.exists()) {
+            for (File file : directory.listFiles()) {
+                file.delete();
+            }
+        }
     }
 
 
