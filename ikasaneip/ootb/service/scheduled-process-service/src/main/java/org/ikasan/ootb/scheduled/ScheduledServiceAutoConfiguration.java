a23a85449455aa66c099e8d91f4bec1924dabb77
Michael Stewart <mick@humeenterprises.co.uk>
2024-06-17 06:24:58 +0100

    IKASAN-2415 removing redundant legacy flows from the first iteration of the scheduler.

--- ikasaneip/ootb/service/scheduled-process-service/src/main/java/org/ikasan/ootb/scheduled/ScheduledServiceAutoConfiguration.java
+++ ikasaneip/ootb/service/scheduled-process-service/src/main/java/org/ikasan/ootb/scheduled/ScheduledServiceAutoConfiguration.java
@@ -40,71 +40,9 @@
  */
 package org.ikasan.ootb.scheduled;
 
-import org.hibernate.SessionFactory;
-import org.ikasan.harvesting.HarvestingJobImpl;
-import org.ikasan.housekeeping.HousekeepingJobImpl;
-import org.ikasan.ootb.scheduled.dao.HibernateScheduledProcessEventDao;
-import org.ikasan.ootb.scheduled.service.ScheduledProcessServiceImpl;
-import org.ikasan.spec.dashboard.DashboardRestService;
-import org.ikasan.spec.harvest.HarvestService;
-import org.ikasan.spec.harvest.HarvestingJob;
-import org.ikasan.spec.housekeeping.HousekeepService;
-import org.ikasan.spec.housekeeping.HousekeepingJob;
-import org.ikasan.spec.scheduled.event.dao.ScheduledProcessEventDao;
-import org.ikasan.spec.scheduled.event.service.ScheduledProcessEventService;
-import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.context.annotation.Bean;
-import org.springframework.core.env.Environment;
-import org.springframework.orm.hibernate5.LocalSessionFactoryBean;
-
-import javax.sql.DataSource;
-import java.io.IOException;
-import java.util.Map;
-import java.util.Properties;
-
 /**
  * Scheduler service related configuration required by the scheduler ootb module.
  */
-public class ScheduledServiceAutoConfiguration
-{
-    public static final String SCHEDULED_PROCESS_EVENTS_PATH = "/rest/harvest/scheduled";
+public class ScheduledServiceAutoConfiguration {
 
-    @Bean
-    public HarvestingJob scheduledProcessEventJob(HarvestService scheduledProcessService, Environment environment, DashboardRestService scheduleProcessEventsDashboardRestService)
-    {
-        return new HarvestingJobImpl("scheduledProcessEventJob", scheduledProcessService, environment, scheduleProcessEventsDashboardRestService);
-    }
-
-    @Bean
-    public HousekeepingJob scheduledProcessEventHousekeepingJob(HousekeepService scheduledProcessService, Environment environment)
-    {
-        return new HousekeepingJobImpl("scheduledProcessEventHousekeepingJob", scheduledProcessService, environment);
-    }
-
-    @Bean
-    public ScheduledProcessEventService scheduledProcessService(ScheduledProcessEventDao scheduledProcessEventDao) {
-        return new ScheduledProcessServiceImpl(scheduledProcessEventDao);
-    }
-
-    @Bean
-    public ScheduledProcessEventDao scheduledProcessEventDao(SessionFactory sessionFactory) {
-        HibernateScheduledProcessEventDao scheduledProcessEventDao = new HibernateScheduledProcessEventDao();
-        scheduledProcessEventDao.setSessionFactory(sessionFactory   );
-
-        return scheduledProcessEventDao;
-    }
-
-    @Bean
-    public SessionFactory sessionFactory(@Qualifier("ikasanXaDataSourceInstance") DataSource dataSource
-        , @Qualifier("platformHibernateProperties") Map hibernateProperties) throws IOException {
-        LocalSessionFactoryBean localSessionFactoryBean = new LocalSessionFactoryBean();
-        localSessionFactoryBean.setDataSource(dataSource);
-        Properties properties = new Properties();
-        properties.putAll(hibernateProperties);
-        localSessionFactoryBean.setHibernateProperties(properties);
-        localSessionFactoryBean.setMappingResources("org/ikasan/ootb/scheduled/model/ScheduledProcessEventImpl.hbm.xml");
-        localSessionFactoryBean.afterPropertiesSet();
-
-        return localSessionFactoryBean.getObject();
-    }
 }
