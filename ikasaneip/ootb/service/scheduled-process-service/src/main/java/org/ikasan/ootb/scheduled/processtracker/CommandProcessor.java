b9327a31638539f114602839d1c998cba5e36dc3
davidhilton68 <davidhilton68@gmail.com>
2024-06-25 19:01:30 +0100

    IKASAN-2423 tighten up cmd/powershell launcher, add ksh support.

--- ikasaneip/ootb/service/scheduled-process-service/src/main/java/org/ikasan/ootb/scheduled/processtracker/CommandProcessor.java
+++ ikasaneip/ootb/service/scheduled-process-service/src/main/java/org/ikasan/ootb/scheduled/processtracker/CommandProcessor.java
@@ -4,15 +4,18 @@ import org.apache.commons.lang3.StringUtils;
 import org.apache.commons.lang3.SystemUtils;
 
 public enum CommandProcessor {
-    WINDOWS_CMD("cmd.exe", "/c", ".cmd"),
-    WINDOWS_POWSHELL("powershell.exe", "-Command", ".ps1"),
-    UNIX_BASH("/bin/bash", "-c", ".sh"),
-    UNKNOWN("", "", "") ;
+    WINDOWS_CMD("CMD","cmd.exe", "/c", ".cmd"),
+    WINDOWS_POWSHELL("POWERSHELL", "powershell.exe", "-Command", ".ps1"),
+    UNIX_BASH("BASH","/bin/bash", "-c", ".sh"),
+    UNIX_KSH("KSH","/bin/ksh", "-c", ".ksh"),
+    UNKNOWN("", "", "", "") ;
+    private final String title;
     private final String name;
     private final String cliFlags;
     private final String scriptFilePostfix;
 
-    CommandProcessor(String name, String cliFlags, String scriptFilePostfix) {
+    CommandProcessor(String title, String name, String cliFlags, String scriptFilePostfix) {
+        this.title = title;
         this.name = name;
         this.cliFlags = cliFlags;
         this.scriptFilePostfix = scriptFilePostfix;
@@ -26,18 +29,23 @@ public enum CommandProcessor {
     public String getName() { return name; }
 
     public static CommandProcessor getCommandProcessor(String[] executionEnvironmentProperties) {
-        CommandProcessor commandProcessor = CommandProcessor.UNIX_BASH;
-        String executionEnvironmentPropertiesStr = executionEnvironmentProperties == null ? "" : String.join("", executionEnvironmentProperties);
+        CommandProcessor commandProcessor = null;
+        String executionEnvironmentPropertiesStr = executionEnvironmentProperties == null ? "" : String.join("", executionEnvironmentProperties).toUpperCase();
         if (StringUtils.isNotBlank(executionEnvironmentPropertiesStr)) {
-            if (executionEnvironmentPropertiesStr.contains(WINDOWS_POWSHELL.name)) {
-                commandProcessor =  CommandProcessor.WINDOWS_POWSHELL;
-            } else if (executionEnvironmentPropertiesStr.contains(WINDOWS_CMD.name)) {
-                commandProcessor =  CommandProcessor.WINDOWS_CMD;
+            for (CommandProcessor processor : CommandProcessor.values()) {
+                if (executionEnvironmentPropertiesStr.contains(processor.title.toUpperCase())) {
+                    commandProcessor = processor;
+                    break;
+                }
             }
-        } else {
+        }
+
+        if (commandProcessor == null || commandProcessor == CommandProcessor.UNKNOWN) {
             // Guess based on current OS
             if (SystemUtils.OS_NAME.contains("Windows")) {
                 commandProcessor =  CommandProcessor.WINDOWS_CMD;
+            } else {
+                commandProcessor =  CommandProcessor.UNIX_BASH;
             }
         }
         return commandProcessor;
