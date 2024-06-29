b2ffe71105bcaffd97632d191996cfa4aa3047f4
Michael Stewart <mick@humeenterprises.co.uk>
2024-06-29 06:53:45 +0100

    IKASAN-2421 start and terminal jobs WIP

--- ikasaneip/spec/service/scheduled/src/main/java/org/ikasan/spec/scheduled/job/dao/GlobalEventJobDao.java
+++ ikasaneip/spec/service/scheduled/src/main/java/org/ikasan/spec/scheduled/job/dao/GlobalEventJobDao.java
@@ -7,12 +7,39 @@ import java.util.List;
 
 public interface GlobalEventJobDao <T extends GlobalEventJobRecord> {
 
+    /**
+     * Finds all records of type T within the specified limit and offset.
+     *
+     * @param limit The maximum number of records to retrieve.
+     * @param offset The number of records to skip from the beginning of the result set.
+     * @return A {@link SearchResults} object containing a list of records and additional information about the query.
+     */
     SearchResults<? extends T> findAll(int limit, int offset);
 
+    /**
+     * Finds all records of type T within the specified limit and offset based on the given contextId.
+     *
+     * @param contextId The ID of the context to filter the records by.
+     * @param limit The maximum number of records to retrieve.
+     * @param offset The number of records to skip from the beginning of the result set.
+     * @return A {@link SearchResults} object containing a list of records and additional information about the query.
+     */
     SearchResults<? extends T> findByContext(String contextId, int limit, int offset);
 
+    /**
+     * Finds a record of type T by its ID.
+     *
+     * @param id The ID of the record to find.
+     * @return The record of type T with the specified ID, or null if not found.
+     */
     T findById(String id);
 
+    /**
+     * Saves a record of type T.
+     *
+     * @param record The record to be saved.
+     * @param <T>    The type of record to save.
+     */
     void save(T record);
 
     /**
