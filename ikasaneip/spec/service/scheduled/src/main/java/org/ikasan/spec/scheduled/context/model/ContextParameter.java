37bf0e47c7c49130211fa25a42987c1f8baf7a97
Michael Stewart <mick@humeenterprises.co.uk>
2024-07-03 00:01:20 +0100

    IKASAN-2424 context start and terminal jobs WIP

--- ikasaneip/spec/service/scheduled/src/main/java/org/ikasan/spec/scheduled/context/model/ContextParameter.java
+++ ikasaneip/spec/service/scheduled/src/main/java/org/ikasan/spec/scheduled/context/model/ContextParameter.java
@@ -4,11 +4,31 @@ import java.io.Serializable;
 
 public interface ContextParameter extends Serializable {
 
+    /**
+     * Retrieves the name of the object.
+     *
+     * @return The name of the object as a String.
+     */
     String getName();
 
+    /**
+     * Sets the name of the object.
+     *
+     * @param name the new name to set
+     */
     void setName(String name);
 
+    /**
+     * Retrieves the default value of the context parameter.
+     *
+     * @return The default value of the context parameter.
+     */
     String getDefaultValue();
 
+    /**
+     * Sets the default value for the context parameter.
+     *
+     * @param defaultValue The default value for the context parameter
+     */
     void setDefaultValue(String defaultValue);
 }
