c99f998dadc3f319c26e22e2426bc7fe6829eead
Michael Stewart <mick@humeenterprises.co.uk>
2024-07-03 00:01:20 +0100

    IKASAN-2424 context start and terminal jobs WIP

--- ikasaneip/ootb/rest/scheduler-agent-rest-module/src/main/java/org/ikasan/job/orchestration/model/context/JobDependencyImpl.java
+++ ikasaneip/ootb/rest/scheduler-agent-rest-module/src/main/java/org/ikasan/job/orchestration/model/context/JobDependencyImpl.java
@@ -6,20 +6,35 @@ import org.ikasan.spec.scheduled.context.model.LogicalGrouping;
 public class JobDependencyImpl implements JobDependency {
     private String jobIdentifier;
     private LogicalGrouping logicalGrouping;
+    private boolean eventDependency = false;
 
+    @Override
     public String getJobIdentifier() {
         return jobIdentifier;
     }
 
+    @Override
     public void setJobIdentifier(String jobIdentifier) {
         this.jobIdentifier = jobIdentifier;
     }
 
+    @Override
     public LogicalGrouping getLogicalGrouping() {
         return logicalGrouping;
     }
 
+    @Override
     public void setLogicalGrouping(LogicalGrouping logicalGrouping) {
         this.logicalGrouping = logicalGrouping;
+    }
+
+    @Override
+    public boolean isEventDependency() {
+        return eventDependency;
+    }
+
+    @Override
+    public void setEventDependency(boolean eventDependency) {
+        this.eventDependency = eventDependency;
     }
 }
--- ikasaneip/spec/service/scheduled/src/main/java/org/ikasan/spec/scheduled/context/model/ScheduledContextViewRecord.java
+++ ikasaneip/spec/service/scheduled/src/main/java/org/ikasan/spec/scheduled/context/model/ScheduledContextViewRecord.java
@@ -4,29 +4,94 @@ import java.io.Serializable;
 
 public interface ScheduledContextViewRecord extends Serializable {
 
+    /**
+     * Retrieves the identifier of an object.
+     *
+     * @return The identifier of the object as a String.
+     */
     String getId();
 
+    /**
+     * Returns the name of the parent context.
+     *
+     * @return the name of the parent context
+     */
     String getParentContextName();
 
+    /**
+     * Sets the parent context name of this ScheduledContextViewRecord.
+     *
+     * @param parentContextName the parent context name to be set
+     */
     void setParentContextName(String parentContextName);
 
+    /**
+     * Retrieves the name of the context.
+     *
+     * @return the name of the context
+     */
     String getContextName();
 
+    /**
+     * Sets the name of the context.
+     *
+     * @param contextName the name of the context to be set
+     */
     void setContextName(String contextName);
 
+    /**
+     * Retrieves the context view of the scheduled context view record.
+     *
+     * @return the context view of the scheduled context view record
+     */
     String getContextView();
 
+    /**
+     * Sets the context view for the ScheduledContextViewRecord.
+     *
+     * @param context the new context view to set
+     */
     void setContextView(String context);
 
+    /**
+     * Retrieves the timestamp representing the current system time.
+     *
+     * @return A long value representing the current system time in milliseconds since the epoch (January 1, 1970, 00:00:00 GMT).
+     */
     long getTimestamp();
 
+    /**
+     * Sets the timestamp for the given object.
+     *
+     * @param timestamp the timestamp to be set
+     */
     void setTimestamp(long timestamp);
 
+    /**
+     * Retrieves the timestamp when the record was last modified.
+     *
+     * @return The timestamp when the record was last modified.
+     */
     long getModifiedTimestamp();
 
+    /**
+     * Sets the modified timestamp for the ScheduledContextViewRecord.
+     *
+     * @param timestamp The new modified timestamp.
+     */
     void setModifiedTimestamp(long timestamp);
 
+    /**
+     * Retrieves the username of the person who last modified the record.
+     *
+     * @return The username of the person who last modified the record.
+     */
     String getModifiedBy();
 
+    /**
+     * Sets the username of the user who last modified the record.
+     *
+     * @param modifiedBy the username of the user who last modified the record
+     */
     void setModifiedBy(String modifiedBy);
 }
