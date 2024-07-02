e310321f9b83b480f9de8a5d5d47f397150d9f1c
Michael Stewart <mick@humeenterprises.co.uk>
2024-07-03 00:01:20 +0100

    IKASAN-2424 context start and terminal jobs WIP

--- ikasaneip/spec/service/scheduled/src/main/java/org/ikasan/spec/scheduled/context/model/JobDependency.java
+++ ikasaneip/spec/service/scheduled/src/main/java/org/ikasan/spec/scheduled/context/model/JobDependency.java
@@ -4,11 +4,48 @@ import java.io.Serializable;
 
 public interface JobDependency extends Serializable {
 
-    public String getJobIdentifier();
+    /**
+     * Checks if the current object has a dependency on an event.
+     *
+     * @return {@code true} if the job is dependent on an event, {@code false} otherwise
+     */
+    boolean isEventDependency();
 
-    public void setJobIdentifier(String jobIdentifier);
+    /**
+     * Sets the event dependency for the JobDependency object.
+     *
+     * The event dependency indicates whether the job has a dependency on an external event.
+     * If set to true, it means the job is dependent on an event, otherwise false.
+     *
+     * @param eventDependency the boolean value indicating the event dependency
+     */
+    void setEventDependency(boolean eventDependency);
 
-    public LogicalGrouping getLogicalGrouping();
+    /**
+     * Retrieves the job identifier associated with this JobDependency object.
+     *
+     * @return The job identifier.
+     */
+    String getJobIdentifier();
 
-    public void setLogicalGrouping(LogicalGrouping logicalGrouping);
+    /**
+     * Sets the job identifier for this object.
+     *
+     * @param jobIdentifier the job identifier to be set
+     */
+    void setJobIdentifier(String jobIdentifier);
+
+    /**
+     * Retrieves the logical grouping associated with this object.
+     *
+     * @return The logical grouping associated with this object.
+     */
+    LogicalGrouping getLogicalGrouping();
+
+    /**
+     * Sets the logical grouping for the given LogicalGrouping object.
+     *
+     * @param logicalGrouping the logical grouping to be set
+     */
+    void setLogicalGrouping(LogicalGrouping logicalGrouping);
 }
