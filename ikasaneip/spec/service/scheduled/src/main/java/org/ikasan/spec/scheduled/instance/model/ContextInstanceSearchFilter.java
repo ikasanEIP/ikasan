d2aa4a3bc2378d93984d617878b5c3423183fa36
Michael Stewart <mick@humeenterprises.co.uk>
2024-07-17 12:23:52 +0100

    IKASAN-2421 changes to support start, terminal and local jobs

--- ikasaneip/spec/service/scheduled/src/main/java/org/ikasan/spec/scheduled/instance/model/ContextInstanceSearchFilter.java
+++ ikasaneip/spec/service/scheduled/src/main/java/org/ikasan/spec/scheduled/instance/model/ContextInstanceSearchFilter.java
@@ -3,51 +3,174 @@ package org.ikasan.spec.scheduled.instance.model;
 import java.util.List;
 
 public interface ContextInstanceSearchFilter {
+    /**
+     * Returns the context search filter.
+     *
+     * @return The context search filter as a String.
+     */
     String getContextSearchFilter();
 
+    /**
+     * Sets the context search filter for the ContextInstanceSearchFilter.
+     *
+     * @param contextSearchFilter the context search filter to be set
+     */
     public void setContextSearchFilter(String contextSearchFilter);
 
+    /**
+     * Returns a list of context instance names.
+     *
+     * @return A list of strings representing context instance names.
+     */
     List<String> getContextInstanceNames();
 
+    /**
+     * Sets the list of context instance names to be used as search criteria.
+     *
+     * @param contextInstanceNames the list of context instance names
+     */
     public void setContextInstanceNames(List<String> contextInstanceNames);
 
+    /**
+     * Retrieves the context instance ID.
+     *
+     * @return The context instance ID.
+     */
     public String getContextInstanceId();
 
+    /**
+     * Sets the context instance ID.
+     *
+     * @param contextInstanceId The ID of the context instance to set.
+     */
     public void setContextInstanceId(String contextInstanceId);
 
+    /**
+     * Retrieves the timestamp when the instance was created.
+     *
+     * @return The timestamp when the instance was created.
+     */
     public long getCreatedTimestamp();
 
+    /**
+     * Sets the timestamp when the instance was created.
+     *
+     * @param createdTimestamp the timestamp when the instance was created
+     */
     public void setCreatedTimestamp(long createdTimestamp);
 
+    /**
+     * Returns the modified timestamp of the object.
+     *
+     * @return The modified timestamp of the object.
+     */
     public long getModifiedTimestamp();
 
+    /**
+     * Sets the modified timestamp of the context instance search filter.
+     *
+     * @param modifiedTimestamp the modified timestamp to set
+     */
     public void setModifiedTimestamp(long modifiedTimestamp);
 
+    /**
+     * Retrieves the start time of the context instance search filter.
+     *
+     * @return the start time of the context instance search filter
+     */
     public long getStartTime();
 
+    /**
+     * Sets the start time of the context instance search filter.
+     * The start time is represented by a Unix timestamp.
+     * This method updates the value of the start time in the context instance search filter.
+     *
+     * @param timestamp the start time to set, represented as a Unix timestamp
+     */
     public void setStartTime(long timestamp);
 
+    /**
+     * Gets the start time range filter for the ContextInstanceSearchFilter.
+     *
+     * @return The start time start value.
+     */
     public long getStartTimeStart();
 
+    /**
+     * Sets the start time start parameter for filtering the search results.
+     * This parameter represents the lower bound of the start time range.
+     *
+     * @param timestamp the start time start parameter to set
+     */
     public void setStartTimeStart(long timestamp);
 
+    /**
+     * Gets the end time range for filtering the context instances.
+     *
+     * @return The end time range for filtering the context instances.
+     */
     public long getStartTimeEnd();
 
+    /**
+     * Sets the end timestamp for the start time criteria in the context instance search filter.
+     *
+     * @param timestamp The end timestamp for the start time criteria
+     */
     public void setStartTimeEnd(long timestamp);
 
+    /**
+     * Returns the end time.
+     *
+     * @return the end time as a long value.
+     */
     public long getEndTime();
 
+    /**
+     * Sets the end time of the context instance search filter.
+     *
+     * @param timestamp the new end time
+     */
     public void setEndTime(long timestamp);
 
+    /**
+     * Retrieves the start time of the end time range for filtering the search results.
+     *
+     * @return The start time of the end time range.
+     */
     public long getEndTimeStart();
 
+    /**
+     * Sets the start time for filtering the search results based on the end time.
+     *
+     * @param timestamp the start time for filtering
+     */
     public void setEndTimeStart(long timestamp);
 
+    /**
+     * Retrieves the end time end value of the search filter for context instances.
+     *
+     * @return The end time end value of the search filter.
+     */
     public long getEndTimeEnd();
 
+    /**
+     * Sets the end time constraint for the search filter.
+     *
+     * @param timestamp The timestamp to set as the end time constraint.
+     */
     public void setEndTimeEnd(long timestamp);
 
+    /**
+     * Retrieves the status of the object.
+     *
+     * @return The status of the object.
+     */
     public String getStatus();
 
+    /**
+     * Sets the status of the object.
+     *
+     * @param status the new status to be set
+     */
     public void setStatus(String status);
 }
--- ikasaneip/spec/service/scheduled/src/main/java/org/ikasan/spec/scheduled/instance/model/SchedulerJobInstance.java
+++ ikasaneip/spec/service/scheduled/src/main/java/org/ikasan/spec/scheduled/instance/model/SchedulerJobInstance.java
@@ -7,31 +7,101 @@ import java.io.Serializable;
 
 public interface SchedulerJobInstance extends SchedulerJob, StatefulEntity, Serializable {
 
+    /**
+     * Returns the context instance ID of the SchedulerJobInstance.
+     *
+     * @return the context instance ID
+     */
     String getContextInstanceId();
 
+    /**
+     * Sets the ID of the context instance.
+     *
+     * @param contextInstanceId The ID of the context instance to set.
+     */
     void setContextInstanceId(String contextInstanceId);
 
+    /**
+     * Retrieves the name of the child context that this job instance belongs to.
+     *
+     * @return The name of the child context.
+     */
     String getChildContextName();
 
+    /**
+     * Set the child context name for the SchedulerJobInstance.
+     *
+     * @param childContextName the name of the child context
+     */
     void setChildContextName(String childContextName);
 
+    /**
+     * Checks whether the job is being held.
+     *
+     * @return true if the job is being held, false otherwise.
+     */
     boolean isHeld();
 
+    /**
+     * Sets the hold flag for the job instance.
+     *
+     * @param held the value indicating if the job instance is on hold
+     */
     void setHeld(boolean held);
 
+    /**
+     * Checks if the initiation event is raised for the given SchedulerJobInstance.
+     *
+     * @return true if the initiation event is raised, false otherwise.
+     */
     boolean isInitiationEventRaised();
 
+    /**
+     * Sets the value indicating whether the initiation event has been raised for the SchedulerJobInstance.
+     *
+     * @param initiationEventRaised true if the initiation event has been raised; otherwise, false.
+     */
     void setInitiationEventRaised(boolean initiationEventRaised) ;
 
+    /**
+     * Retrieves the status of the current instance.
+     *
+     * @return The current status of the instance.
+     */
     InstanceStatus getStatus();
 
+    /**
+     * Sets the status of an instance.
+     *
+     * @param status The new status to be set.
+     */
     void setStatus(InstanceStatus status);
 
+    /**
+     * Retrieves the ScheduledProcessEvent associated with the SchedulerJobInstance.
+     *
+     * @return the ScheduledProcessEvent of the SchedulerJobInstance
+     */
     ScheduledProcessEvent getScheduledProcessEvent();
 
+    /**
+     * Sets the scheduled process event for the scheduler job instance.
+     *
+     * @param scheduledProcessEvent The scheduled process event to set.
+     */
     void setScheduledProcessEvent(ScheduledProcessEvent scheduledProcessEvent);
 
+    /**
+     * Returns whether the job instance should be skipped.
+     *
+     * @return true if the job instance should be skipped, false otherwise.
+     */
     boolean isSkip();
 
+    /**
+     * Sets whether the job should be skipped or not.
+     *
+     * @param skip true if the job should be skipped, false otherwise
+     */
     void setSkip(boolean skip);
 }
