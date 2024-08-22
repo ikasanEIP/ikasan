83136b4db9c5eda2d7e14d98bee76396a72b6bd5
nlohani <nehalohani87@gmail.com>
2024-08-22 22:25:27 +0100

    IKASAN-2452 h2 backup port binding issue fix

--- ikasaneip/platform/ikasan-h2-backup/src/test/java/org/ikasan/backup/h2/service/H2DatabaseValidatorTest.java
+++ ikasaneip/platform/ikasan-h2-backup/src/test/java/org/ikasan/backup/h2/service/H2DatabaseValidatorTest.java
@@ -14,6 +14,7 @@ import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.util.TestSocketUtils;
+import org.h2.tools.Server;
 
 import java.io.IOException;
 import java.nio.file.Paths;
