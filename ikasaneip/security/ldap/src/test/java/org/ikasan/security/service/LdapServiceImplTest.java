0fb2848c5f7534cd2b16201910295ef010fed310
Michael Stewart <mick@humeenterprises.co.uk>
2025-01-27 05:29:12 +0000

    IKASAN-2541 expose ldap connect and read time out configuration values

--- ikasaneip/security/ldap/src/test/java/org/ikasan/security/service/LdapServiceImplTest.java
+++ ikasaneip/security/ldap/src/test/java/org/ikasan/security/service/LdapServiceImplTest.java
@@ -1,19 +1,51 @@
 package org.ikasan.security.service;
 
+import com.unboundid.ldap.listener.Base64PasswordEncoderOutputFormatter;
+import com.unboundid.ldap.listener.ClearInMemoryPasswordEncoder;
+import com.unboundid.ldap.listener.InMemoryDirectoryServer;
+import com.unboundid.ldap.listener.InMemoryDirectoryServerConfig;
+import com.unboundid.ldap.sdk.LDAPException;
+import com.unboundid.ldif.LDIFReader;
+import org.ikasan.security.LdapSecurityTestAutoConfiguration;
+import org.ikasan.security.SecurityAutoConfiguration;
 import org.ikasan.security.dao.SecurityDao;
 import org.ikasan.security.dao.UserDao;
+import org.ikasan.security.dao.constants.SecurityConstants;
 import org.ikasan.security.model.AuthenticationMethod;
+import org.junit.After;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
-import org.mockito.InjectMocks;
+import org.junit.runner.RunWith;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
+import org.springframework.beans.factory.annotation.Autowired;
+import javax.naming.NamingException;
+
+import org.springframework.ldap.UncategorizedLdapException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
-import static org.mockito.Mockito.when;
+import org.springframework.ldap.CommunicationException;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
+import java.io.File;
+import java.io.IOException;
+import java.net.SocketTimeoutException;
+import java.util.Optional;
+import java.util.stream.Stream;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(classes = {SecurityAutoConfiguration.class, LdapSecurityTestAutoConfiguration.class})
 public class LdapServiceImplTest {
+
+    private InMemoryDirectoryServer inMemoryDirectoryServer;
+
+    @Autowired
+    InMemoryDirectoryServerConfig inMemoryDirectoryServerConfig;
+
+    @Autowired
+    private String ldapServerUrl;
 
     @Mock
     private SecurityDao securityDao;
@@ -24,15 +56,92 @@ public class LdapServiceImplTest {
     @Mock
     private PasswordEncoder passwordEncoder;
 
-    @Mock
-    private AuthenticationMethod authenticationMethod;
-
-    @InjectMocks
     public LdapServiceImpl ldapService;
 
     @Before
-    public void setup() {
+    public void setup() throws LDAPException, IOException {
         MockitoAnnotations.initMocks(this);
+        this.setupLdapServer();
+    }
+
+    public void setupLdapServer() throws LDAPException, IOException
+    {
+        this.inMemoryDirectoryServerConfig.setPasswordEncoders(new ClearInMemoryPasswordEncoder("{CLEAR}", null),
+            new ClearInMemoryPasswordEncoder("{BASE64}", Base64PasswordEncoderOutputFormatter.getInstance()));
+        this.inMemoryDirectoryServer = new InMemoryDirectoryServer(this.inMemoryDirectoryServerConfig);
+        inMemoryDirectoryServer.importFromLDIF(
+            true,
+            new LDIFReader(new File(new File(".").getCanonicalPath() + "/src/test/resources/data.ldif")));
+
+        inMemoryDirectoryServer.startListening();
+
+        ldapService = new LdapServiceImpl(this.securityDao, this.userDao, this.passwordEncoder
+            , 1000, 1000);
+    }
+
+    @After
+    public void teardownLdapServer()
+    {
+        // Disconnect from the server and cause the server to shut down.
+
+        inMemoryDirectoryServer.clear();
+        inMemoryDirectoryServer.shutDown(true);
+    }
+
+    @Test(expected = CommunicationException.class)
+    public void test_connect_timeout_exception() throws LdapServiceException {
+        AuthenticationMethod authMethod = new AuthenticationMethod();
+        authMethod.setMethod(SecurityConstants.AUTH_METHOD_LDAP);
+        authMethod.setLdapServerUrl("ldap://server.ldap.com:389");
+        authMethod.setOrder(1L);
+        authMethod.setEnabled(true);
+        authMethod.setLdapBindUserDn("cn=Directory Manager");
+        authMethod.setLdapBindUserPassword("password");
+        authMethod.setLdapUserSearchBaseDn("ou=people,ou=IL-Sunset,dc=slidev,dc=org");
+        authMethod.setLdapUserSearchFilter("(uid={0})");
+        ldapService = new LdapServiceImpl(this.securityDao, this.userDao, this.passwordEncoder
+            , 1000, 1000);
+
+        try {
+            ldapService.synchronize(authMethod);
+        }
+        catch (Exception exception) {
+            Optional<Throwable> rootCause = Stream.iterate(exception, Throwable::getCause)
+                .filter(element -> element.getCause() == null)
+                .findFirst();
+            Assert.assertEquals(SocketTimeoutException.class, rootCause.get().getClass());
+            Assert.assertEquals("Connect timed out", rootCause.get().getMessage());
+            throw exception;
+        }
+    }
+
+    @Test(expected = UncategorizedLdapException.class)
+    public void test_read_timeout_exception() throws LdapServiceException {
+        AuthenticationMethod authMethod = new AuthenticationMethod();
+        authMethod.setMethod(SecurityConstants.AUTH_METHOD_LDAP);
+        authMethod.setLdapServerUrl(this.ldapServerUrl);
+        authMethod.setOrder(1L);
+        authMethod.setEnabled(true);
+        authMethod.setLdapBindUserDn("cn=Directory Manager");
+        authMethod.setLdapBindUserPassword("password");
+        authMethod.setLdapUserSearchBaseDn("ou=people,ou=IL-Sunset,dc=slidev,dc=org");
+        authMethod.setApplicationSecurityBaseDn("ou=application,ou=IL-Sunset,dc=slidev,dc=org");
+        authMethod.setLdapUserSearchFilter("(uid={0})");
+        authMethod.setGroupSynchronisationFilter("(uid={0})");
+        ldapService = new LdapServiceImpl(this.securityDao, this.userDao, this.passwordEncoder
+            , 1, 1000);
+
+        try {
+            ldapService.synchronize(authMethod);
+        }
+        catch (Exception exception) {
+            Optional<Throwable> rootCause = Stream.iterate(exception, Throwable::getCause)
+                .filter(element -> element.getCause() == null)
+                .findFirst();
+            Assert.assertEquals(NamingException.class, rootCause.get().getClass());
+            Assert.assertEquals("LDAP response read timed out, timeout used: 1 ms.", rootCause.get().getMessage());
+            throw exception;
+        }
     }
 
     @Test
@@ -50,7 +159,6 @@ public class LdapServiceImplTest {
         boolean validEncoding = ldapService.isValidEncoding(ldapUser);
 
         Assert.assertTrue(validEncoding);
-
     }
 
     @Test
@@ -68,7 +176,6 @@ public class LdapServiceImplTest {
         boolean validEncoding = ldapService.isValidEncoding(ldapUser);
 
         Assert.assertTrue(validEncoding);
-
     }
 
     @Test
@@ -81,7 +188,6 @@ public class LdapServiceImplTest {
         boolean validEncoding = ldapService.isValidEncoding(ldapUser);
 
         Assert.assertTrue(validEncoding);
-
     }
 
     @Test
@@ -94,7 +200,6 @@ public class LdapServiceImplTest {
         boolean validEncoding = ldapService.isValidEncoding(ldapUser);
 
         Assert.assertTrue(validEncoding);
-
     }
 
     @Test
@@ -107,7 +212,6 @@ public class LdapServiceImplTest {
         boolean validEncoding = ldapService.isValidEncoding(ldapUser);
 
         Assert.assertTrue(!validEncoding);
-
     }
 
     @Test
@@ -120,7 +224,6 @@ public class LdapServiceImplTest {
         boolean validEncoding = ldapService.isValidEncoding(ldapUser);
 
         Assert.assertTrue(validEncoding);
-
     }
 
     @Test
@@ -133,7 +236,6 @@ public class LdapServiceImplTest {
         boolean validEncoding = ldapService.isValidEncoding(ldapUser);
 
         Assert.assertTrue(!validEncoding);
-
     }
 
     @Test
@@ -146,7 +248,6 @@ public class LdapServiceImplTest {
         boolean validEncoding = ldapService.isValidEncoding(ldapUser);
 
         Assert.assertTrue(validEncoding);
-
     }
 
     @Test
@@ -159,7 +260,6 @@ public class LdapServiceImplTest {
         boolean validEncoding = ldapService.isValidEncoding(ldapUser);
 
         Assert.assertTrue(!validEncoding);
-
     }
 
     @Test
@@ -172,7 +272,6 @@ public class LdapServiceImplTest {
         boolean validEncoding = ldapService.isValidEncoding(ldapUser);
 
         Assert.assertTrue(validEncoding);
-
     }
 
     @Test
@@ -185,7 +284,6 @@ public class LdapServiceImplTest {
         boolean validEncoding = ldapService.isValidEncoding(ldapUser);
 
         Assert.assertTrue(!validEncoding);
-
     }
 
     @Test
@@ -198,7 +296,6 @@ public class LdapServiceImplTest {
         boolean validEncoding = ldapService.isValidEncoding(ldapUser);
 
         Assert.assertTrue(validEncoding);
-
     }
 
     @Test
@@ -211,7 +308,6 @@ public class LdapServiceImplTest {
         boolean validEncoding = ldapService.isValidEncoding(ldapUser);
 
         Assert.assertTrue(!validEncoding);
-
     }
 
 
@@ -237,7 +333,6 @@ public class LdapServiceImplTest {
         boolean validEncoding = ldapService.isValidEncoding(ldapUser);
 
         Assert.assertTrue(!validEncoding);
-
     }
 
     @Test
@@ -251,7 +346,6 @@ public class LdapServiceImplTest {
         boolean validEncoding = ldapService.isValidEncoding(ldapUser);
 
         Assert.assertTrue(!validEncoding);
-
     }
 
 }
