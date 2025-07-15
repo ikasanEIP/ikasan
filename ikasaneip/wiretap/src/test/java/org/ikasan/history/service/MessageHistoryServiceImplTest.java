31e3e95080138b75959b14f6779bc333360428ce
Michael Stewart <mick@humeenterprises.co.uk>
2025-07-15 09:05:41 +0100

    IKASAN-2602 exposing message.history.events.ttl.days to allow metrics events TTL to be externally exposed via properties.

--- ikasaneip/wiretap/src/test/java/org/ikasan/history/service/MessageHistoryServiceImplTest.java
+++ ikasaneip/wiretap/src/test/java/org/ikasan/history/service/MessageHistoryServiceImplTest.java
@@ -99,6 +99,7 @@ public class MessageHistoryServiceImplTest
     {
         messageHistoryService = new MessageHistoryServiceImpl(messageHistoryDao, wiretapSerialiser);
         mockMessageHistoryService.setHistoryEventFactory(historyEventFactory);
+        mockMessageHistoryService.setMessageHistoryDaysToLive(3);
     }
 
     @Test(expected = IllegalArgumentException.class)
@@ -121,7 +122,7 @@ public class MessageHistoryServiceImplTest
     public void test_save()
     {
         mockery.checking(new Expectations(){{
-            oneOf(historyEventFactory).newEvent("moduleName", "flowName", flowInvocationContext, 7);
+            oneOf(historyEventFactory).newEvent("moduleName", "flowName", flowInvocationContext, 3);
             will(returnValue(flowInvocationMetric));
             oneOf(mockMessageHistoryDao).save(flowInvocationMetric);
         }});
