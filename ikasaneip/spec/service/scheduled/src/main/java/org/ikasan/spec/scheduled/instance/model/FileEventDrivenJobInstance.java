a330b69322fa173514258a397d20d52b1455b437
Michael Stewart <mick@humeenterprises.co.uk>
2025-08-19 06:36:12 +0100

    IKASAN-2622 WIP adding new fields to support spel on file watcher jobs.

--- ikasaneip/spec/service/scheduled/src/main/java/org/ikasan/spec/scheduled/instance/model/FileEventDrivenJobInstance.java
+++ ikasaneip/spec/service/scheduled/src/main/java/org/ikasan/spec/scheduled/instance/model/FileEventDrivenJobInstance.java
@@ -1,10 +1,8 @@
 package org.ikasan.spec.scheduled.instance.model;
 
 import org.ikasan.spec.scheduled.job.model.FileEventDrivenJob;
-import org.ikasan.spec.scheduled.job.model.QuartzScheduleDrivenJob;
 
 import java.io.Serializable;
-import java.util.List;
 
 public interface FileEventDrivenJobInstance extends QuartzScheduleDrivenJobInstance, FileEventDrivenJob, Serializable {
 }
