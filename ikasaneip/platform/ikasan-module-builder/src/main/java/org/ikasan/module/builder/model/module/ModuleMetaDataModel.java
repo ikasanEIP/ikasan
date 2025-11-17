b333839d87cf4d17ed98130ea5e3b7f23504a046
Michael Stewart <mick@humeenterprises.co.uk>
2025-11-17 13:31:11 +0000

    IKASAN-2607 adding code remove duplicates from components passed to component factory freemarker template.

--- ikasaneip/platform/ikasan-module-builder/src/main/java/org/ikasan/module/builder/model/module/ModuleMetaDataModel.java
+++ ikasaneip/platform/ikasan-module-builder/src/main/java/org/ikasan/module/builder/model/module/ModuleMetaDataModel.java
@@ -7,6 +7,7 @@ import org.ikasan.spec.metadata.ModuleMetaData;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.function.Function;
 import java.util.stream.Collectors;
 
 public class ModuleMetaDataModel {
@@ -30,7 +31,13 @@ public class ModuleMetaDataModel {
     public List<FlowElementMetaData> getFlowElementMetaData() {
         return moduleMetaData.getFlows().stream()
             .flatMap(flowMetaData -> flowMetaData.getFlowElements().stream())
-            .distinct()
+            .collect(Collectors.toMap(
+                FlowElementMetaData::getComponentName, // Key: the field to be distinct by
+                Function.identity(), // Value: the Person object itself
+                (existing, replacement) -> existing // Merge function: keep the existing one if names are the same
+            ))
+            .values()
+            .stream()
             .collect(Collectors.toList());
     }
 }
